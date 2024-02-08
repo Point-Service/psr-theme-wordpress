beb91285bca4d2828ebd43a46c6a0f0de13f1d4b
258ms <valeriotriolo3010@gmail.com>
2024-02-08 12:05:07 +0100

    fix load more

--- inc/load_more.php
+++ inc/load_more.php
@@ -8,9 +8,6 @@ function load_more_script() {
     wp_enqueue_script( 'dci-load_more', get_template_directory_uri() . '/assets/js/load_more.js', array('jquery'), null, true );
     $variables = array(
         'ajaxurl' => admin_url( 'admin-ajax.php' ),
-        'wp_query' => json_encode( $wp_query ), 
-        'the_query' => json_encode( $the_query ), 
-        'wp_the_query' => json_encode( $wp_the_query ), 
         'posts' => json_encode( $wp_query->query_vars ), 
 		'current_page' => get_query_var( 'paged' ) ? get_query_var('paged') : 1,
 		'max_page' => $wp_query->max_num_pages,
@@ -30,15 +27,22 @@ function load_template_part($template_name, $part_name=null) {
 add_action("wp_ajax_load_more" , "load_more");
 add_action("wp_ajax_nopriv_load_more" , "load_more");
 function load_more(){
-	global $wp_query, $i, $hide_categories;
+	global $wp_query, $servizio, $i, $hide_categories;
 	
     // prepare our arguments for the query
 	$load_card_type = $_POST['load_card_type'];
 	$post_types = json_decode( stripslashes( $_POST['post_types'] ), true );
-	$tax_query = json_decode( stripslashes( $_POST['tax_query'] ), true );
 	$url_query_params =  json_decode( stripslashes( $_POST['query_params'] ), true );
 	$additional_filter =  json_decode( stripslashes( $_POST['additional_filter'] ), true );
 
+	$args = array(
+        's' => $_POST['search'],
+        'posts_per_page' => $_POST['post_count'] + $_POST['load_posts'],
+        'post_type'      => $post_types,
+        'orderby'        => 'date',
+        'order'          => 'DESC'
+    );
+	
 	if ( $post_types != "notizia" ) {
 		$args = array(
 			's' => $_POST['search'],
@@ -50,20 +54,20 @@ function load_more(){
 	}
 
 	if ( isset($url_query_params["post_terms"]) ) {
-		$args['tax_query'] = array(
+		$taxquery = array(
 			array(
 				'taxonomy' => 'argomenti',
 				'field' => 'id',
 				'terms' => $url_query_params["post_terms"]
 			)
 		);
-	} elseif ( $tax_query ) {
-		$args['tax_query'] =  $tax_query;
+	
+		$args['tax_query'] = $taxquery;
 	}
 	if ( isset($url_query_params["post_types"]) ) $args['post_type'] = $url_query_params["post_types"];
 	if ( isset($url_query_params["s"]) ) $args['s'] = $url_query_params["s"];
 	if ( isset($additional_filter) ) $args = $args + $additional_filter;
-
+ 
 	// it is always better to use WP_Query but not here
 	$new_query = query_posts( $args );
 
@@ -76,20 +80,19 @@ function load_more(){
 		$post = get_post();
 		++$i;
 
-		//die($load_card_type);
-
-		
 		if ($load_card_type == "servizio"){
-			$out .= load_template_part("template-parts/servizio/card");
+			$servizio = $post;
+			$out .= load_template_part("template-parts/servizio/card");  
 		}
 		if ($load_card_type == "categoria_servizio"){
+			$servizio = $post;
 			$hide_categories = true;
-			$out .= load_template_part("template-parts/servizio/cards-list");
+			$out .= load_template_part("template-parts/servizio/card");  
 		}
 		if ($load_card_type == "notizia"){
 			$out .= load_template_part("template-parts/novita/cards-list");  
 		}
-		if ($load_card_type == "documento_pubblico"){
+		if ($load_card_type == "documento"){
 			$out .= load_template_part("template-parts/documento/cards-list");  
 		}
 		if ($load_card_type == "global-search"){
@@ -97,22 +100,7 @@ function load_more(){
 		}	
 		if ($load_card_type == "domanda-frequente"){
 			$out .= load_template_part("template-parts/domanda-frequente/item");  
-		}
-		if ($load_card_type == "aree-amministrative"){
-			$out .= load_template_part("template-parts/aree-amministrative/cards-list");
-		}
-		if ($load_card_type == "personale-amministrativo"){
-			$out .= load_template_part("template-parts/personale-amministrativo/cards-list");
-		}
-		if ($load_card_type == "unita-organizzativa"){
-			$out .= load_template_part("template-parts/unita-organizzativa/cards-list");
-		}
-		if ($load_card_type == "luogo"){
-			$out .= load_template_part("template-parts/luogo/card-full");
-		}
-		if ($load_card_type == "evento"){
-			$out .= load_template_part("template-parts/evento/card-full");
-		}
+		}	
  
 		endwhile;
  
