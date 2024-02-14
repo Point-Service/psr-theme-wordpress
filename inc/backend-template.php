682da5a5a1419f6ba0039d7f631abf40e9dc1935
258ms <valeriotriolo3010@gmail.com>
2024-02-14 11:05:48 +0100

    fix wp-login stylesheets

--- inc/backend-template.php
+++ inc/backend-template.php
@@ -13,15 +13,17 @@ require get_template_directory()."/inc/comuni_config.php";
 function dci_icon_script() {
 
     wp_register_script( 'dci-icon-script', get_template_directory_uri() . '/inc/admin-js/admin.js');
+    
     wp_enqueue_script('dci-icon-script');
 
     $dci_data =   array( 'stylesheet_directory_uri' => get_template_directory_uri() );
 
     wp_localize_script( 'dci-icon-script', 'dci_data', $dci_data );
 
+}
 
-
-    //console_log(get_stylesheet_directory_uri() ,$dci_data);
-
+function dci_enqueue_stylesheets() {
+    wp_enqueue_style( 'dci-boostrap-italia-min', get_template_directory_uri() . '/assets/css/bootstrap-italia.min.css', false, '1.0.0' );
 }
 add_action( 'admin_enqueue_scripts', 'dci_icon_script' );
+add_action( 'login_enqueue_scripts', 'dci_enqueue_stylesheets' );
