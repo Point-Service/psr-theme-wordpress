334685cc2999661791f287b8d6a0bee9c260f0c4
258ms <valeriotriolo3010@gmail.com>
2024-02-15 09:57:35 +0100

    login logo point

--- inc/backend-template.php
+++ inc/backend-template.php
@@ -22,8 +22,24 @@ function dci_icon_script() {
 
 }
 
+function psr_login_logo() { 
+    echo '<style type="text/css">
+        #login h1 a, .login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo-point.png);
+        </style>
+    }';
+}
+function get_login_in_webmail ( $text ) {
+    if ($text == 'Lost your password?'){
+        $text .= '<br /><a href="http://codebing.com">Visit Code Bing</a>';
+    }
+    return $text;
+}
+add_filter( 'gettext', 'get_login_in_webmail' );
+
 function dci_enqueue_stylesheets() {
     wp_enqueue_style( 'dci-boostrap-italia-min', get_template_directory_uri() . '/assets/css/bootstrap-italia.min.css', false, '1.0.0' );
 }
 add_action( 'admin_enqueue_scripts', 'dci_icon_script' );
 add_action( 'login_enqueue_scripts', 'dci_enqueue_stylesheets' );
+add_action( 'login_enqueue_scripts', 'psr_login_logo' );
