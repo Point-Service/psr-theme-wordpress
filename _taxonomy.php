db77c699d8be2731d590f229c4aeedca9f6e367e
258ms <valeriotriolo3010@gmail.com>
2024-03-13 16:38:44 +0100

    fix taxonomy

--- _taxonomy.php
+++ _taxonomy.php
@@ -8,7 +8,7 @@ else if ( $obj->post_type=="documento_pubblico" ||  $obj->taxonomy == "tipi_docu
 	get_template_part( "archive-documento" );
 }
 else if ( $obj->post_type=="tipi_notizia" ||  $obj->taxonomy == "tipi_notizia" || $obj->taxonomy == "tipi_notizia"){
-	get_template_part( "archive-notizia" );
+	get_template_part( "taxonomy-tipi_notizia" );
 }
 else if($obj->taxonomy == "argomenti")
 	get_template_part("archive-argomento");
