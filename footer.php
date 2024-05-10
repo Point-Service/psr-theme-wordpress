5b06c343acd29fbc654b1d45dd281e3a472159c2
Point-Service <tecnico@p-service.it>
2024-05-10 17:36:41 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -212,7 +212,7 @@
 							if(strpos($headers[0],'404') === false)
 							{ 
 						      ?>
-							  <a target="_blank" href="<?php echo dci_get_option("sitemap",'footer'); ?>/index.php/page-sitemap.html">Mappa del sito1</a>
+							  <a target="_blank" href="<?php echo dci_get_option("sitemap",'footer'); ?>/index.php/page-sitemap.html">Mappa del sito</a>
 						     <?php }
 							else
 							{
