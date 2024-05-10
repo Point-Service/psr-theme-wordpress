9b55f6d85ce0db1f11bfc2006417538241c2501e
Point-Service <tecnico@p-service.it>
2024-05-10 17:46:21 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -204,8 +204,15 @@
 							<?php } ?>
 						      */
 	                                             ?>	
+
+						     <?php if (strlen($ dci_get_option("sitemap",'footer')) > 3) { ?>
+							    <a target="_blank" href="<?php echo dci_get_option("sitemap",'footer'); ?>/index.php/page-sitemap.html">Mappa del sito</a>
+						    <?php } ?>
 			    
-						<a target="_blank" href="/index.php/page-sitemap.html">Mappa del sito</a>		    
+						<a target="_blank" href="/index.php/page-sitemap.html">Mappa del sito</a>
+
+
+			    
 						<a href="<?php echo dci_get_option("servizi",'header'); ?>/index.php/servizi">Servizi</a>
 			                        <a target="_blank" href="<?php echo dci_get_option("Area Riservata",'footer'); ?>/wp-admin">Area Riservata</a>
 						
