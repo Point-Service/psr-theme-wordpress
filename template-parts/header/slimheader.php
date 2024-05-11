364a03e3731e68d2a4c25a252e3581785b09a101
Point-Service <tecnico@p-service.it>
2024-05-11 11:10:29 +0200

    Update slimheader.php

--- template-parts/header/slimheader.php
+++ template-parts/header/slimheader.php
@@ -53,7 +53,7 @@
               </div>
               
             </div>
-
+	[google-translator]
             <?php
                 if(!is_user_logged_in()) {
                     get_template_part("template-parts/header/header-anon");
