d23a15176e34ac17a411a3ae570604bdc8eee72b
Point-Service <tecnico@p-service.it>
2024-05-13 12:17:46 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
                         </a>
                    </div>	
 		
-               <img class="ue-logo" src="<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-eu-inverted.svg" alt="logo Unione Europea" width="" height=""  style="float: left; width: 0; height: 0">
+               <img class="ue-logo" src="<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-eu-inverted.svg" alt="logo Unione Europea" width="" height=""  style="position: relative">
                    
                 </div>
             </div>
