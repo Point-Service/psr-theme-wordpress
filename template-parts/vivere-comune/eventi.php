8b73fc44523827c615fab47178f3e6f05dbbefe9
Point-Service <tecnico@p-service.it>
2024-05-15 11:01:03 +0200

    Update eventi.php

--- template-parts/vivere-comune/eventi.php
+++ template-parts/vivere-comune/eventi.php
@@ -9,29 +9,6 @@
     if (is_array($eventi) && count($eventi)) {
 ?>
 
-<div class="bg-grey-dsk py-5">
-    <div class="container">
-        <h2 class="title-xxlarge mb-4">Eventi in evidenza</h2>
-        <div class="row g-4">
-            <?php
-                foreach ($eventi as $evento_id) {
-                    $post = get_post($evento_id);
-                    get_template_part("template-parts/evento/card-full");
-                }
-            ?>
-            <div class="d-flex justify-content-end">
-                <button
-                    type="button"
-                    class="btn btn-primary px-5 py-3 full-mb"
-                    label="Tutti gli eventi" 
-                    buttonNext=true
-                    data-element="live-button-events"
-                    onclick="location.href='./eventi'"
-                >
-                    <span class="">Tutti gli eventi</span>
-                </button>
-            </div>
-        </div>
-    </div>
+
 </div>
 <?php } ?>
