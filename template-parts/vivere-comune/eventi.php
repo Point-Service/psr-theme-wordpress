4b87c0acba4b0e76e536ad686cda0e42ca00ffc9
Point-Service <tecnico@p-service.it>
2024-05-15 11:02:33 +0200

    Update eventi.php

--- template-parts/vivere-comune/eventi.php
+++ template-parts/vivere-comune/eventi.php
@@ -1,14 +1,1 @@
-<?php
-    // Per selezionare i contenuti in evidenza tramite flag
-    // $eventi = dci_get_highlighted_posts(['evento'], 6);
 
-    //Per selezionare i contenuti in evidenza tramite configurazione
-    $eventi = dci_get_option('eventi_evidenziati','vivi');
-
-    $url_eventi = get_permalink( get_page_by_title('Eventi') );
-    if (is_array($eventi) && count($eventi)) {
-?>
-
-
-</div>
-<?php } ?>
