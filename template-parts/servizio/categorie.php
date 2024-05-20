a3e4273f3a07cdab83b6f6b778e14f60b80456de
Point-Service <tecnico@p-service.it>
2024-05-20 09:56:13 +0200

    Update categorie.php

--- template-parts/servizio/categorie.php
+++ template-parts/servizio/categorie.php
@@ -24,9 +24,7 @@ $categorie_servizio_names = dci_categorie_servizio_array();
             <div class="cmp-card-simple card-wrapper pb-0 rounded border border-light">
                 <div class="card shadow-sm rounded">
                     <div class="card-body">
-                        <a data-element="service-category-link" class="text-decoration-none" href="<?php echo $url; ?>" >
-						 <h3 class="card-title t-primary title-xlarge"><?php echo $categoria->name; ?></h3>
-						</a>
+                         <a class="text-decoration-none" href="<?php echo $url; ?>" data-element="service-category-link"><h3 class="card-title t-primary title-xlarge"><?php echo $categoria->name; ?></h3></a>
                         <p class="titillium text-paragraph mb-0">
                             <?php echo $categoria->description; ?>
                         </p>
