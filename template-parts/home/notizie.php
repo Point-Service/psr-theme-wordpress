9eba6ab5cbc862dc83af9284bedd447e527cd523
Point-Service <tecnico@p-service.it>
2024-06-04 15:22:45 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -1,5 +1,5 @@
 <?php
-global $scheda;
+global $count, $scheda;
 // Per mostrare la notizia più recente
 // $args = array('post_type' => 'notizia',
 //              'posts_per_page' => 1,
@@ -102,33 +102,7 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
         </div>
     </div>
 </section>
-<?php
-if ($schede && count($schede) > 0) { ?>
-    <section aria-describedby="contenuti_evidenza">
-        <div class="section-content">
-            <div class="section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
-                <div class="container">
-                    <div class="row row-title pt-5 pt-lg-60 pb-3">
-                        <div class="col-12 d-lg-flex justify-content-between">
-                            <h2 id="contenuti_evidenza" class="mb-lg-0">Contenuti in evidenza</h2>
-                        </div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="card-wrapper px-0 card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
-                            <?php $count = 1;
-                            foreach ($schede as $scheda) {
-                                if ($scheda) {
-                                    get_template_part("template-parts/home/scheda-evidenza");
-                                }
-                                ++$count;
-                            } ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-<?php } ?>
+
 
 <section id="calendario">
   <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
