cc4fba33bb78a488b78ac2ffd8765a9b281b245f
Point-Service <tecnico@p-service.it>
2024-06-04 16:39:48 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -17,31 +17,18 @@ $monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
 $descrizione_breve = dci_get_meta("descrizione_breve", '_dci_notizia_', $post->ID);
 $argomenti = dci_get_meta("argomenti", '_dci_notizia_', $post->ID);
 
-// Funzione per recuperare tutte le opzioni CMB2
-function get_all_options() {
-    global $wpdb;
-    $options = $wpdb->get_results("SELECT option_name, option_value FROM {$wpdb->options}");
-    return $options;
-}
+$scheda1 = dci_get_option('schede_evidenziate_1','homepage', true )[0] ?? null;
+$scheda2 = dci_get_option('schede_evidenziate_2','homepage', true )[0] ?? null;
+$scheda3 = dci_get_option('schede_evidenziate_3','homepage', true )[0] ?? null;
+$scheda4 = dci_get_option('schede_evidenziate_4','homepage', true )[0] ?? null;
+$scheda5 = dci_get_option('schede_evidenziate_5','homepage', true )[0] ?? null;
+$scheda6 = dci_get_option('schede_evidenziate_6','homepage', true )[0] ?? null;
+$scheda7 = dci_get_option('schede_evidenziate_7','homepage', true )[0] ?? null;
+$scheda8 = dci_get_option('schede_evidenziate_8','homepage', true )[0] ?? null;
+$scheda9 = dci_get_option('schede_evidenziate_9','homepage', true )[0] ?? null;
+$schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7 ,$scheda8 ,$scheda9  );
 
-// Recupera tutte le opzioni
-$options = get_all_options();
 
-// Filtra le opzioni per ottenere solo quelle delle schede evidenziate
-$schede = [];
-foreach ($options as $option) {
-    if (strpos($option->option_name, 'schede_evidenziate_') === 0) {
-        $scheda_contenuto = dci_get_option($option->option_name, 'homepage', true)[0] ?? null;
-        if ($scheda_contenuto) {
-            $schede[$option->option_name] = $scheda_contenuto;
-        }
-    }
-}
-
-// Esempio di visualizzazione delle schede con contenuto
-foreach ($schede as $key => $contenuto) {
-    echo "Scheda: $key, Contenuto: " . print_r($contenuto, true) . "<br>";
-}
 ?>
 
     
@@ -106,7 +93,15 @@ foreach ($schede as $key => $contenuto) {
                     </div>
                 </div>
 
-
+                <div class="row my-4 justify-content-md-center">
+                     <div class="container">
+                       <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary-outline mt-40" >   
+                         <svg class="icon icon-sm" aria-hidden="true">
+                           <use xlink:href="#it-calendar"></use>
+                          </svg>
+                          Visualizza tutte le novità
+                     </a>
+                  </div>
 
 
 
