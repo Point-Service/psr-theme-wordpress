17ec92ca94aa4f822a4b0b19e8790747f384d454
Point-Service <tecnico@p-service.it>
2024-06-04 17:34:09 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -92,7 +92,7 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
                 </div>
                 <div class="row my-4 justify-content-md-center">
                      <div class="container">
-                       <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary-outline mt-40" >   
+                       <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary mt-40" >   
                          <svg class="icon icon-sm" aria-hidden="true">
                            <use xlink:href="#it-calendar"></use>
                           </svg>
