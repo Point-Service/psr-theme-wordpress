c24f8ab5e3cbdd5ee769ee361012e6eb2202b9d6
Point-Service <tecnico@p-service.it>
2024-06-04 09:54:05 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -24,7 +24,10 @@ $scheda3 = dci_get_option('schede_evidenziate_3','homepage', true )[0] ?? null;
 $scheda4 = dci_get_option('schede_evidenziate_4','homepage', true )[0] ?? null;
 $scheda5 = dci_get_option('schede_evidenziate_5','homepage', true )[0] ?? null;
 $scheda6 = dci_get_option('schede_evidenziate_6','homepage', true )[0] ?? null;
-$schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5,$scheda6 );
+$scheda6 = dci_get_option('schede_evidenziate_7','homepage', true )[0] ?? null;
+$scheda6 = dci_get_option('schede_evidenziate_8','homepage', true )[0] ?? null;
+$scheda6 = dci_get_option('schede_evidenziate_9','homepage', true )[0] ?? null;
+$schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5,$scheda6,$scheda7,$scheda8,$scheda9);
 
 
 ?>
