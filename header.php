bc4fc283b916a843325d77d989fd8011115962f0
Point-Service <tecnico@p-service.it>
2024-06-04 11:55:42 +0200

    Update header.php

--- header.php
+++ header.php
@@ -198,11 +198,3 @@ $current_group = dci_get_current_group();
 if(!is_user_logged_in())
     get_template_part("template-parts/common/access-modal");
 ?>
-
-
-
- 
-    <div class="it-header-center-wrapper">
-       <div class="bg-image"></div>
-    </div>
-
