d2b0fb7990f875c7ea0490e90ea2b4eddd86afde
Point-Service <tecnico@p-service.it>
2024-06-04 12:26:09 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -59,15 +59,14 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
             </p>
             <?php get_template_part("template-parts/common/badges-argomenti"); ?>                
           </div>
-        </div>
-           <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2 text-center">
+        </div>       
+      </div>
+                <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2 text-center">
                <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary mt-40">Mostra tutti</a>
            </div>    
            
 
 
-       
-      </div>
       <div class="col-lg-6 offset-lg-1 order-1 order-lg-2 px-0 px-lg-2">
         <?php if ($img) { 
           dci_get_img($img, 'img-fluid');
