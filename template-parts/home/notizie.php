f18a5f627a8b7addc34f57e0ebcfe52aa4efe7dd
Point-Service <tecnico@p-service.it>
2024-06-04 16:05:13 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -35,7 +35,6 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
     <div class="section-content">
         <div class="container">
             <h2 id="novita-in-evidenza" class="visually-hidden">Novità in evidenza</h2>
-
             <?php if ($post_id) {
                 $overlapping = "card-overlapping";
             ?>
@@ -71,7 +70,36 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
                     </div>
                 </div>
 
-
+            <?php }
+            if ($posts && is_array($posts) && count($posts) > 0) { ?>
+                <?php if (!$post_id) { ?>
+                    <div class="row row-title pt-lg-60 pb-3">
+                        <div class="col-12 d-lg-flex justify-content-between">
+                            <h2 id="ultime-news" class="mb-lg-0">Ultime news</h2>
+                        </div>
+                    </div>
+                <?php } ?>
+                <div class="row mb-2">
+                    <div class="card-wrapper px-0 <?php echo $overlapping; ?> card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                        <?php
+                        foreach ($posts as $post) {
+                            if ($post) {
+                                get_template_part("template-parts/home/notizia-evidenza");
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="row my-4 justify-content-md-center">
+                    <a class="read-more pb-3" href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>">
+                        <button type="button" class="btn btn-outline-primary">Tutte le novità
+                            <svg class="icon">
+                                <use xlink:href="#it-arrow-right"></use>
+                            </svg>
+                        </button>
+                    </a>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </section>
