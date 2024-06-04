16c4e59024ddb1ff25a1424b36e14d751ee92691
Point-Service <tecnico@p-service.it>
2024-06-04 08:38:15 +0200

    Update page.php

--- page.php
+++ page.php
@@ -40,8 +40,6 @@ get_header();
                                 <?php
                                 the_content();
                                 ?>
-                                <div class="callout note">
-                                </div>
                             </div>
                         </div>
                         <div class="row variable-gutters">
