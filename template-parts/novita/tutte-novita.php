b78995c3a236625ac25d262aee723615fbc15483
Point-Service <tecnico@p-service.it>
2024-06-04 13:00:46 +0200

    Update tutte-novita.php

--- template-parts/novita/tutte-novita.php
+++ template-parts/novita/tutte-novita.php
@@ -2,7 +2,7 @@
 global $the_query, $load_posts, $load_card_type, $tipo_notizia;
 
     $max_posts = isset($_GET['max_posts']) ? $_GET['max_posts'] : 9;
-    $load_posts = 15;
+    $load_posts = 10;
     $query = isset($_GET['search']) ? dci_removeslashes($_GET['search']) : null;
     $args = array(
         's'         => $query,
