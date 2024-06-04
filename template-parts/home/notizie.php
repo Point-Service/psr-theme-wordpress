61536ba0370beb4ef51c59036bab8dc133609714
Point-Service <tecnico@p-service.it>
2024-06-04 10:22:23 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -56,9 +56,6 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5,$scheda6,$scheda7,$
               <?php echo $descrizione_breve ?>
             </p>
             <?php get_template_part("template-parts/common/badges-argomenti"); ?>
-          </div>
-        </div>
-      </div>
 
            <a class="read-more pb-3"
               href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>">
@@ -67,6 +64,12 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5,$scheda6,$scheda7,$
                 <use xlink:href="#it-arrow-right"></use>
               </svg>
             </a>
+            
+          </div>
+        </div>
+      </div>
+
+
       
       <div class="col-lg-6 offset-lg-1 order-1 order-lg-2 px-0 px-lg-2">
         <?php if ($img) { 
