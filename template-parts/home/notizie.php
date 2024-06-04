0625b7b6aff4a97560c4553eacf87c966a9239b4
Point-Service <tecnico@p-service.it>
2024-06-04 12:18:39 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -58,7 +58,17 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
               <?php echo $descrizione_breve ?>
             </p>
             <?php get_template_part("template-parts/common/badges-argomenti"); ?>
-     
+        <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
+         <a
+              class="read-more pb-3"
+              href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>"
+            >
+              <span class="text">Tutte le novità</span>
+              <svg class="icon">
+                <use xlink:href="#it-arrow-right"></use>
+              </svg>
+            </a>   
+     </div>
           </div>
         </div>
       </div>
@@ -73,17 +83,6 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
     <?php } ?>
   </div>
  </div>
-   <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
-         <a
-              class="read-more pb-3"
-              href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>"
-            >
-              <span class="text">Tutte le novità</span>
-              <svg class="icon">
-                <use xlink:href="#it-arrow-right"></use>
-              </svg>
-            </a>   
-     </div>
 </section>
 <section id="calendario">
   <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
