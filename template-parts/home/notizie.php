a7273a4d928c76605758814f7791ef9158109c73
Point-Service <tecnico@p-service.it>
2024-06-04 12:08:36 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -33,6 +33,7 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
 ?>
 <!-- Tag section is opened in home.php -->
 <section id="notizie" aria-describedby="novita-in-evidenza">
+ <div class="section-content">
   <div class="container">
     <?php if ($post_id) { ?>
     <div class="row">
@@ -79,6 +80,7 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
       <div style="height: 34px;"></div>
     <?php } ?>
   </div>
+ </div>
 </section>
 <section id="calendario">
   <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
