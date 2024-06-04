f0b595f3b327517add7a2acc1dec230a5f4e5252
Point-Service <tecnico@p-service.it>
2024-06-04 12:24:14 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -59,10 +59,7 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
             </p>
             <?php get_template_part("template-parts/common/badges-argomenti"); ?>
                          
-           <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2 text-center">
-               <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary mt-40">Mostra tutti</a>
-           </div>    
-           
+
            
           </div>
         </div>
@@ -79,6 +76,13 @@ $schede = array($scheda1,$scheda2,$scheda3,$scheda4,$scheda5 ,$scheda6 ,$scheda7
   </div>
  </div>
 </section>
+
+           <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2 text-center">
+               <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary mt-40">Mostra tutti</a>
+           </div>    
+           
+
+
 <section id="calendario">
   <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
     <div class="container">
