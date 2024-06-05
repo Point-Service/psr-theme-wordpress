76b2a324ea37286ed54032e7ae72cd758b08bbfd
Point-Service <tecnico@p-service.it>
2024-06-05 09:44:12 +0200

    Update pagina_home.php

--- inc/admin/options/pagina_home.php
+++ inc/admin/options/pagina_home.php
@@ -106,133 +106,64 @@ for ($i = 1; $i <= 12; $i++) {
             'data-maximum-selection-length' => '12',
         ),
     ) );
-    //sezione Argomenti
-    $home_options->add_field( array(
-        'id' => $prefix . 'argomenti_title',
-        'name'        => __( 'Sezione Argomenti', 'design_comuni_italia' ),
-        'desc' => __( 'Gestione Argomenti mostrati in homepage.' , 'design_comuni_italia' ),
-        'type' => 'title',
-    ) );
-    $argomenti_group_id = $home_options->add_field( array(
-        'id'           => $prefix . 'argomenti_evidenziati_1',
-        'type'        => 'group',
-        'name'        => 'Argomenti in evidenza',
-        'desc' => __( 'Definisci il contenuto delle Card degli argomenti' , 'design_comuni_italia' ),
-        'repeatable'  => false,
-        'options'     => array(
-            'group_title'   => __( 'Argomento 1: ', 'design_comuni_italia' ),
-            'closed' => true
-        ),
-    ) );
-    $home_options->add_group_field( $argomenti_group_id, array(
-        'id' => $prefix . 'argomento_1_argomento',
-        'name'        => __( 'Argomento', 'design_comuni_italia' ),
-        'desc' => __( 'Seleziona l\'Argomento' , 'design_comuni_italia' ),
-        'type' => 'taxonomy_select',
-        'taxonomy'=>'argomenti'
-    ) );
-    $home_options->add_group_field( $argomenti_group_id, array(
-        'id' => $prefix . 'argomento_1_siti_tematici',
-        'name'        => __( 'Sito Tematico ', 'design_comuni_italia' ),
-        'desc' => __( 'Selezionare il sito tematico da inserire nella Card' , 'design_comuni_italia' ),
-        'type'    => 'pw_select',
-        'options' => dci_get_posts_options('sito_tematico'),
-    ) );
-    $home_options->add_group_field( $argomenti_group_id, array(
-            'name' => __('<h5>Selezione contenuti</h5>', 'design_comuni_italia'),
-            'desc' => __('Seleziona i contenuti da mostrare nella Card dell\'Argomento. ', 'design_comuni_italia'),
-            'id' => $prefix . 'argomento_1_contenuti',
-            'type'    => 'custom_attached_posts',
-            'column'  => true, // Output in the admin post-listing as a custom column. https://github.com/CMB2/CMB2/wiki/Field-Parameters#column
-            'options' => array(
-                'show_thumbnails' => false, // Show thumbnails on the left
-                'filter_boxes'    => true, // Show a text box for filtering the results
-                'query_args'      => array(
-                    'posts_per_page' => -1,
-                    'post_type'      => array('evento','luogo','unita_organizzativa','documento_pubblico','servizio','notizia'),
-                ), // override the get_posts args
-            )
-        )
-    );
-    $argomenti_group_id = $home_options->add_field( array(
-        'id'           => $prefix . 'argomenti_evidenziati_2',
-        'type'        => 'group',
-        'repeatable'  => false,
-        'options'     => array(
-            'group_title'   => __( 'Argomento 2: ', 'design_comuni_italia' ),
-            'closed' => true
-        ),
-    ) );
-    $home_options->add_group_field( $argomenti_group_id, array(
-        'id' => $prefix . 'argomento_2_argomento',
-        'name'        => __( 'Argomento', 'design_comuni_italia' ),
-        'desc' => __( 'Seleziona l\'Argomento' , 'design_comuni_italia' ),
-        'type' => 'taxonomy_select',
-        'taxonomy'=>'argomenti'
-    ) );
-    $home_options->add_group_field( $argomenti_group_id, array(
-        'id' => $prefix . 'argomento_2_siti_tematici',
-        'name'        => __( 'Sito Tematico ', 'design_comuni_italia' ),
-        'desc' => __( 'Selezionare il sito tematico da inserire nella Card' , 'design_comuni_italia' ),
-        'type'    => 'pw_select',
-        'options' => dci_get_posts_options('sito_tematico'),
-    ) );
-    $home_options->add_group_field( $argomenti_group_id, array(
-            'name' => __('<h5>Selezione contenuti</h5>', 'design_comuni_italia'),
-            'desc' => __('Seleziona i contenuti da mostrare nella Card dell\'Argomento. ', 'design_comuni_italia'),
-            'id' => $prefix . 'argomento_2_contenuti',
-            'type'    => 'custom_attached_posts',
-            'column'  => true, // Output in the admin post-listing as a custom column. https://github.com/CMB2/CMB2/wiki/Field-Parameters#column
-            'options' => array(
-                'show_thumbnails' => false, // Show thumbnails on the left
-                'filter_boxes'    => true, // Show a text box for filtering the results
-                'query_args'      => array(
-                    'posts_per_page' => -1,
-                    'post_type'      => array('evento','luogo','unita_organizzativa','documento_pubblico','servizio','notizia'),
-                ), // override the get_posts args
-            )
-        )
-    );
+
+
 	
+// Sezione Argomenti
+$home_options->add_field( array(
+    'id'   => $prefix . 'argomenti_title',
+    'name' => __( 'Sezione Argomenti', 'design_comuni_italia' ),
+    'desc' => __( 'Gestione Argomenti mostrati in homepage.', 'design_comuni_italia' ),
+    'type' => 'title',
+) );
+
+$num_argomenti = 3; // Numero di argomenti
+
+for ($i = 1; $i <= $num_argomenti; $i++) {
     $argomenti_group_id = $home_options->add_field( array(
-        'id'           => $prefix . 'argomenti_evidenziati_3',
+        'id'          => $prefix . 'argomenti_evidenziati_' . $i,
         'type'        => 'group',
         'repeatable'  => false,
         'options'     => array(
-            'group_title'   => __( 'Argomento 3: ', 'design_comuni_italia' ),
-            'closed' => true
+            'group_title' => sprintf( __( 'Argomento %d: ', 'design_comuni_italia' ), $i ),
+            'closed'      => true
         ),
     ) );
+
     $home_options->add_group_field( $argomenti_group_id, array(
-        'id' => $prefix . 'argomento_3_argomento',
-        'name'        => __( 'Argomento', 'design_comuni_italia' ),
-        'desc' => __( 'Seleziona l\'Argomento' , 'design_comuni_italia' ),
-        'type' => 'taxonomy_select',
-        'taxonomy'=>'argomenti'
+        'id'       => $prefix . 'argomento_' . $i . '_argomento',
+        'name'     => __( 'Argomento', 'design_comuni_italia' ),
+        'desc'     => __( 'Seleziona l\'Argomento', 'design_comuni_italia' ),
+        'type'     => 'taxonomy_select',
+        'taxonomy' => 'argomenti'
     ) );
+
     $home_options->add_group_field( $argomenti_group_id, array(
-        'id' => $prefix . 'argomento_3_siti_tematici',
-        'name'        => __( 'Sito Tematico ', 'design_comuni_italia' ),
-        'desc' => __( 'Selezionare il sito tematico da inserire nella Card' , 'design_comuni_italia' ),
-        'type'    => 'pw_select',
-        'options' => dci_get_posts_options('sito_tematico'),
+        'id'       => $prefix . 'argomento_' . $i . '_siti_tematici',
+        'name'     => __( 'Sito Tematico', 'design_comuni_italia' ),
+        'desc'     => __( 'Selezionare il sito tematico da inserire nella Card', 'design_comuni_italia' ),
+        'type'     => 'pw_select',
+        'options'  => dci_get_posts_options('sito_tematico'),
     ) );
+
     $home_options->add_group_field( $argomenti_group_id, array(
-            'name' => __('<h5>Selezione contenuti</h5>', 'design_comuni_italia'),
-            'desc' => __('Seleziona i contenuti da mostrare nella Card dell\'Argomento. ', 'design_comuni_italia'),
-            'id' => $prefix . 'argomento_3_contenuti',
-            'type'    => 'custom_attached_posts',
-            'column'  => true, // Output in the admin post-listing as a custom column. https://github.com/CMB2/CMB2/wiki/Field-Parameters#column
-            'options' => array(
-                'show_thumbnails' => false, // Show thumbnails on the left
-                'filter_boxes'    => true, // Show a text box for filtering the results
-                'query_args'      => array(
-                    'posts_per_page' => -1,
-                    'post_type'      => array('evento','luogo','unita_organizzativa','documento_pubblico','servizio','notizia'),
-                ), // override the get_posts args
-            )
+        'name'     => __('<h5>Selezione contenuti</h5>', 'design_comuni_italia'),
+        'desc'     => __('Seleziona i contenuti da mostrare nella Card dell\'Argomento.', 'design_comuni_italia'),
+        'id'       => $prefix . 'argomento_' . $i . '_contenuti',
+        'type'     => 'custom_attached_posts',
+        'column'   => true,
+        'options'  => array(
+            'show_thumbnails' => false,
+            'filter_boxes'    => true,
+            'query_args'      => array(
+                'posts_per_page' => -1,
+                'post_type'      => array('evento', 'luogo', 'unita_organizzativa', 'documento_pubblico', 'servizio', 'notizia'),
+            ),
         )
-    );
+    ) );
+}
+
+	
     $home_options->add_field( array(
         'id' => $prefix . 'argomenti_altri',
         'name'        => __( 'Altri argomenti', 'design_comuni_italia' ),
