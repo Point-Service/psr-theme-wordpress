1efd96600fd24ee2760b273553bd3b4b99789aef
Point-Service <tecnico@p-service.it>
2024-06-05 15:26:45 +0200

    Update page.php

--- page.php
+++ page.php
@@ -1,4 +1,4 @@
-A<?php
+<?php
 /*
  * Generic Page Template
  *
@@ -35,10 +35,29 @@ get_header();
                 <div class="container ">
                     <article class="article-wrapper">
 
-                                              <div class="row variable-gutters">
+                        <div class="row variable-gutters">
                             <div class="col-lg-12">
                                 <?php
                                 the_content();
+                                ?>
+                                <div class="callout note">
+                                    <div class="callout-inner">
+                                        <div class="callout-title">
+                                            <svg class="icon"><use href="#it-info-circle"></use></svg>
+                                            <span class="visually-hidden">Attenzione</span> 
+                                            <span class="text">Attenzione</span>
+                                        </div>
+                                        <p><strong>Il modello di questa pagina non è ancora disponibile.</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row variable-gutters">
+                            <div class="col-lg-12">
+                                <?php
+                                if ( comments_open() || get_comments_number() ) :
+                                    comments_template();
+                                endif;
                                 ?>
                             </div>
                         </div>
