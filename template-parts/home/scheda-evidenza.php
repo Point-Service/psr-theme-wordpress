b74da247f0668d45c1eff5b3ee952593e1b60265
Point-Service <tecnico@p-service.it>
2024-06-05 11:29:41 +0200

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -1,15 +1,19 @@
-Luca<?php
-global $scheda, $count;
+<?php
+global $scheda;
 
-$post = get_post($scheda['scheda_'.$count.'_contenuto'][0]);
-$img = dci_get_meta('immagine');
-$descrizione_breve = dci_get_meta('descrizione_breve');
+$post = get_post($scheda)??null;
+$img = get_the_post_thumbnail_url();
+$descrizione_breve = dci_get_meta('descrizione_breve') ?: dci_get_meta('_dci_page_descrizione');
 $icon = dci_get_post_type_icon_by_id($post->ID);
 
-$page = get_page_by_path( dci_get_group($post->post_type) );    
+$post_type = $post->post_type == 'page' ? 'Pagina' : get_page_by_path( dci_get_group($post->post_type) )->post_title;
 
 $page_macro_slug = dci_get_group($post->post_type);
 $page_macro = get_page_by_path($page_macro_slug);
+
+if (!isset($titlelevel) || $titlelevel === null || trim($titlelevel) === '') {
+    $titleheading = "h3"; 
+}
 ?>
 
 <?php if ($img) { ?>
@@ -17,16 +21,13 @@ $page_macro = get_page_by_path($page_macro_slug);
     <div class="card-image-wrapper with-read-more">
         <div class="card-body p-3 u-grey-light">
             <div class="category-top">
-            <!-- <svg class="icon">
-                <use xlink:href="#<?php #echo $icon ?>"></use>
-            </svg> -->
-            <span class="category title-xsmall-semi-bold fw-semibold" ><?php echo $page->post_title ?></span>
+            <span class="category title-xsmall-semi-bold fw-semibold" ><?= $post_type ?></span>
             </div>
-            <p class="card-title text-paragraph-medium u-grey-light"><?php echo $post->post_title ?></p>
+            <?php echo '<' . $titleheading . ' class="card-title text-paragraph-medium u-grey-light">' . $post->post_title . '</' . $titleheading . '>'; ?>
             <p class="text-paragraph-card u-grey-light m-0" style="margin-bottom: 40px!important;"><?php echo $descrizione_breve ?></p>
         </div>
         <div class="card-image card-image-rounded pb-5">            
-            <?php dci_get_img($img); ?>
+            <?php dci_get_img($img, size:'thumbnail'); ?>
         </div>
     </div>
     <a
@@ -42,17 +43,15 @@ $page_macro = get_page_by_path($page_macro_slug);
     </a>
 </div>
 <?php } else { ?>
-    <div class="card card-teaser no-after rounded shadow-sm mb-0">
+    <div class="card card-teaser no-after rounded shadow-sm mb-0 border border-light">
         <div class="card-body pb-5">
         <div class="category-top">
             <!-- <svg class="icon">
                 <use xlink:href="#<?php #echo $icon ?>"></use>
             </svg> -->
-            <span class="category title-xsmall-semi-bold fw-semibold"><?php echo $page->post_title ?></span>
+            <span class="category title-xsmall-semi-bold fw-semibold"><?= $post_type ?></span>
         </div>
-        <p class="card-title text-paragraph-medium u-grey-light">
-            <?php echo $post->post_title ?>
-        </p>
+        <?php echo '<' . $titleheading . ' class="card-title text-paragraph-medium u-grey-light">' . $post->post_title . '</' . $titleheading . '>'; ?>
         <p class="text-paragraph-card u-grey-light m-0">
             <?php echo $descrizione_breve ?>
         </p>
