7a2f1dde5ffaa5083349179af7c7ea7e5a8daf68
Point-Service <tecnico@p-service.it>
2024-06-05 17:08:27 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -94,7 +94,7 @@ for ($i = 1; $i <= 20; $i++) {
     </div>
 </section>
 
-  <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
+
     <div class="container">
       <div class="row mb-2">
         <div class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
@@ -111,7 +111,7 @@ for ($i = 1; $i <= 20; $i++) {
             </div>
           </div>
          </div>
-       </div>
+  
 
 <section id="calendario">
   <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
