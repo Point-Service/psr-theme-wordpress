924c195f27b6c2b7a6429f9cb26eee47bcbc4a0b
Point-Service <tecnico@p-service.it>
2024-06-05 12:30:02 +0200

    Update slimheader.php

--- template-parts/header/slimheader.php
+++ template-parts/header/slimheader.php
@@ -1,55 +1,41 @@
-<?php
-$nascondi_login = dci_get_option('nascondi_pulsante_login');
-$barra_superiore_light = dci_get_option("tema_chiaro_nav_superiore");
-?>
-
-<div class="it-header-slim-wrapper <?= $barra_superiore_light ? 'theme-light' : '' ?>">
+<div class="it-header-slim-wrapper">
   <div class="container">
     <div class="row">
       <div class="col-12">
         <div class="it-header-slim-wrapper-content">
-          <a class="d-lg-block navbar-brand" target="_blank" href="<?php echo dci_get_option("url_sito_regione"); ?>" aria-label="Vai al portale <?php echo dci_get_option("nome_regione"); ?> - link esterno - apertura nuova scheda" title="Vai al portale <?php echo dci_get_option("nome_regione"); ?>"><?php echo dci_get_option("nome_regione"); ?></a>
 
+          <a class="d-lg-block navbar-brand" target="_blank" href="<?php echo dci_get_option("url_sito_regione"); ?>" target="_blank" aria-label="Vai al portale <?php echo dci_get_option("nome_regione"); ?> - link esterno - apertura nuova scheda" title="Vai al portale <?php echo dci_get_option("nome_regione"); ?>">
+            
+            <?php echo dci_get_option("nome_regione"); ?></a>
           <div class="it-header-slim-right-zone" role="navigation">
-            <div class="nav-mobile text-end">
-              <nav aria-label="Navigazione accessoria">
-                <a class="it-opener d-lg-none" data-bs-toggle="collapse" href="#menu1a" role="button" aria-expanded="false" aria-controls="menu4">
-                  <span>Link rapidi</span>
-                  <svg class="icon" aria-hidden="true">
-                    <use href="#it-expand"></use>
-                  </svg>
-                </a>
-                <div class="link-list-wrapper collapse" id="menu1a">
-                  <?php
-                  if (has_nav_menu('menu-barra-superiore')) {
-                    wp_nav_menu(array(
-                      'theme_location' => 'menu-barra-superiore',
-                      'list_item_class' => 'list-item dropdown-item',
-                      'menu_class' => 'link-list ' . ($nascondi_login ? 'me-0' : ''),
-                      'container' => false,
-                      'depth' => 1,
-                      'current_group' => basename(get_permalink()),
-                      'walker' => new Main_Menu_Walker()
-                    ));
-                  }
-                  ?>
-                </div>
-              </nav>
+            <?php if(dci_get_option("link_ammtrasparente")) { ?>
+            <div class="it-user-wrapper nav-item dropdown">
+               <a aria-expanded="false" class="btn btn-primary btn-icon btn-full" data-toggle="dropdown" target="_blank" href=" <?php echo dci_get_option("link_ammtrasparente"); ?>" data-focus-mouse="false">
+                  Amministrazione trasparente
+               </a>
             </div>
-                        <?php }?>
+            <?php }?>
+            <?php if(dci_get_option("link_albopretorio")) { ?>
+            <div class="it-user-wrapper nav-item dropdown">
+               <a aria-expanded="false" class="btn btn-primary btn-icon btn-full" target="_blank" data-toggle="dropdown" href="<?php echo dci_get_option("link_albopretorio"); ?>" data-focus-mouse="false">
+                  Albo pretorio
+               </a>
+            </div>
+            <?php }?>
                 
                         <?php echo do_shortcode('[google-translator]'); ?>
+                     
             <?php
-            if (!is_user_logged_in()) {
-              if (!$nascondi_login)
-                get_template_part("template-parts/header/header-anon");
-            } else {
-              get_template_part("template-parts/header/header-logged");
-            }
-            ?>
+                if(!is_user_logged_in()) {
+                    get_template_part("template-parts/header/header-anon");
+                }else{
+                    get_template_part("template-parts/header/header-logged");
+                }
+              ?>
           </div>
         </div>
       </div>
     </div>
   </div>
 </div>
+
