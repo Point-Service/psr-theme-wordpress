ed1c7ccf4e8e49526a29c8ba427f42e8b53cb60e
Point-Service <tecnico@p-service.it>
2024-06-05 12:18:03 +0200

    Update header-anon.php

--- template-parts/header/header-anon.php
+++ template-parts/header/header-anon.php
@@ -1,8 +1,9 @@
-<button type="button" style="flex-basis: auto;" class="btn btn-primary btn-icon btn-full" aria-label="<?php _e("Accedi all'area personale", "design_comuni_italia"); ?>" data-bs-toggle="modal" data-bs-target="#access-modal">
+<a class="btn btn-primary btn-icon btn-full" href="<?php echo get_admin_url(); ?>" data-element="personal-area-login">
     <span class="rounded-icon" aria-hidden="true">
         <svg class="icon icon-primary">
             <use xlink:href="#it-user"></use>
         </svg>
     </span>
-    <span class="mr-2 d-none d-lg-block"><?php _e("Accedi all'area personale", "design_comuni_italia"); ?></span>
-</button>
+    <span class="d-none d-lg-block">Accedi all'area personale</span>
+</a>
+
