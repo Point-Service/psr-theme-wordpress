39579f8bde7eecfc9ff5c42c5bcf0b708a4b988b
Point-Service <tecnico@p-service.it>
2024-06-06 12:35:32 +0200

    Update siti-tematici.php

--- template-parts/home/siti-tematici.php
+++ template-parts/home/siti-tematici.php
@@ -1,25 +1,30 @@
 <?php
-global $sito_tematico_id, $count;
+global $sito_tematico_id, $count, $location;
 
-$siti_tematici = dci_get_option('siti_tematici','homepage');
-if(is_array($siti_tematici) && count($siti_tematici)) {
+$siti_tematici = dci_get_option('siti_tematici', $location??'homepage');
+if (is_array($siti_tematici) && count($siti_tematici)) {
 ?>
 
-<div class="container">
-  <div class="row pt-5">
-    <h2 class="mb-0 u-grey-light">Siti tematici</h2>
-  </div>
-  <div class="pt-4 pt-lg-30">
-    <div
-      class="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3"
-    >
-      <?php
-      $count = 0;
-      foreach ($siti_tematici as $sito_tematico_id) {
-        get_template_part("template-parts/sito-tematico/card");
+  <div class="container mb-4">
+    <div class="row">
+      <h2 class="mb-0">Siti tematici</h2>
+    </div>
+    <div class="pt-4 pt-lg-30">
+      <div class="row gy-4">
+        <?php
+        $count = 0;
+        foreach ($siti_tematici as $sito_tematico_id) {
+        ?>
+          <div class="col-12 col-md-6 col-lg-4 card-wrapper pb-0">
+            <?php
+            get_template_part("template-parts/sito-tematico/card");
+            ?>
+          </div>
+        <?php
         ++$count;
-      } ?>
+        }
+        ?>
+      </div>
     </div>
   </div>
-</div>
 <?php } ?>
