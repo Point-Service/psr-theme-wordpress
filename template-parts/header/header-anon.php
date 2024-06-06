1d33f9256c46faf4b932044187eea10f6633ac21
Point-Service <tecnico@p-service.it>
2024-06-06 16:24:22 +0200

    Update header-anon.php

--- template-parts/header/header-anon.php
+++ template-parts/header/header-anon.php
@@ -7,7 +7,15 @@
     </span>
     <span class="d-none d-lg-block">Accedi all'area personale</span>
   </a>
-<?php }?>
-
+<?php } else { ?>
+   <a class="btn btn-primary btn-icon btn-full" href="<?php echo get_admin_url(); ?>" data-element="personal-area-login">
+    <span class="rounded-icon" aria-hidden="true">
+        <svg class="icon icon-primary">
+            <use xlink:href="#it-user"></use>
+        </svg>
+    </span>
+    <span class="d-none d-lg-block">Accedi all'area personale</span>
+  </a>
+<?php } ?>
 
 
