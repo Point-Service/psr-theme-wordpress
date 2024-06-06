9c1888bef1cf6275f0090b1fd1031f4354361780
Point-Service <tecnico@p-service.it>
2024-06-06 11:18:48 +0200

    Update argomenti.php

--- template-parts/home/argomenti.php
+++ template-parts/home/argomenti.php
@@ -15,7 +15,7 @@ $altri_argomenti = dci_get_option('argomenti_altri','homepage');
 <div class="container">
   <?php if ($argomenti_evidenza) { ?>
     <div class="row">
-           
+        <?php     
         if (is_array($argomenti_evidenza) && $contatore >= 1) { ?> 
             <h2 class="text-black title-xlarge mb-3">Argomenti in Evidenza (Totale: <?php echo $contatore; ?>)</h2> 
         <?php } ?>
