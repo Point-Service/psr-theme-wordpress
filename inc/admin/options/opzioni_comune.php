99572ca82de69778de42de12e768589364d3252f
Point-Service <tecnico@p-service.it>
2024-06-06 15:52:19 +0200

    Update opzioni_comune.php

--- inc/admin/options/opzioni_comune.php
+++ inc/admin/options/opzioni_comune.php
@@ -133,10 +133,5 @@ function dci_register_comune_options(){
         'type' => 'text'
     ));
     
-    $header_options->add_field( array(
-        'id'    => $prefix . 'Accedi_area_personale',
-        'name' => __('Accedi all'area personale', 'design_comuni_italia' ),
-        'desc' => __( 'Accedi all'area personale'),
-        'type' => 'text'
-    ));
+
 }
