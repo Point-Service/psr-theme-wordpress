59652bf47be786baea74935590782a5939fcaf24
Point-Service <tecnico@p-service.it>
2024-06-07 10:47:42 +0200

    Update page_bottom-simple.php

--- template-parts/single/page_bottom-simple.php
+++ template-parts/single/page_bottom-simple.php
@@ -3,10 +3,7 @@
 
     // metadata
     $last_edit = explode(' ', $post->post_modified);
-    $last_edit_date = new DateTime($last_edit[0]);
-
-	$formatter = new IntlDateFormatter('it_IT', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT);
-	$formatter->setPattern('d LLLL yyyy');
+    $last_edit_day = implode('/',array_reverse(explode('-',$last_edit[0])));
 ?>
 
-<p class="text-paragraph-small mb-0">Pagina aggiornata il <?php echo $formatter->format($last_edit_date); ?></p>
+<p class="text-paragraph-small mb-0">Pagina aggiornata il <?php echo $last_edit_day; ?></p>
