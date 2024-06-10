7b0f69813b79675db8f43ad777f23d81e74b9433
Point-Service <tecnico@p-service.it>
2024-06-10 17:47:44 +0200

    Update card.php

--- template-parts/unita-organizzativa/card.php
+++ template-parts/unita-organizzativa/card.php
@@ -1,47 +1,37 @@
 <?php
-    global $uo_id, $with_border;
+    global $uo_id, $with_border, $data_element;
     $ufficio = get_post( $uo_id );
 
-     $prefix = '_dci_unita_organizzativa_';
-     $descrizione_breve = dci_get_meta("descrizione_breve", $prefix, $ufficio->ID);
+    $prefix = '_dci_unita_organizzativa_';
+    $img = dci_get_meta('immagine', $prefix, $uo_id);
+    $contatti = dci_get_meta('contatti', $prefix, $uo_id);
+    $descrizione_breve = dci_get_meta("descrizione_breve", $prefix, $ufficio->ID);
 
-
-		$punti_contatto = dci_get_meta('contatti', $prefix, $uo_id);
-		$prefix = '_dci_punto_contatto_';
-		$contatti = array();
-
-		foreach ($punti_contatto as $pc_id) {
-		    $contatto = dci_get_full_punto_contatto($pc_id);
-		    array_push($contatti, $contatto);
-		}
-		$other_contacts = array(
-		    'linkedin',
-		    'pec',
-		    'skype',
-		    'telegram',
-		    'twitter',
-		    'whatsapp'
-		);
-
-
-
-
-
-    $img = get_the_post_thumbnail_url($ufficio->ID);
-
+    $prefix = '_dci_punto_contatto_';
+    $indirizzi = array();
+    foreach ($contatti as $punto_contatto_id) {
+        $voci = dci_get_meta('voci', $prefix, $punto_contatto_id);
+        foreach ($voci as $voce) {
+            if ($voce[$prefix.'tipo_punto_contatto'] == 'indirizzo')
+                array_push($indirizzi, $voce[$prefix.'valore']);
+        }
+    }
+    
     if($with_border) {
 ?>
 
-<div class="card card-teaser card-teaser-info rounded shadow-sm p-3 mb-2">
+<div class="card card-teaser card-teaser-info rounded shadow-sm p-3">
     <div class="card-body pe-3">
-        <h4 class="card-title text-paragraph-regular-medium-semi mb-3">
-            <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>" data-element="service-area">
+        <p class="card-title text-paragraph-regular-medium-semi mb-3">
+            <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>" data-element="<?php echo $data_element != null ? $data_element : 'service_area'; ?>">
             <?php echo $ufficio->post_title; ?>
             </a>
-        </h4>
-	   <?php if ($descrizione_breve) {
-		        echo '<div class="card-text"><p class="u-main-black">'.$descrizione_breve.'</p></div>';
-	   } ?>
+        </p>
+        <div class="card-text">
+            <?php foreach ($indirizzi as $indirizzo) {
+                echo '<p class="u-main-black">'.$indirizzo.'</p>';
+            }?>
+        </div>
     </div>
     <?php if ($img) { ?>
         <div class="avatar size-xl">
@@ -57,14 +47,11 @@
             <?php echo $ufficio->post_title; ?>
             </a>
         </h4>
-	   <?php if ($descrizione_breve) {
-		        echo '<div class="card-text"><p class="u-main-black">'.$descrizione_breve.'</p></div>';
-	   } ?>
-
-        
-
-    
-	    
+        <div class="card-text">
+            <?php foreach ($indirizzi as $indirizzo) {
+                echo '<p>'.$indirizzo.'</p>';
+            }?>
+        </div>
     </div>
     <?php if ($img) { ?>
     <div class="avatar size-xl">
