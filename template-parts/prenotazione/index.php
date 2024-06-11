e6d492048edfb19f0ec89e9cf1b5b1bee5682a8f
Point-Service <tecnico@p-service.it>
2024-06-11 17:35:59 +0200

    Update index.php

--- template-parts/prenotazione/index.php
+++ template-parts/prenotazione/index.php
@@ -1,7 +1,5 @@
 <!-- Step 1 -->
-<div class="col-12 col-lg-3 mb-4 d-none" data-index="1" style="display: none !important">
-</div>
-<div class="col-12 col-lg-3 mb-4 d-none" data-index="2">
+<div class="col-12 col-lg-3 mb-4 d-none" data-index="1">
     <div class="cmp-navscroll sticky-top" aria-labelledby="accordion-title-one">
         <nav class="navbar it-navscroll-wrapper navbar-expand-lg" aria-label="INFORMAZIONI RICHIESTE" data-bs-navscroll>
             <div class="navbar-custom" id="navbarNavProgress">
@@ -19,13 +17,13 @@
                                 </span>
                                 <div class="progress">
                                     <div class="progress-bar it-navscroll-progressbar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                                <div id="collapse-one" class="accordion-collapse collapse show" role="region" aria-labelledby="accordion-title-one">
+                                    </div>
+                                    <div id="collapse-one" class="accordion-collapse collapse show" role="region" aria-labelledby="accordion-title-one">
                                     <div class="accordion-body">
                                         <ul class="link-list" data-element="page-index">
                                             <li class="nav-item">
                                                 <a class="nav-link" href="#office">
-                                                    <span class="title-medium">Ufficio</span>
+                                                <span class="title-medium">Ufficio</span>
                                                 </a>
                                             </li>
                                         </ul>
@@ -40,7 +38,7 @@
     </div>
 </div>
 <!-- Step 2 -->
-<div class="col-12 col-lg-3 mb-4 d-none" data-index="3">
+<div class="col-12 col-lg-3 mb-4 d-none" data-index="2">
     <div class="cmp-navscroll sticky-top" aria-labelledby="accordion-title-one">
         <nav class="navbar it-navscroll-wrapper navbar-expand-lg" aria-label="INFORMAZIONI RICHIESTE" data-bs-navscroll>
             <div class="navbar-custom" id="navbarNavProgress">
@@ -65,12 +63,12 @@
                                         <ul class="link-list" data-element="page-index">
                                             <li class="nav-item">
                                                 <a class="nav-link" href="#appointment-available">
-                                                    <span class="title-medium">Appuntamenti disponibili</span>
+                                                <span class="title-medium">Appuntamenti disponibili</span>
                                                 </a>
                                             </li>
                                             <li class="nav-item">
                                                 <a class="nav-link" href="#office-2">
-                                                    <span class="title-medium">Ufficio</span>
+                                                <span class="title-medium">Ufficio</span>
                                                 </a>
                                             </li>
                                         </ul>
@@ -85,7 +83,7 @@
     </div>
 </div>
 <!-- Step 3 -->
-<div class="col-12 col-lg-3 mb-4 d-none" data-index="4">
+<div class="col-12 col-lg-3 mb-4 d-none" data-index="3">
     <div class="cmp-navscroll sticky-top" aria-labelledby="accordion-title-one">
         <nav class="navbar it-navscroll-wrapper navbar-expand-lg" aria-label="INFORMAZIONI RICHIESTE" data-bs-navscroll>
             <div class="navbar-custom" id="navbarNavProgress">
@@ -109,12 +107,12 @@
                                         <ul class="link-list" data-element="page-index">
                                             <li class="nav-item">
                                                 <a class="nav-link" href="#reason">
-                                                    <span class="title-medium">Motivo</span>
+                                                <span class="title-medium">Motivo</span>
                                                 </a>
                                             </li>
                                             <li class="nav-item">
                                                 <a class="nav-link" href="#details">
-                                                    <span class="title-medium">Dettagli</span>
+                                                <span class="title-medium">Dettagli</span>
                                                 </a>
                                             </li>
                                         </ul>
@@ -129,7 +127,7 @@
     </div>
 </div>
 <!-- Step 4 -->
-<div class="col-12 col-lg-3 mb-4 d-none" data-index="5">
+<div class="col-12 col-lg-3 mb-4 d-none" data-index="4">
     <div class="cmp-navscroll sticky-top" aria-labelledby="accordion-title-one">
         <nav class="navbar it-navscroll-wrapper navbar-expand-lg" aria-label="INFORMAZIONI RICHIESTE" data-bs-navscroll>
             <div class="navbar-custom" id="navbarNavProgress">
@@ -153,7 +151,7 @@
                                         <ul class="link-list" data-element="page-index">
                                             <li class="nav-item">
                                                 <a class="nav-link" href="#applicant">
-                                                    <span class="title-medium">Richiedente</span>
+                                                <span class="title-medium">Richiedente</span>
                                                 </a>
                                             </li>
                                         </ul>
