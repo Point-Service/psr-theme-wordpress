30fa7becf99dcf6f7b1197dc9592905f08591326
Point-Service <tecnico@p-service.it>
2024-06-11 12:29:21 +0200

    Update single-servizio.php

--- single-servizio.php
+++ single-servizio.php
@@ -451,6 +451,24 @@ get_header();
                                         </ul>
                                         <?php get_template_part('template-parts/single/page_bottom',"simple"); ?>
                                     </div>
+
+                                    <div class="col-12 mb-30">
+                                      <span class="text-paragraph-small">Categorie:</span>
+                                        <ul class="d-flex flex-wrap gap-2 mt-10 mb-30">
+                                            <?php foreach ( $categorie as $item ) { ?>
+                                                <li>
+                                                    <a href="<?php echo get_term_link($item); ?>" class="chip chip-simple" data-element="service-topic">
+                                                        <span class="chip-label">
+                                                            <?php echo $item->name; ?>
+                                                        </span>
+                                                    </a>
+                                                </li>
+                                            <?php } ?>
+                                        </ul>
+                                        <?php get_template_part('template-parts/single/page_bottom',"simple"); ?>
+                                    </div>
+
+                                
                                 </div>
                           
                             </section>
