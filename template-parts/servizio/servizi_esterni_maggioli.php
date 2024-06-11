d8f3fc2affbe3b9fceab555f8c76ed0de9ec8cd5
Point-Service <tecnico@p-service.it>
2024-06-11 10:14:18 +0200

    Update servizi_esterni_maggioli.php

--- template-parts/servizio/servizi_esterni_maggioli.php
+++ template-parts/servizio/servizi_esterni_maggioli.php
@@ -47,7 +47,7 @@ function get_procedures_data($search_term = null)
             }
 
             
-            echo"<div style="display:inline;"></div>hh"
+            echo"<div></div>"
                 
             // Output del totale
             echo "<h2>Servizi Aggiuntivi ( $total_services )</h2>";
