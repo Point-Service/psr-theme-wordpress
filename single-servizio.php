aad7d28df8dd7cf358600a98f5579a30f67e55dd
Point-Service <tecnico@p-service.it>
2024-06-11 11:31:46 +0200

    Update single-servizio.php

--- single-servizio.php
+++ single-servizio.php
@@ -409,14 +409,16 @@ get_header();
                             <section class="it-page-section mb-30">
                                 <h2 class="mb-3" id="contacts">Contatti</h2>
                                   <div class="row">                            
-                                        <div class="col-12 col-lg-8 col-lg-6 mb-30">                         
-                                        <div class="card-wrapper rounded h-auto mt-10">
-                                            <?php
+
+                                   <div class="col-12 mb-30">
+                                        <span class="text-paragraph-small">Contatti:</span>
+                                        <ul class="d-flex flex-wrap gap-2 mt-10 mb-30">
+                                                <?php
                                                 $with_border = true;
                                                 get_template_part("template-parts/unita-organizzativa/card");
-                                            ?>
-                                    </div>                                       
-                                   </div>
+                                              ?>
+                                        </ul>
+                                    </div>                                      
                                     <div class="col-12 mb-30">
                                         <span class="text-paragraph-small">Argomenti:</span>
                                         <ul class="d-flex flex-wrap gap-2 mt-10 mb-30">
