2e5bb66af441ea9cceb28dbe2e0fa6fcad8a9a9f
Point-Service <tecnico@p-service.it>
2024-06-11 16:11:31 +0200

    Update single-servizio.php

--- single-servizio.php
+++ single-servizio.php
@@ -406,63 +406,63 @@ get_header();
                             <?php } ?>
                                         
 
-                          <section class="it-page-section mb-30">
-    <h3 class="mb-3" id="contacts">Contatta ufficio</h3>
-    <div class="row">
-        <div class="col-12 col-md-8 col-lg-6 mb-30">                                 
-            <div data-element="service-area">
-                <?php
-                    $with_border = true;
-                    $no_vertical_margin = true;
-                    get_template_part("template-parts/unita-organizzativa/card-full");
-                ?>
-            </div>
-        </div>
-    </div>
-    <?php
-        $punti_contatto_id = dci_get_meta("punti_contatto");
-        if(!empty($punti_contatto_id)){                                      
-    ?>
-    <h3 class="mb-3" id="contacts">Contatti dedicati</h3>
-    <div class="row">
-        <?php foreach($punti_contatto_id as $pc_id) { ?>
-            <div class="col-12 col-md-6 col-lg-4 mb-30" data-element="service-area">
-                <?php get_template_part("template-parts/punto-contatto/card"); ?>
-            </div>
-        <?php } ?>
-    </div>
-    <?php } ?>                                   
-    <div class="col-12 mb-30">
-        <span class="text-paragraph-small">Argomenti:</span>
-        <ul class="d-flex flex-wrap gap-2 mt-10 mb-30">
-            <?php foreach ( $argomenti as $item ) { ?>
-                <li>
-                    <a href="<?php echo get_term_link($item); ?>" class="chip chip-simple" data-element="service-topic">
-                        <span class="chip-label">
-                            <?php echo $item->name; ?>
-                        </span>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
-    </div>
-
-    <div class="col-12 mb-30">
-        <span class="text-paragraph-small">Categorie:</span>
-        <ul class="d-flex flex-wrap gap-2 mt-10 mb-30">
-            <?php foreach ( $categorie as $item ) { ?>
-                <li>
-                    <a href="<?php echo get_term_link($item); ?>" class="chip chip-simple" data-element="service-topic">
-                        <span class="chip-label">
-                            <?php echo $item->name; ?>
-                        </span>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
-        <?php get_template_part('template-parts/single/page_bottom',"simple"); ?>
-    </div>
-</section>
+                     <section class="it-page-section mb-30">
+                      <h3 class="mb-3" id="contacts">Contatta ufficio</h3>
+                      <div class="row">
+                          <div class="col-12 col-md-8 col-lg-6 mb-30">                                 
+                              <div data-element="service-area">
+                                  <?php
+                                      $with_border = true;
+                                      $no_vertical_margin = true;
+                                      get_template_part("template-parts/unita-organizzativa/card");
+                                  ?>
+                              </div>
+                          </div>
+                      </div>
+                      <?php
+                          $punti_contatto_id = dci_get_meta("punti_contatto");
+                          if(!empty($punti_contatto_id)){                                      
+                      ?>
+                      <h3 class="mb-3" id="contacts">Contatti dedicati</h3>
+                      <div class="row">
+                          <?php foreach($punti_contatto_id as $pc_id) { ?>
+                              <div class="col-12 col-md-6 col-lg-4 mb-30" data-element="service-area">
+                                  <?php get_template_part("template-parts/punto-contatto/card"); ?>
+                              </div>
+                          <?php } ?>
+                      </div>
+                      <?php } ?>                                   
+                      <div class="col-12 mb-30">
+                          <span class="text-paragraph-small">Argomenti:</span>
+                          <ul class="d-flex flex-wrap gap-2 mt-10 mb-30">
+                              <?php foreach ( $argomenti as $item ) { ?>
+                                  <li>
+                                      <a href="<?php echo get_term_link($item); ?>" class="chip chip-simple" data-element="service-topic">
+                                          <span class="chip-label">
+                                              <?php echo $item->name; ?>
+                                          </span>
+                                      </a>
+                                  </li>
+                              <?php } ?>
+                          </ul>
+                      </div>
+                  
+                      <div class="col-12 mb-30">
+                          <span class="text-paragraph-small">Categorie:</span>
+                          <ul class="d-flex flex-wrap gap-2 mt-10 mb-30">
+                              <?php foreach ( $categorie as $item ) { ?>
+                                  <li>
+                                      <a href="<?php echo get_term_link($item); ?>" class="chip chip-simple" data-element="service-topic">
+                                          <span class="chip-label">
+                                              <?php echo $item->name; ?>
+                                          </span>
+                                      </a>
+                                  </li>
+                              <?php } ?>
+                          </ul>
+                          <?php get_template_part('template-parts/single/page_bottom',"simple"); ?>
+                      </div>
+                  </section>
                     </div>
                 </div>
             </div>
