b8b6dd35f1cf730f7138649ef73e51fa4fb81083
Point-Service <tecnico@p-service.it>
2024-06-11 12:22:36 +0200

    Update card-full.php

--- template-parts/unita-organizzativa/card-full.php
+++ template-parts/unita-organizzativa/card-full.php
@@ -43,7 +43,7 @@
                     } ?>
                     <?php if ( isset($full_contatto['telefono']) && is_array($full_contatto['telefono']) && count ($full_contatto['telefono']) ) {
                         foreach ($full_contatto['telefono'] as $value) {
-                            echo '<p>T '.$value.'</p>';
+                            echo '<p>'.$value.'</p>';
                         }
                     } ?>
                     <?php if ( isset($full_contatto['url']) && is_array($full_contatto['url']) && count ($full_contatto['url']) ) {
