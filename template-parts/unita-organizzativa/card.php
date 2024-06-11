bc50ab7525c1fe8cb4fbfcdec6686012e3c6be38
Point-Service <tecnico@p-service.it>
2024-06-11 09:07:01 +0200

    Update card.php

--- template-parts/unita-organizzativa/card.php
+++ template-parts/unita-organizzativa/card.php
@@ -83,13 +83,54 @@
 	  <?php if ($descrizione_breve) {
 	     echo '<div class="card-text"><p class="u-main-black">'.$descrizione_breve.'</p></div>';
 	   } ?>      
-	
-	  <div class="field--name-field-ita-mail">
-            <?php foreach ($pec as $pec) {
-                echo '<h4><a href="mailto:'.$pec.'">'.$pec.'</a></h4>';
-            }?>
-         </div>
+        <?php foreach ($contatti as $full_contatto) { ?>	
 
+
+	                        <?php if ( isset($full_contatto['indirizzo']) && is_array($full_contatto['indirizzo']) && count ($full_contatto['indirizzo']) ) {
+                        foreach ($full_contatto['indirizzo'] as $value) {
+                            echo '<p>'.$value.'</p>';
+                        } 
+                    } ?>
+                    <?php if ( isset($full_contatto['telefono']) && is_array($full_contatto['telefono']) && count ($full_contatto['telefono']) ) {
+                        foreach ($full_contatto['telefono'] as $value) {
+                            echo '<p>T '.$value.'</p>';
+                        }
+                    } ?>
+                    <?php if ( isset($full_contatto['url']) && is_array($full_contatto['url']) && count ($full_contatto['url']) ) {
+                        foreach ($full_contatto['url'] as $value) { ?>
+                            <p>
+                                <a 
+                                target="_blank" 
+                                aria-label="scopri di più su <?php echo $value; ?> - link esterno - apertura nuova scheda" 
+                                href="<?php echo $value; ?>">
+                                    <?php echo $value; ?>
+                                </a>
+                            </p>
+                    <?php }
+                    } ?>
+                    <?php if ( isset($full_contatto['email']) && is_array($full_contatto['email']) && count ($full_contatto['email']) ) {
+                        foreach ($full_contatto['email'] as $value) { ?>
+                            <p>
+                                <a  
+                                target="_blank" 
+                                aria-label="invia un'email a <?php echo $value; ?>"
+                                href="mailto:<?php echo $value; ?>">
+                                    <?php echo $value; ?>
+                                </a>
+                            </p>
+                    <?php }
+                    } ?>
+                    <?php foreach ($other_contacts as $type) {
+                        if ( isset($full_contatto[$type]) && is_array($full_contatto[$type]) && count ($full_contatto[$type]) ) {
+                            foreach ($full_contatto[$type] as $value) {
+                                echo '<p>'.$type.': '.$value.'</p>';
+                            }
+                        } 
+                    } ?>
+
+
+    <?php } ?>	  
+	      
     <?php if ($img) { ?>
     <div class="avatar size-xl">
         <?php dci_get_img($img); ?>
