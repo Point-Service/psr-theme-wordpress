62e8592e4870c4c90baf3972cb80f8540282400b
Point-Service <tecnico@p-service.it>
2024-06-13 17:00:59 +0200

    Update single-luogo.php

--- single-luogo.php
+++ single-luogo.php
@@ -264,7 +264,7 @@ get_header();
 
 
 		
-          <?php //get_template_part('template-parts/single/page_bottom'); ?>
+          <?php get_template_part('template-parts/single/page_bottom'); ?>
           </section>
       </div>
     </div>
