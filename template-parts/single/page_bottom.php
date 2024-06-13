cd4aa580a19a3f1b227e72f949042936a033dda0
Point-Service <tecnico@p-service.it>
2024-06-13 16:58:37 +0200

    Update page_bottom.php

--- template-parts/single/page_bottom.php
+++ template-parts/single/page_bottom.php
@@ -3,15 +3,13 @@
 
     // metadata
     $last_edit = explode(' ', $post->post_modified);
-    $last_edit_date = new DateTime($last_edit[0]);
+    $last_edit_day = implode('/',array_reverse(explode('-',$last_edit[0])));
     $last_edit_hour_arr = explode(':', $last_edit[1]);
-    $last_edit_hour = $last_edit_hour_arr[0].":".$last_edit_hour_arr[1]; 
-
-	$formatter = new IntlDateFormatter('it_IT', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT);
-	$formatter->setPattern('d LLLL yyyy');
+    $last_edit_hour = $last_edit_hour_arr[0].":".$last_edit_hour_arr[1];  
 ?>
 
 <article id="ultimo-aggiornamento" class="it-page-section mt-5">
-    <h3 class="h6">Ultimo aggiornamento</h3>
-    <span class="h6 fw-normal"><?php echo $formatter->format($last_edit_date).', '.$last_edit_hour; ?></span>
+    <h4 class="h6">Ultimo aggiornamento:
+    <span class="h6 fw-normal"><?php echo $last_edit_day.', '.$last_edit_hour; ?></span>
+    </h4>
 </article>
