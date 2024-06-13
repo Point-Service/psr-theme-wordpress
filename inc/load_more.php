8af9759a6b1fb7430944f96e72297fce47d65574
Point-Service <tecnico@p-service.it>
2024-06-13 17:09:17 +0200

    Update load_more.php

--- inc/load_more.php
+++ inc/load_more.php
@@ -106,7 +106,9 @@ function load_more(){
 		if ($load_card_type == "domanda-frequente"){
 			$out .= load_template_part("template-parts/domanda-frequente/item");  
 		}	
- 
+		if ($load_card_type == "luogo"){
+			$out .= load_template_part("template-parts/luogo/card-full");  
+		}	 
 		endwhile;
  
 	endif;
