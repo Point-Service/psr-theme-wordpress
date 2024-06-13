2372a959bab45d7d1d52487fc5466a5e1a415825
Point-Service <tecnico@p-service.it>
2024-06-13 12:45:14 +0200

    Update calendario.php

--- template-parts/home/calendario.php
+++ template-parts/home/calendario.php
@@ -79,7 +79,7 @@ foreach ($date as $data) {
 		<div class="it-carousel-wrapper it-carousel-landscape-abstract-four-cols it-calendar-wrapper">
 			<div class="it-header-block">
 				<div class="it-header-block-title">
-				<h1 class="mb-0 text-center home-carousel-title"><?php echo $full_date; ?></h4>
+				<h3 class="mb-0 text-center home-carousel-title"><?php echo $full_date; ?></h3>
 				</div>
 			</div>
 		</div>
