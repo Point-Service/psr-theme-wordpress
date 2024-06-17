30309503d9cbc2cffc0879e0fc54fe95a5b9013a
Point-Service <tecnico@p-service.it>
2024-06-17 16:04:43 +0200

    Update single-evento.php

--- single-evento.php
+++ single-evento.php
@@ -71,8 +71,6 @@ get_header();
       </div>
     </div>
 
-    <?php get_template_part('template-parts/single/image-large'); ?>
-  
     <div class="container">
       <div class="row border-top row-column-border row-column-menu-left border-light">
         <aside class="col-lg-4">
@@ -178,6 +176,7 @@ get_header();
         </aside>
 
         <section class="col-lg-8 it-page-sections-container border-light">
+              <?php get_template_part('template-parts/single/image-large'); ?>
           <article id="cos-e" class="it-page-section mb-5" data-audio>
               <h2 class="mb-3">Cos'è</h2>
               <div class="richtext-wrapper font-serif">
