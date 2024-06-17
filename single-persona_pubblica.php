2ef2cf760bd778848ecc7e96914972810c8afd49
Point-Service <tecnico@p-service.it>
2024-06-17 10:51:00 +0200

    Update single-persona_pubblica.php

--- single-persona_pubblica.php
+++ single-persona_pubblica.php
@@ -157,17 +157,6 @@ get_header();
                             </div>
                         </div>
                     </div>
-                    <?php if($img) { ?>
-                        <section class="hero-img mb-20 mb-lg-50">
-                          <section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
-                             <div class="img-responsive-wrapper">
-
-                                   <div class="img-wrapper"><img width="300" src="<?php echo $img[0]; ?>" title="titolo immagine" alt="descrizione immagine"></div>
-
-                             </div>
-                          </section>
-                        </section>
-                    <?php } ?>
                     <hr class="d-none d-lg-block mt-2"/>
                 </div>
             </div>
@@ -306,6 +295,16 @@ get_header();
                     </div>
                     <div class="col-12 col-lg-8 offset-lg-1">
                         <div class="it-page-sections-container">
+                            <?php if($img) { ?>
+                                <section class="hero-img mb-20 mb-lg-50">
+                                  <section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
+                                     <div class="img-responsive-wrapper">
+                                           <div class="img-wrapper"><img width="300" src="<?php echo $img[0]; ?>" title="titolo immagine" alt="descrizione immagine"></div>
+                                     </div>
+                                  </section>
+                                </section>
+                            <?php } ?>
+                            
                              <?php if ($incarico) { ?>
                             <section class="it-page-section mb-30">
                                 <h2 class="title-xxlarge mb-3" id="who-needs">Incarico</h2>
