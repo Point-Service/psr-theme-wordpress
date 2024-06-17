bba8f5d4063fc8d4d8a5ddca86f5e16406118702
Point-Service <tecnico@p-service.it>
2024-06-17 10:41:33 +0200

    Update single-persona_pubblica.php

--- single-persona_pubblica.php
+++ single-persona_pubblica.php
@@ -157,13 +157,18 @@ get_header();
                             </div>
                         </div>
                     </div>
+                    <?php if($img) { ?>
+                        <section class="hero-img mb-20 mb-lg-50">
+                          <section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
+                             <div class="img-responsive-wrapper">
+                                <div class="img-responsive">
+                                   <div class="img-wrapper"><img src="<?php echo $img[0]; ?>" title="titolo immagine" alt="descrizione immagine"></div>
+                                </div>
+                             </div>
+                          </section>
+                        </section>
+                    <?php } ?>
                     <hr class="d-none d-lg-block mt-2"/>
-                </div>
-            </div>
-
-            <div class="container">
-                <div class="row justify-content-center">
-                    <hr class="d-none d-lg-block mt-2" />
                 </div>
             </div>
 
@@ -300,17 +305,6 @@ get_header();
                         </div>
                     </div>
                     <div class="col-12 col-lg-8 offset-lg-1">
-            <?php if($img) { ?>
-            <section class="hero-img mb-20 mb-lg-50">
-              <section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
-                 <div class="img-responsive-wrapper">
-                    <div class="img-responsive">
-                       <div class="img-wrapper"><img src="<?php echo $img[0]; ?>" title="titolo immagine" alt="descrizione immagine"></div>
-                    </div>
-                 </div>
-              </section>
-            </section>
-            <?php } ?>
                         <div class="it-page-sections-container">
                              <?php if ($incarico) { ?>
                             <section class="it-page-section mb-30">
