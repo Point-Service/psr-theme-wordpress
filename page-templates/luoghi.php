5ce9a464392f0e23f2093af4351ab475844bf0cb
Point-Service <tecnico@p-service.it>
2024-06-18 17:33:58 +0200

    Update luoghi.php

--- page-templates/luoghi.php
+++ page-templates/luoghi.php
@@ -13,6 +13,37 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
+			$img = dci_get_option('immagine', 'luoghi');
+			$didascalia = dci_get_option('didascalia', 'luoghi');
+		
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'luogo',
+				'post_status'    => 'publish'
+			);
+			$schede_luoghi = get_posts($args);
+
+		?>
+		<?php 
+			$with_shadow = true;
+			get_template_part("template-parts/hero/hero"); 
+		?>
+			
+		<?php if( $img ) { ?>
+			<section class="hero-img mb-20 mb-lg-50">
+				<section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
+					<div class="img-responsive-wrapper">
+						<div class="img-responsive">
+							<div class="img-wrapper">
+								<?php dci_get_img($img); ?>
+							</div>
+						</div>
+					</div>
+				</section>
+				<p class="title-xsmall cmp-hero-img-small__description">
+					<?php echo $didascalia; ?>
+				</p>
+			</section>
 			?>
 			<?php get_template_part("template-parts/hero/hero"); ?>
 			<?php get_template_part("template-parts/vivere-comune/tutti-luoghi"); ?>
