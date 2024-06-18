da7a0c3f77547e28d74f13a6e392ee0e17ba5812
Point-Service <tecnico@p-service.it>
2024-06-18 17:37:59 +0200

    Update luoghi.php

--- page-templates/luoghi.php
+++ page-templates/luoghi.php
@@ -28,7 +28,26 @@ get_header();
 		<?php 
 			$with_shadow = true;
 			get_template_part("template-parts/hero/hero"); 
-		?>		
+		?>	
+
+		<?php if( $img ) { ?>
+			<section class="hero-img mb-20 mb-lg-50">
+				<section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
+					<div class="img-responsive-wrapper">
+						<div class="img-responsive">
+							<div class="img-wrapper">
+								<?php dci_get_img($img); ?>
+							</div>
+						</div>
+					</div>
+				</section>
+				<p class="title-xsmall cmp-hero-img-small__description">
+					<?php echo $didascalia; ?>
+				</p>
+			</section>
+		<?php } ?>
+
+		
 			<?php get_template_part("template-parts/vivere-comune/tutti-luoghi"); ?>
 			<?php get_template_part("template-parts/common/valuta-servizio"); ?>
 			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
