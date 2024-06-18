8a2ba74646d7732efeb1565339fe6853693ea078
Point-Service <tecnico@p-service.it>
2024-06-18 18:09:13 +0200

    Update single-luogo.php

--- single-luogo.php
+++ single-luogo.php
@@ -195,15 +195,12 @@ get_header();
 
           <?php if($indirizzo) {?>
           <article id="indirizzo" class="it-page-section mb-5">
-            <h2 class="mb-3">Indirizzo</h2>
-			<div class="row">
-				<div class="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal">
+            <h2 class="mb-3">Indirizzo</h2><center>
 				<?php 
 					$luoghi = array($luogo);
 					get_template_part("template-parts/luogo/map"); 
 			         ?>
-				</div>
-			</div>
+</center>
                    <div class="richtext-wrapper font-serif mt-3">  
 				<?php echo $indirizzo; ?>
 		   </div>
