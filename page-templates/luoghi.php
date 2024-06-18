14be4cd768766eafa039ec5c3640764fbe18dd07
Point-Service <tecnico@p-service.it>
2024-06-18 17:31:42 +0200

    Update luoghi.php

--- page-templates/luoghi.php
+++ page-templates/luoghi.php
@@ -1,7 +1,7 @@
 <?php
-/* Template Name: luoghi
+/* Template Name: I Luoghi
  *
- * luoghi template file
+ * I luoghi template file
  *
  * @package Design_Comuni_Italia
  */
@@ -13,11 +13,44 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-			?>
-			<?php get_template_part("template-parts/hero/hero"); ?>
-			<?php get_template_part("template-parts/vivere-comune/tutti-luoghi"); ?>
-			<?php get_template_part("template-parts/common/valuta-servizio"); ?>
-			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
+			
+			$img = dci_get_option('immagine', 'luoghi');
+			$didascalia = dci_get_option('didascalia', 'luoghi');
+		
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'luogo',
+				'post_status'    => 'publish'
+			);
+			$schede_luoghi = get_posts($args);
+
+		?>
+		<?php 
+			$with_shadow = true;
+			get_template_part("template-parts/hero/hero"); 
+		?>
+			
+		<?php if( $img ) { ?>
+			<section class="hero-img mb-20 mb-lg-50">
+				<section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
+					<div class="img-responsive-wrapper">
+						<div class="img-responsive">
+							<div class="img-wrapper">
+								<?php dci_get_img($img); ?>
+							</div>
+						</div>
+					</div>
+				</section>
+				<p class="title-xsmall cmp-hero-img-small__description">
+					<?php echo $didascalia; ?>
+				</p>
+			</section>
+		<?php } ?>
+		
+		<?php get_template_part("template-parts/luogo/evidenza"); ?>
+		<?php get_template_part("template-parts/luogo/tutti-luoghi"); ?>
+		<?php get_template_part("template-parts/common/valuta-servizio"); ?>
+		<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
 							
 		<?php 
 			endwhile; // End of the loop.
@@ -26,6 +59,3 @@ get_header();
 
 <?php
 get_footer();
-
-
-
