8d75c325995e05bc332b26791876fb81e1ef2f67
Point-Service <tecnico@p-service.it>
2024-06-19 16:51:51 +0200

    Update welcome.php

--- inc/welcome.php
+++ inc/welcome.php
@@ -5,26 +5,17 @@ require get_template_directory() . '/inc/lib/parsedown.php';
 /**
  * Welcome page
  */
-
 remove_action('welcome_panel', 'wp_welcome_panel');
 add_action( 'welcome_panel', 'dci_welcome_panel' );
 function dci_welcome_panel(){
     ?>
     <div class="welcome-panel-content" style="padding-bottom:30px;">
-        <img src="<?php echo get_template_directory_uri() . '/assets/img/designers-italia-wordpress-dashboard.png'?>"  style="width: 10%; margin: 0 auto;" />
-        <img src="<?php echo get_template_directory_uri() . '/assets/img/logo-point.png'?>"  style="margin: 0 auto; width:10%;" />
-        <h2 style="color: #fff; font-size: 24px; text-align:center"><?php _e( 'Tema basato su Design Comuni Italia: il tema di Developers Italia per i Comuni Italiani', "design_comuni_italia" ); ?></h2>
-        <a href="https://p-service.it" style="text-align:center;"><button type="button" class="button button-primary" style="text-align:center;margin:0auto;">Sito Web</button></a>
-        <a href="https://www.servizipa.com/guide/pointservice/" style="text-align:center;"><button type="button" class="button button-primary" style="text-align:center;margin:0auto;">Link alle Guide</button></a>
+        <img src="<?php echo get_template_directory_uri() . '/assets/img/designers-italia-wordpress-dashboard.png'?>"  style="float:left; margin:0px 0px 20px 0px;" />
+        <div class="welcome-panel-header">
+            <h2><?php echo('Design Enti Socio-Assistenziali: il tema dedicato al settore socio-assistenziale'); ?></h2>
+            <h3>Il tema è stato preparato da Robyone sulla base del <a href="https://github.com/italia/design-comuni-wordpress-theme">tema di Developers Italia predisposto per i Comuni Italiani</a></h3>
+        </div>
     </div>
-
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <?php get_template_part( "template-parts/single/bottom" ); ?>
-                        </div><!-- /col-lg-9 -->
-                    </div><!-- /row -->
-
     <?php
 }
 
@@ -44,13 +35,15 @@ add_action( 'wp_dashboard_setup', 'dci_add_dashboard_widgets' );
 function dci_add_dashboard_widgets() {
     wp_add_dashboard_widget(
         'dci_dashboard_widget', // Widget slug
-        'Tema Point Service S.r.l', // Widget title
+        'Design Enti Socio-Assistenziali Italia', // Widget title
         'dci_new_dashboard_widget_function' // Function name to display the widget
     );
 }
 // Initialize the function to output the contents of your new dashboard widget
 function dci_new_dashboard_widget_function() {
-    echo "Basato su Design Comuni Italia: il tema di Developers Italia per i Comuni Italiani ";
+    echo "<p>Design Enti Socio-Assistenziali: il tema dedicato agli Enti socio-assistenziali</p>";
+    echo "<p>Il tema è stato preparato sulla base del <a href=\"https://github.com/italia/design-comuni-wordpress-theme\">tema di Developers Italia predisposto per i Comuni Italiani</a></p>";
+    echo "<p><a href=\"https://github.com/robyone-srl/design-esa-wordpress-theme\">Documentazione del tema su Github</a></p>";
 }
 
 /**
@@ -95,13 +88,13 @@ function dci_screen_layout_dashboard() {
 add_filter('get_user_option_screen_layout_dashboard', 'dci_screen_layout_dashboard');
 
 add_action ('admin_menu', function () {
-  //  add_management_page('Manuale Tema Comuni', 'Manuale Tema Comuni', 'read', 'manuale-comuni', 'dci_readme_render_manual', '');
+  //  add_management_page('Manuale Tema ESA', 'Manuale Tema ESA', 'read', 'manuale-esa', 'dci_readme_render_manual', '');
 });
 
 function dci_readme_render_manual(){
 echo '<div class="wrap manuale">';
 
-    $response = wp_remote_get( 'https://raw.githubusercontent.com/italia/design-comuni-wordpress-theme/master/README.md?test=1' );
+    $response = wp_remote_get( 'https://raw.githubusercontent.com/robyone-srl/design-esa-wordpress-theme/main/README.md' );
 
     if ( is_array( $response ) && ! is_wp_error( $response ) ) {
 
@@ -117,11 +110,11 @@ add_action('admin_bar_menu', 'dci_add_toolbar_manual', 100);
 function dci_add_toolbar_manual($admin_bar)
 {
     $admin_bar->add_menu(array(
-        'id' => 'manuale',
-        'title' => 'Manuale',
-        'href' => 'https://www.servizipa.com/guide/pointservice',
+        'id' => 'norme_riferimenti',
+        'title' => 'Norme e riferimenti',
+        'href' => 'https://designers.italia.it/norme-e-riferimenti/',
         'meta' => array(
-            'title' => __('Manuale'),
+            'title' => __('La documentazione ufficiale che ti guida nella progettazione per la Pubblica Amministrazione, mettendo le persone al centro'),
             'target' => '_blank'
         ),
     ));
