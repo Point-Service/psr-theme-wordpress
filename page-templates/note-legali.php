53a157f8194b132defad82db8de4e16bd4c7a4fb
Point-Service <tecnico@p-service.it>
2024-06-20 12:34:14 +0200

    Update note-legali.php

--- page-templates/note-legali.php
+++ page-templates/note-legali.php
@@ -43,11 +43,9 @@ get_header();
                                                     <li class="nav-item">
                                                         <a class="nav-link active" href="#licenza"><span>Licenza dei contenuti</span></a>
                                                     </li>
-                                                       
-						</ul>
-                                            </div>						
-                                        </div>	
-
+                                                                                                                                                                                                                                                                </ul>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -59,17 +57,24 @@ get_header();
         <div class="col-12 col-lg-8 offset-lg-1">
             <div class="it-page-sections-container">
             <article id="cos-e" class="it-page-section anchor-offset">
-            <h1 data-element="legal-notes-section">Descrizione sulle Note Legali</h1>
-         <p><strong>Premessa</strong><br />Nel rispetto delle Linee guida per i siti web della PA (art. 4 della Direttiva 8/09 del Ministro per la pubblica amministrazione e l'innovazione) si forniscono le seguenti informazioni che regolano l'accesso al sito web e disciplinano i termini e le condizioni d'uso del sito stesso. Accedendo e consultando questo sito, il visitatore ne accetta i seguenti termini e condizioni d'uso.<br /><br />
+                        <h4 data-element="legal-notes-section">Licenza dei contenuti</h4>
+                        <div class="the-content"><p data-element="legal-notes-body">In applicazione del principio open by default ai sensi dell’articolo 52 del decreto legislativo 7 marzo 2005, n. 82 (CAD) e salvo dove diversamente specificato (compresi i contenuti incorporati di terzi), i dati, i documenti e le informazioni pubblicati sul sito sono rilasciati con licenza <a href="https://creativecommons.org/licenses/by/4.0/legalcode.it"><strong>CC-BY 4.0</strong>.</a><br>
+Gli utenti sono quindi liberi di condividere (riprodurre, distribuire, comunicare al pubblico, esporre in pubblico), rappresentare, eseguire e recitare questo materiale con qualsiasi mezzo e formato e modificare (trasformare il materiale e utilizzarlo per opere derivate) per qualsiasi fine, anche commerciale con il solo onere di attribuzione, senza apporre restrizioni aggiuntive.</p>
+  <p><strong>Premessa</strong><br />Nel rispetto delle Linee guida per i siti web della PA (art. 4 della Direttiva 8/09 del Ministro per la pubblica amministrazione e l'innovazione) si forniscono le seguenti informazioni che regolano l'accesso al sito web e disciplinano i termini e le condizioni d'uso del sito stesso. Accedendo e consultando questo sito, il visitatore ne accetta i seguenti termini e condizioni d'uso.<br /><br />
 	<strong>Copyright</strong><br />Tutti i contenuti, le immagini e le informazioni presenti all'interno del sito sono protetti ai sensi delle normative sul diritto d'autore, sui brevetti e su quelle relative alla propriet&agrave; intellettuale; pertanto nulla, neppure in parte, potr&agrave; essere copiato, modificato o rivenduto per fini di lucro o per trarne qualsivoglia utilit&agrave;.<br /><br />
 	<strong>Utilizzo del sito</strong><br /><p data-element="legal-notes-body">In nessun caso l'Ente potr&agrave; essere ritenuto responsabile dei danni di qualsiasi natura causati direttamente o indirettamente dall'accesso al sito e dall'utilizzo che venga fatto delle informazioni in esso contenute; inoltre, l'Ente non ha alcuna responsabilit&agrave; per eventuali problemi causati dall'utilizzazione del presente sito o di siti ad esso collegati.<br />
 	Sebbene i contenuti forniti si ritengano adeguati ed aggiornati, l'Ente proprietario, titolare e gestore del sito non garantisce che il sito web stesso sia privo di imprecisioni, errori e/o omissioni.<br />L'Ente si riserva il diritto di modificare i contenuti in ogni momento.<br /><br /><strong>Accesso a siti esterni collegati</strong><br />
 	L'Ente non pu&ograve; essere ritenuto responsabile di quanto contenuto nei siti ai quali &egrave; possibile accedere tramite i collegamenti posti all'interno del sito stesso, declinando ogni responsabilit&agrave; per le informazioni, la grafica, gli aggiornamenti ed i riferimenti in essi contenuti;<br /><br /><strong>Download</strong><br />
 	I contenuti e la documentazione scaricabile dal sito e reperibile via download sono liberamente e gratuitamente disponibili, alle condizioni stabilite al punto Copyright, salvo diversa indicazione. Quanto reperibile via download in questo sito tramite link a siti esterni pu&ograve; essere coperto da copyright, diritti d&rsquo;uso e/o copia dei rispettivi proprietari; pertanto l'Ente invita a verificare condizioni di utilizzo e diritti e si ritiene esplicitamente sollevato da qualsiasi responsabilit&agrave; in merito.<br /><br />
 	<strong>Licenza dei contenuti</strong></p><br />
-        <p data-element='legal-notes-body'>In applicazione del principio open by default ai sensi dell&rsquo;articolo 52 del decreto legislativo 7 marzo 2005, n. 82 (CAD) e salvo dove diversamente specificato (compresi i contenuti incorporati di terzi), i dati, i documenti e le informazioni pubblicati sul sito sono rilasciati con licenza CC-BY 4.0. Gli utenti sono quindi liberi di condividere (riprodurre, distribuire, comunicare al pubblico, esporre in pubblico), rappresentare, eseguire e recitare questo materiale con qualsiasi mezzo e formato e modificare (trasformare il materiale e utilizzarlo per opere derivate) per qualsiasi fine, anche commerciale con il solo onere di attribuzione, senza apporre restrizioni aggiuntive.</p></p><br />               
+			</div>                   
+	    
+	    </article>
                 
-        
+                
+                
+                
+                
                 
                 <article id="contatti" class="it-page-section anchor-offset mt-5">
                                                         </article>
