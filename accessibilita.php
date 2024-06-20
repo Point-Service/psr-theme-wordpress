8272c75e48f6cf548939b155da31e5e3dc2fa385
Point-Service <tecnico@p-service.it>
2024-06-20 15:22:27 +0200

    Update accessibilita.php

--- accessibilita.php
+++ accessibilita.php
@@ -9,22 +9,10 @@ global $post;
 get_header();
 
 ?>
-	<main>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
-			<?php get_template_part("template-parts/hero/hero"); ?>
-			<?php get_template_part("template-parts/amministrazione/evidenza"); ?>
-			<?php get_template_part("template-parts/amministrazione/cards-list"); ?>
-			<?php get_template_part("template-parts/politici/cards-list"); ?>
-			<?php get_template_part("template-parts/common/valuta-servizio"); ?>
-			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
-							
-		<?php 
-			endwhile; // End of the loop.
-		?>
-	</main>
+
+
+
+
 
 <?php
 get_footer();
