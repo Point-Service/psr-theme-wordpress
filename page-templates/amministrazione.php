3fec56953d1a587d4eddd82386c21ce2776a0a7a
Point-Service <tecnico@p-service.it>
2024-06-20 11:14:13 +0200

    Update amministrazione.php

--- page-templates/amministrazione.php
+++ page-templates/amministrazione.php
@@ -17,6 +17,7 @@ get_header();
 			<?php get_template_part("template-parts/hero/hero"); ?>
 			<?php get_template_part("template-parts/amministrazione/evidenza"); ?>
 			<?php get_template_part("template-parts/amministrazione/cards-list"); ?>
+			echo 'ssss'	
 			<?php get_template_part("template-parts/politici/cards-list"); ?>
 			<?php get_template_part("template-parts/common/valuta-servizio"); ?>
 			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
