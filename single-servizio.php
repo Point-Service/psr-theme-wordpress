230a732447294d70710a2c8b8fdd886dcf0b266d
Point-Service <tecnico@p-service.it>
2024-06-26 12:19:48 +0200

    Update single-servizio.php

--- single-servizio.php
+++ single-servizio.php
@@ -498,6 +498,11 @@ get_header();
                                 <button type="button" class="btn btn-outline-primary t-primary bg-white mobile-full" onclick="location.href='<?php echo dci_get_template_page_url('page-templates/prenota-appuntamento.php'); ?>';" data-element="service-booking-access">
                                     <span class="">Prenota appuntamento</span>
                                 </button>
+                                <?php } ?>
+                                   <?php foreach ($canale_fisico_luoghi_id as $luogo_id) {
+                                    $luogo = get_post($luogo_id);
+                                    get_template_part("template-parts/luogo/card-title");
+                                } ?>
                             </section>
                             <?php if ( $more_info ) {  ?>
                             <section class="it-page-section mb-30">
