80f6184d964c683683ec4a4af26e2515d6821e45
Point-Service <tecnico@p-service.it>
2024-06-27 16:18:27 +0200

    Update tassonomia_categorie_servizio.php

--- inc/admin/tassonomie/tassonomia_categorie_servizio.php
+++ inc/admin/tassonomie/tassonomia_categorie_servizio.php
@@ -36,9 +36,16 @@ function dci_register_taxonomy_categorie_servizio() {
 
     register_taxonomy( 'categorie_servizio', array( 'servizio' ), $args );
 
+    
+// Verifica se siamo nella pagina corretta dell'amministrazione di WordPress
+$current_url = admin_url('edit-tags.php?taxonomy=categorie_servizio&post_type=servizio');
 
+if (stripos($_SERVER['REQUEST_URI'], $current_url) !== false) {
     // Aggiungi il pulsante per svuotare tutte le categorie di servizio
-    add_action(  'categorie_servizio', 'add_empty_categories_button' );
+    add_action( 'admin_footer', 'add_empty_categories_button' );
+}
+
+
 }
 
 function add_empty_categories_button() {
