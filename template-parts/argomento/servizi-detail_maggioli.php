a8286a15bc35e17acd468630226bd4dfaded83c5
Point-Service <tecnico@p-service.it>
2024-06-27 18:34:42 +0200

    Update servizi-detail_maggioli.php

--- template-parts/argomento/servizi-detail_maggioli.php
+++ template-parts/argomento/servizi-detail_maggioli.php
@@ -3,7 +3,7 @@
 
     $posts = dci_get_grouped_posts_by_term( 'servizi' , 'argomenti', $argomento->slug, 3);
 
-?><?php echo $argomento->name; ?>
+?><?php echo // $argomento->name; ?>
 <section id="servizi">
             <div class="pb-40 pt-40 pt-lg-80">
                 <div class="container">
@@ -19,7 +19,12 @@
                         <?php
 
 
-    $argomento_segment = $argomento->name; 
+    // Ottieni l'URL della pagina corrente
+    $current_url = home_url(add_query_arg(array(), $wp->request));
+
+    // Estrai il segmento desiderato dall'URL
+    $segments = explode('/', $current_url);
+    $argomento_segment = end($segments); // Prendi l'ultimo segmento dell'URL
 
 
 
