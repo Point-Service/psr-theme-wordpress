bb34e128281127f497eeca092772fef1b422578c
Point-Service <tecnico@p-service.it>
2024-06-27 17:37:53 +0200

    Update servizi-detail_maggioli.php

--- template-parts/argomento/servizi-detail_maggioli.php
+++ template-parts/argomento/servizi-detail_maggioli.php
@@ -119,13 +119,10 @@
     // Chiamata alla funzione per ottenere i dati e salvare il totale dei servizi
     $search_term = isset($_GET['search']) ? $_GET['search'] : null;
     $total_services_loaded = get_procedures_data($search_term, $category_segment, $title);
-    echo "<p>Servizi aggiuntivi: $total_services_loaded</p>";
 ?>
 
 
-       
-                </div>
-            </div>
+ 
             <div class="row mt-4">
                 <div class="col-12 col-lg-3 offset-lg-9">
                     <button 
