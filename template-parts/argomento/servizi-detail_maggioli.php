e9d8076f56f05dfd8220bffead4f5d63afcf55d2
Point-Service <tecnico@p-service.it>
2024-06-27 17:28:44 +0200

    Update servizi-detail_maggioli.php

--- template-parts/argomento/servizi-detail_maggioli.php
+++ template-parts/argomento/servizi-detail_maggioli.php
@@ -1,102 +1,52 @@
 <?php
-// Funzione per ottenere i dati dal servizio web
-function get_procedures_data($search_term = null)
-{
-    $url = dci_get_option('servizi_maggioli_url', 'servizi');
-    $response = wp_remote_get($url);
-    $total_services = 0; // Inizializza il contatore
+    global $argomento;
 
-    if (is_array($response) && !is_wp_error($response)) {
-        $body = wp_remote_retrieve_body($response);
-        $data = json_decode($body, true);
-
-        if ($data) {
-            // Inizializza array per servizi in evidenza e non in evidenza
-            $in_evidenza_services = [];
-            $other_services = [];
-
-            foreach ($data as $procedure) {
-                // Verifica se il termine di ricerca è presente nel nome del servizio
-                if ($search_term && stripos($procedure['nome'], $search_term) === false) {
-                    continue; // Ignora questo servizio se il termine di ricerca non è presente
-                }
-
-                $name = $procedure['nome'];
-                $description = $procedure['descrizione_breve'];
-                $category = is_array($procedure['categoria']) ? implode(', ', $procedure['categoria']) : $procedure['categoria'];
-                $in_evidenza = filter_var($procedure['in_evidenza'], FILTER_VALIDATE_BOOLEAN);
-                $url = $procedure['url'];
-
-                // Aggiungi il servizio all'array corretto
-                $service = [
-                    'name' => $name,
-                    'description' => $description,
-                    'category' => $category,
-                    'url' => $url
-                ];
-
-                if ($in_evidenza) {
-                    $in_evidenza_services[] = $service;
-                } else {
-                    $other_services[] = $service;
-                }
-
-                // Incrementa il contatore ad ogni iterazione
-                $total_services++;
-            }
-            
-            // Output del totale
-            echo "<h2>Servizi Aggiuntivi ( $total_services )</h2>";
-
-            // Output dei servizi in evidenza
-            echo "<h4>Servizi in Evidenza</h4>";
-            output_services($in_evidenza_services);
-
-            // Output degli altri servizi
-            echo "<h4>Altri Servizi</h4>";
-            output_services($other_services);
-        }
-    } else {
-        echo "Non riesco a leggere i servizi aggiuntivi.";
-    }
-
-    // Restituisci il totale dei servizi caricati
-    return $total_services;
-}
-
-// Funzione per stampare i servizi
-function output_services($services)
-{
-    foreach ($services as $service) {
-        // Genera il link alla categoria basato sul nome del servizio
-        $category_slug = sanitize_title($service['category']);
-        $category_link = "/servizi-categoria/$category_slug";
+    $posts = dci_get_grouped_posts_by_term( 'servizi' , 'argomenti', $argomento->slug, 3);
 ?>
-        <div class="cmp-card-latest-messages card-wrapper" data-bs-toggle="modal" data-bs-target="#">
-            <div class="card shadow-sm px-4 pt-4 pb-4 rounded border border-light">
-                <span class="visually-hidden">Categoria:</span>
-                <div class="card-header border-0 p-0">
-                    <?php if ($service['category']) {
-                        echo '<a href="'. esc_url($category_link) .'" class="text-decoration-none"><div class="text-decoration-none title-xsmall-bold mb-2 category text-uppercase">' . $service['category'] . '</a></div>';
-                    } ?>
-                </div>
-                <div class="card-body p-0 my-2">
-                    <h3 class="green-title-big t-primary mb-8">
-                        <a class="text-decoration-none" href="<?php echo esc_url($service['url']); ?>" data-element="service-link"><?php echo $service['name']; ?></a>
+
+<section id="servizi">
+    <div class="pb-40 pt-40 pt-lg-80">
+        <div class="container">
+            <div class="row row-title">
+                <div class="col-12">
+                    <h3 class="u-grey-light border-bottom border-semi-dark pb-2 pb-lg-3 title-large-semi-bold">
+                        Servizi
                     </h3>
-                    <p class="text-paragraph">
-                        <?php echo $service['description']; ?>
-                    </p>
+                </div>
+            </div>
+            <div class="row mx-0">
+                <div class="card-wrapper px-0 card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                    <?php foreach ($posts as $post) { 
+                        $description = dci_get_meta('descrizione_breve');
+                        $categoria_servizio = get_the_terms($post->ID, 'categorie_servizio')[0];
+                    ?>
+                        <div class="card card-teaser card-teaser-image card-flex no-after rounded shadow-sm border border-light mb-0">
+                            <div class="card-image-wrapper with-read-more">
+                                <div class="card-body p-3">
+                                    <div class="category-top">
+                                        <a class="title-xsmall-semi-bold fw-semibold text-decoration-none" href="<?php echo get_term_link($categoria_servizio->term_id); ?>"><?php echo $categoria_servizio->name; ?></a>
+                                    </div>
+                                        <h4 class="card-title text-paragraph-medium u-grey-light">
+                                            <a href="<?php echo get_permalink(); ?>" class="text-decoration-none"><?php echo the_title(); ?></a>
+                                        </h4>
+                                    <p class="text-paragraph-card u-grey-light m-0"><?php echo $description; ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-12 col-lg-3 offset-lg-9">
+                    <button 
+                        type="button" 
+                        class="btn btn-primary text-button w-100"
+                        onclick="location.href='<?php echo dci_get_template_page_url('page-templates/servizi.php'); ?>'"
+                    >
+                        Tutti i servizi
+                    </button>
                 </div>
             </div>
         </div>
-        <p></p>
-<?php
-    }
-}
-
-// Chiamata alla funzione per ottenere i dati e salvare il totale dei servizi
-$search_term = isset($_GET['search']) ? $_GET['search'] : null;
-$total_services_loaded = get_procedures_data($search_term);
-echo "<p>Servizi aggiuntivi: $total_services_loaded</p>";
-?>
+    </div>
+</section>
