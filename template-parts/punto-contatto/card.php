0b1978c98efde72645646fe98b21ff61142b63c8
Point-Service <tecnico@p-service.it>
2024-07-01 17:27:18 +0200

    Update card.php

--- template-parts/punto-contatto/card.php
+++ template-parts/punto-contatto/card.php
@@ -8,6 +8,7 @@ $voci = dci_get_meta('voci', $prefix, $pc_id);
 
 $other_contacts = array(
     'linkedin',
+    'pec',
     'skype',
     'telegram',
     'twitter',
@@ -77,15 +78,12 @@ $other_contacts = array(
                     </p>
                <?php }
             } ?>
-            <?php foreach ($other_contacts as $type) {
-                if (array_key_exists($type, $full_contatto) &&  is_array($full_contatto[$type]) && count ($full_contatto[$type]) ) {
-                    foreach ($full_contatto[$type] as $value) {
-                        echo '<p><svg class="icon">
-                            <use xlink:href="#it-list"></use>
-                            </svg>'.$type.': '.$value.'</p>';
-                    }
-                } 
-            } ?>
+
+
+
+
+
+            
         </div>
     </div>
 </div>
