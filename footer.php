e767e51638db484cd52cbfa2201ed5098eab26f6
Point-Service <tecnico@p-service.it>
2024-07-02 12:13:25 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -228,6 +228,7 @@
                                                       <?php } ?>
 		    
 						<a href="<?php echo dci_get_option("servizi",'header'); ?>/index.php/servizi">Servizi</a>
+			                        <div id="area_personale_admin"></div>
 						<a href="<?php echo get_admin_url(); ?>">Area Riservata</a>
 						
 			                      
