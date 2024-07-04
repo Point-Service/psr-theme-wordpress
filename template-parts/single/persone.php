a072acd295ec48907f0e005f08124136142e45b0
Point-Service <tecnico@p-service.it>
2024-07-04 10:10:06 +0200

    Update persone.php

--- template-parts/single/persone.php
+++ template-parts/single/persone.php
@@ -7,7 +7,8 @@
                                                <?php foreach ($persone as $person_id) { 
                                                 $prefix = '_dci_persona_pubblica_';
                                                 $nome = dci_get_meta('nome', $prefix, $person_id);
-                                                $cognome = dci_get_meta('cognome', $prefix, $person_id); ?>                                             
+                                                $cognome = dci_get_meta('cognome', $prefix, $person_id); ?>       
+                                                $incarico = dci_get_meta('incarico', $prefix, $person_id); ?>        
                                               <div class="card-body pe-3">
                                                   <p class="card-title text-paragraph-regular-medium-semi mb-3">
                                                       <a href="<?php echo get_permalink($person_id); ?>">
@@ -16,7 +17,7 @@
                                                   </p>
                                                     <div class="card-text">
                                                         <div class="richtext-wrapper lora">
-                                                          <?php echo $incarico ?>àà
+                                                          <?php echo $incarico ?>
                                                         </div>
                                                     </div>
                                                  </div>    
