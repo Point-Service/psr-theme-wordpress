d3a835ac8f09e0710c1e41e86c15b5d200086e6a
Point-Service <tecnico@p-service.it>
2024-07-04 10:39:29 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -32,15 +32,21 @@
             <div class="row">
                 <div class="col-12 footer-items-wrapper logo-wrapper">
 		 <div class="it-brand-wrapper">
+
+                            <div class="it-brand-text">
+                                 <img class="ue-logo" src="<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-eu-inverted.svg" alt="logo Unione Europea" width="178" height="56" >
+                            </div>
+          
+                   </div>	
+		 <div class="it-brand-wrapper">
                         <a href="<?php echo home_url() ?>">
                             <?php get_template_part("template-parts/common/logo");?>
                             <div class="it-brand-text">
                                 <h2 class="no_toc"><?php echo dci_get_option("nome_comune");?></h2>
                             </div>
                         </a>
-                   </div>	
-		
-               <img class="ue-logo" src="<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-eu-inverted.svg" alt="logo Unione Europea" width="178" height="56" >
+                   </div>			
+            
                    
                 </div>
             </div>
