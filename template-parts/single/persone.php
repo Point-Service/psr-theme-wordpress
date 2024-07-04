a7c8d70742a9029c67bd038f8a503afba1dec9fe
Point-Service <tecnico@p-service.it>
2024-07-04 10:19:20 +0200

    Update persone.php

--- template-parts/single/persone.php
+++ template-parts/single/persone.php
@@ -2,16 +2,17 @@
     global $persone;
     if ($persone && is_array($persone) && count($persone)>0) { ?>
          <?php foreach ($persone as $person_id) { 
-                                       <div class="col-12 col-md-8 col-lg-6 mb-30">
-                                        <div class="card-wrapper rounded h-auto mt-10">
-                                            <div class="card card-teaser shadow-sm p-4s rounded border border-light flex-nowrap">
-                                        
-                                                $prefix = '_dci_persona_pubblica_';
+                  $prefix = '_dci_persona_pubblica_';
                                                 $nome = dci_get_meta('nome', $prefix, $person_id);
                                                 $cognome = dci_get_meta('cognome', $prefix, $person_id); 
                                                 $incarichi = dci_get_meta("incarichi", $prefix, $person_id);                                    
                                                 $incarico = get_the_title($incarichi[0]);        
-                                                 ?>        
+                                                 ?>  
+                                       <div class="col-12 col-md-8 col-lg-6 mb-30">
+                                        <div class="card-wrapper rounded h-auto mt-10">
+                                            <div class="card card-teaser shadow-sm p-4s rounded border border-light flex-nowrap">
+                                        
+                                            
                                                  <div class="card-body pe-3">
                                                         <p class="card-title text-paragraph-regular-medium-semi mb-3">
                                                              <a href="<?php echo get_permalink($person_id); ?>">
