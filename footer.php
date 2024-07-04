61f3d45e4c529c9f7eca5f55d016e8f46db88d40
Point-Service <tecnico@p-service.it>
2024-07-04 10:40:48 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -31,9 +31,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 footer-items-wrapper logo-wrapper">
-		 <div class="it-brand-wrapper">                    
+		 <div>                    
                                  <img class="ue-logo" src="<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-eu-inverted.svg" alt="logo Unione Europea" width="178" height="56" >                               
-                   </div>	
+                </div>	
 		 <div class="it-brand-wrapper">
                         <a href="<?php echo home_url() ?>">
                             <?php get_template_part("template-parts/common/logo");?>
