9c7a7b5be500c2ee76fa8eb2123160a35c50c7e3
Point-Service <tecnico@p-service.it>
2024-07-04 10:47:18 +0200

    Update logo.php

--- template-parts/common/logo.php
+++ template-parts/common/logo.php
@@ -1,7 +1,7 @@
 <?php
 if (dci_get_option("stemma_comune")) {
 ?>
-<svg width="82" height="82" class="icon" aria-hidden="true">       
-     <image xlink:href="<?php echo dci_get_option("stemma_comune");?>" width="82" height="82"/>    
+<svg width="82" height="82" class="icon" aria-hidden="true">  
+     <image xlink:href="https://mlsy3spd7v5e.i.optimole.com/w:auto/h:auto/q:mauto/ig:avif/<?php echo dci_get_option("stemma_comune");?>" width="50" height="50"/>    
 </svg>
 <?php } ?>
