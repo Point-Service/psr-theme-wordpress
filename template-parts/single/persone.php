07b31f6e95f44ca0ef3c8412e246e241416b7b9d
Point-Service <tecnico@p-service.it>
2024-07-04 10:01:34 +0200

    Update persone.php

--- template-parts/single/persone.php
+++ template-parts/single/persone.php
@@ -2,17 +2,28 @@
     global $persone;
 
     if ($persone && is_array($persone) && count($persone)>0) { ?>
-        <ul class="d-flex flex-wrap gap-1 mt-2">
-            <?php foreach ($persone as $person_id) { 
-                $prefix = '_dci_persona_pubblica_';
-                $nome = dci_get_meta('nome', $prefix, $person_id);
-                $cognome = dci_get_meta('cognome', $prefix, $person_id); ?>
-                <li>
-                    <a class="chip chip-simple" href="<?php echo get_permalink($person_id); ?>">
-                        <span class="chip-label"><?php echo $nome.' '.$cognome; ?></span>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
+                                       <div class="col-12 col-md-8 col-lg-6 mb-30">
+                                        <div class="card-wrapper rounded h-auto mt-10">
+                                            <div class="card card-teaser shadow-sm p-4s rounded border border-light flex-nowrap">
+                                               <?php foreach ($persone as $person_id) { 
+                                                $prefix = '_dci_persona_pubblica_';
+                                                $nome = dci_get_meta('nome', $prefix, $person_id);
+                                                $cognome = dci_get_meta('cognome', $prefix, $person_id); ?>                                             
+                                              <div class="card-body pe-3">
+                                                  <p class="card-title text-paragraph-regular-medium-semi mb-3">
+                                                      <a class="chip chip-simple" href="<?php echo get_permalink($person_id); ?>">
+                                                        <span class="chip-label"><?php echo $nome.' '.$cognome; ?></span>
+                                                     </a>
+                                                  </p>
+                                                    <div class="card-text">
+    
+                                                    </div>
+                                                 </div>    
+                                                <?php } ?>
+                                             </div>
+                                           </div>
+                                        </div>
+
+
     <?php }
 ?>
