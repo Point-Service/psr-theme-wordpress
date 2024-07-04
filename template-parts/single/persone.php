8c3d5a9b0ac0955cf21d59f575bc1e57bcb9aa1e
Point-Service <tecnico@p-service.it>
2024-07-04 10:05:59 +0200

    Update persone.php

--- template-parts/single/persone.php
+++ template-parts/single/persone.php
@@ -1,6 +1,5 @@
 <?php 
     global $persone;
-
     if ($persone && is_array($persone) && count($persone)>0) { ?>
                                        <div class="col-12 col-md-8 col-lg-6 mb-30">
                                         <div class="card-wrapper rounded h-auto mt-10">
@@ -11,7 +10,7 @@
                                                 $cognome = dci_get_meta('cognome', $prefix, $person_id); ?>                                             
                                               <div class="card-body pe-3">
                                                   <p class="card-title text-paragraph-regular-medium-semi mb-3">
-                                                      <a class="chip chip-simple" href="<?php echo get_permalink($person_id); ?>">
+                                                      <a href="<?php echo get_permalink($person_id); ?>">
                                                         <span class="chip-label"><?php echo $nome.' '.$cognome; ?></span>
                                                      </a>
                                                   </p>
@@ -25,5 +24,5 @@
                                         </div>
 
 
-    <?php }
+      <?php }
 ?>
