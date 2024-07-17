05c9898f45a193c7e30d822bfcd6790b859916c3
Point-Service <tecnico@p-service.it>
2024-07-17 17:00:38 +0200

    Update home.php

--- home.php
+++ home.php
@@ -6,13 +6,10 @@
  *
  * @package Design_Comuni_Italia
  */
+
 get_header();
 ?>
-
-	    
-       <main id="main-container" class="main-container redbrown">
-       <div class="bg-image"></div>
-       
+    <main id="main-container" class="main-container redbrown">
         <h1 class="visually-hidden">
             <?php echo dci_get_option("nome_comune"); ?>
         </h1>
@@ -24,27 +21,15 @@ get_header();
                 get_template_part("template-parts/home/messages");
             }
 		    ?>
-            <?php // get_template_part("template-parts/home/carosello"); ?>
-	    <p></p>
             <?php get_template_part("template-parts/home/notizie"); ?>
             <?php get_template_part("template-parts/home/calendario"); ?>
         </section>
         <section id="evidenza" class="evidence-section">
-            <div class="section py-5 pb-lg-80 px-lg-5 position-relative">
+            <div class="section py-5 pb-lg-80 px-lg-5 position-relative" style="<?php if (file_exists(get_stylesheet_directory().'/assets/img/evidenza-header.png')){ ?>background-image: url('<?php echo esc_url( get_stylesheet_directory_uri()); ?>/assets/img/evidenza-header.png');<?php }else{ ?>background-image: url('<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/evidenza-header.png');<?php } ?>">
                 <?php get_template_part("template-parts/home/argomenti"); ?>
                 <?php get_template_part("template-parts/home/siti","tematici"); ?>
             </div>
         </section>
-        <section id="accesso-rapido" class="quick-access-section">
-
-            <?php 
-                $boxes = dci_get_option( "quickboxes", "accesso_rapido" );
-                get_template_part("template-parts/home/accesso-rapido"); 
-            ?>
-        </section>
-	  <?php if ($mostra_gallery) {
-	  get_template_part("template-parts/vivere-comune/galleria-foto");
-  	  } ?>
         <?php get_template_part("template-parts/home/ricerca"); ?>
         <?php get_template_part("template-parts/common/valuta-servizio"); ?>
         <?php get_template_part("template-parts/common/assistenza-contatti"); ?>
