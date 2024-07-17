b11fc058d256c3ccb35e0a1fff01edce4e43ebee
Point-Service <tecnico@p-service.it>
2024-07-17 17:35:05 +0200

    Update vivere-il-comune.php

--- page-templates/vivere-il-comune.php
+++ page-templates/vivere-il-comune.php
@@ -18,12 +18,9 @@ get_header();
 			$didascalia = dci_get_option('didascalia', 'vivi');
 			$data_element = 'data-element="page-name"';
 			?>
-			<?php get_template_part("template-parts/hero/hero"); ?>	
+			<?php get_template_part("template-parts/hero/hero"); ?>
 			<section class="hero-img mb-20 mb-lg-50">
-				<div class="container">
-					<div class="row">
-				<?php if($img != null) { ?>
-				<section class="it-hero-small-size cmp-hero-img-small">
+				<section class="it-hero-wrapper it-hero-small-size cmp-hero-img-small">
 					<div class="img-responsive-wrapper">
 						<div class="img-responsive">
 							<div class="img-wrapper">
@@ -32,12 +29,9 @@ get_header();
 						</div>
 					</div>
 				</section>
-				<?php } ?>
-						<p class="title-big cmp-hero-img-big__description">
-							<?php echo $didascalia; ?>
-						</p>
-					</div>
-				</div>
+				<p class="title-xsmall cmp-hero-img-small__description">
+					<?php echo $didascalia; ?>
+				</p>
 			</section>
 			<?php get_template_part("template-parts/vivere-comune/eventi"); ?>
 			<?php get_template_part("template-parts/vivere-comune/luoghi"); ?>
