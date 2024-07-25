e22ad97f76b037248317989b7a9afd53069bb7c8
Point-Service <tecnico@p-service.it>
2024-07-25 10:28:33 +0200

    Update header.php

--- header.php
+++ header.php
@@ -16,11 +16,6 @@ $current_group = dci_get_current_group();
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="theme-color" content="#0968b4" />
-<link rel="preload" as="font" href="">
-<meta
-  http-equiv="Content-Security-Policy"
-  content="upgrade-insecure-requests" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -29,11 +24,97 @@ $current_group = dci_get_current_group();
 <?php get_template_part("template-parts/common/sprites"); ?>
 <?php get_template_part("template-parts/common/skiplink"); ?>
 
+<!-- Sticky Header -->
+  <header class="it-header-wrapper sticky-header sticky-hidden" id="sticky-header" data-bs-target="#header-nav-wrapper-sticky">
+    <div class="it-nav-wrapper">
+      <div class="it-header-navbar-wrapper" id="header-nav-wrapper-sticky">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="navbar navbar-expand-lg has-megamenu">
+                <!-- <button class="custom-navbar-toggler" type="button" aria-controls="nav4" aria-expanded="false" aria-label="Mostra/Nascondi la navigazione" data-bs-target="#nav4" data-bs-toggle="navbarcollapsible">
+                  <svg class="icon">
+                    <use href="#it-burger"></use>
+                  </svg>
+                </button> -->
+                <div class="navbar-collapsable">
+                  <!-- <div class="overlay" style="display: none"></div>
+                  <div class="close-div">
+                    <button class="btn close-menu" type="button">
+                      <span class="visually-hidden">Nascondi la navigazione</span>
+                      <svg class="icon">
+                        <use href="#it-close-big"></use>
+                      </svg>
+                    </button>
+                  </div> -->
+                  <div class="menu-wrapper">
+                    <a href="<?php echo home_url(); ?>" aria-label="Vai alla homepage" class="logo-hamburger">
+                      <?php get_template_part("template-parts/common/logo-mobile"); ?>
+                      <div class="it-brand-text">
+                        <div class="it-brand-title"><?php echo dci_get_option("nome_comune"); ?></div>
+                      </div>
+                    </a>
+                    <nav aria-label="Principale">
+                      <a href="<?php echo home_url(); ?>" <?php if (!is_front_page()) echo 'title="Vai alla Homepage"'; ?> style="float:left; margin-top: 14px;">
+                        <?php get_template_part("template-parts/common/logo"); ?>
+                      </a>
+                      <?php
+                      $location = "menu-header-main";
+                      if (has_nav_menu($location)) {
+                        wp_nav_menu(array(
+                          "theme_location" => $location,
+                          "depth" => 0,
+                          "menu_class" => "navbar-nav",
+                          'items_wrap' => '<ul class="%2$s" id="%1$s" data-element="main-navigation">%3$s</ul>',
+                          "container" => "",
+                          'list_item_class'  => 'nav-item',
+                          'link_class'   => 'nav-link',
+                          'current_group' => $current_group,
+                          'walker' => new Main_Menu_Walker()
+                        ));
+                      }
+                      ?>
+                    </nav>
+                    <!-- <nav aria-label="Secondaria">
+                      <?php
+                      $location = "menu-header-right";
+                      if (has_nav_menu($location)) {
+                        wp_nav_menu(array(
+                          "theme_location" => $location,
+                          "depth" => 0,
+                          "menu_class" => "navbar-nav navbar-secondary",
+                          "container" => "",
+                          'list_item_class'  => 'nav-item',
+                          'link_class'   => 'nav-link',
+                          'walker' => new Menu_Header_Right_Walker()
+                        ));
+                      }
+                      ?>
+                    </nav> -->
+                    <div class="it-search-wrapper" style="">
+                      <span class="d-none d-md-block" style="color: white; margin-right: 14px; position: absolute; margin-left: -36px; margin-top: -47px;">Cerca</span>
+                      <button class="search-link rounded-icon" style="margin-top: -58px;" type="button" data-bs-toggle="modal" data-bs-target="#search-modal" aria-label="Cerca nel sito">
+                        <svg class="icon">
+                          <use href="#it-search"></use>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </header>
+
 <header
     class="it-header-wrapper"
     data-bs-target="#header-nav-wrapper"
     style=""
-   >
+>
     <?php get_template_part("template-parts/header/slimheader"); ?> 
 
     <div class="it-nav-wrapper">
@@ -44,7 +125,7 @@ $current_group = dci_get_current_group();
             <div class="it-header-center-content-wrapper">
               <div class="it-brand-wrapper">
                 <a 
-                href="<?php echo home_url(); ?>" class="brand-title-text" 
+                href="<?php echo home_url(); ?>" 
                 <?php if(!is_front_page()) echo 'title="Vai alla Homepage"'; ?>>
                     <div class="it-brand-text d-flex align-items-center">
                       <?php get_template_part("template-parts/common/logo"); ?>
@@ -80,7 +161,7 @@ $current_group = dci_get_current_group();
                     <?php endif ?>
                 <div class="it-search-wrapper">
                   <span class="d-none d-md-block">Cerca</span>
-                  <button class="search-link rounded-icon" type="button" data-bs-toggle="modal" data-bs-target="#search-modal" aria-label="Cerca nel sito" id="search-home">
+                  <button class="search-link rounded-icon" type="button" data-bs-toggle="modal" data-bs-target="#search-modal" aria-label="Cerca nel sito">
                       <svg class="icon">
                         <use href="#it-search"></use>
                       </svg>
@@ -191,6 +272,7 @@ $current_group = dci_get_current_group();
       </div>
     </div>
   </div>
+
 </header>
 
 <?php get_template_part("template-parts/common/search-modal"); ?>
