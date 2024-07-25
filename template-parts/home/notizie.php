1cb65393d25c1b09babcf6984448a47311e1ae6f
Point-Service <tecnico@p-service.it>
2024-07-25 10:50:58 +0200

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -84,13 +84,21 @@ for ($i = 1; $i <= 20; $i++) {
                             }
                         }
                         ?>
-                                                                                      
-                    <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary-outline mt-40" >   
-                         <svg class="icon icon-sm" aria-hidden="true">
-                           <use xlink:href="#it-calendar"></use>
-                          </svg>
-                          Visualizza tutte le novità
-                     </a>      
+                     <div class="container">
+                          <div class="row mb-2">
+                            <div class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                                    <div class="row my-4 justify-content-md-center">                 
+                                           <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary-outline mt-40" >   
+                                             <svg class="icon icon-sm" aria-hidden="true">
+                                               <use xlink:href="#it-calendar"></use>
+                                              </svg>
+                                              Visualizza tutte le novità
+                                         </a>                 
+                                  </div>
+                                </div>
+                              </div>
+                             </div>
+      
                     </div>                    
                
           </div>
