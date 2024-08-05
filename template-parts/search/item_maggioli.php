4dff84c26ff4706207d74ce26c1cf9656ce2a70e
Point-Service <tecnico@p-service.it>
2024-08-05 17:24:05 +0200

    Update item_maggioli.php

--- template-parts/search/item_maggioli.php
+++ template-parts/search/item_maggioli.php
@@ -1,32 +1,51 @@
 <?php
 global $post;
 
-$descrizione = dci_get_meta('descrizione_breve') ?: dci_get_meta('_dci_page_descrizione');
+// URL remoto da cui ottenere i dati
+$remote_url = "https://sportellotelematico.comune.roccalumera.me.it/rest/pnrr/procedures";
+
+// Ottieni i dati dal link remoto
+$response = file_get_contents($remote_url);
+
+// Decodifica il JSON in un array associativo
+$data = json_decode($response, true);
+
+// Filtra i dati in base al testo di ricerca
+$filtered_data = array_filter($data, function($item) use ($search_text) {
+    return stripos($item['title'], $search_text) !== false || stripos($item['descrizione'], $search_text) !== false;
+});
+
+// Cicla attraverso i risultati filtrati e visualizzali
+foreach ($filtered_data as $item) {
+    // Qui puoi utilizzare i dati come nel tuo codice originale, ad esempio:
+    ?>
+    <div class="cmp-card-latest-messages mb-3 mb-30" data-bs-toggle="modal" data-bs-target="#">
+        <div class="card shadow-sm px-4 pt-4 pb-4 rounded">
+            <div class="card-header border-0 p-0">
+                <?php
+                if ($post_type = dci_get_group_name($post->post_type)) {
+                ?>
+                    <a class="text-decoration-none title-xsmall-bold mb-2 category text-uppercase" href="<?php echo get_permalink(get_page_by_path(dci_get_group($post->post_type))); ?>">
+                        <?= $post_type ?>
+                    </a>
+                <?php
+                } else {
+                ?>
+                    <span class="title-xsmall-bold mb-2 category text-uppercase text-primary">Pagina</span>
+                <?php
+                }
+                ?>
+            </div>
+            <div class="card-body p-0 my-2">
+                <h3 class="green-title-big t-primary mb-8">
+                    <a class="text-decoration-none" href="<?= $item['link'] ?>" data-element="service-link"><?php echo $item['title']; ?></a>
+                </h3>
+                <p class="text-paragraph">
+                    <?php echo $item['descrizione']; ?>
+                </p>
+            </div>
+        </div>
+    </div>
+    <?php
+}
 ?>
-<div class="cmp-card-latest-messages mb-3 mb-30" data-bs-toggle="modal" data-bs-target="#">
-	<div class="card shadow-sm px-4 pt-4 pb-4 rounded">
-		<div class="card-header border-0 p-0">
-			<?php
-			if ($post_type = dci_get_group_name($post->post_type)) {
-			?>
-				<a class="text-decoration-none title-xsmall-bold mb-2 category text-uppercase" href="<?php echo get_permalink(get_page_by_path(dci_get_group($post->post_type))); ?>">
-					<?= $post_type ?>
-				</a>
-			<?php
-			} else {
-			?>
-				<span class="title-xsmall-bold mb-2 category text-uppercase text-primary">Pagina</span>
-			<?php
-			}
-			?>
-		</div>
-		<div class="card-body p-0 my-2">
-			<h3 class="green-title-big t-primary mb-8">
-				<a class="text-decoration-none" href="<?= ($post->post_type == 'sito_tematico') ? dci_get_meta('link') : get_permalink() ?>" data-element="service-link"><?php echo the_title(); ?></a>
-			</h3>
-			<p class="text-paragraph">
-				<?php echo $descrizione; ?>
-			</p>
-		</div>
-	</div>
-</div>
