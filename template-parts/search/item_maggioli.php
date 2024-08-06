0555a66a02847da49cce5d9be772cfecce999996
Point-Service <tecnico@p-service.it>
2024-08-06 11:11:28 +0200

    Update item_maggioli.php

--- template-parts/search/item_maggioli.php
+++ template-parts/search/item_maggioli.php
@@ -2,7 +2,8 @@
 global $post;
 
 // URL remoto da cui ottenere i dati
-$remote_url = "https://sportellotelematico.comune.roccalumera.me.it/rest/pnrr/procedures";
+ $url = dci_get_option('servizi_maggioli_url', 'servizi');
+ $remote_url = wp_remote_get($url);
 
 // Ottieni i dati dal link remoto
 $response = file_get_contents($remote_url);
