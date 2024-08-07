554f1c3b534487b2fcdbf4594d2add34e75d9c99
Point-Service <tecnico@p-service.it>
2024-08-07 12:18:30 +0200

    Update activation.php

--- inc/activation.php
+++ inc/activation.php
@@ -476,7 +476,7 @@ function dci_create_page_template($name, $slug, $template, $parent_id = '', $con
 
     $new_page_title    = $name;
     $new_page_content  = $content;
-    $new_page_template = 'page-templates/'.$template.'.php';
+    $new_page_template = $template ? 'page-templates/'.$template.'.php' : '';
     $page_check        = get_page_by_title( $new_page_title);
 
     $new_page = array(
@@ -485,7 +485,7 @@ function dci_create_page_template($name, $slug, $template, $parent_id = '', $con
         'post_content' => $new_page_content,
         'post_status'  => 'publish',
         'post_author'  => 1,
-        'post_slug' => $slug,
+        'post_name' => $slug,
         'post_parent' => $parent_id
     );
 
