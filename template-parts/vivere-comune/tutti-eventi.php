99e873e661171133a8f6c625cdd953682d761643
Point-Service <tecnico@p-service.it>
2024-08-19 11:41:55 +0200

    Update tutti-eventi.php

--- template-parts/vivere-comune/tutti-eventi.php
+++ template-parts/vivere-comune/tutti-eventi.php
@@ -13,7 +13,7 @@ global $the_query, $load_posts, $load_card_type;
     $posts = $the_query->posts;
 ?>
 
-
+ggggggggggg
 <div class="bg-grey-card py-5">
     <form role="search" id="search-form" method="get" class="search-form">
         <button type="submit" class="d-none"></button>
