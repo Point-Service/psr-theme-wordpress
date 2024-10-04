d1c6ee31589a26e4b59b848431db4f5d3f64c040
Point-Service <tecnico@p-service.it>
2024-10-04 18:11:39 +0200

    Update pagina_multimedia.php

--- inc/admin/options/pagina_multimedia.php
+++ inc/admin/options/pagina_multimedia.php
@@ -3,20 +3,6 @@
 function dci_register_pagina_multimedia_options(){
     $prefix = '';
 
-       /**
-     * Registers options page "Alerts".
-     */
-
-    $args = array(
-        'id'           => 'dci_options_multimedia',
-        'title'        => esc_html__( 'Multimedia', 'design_comuni_italia' ),
-        'object_types' => array( 'options-page' ),
-        'option_key'   => 'multimedia',
-        'capability'    => 'manage_options',
-        'parent_slug'  => 'dci_options',
-        'tab_group'    => 'dci_options',
-        'tab_title'    => __('Multimedia', "design_comuni_italia"),	);
-
 
 
 }
