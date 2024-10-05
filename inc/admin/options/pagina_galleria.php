ef05faa4cafcfd681a73958674959d296f9247d3
Point-Service <tecnico@p-service.it>
2024-10-05 12:12:59 +0200

    Update pagina_galleria.php

--- inc/admin/options/pagina_galleria.php
+++ inc/admin/options/pagina_galleria.php
@@ -8,7 +8,7 @@ function dci_register_pagina_galleria_options(){
         'id'           => 'dci_options_galleria',
         'title'        => esc_html__( 'Galleria', 'design_comuni_italia' ),
         'object_types' => array( 'options-page' ),
-        'option_key'   => 'Galleria Foto',
+        'option_key'   => 'Galleria',
         'capability'    => 'manage_options',
         'parent_slug'  => 'dci_options',
         'tab_group'    => 'dci_options',
