b3199c0bc5a82a29bf8639df6714cfd3a6d42396
Point-Service <tecnico@p-service.it>
2024-10-05 10:12:59 +0200

    Update galleria-foto.php

--- template-parts/vivere-comune/galleria-foto.php
+++ template-parts/vivere-comune/galleria-foto.php
@@ -5,24 +5,7 @@ $sfondo_grigio = $sfondo_grigio ?? true;
 
 $gallery = dci_get_option('gallery_items', 'vivi') ?: [];
 $nome_sezione = dci_get_option('gallery_title', 'vivi') ?: "";
-
-// Numero di elementi per pagina
-$items_per_page = 6;
-
-// Pagina corrente (se non definita, è la pagina 1)
-$current_page = isset($_GET['pagina']) ? intval($_GET['pagina']) : 1;
-
-// Calcola l'indice iniziale e finale degli elementi da visualizzare
-$offset = ($current_page - 1) * $items_per_page;
-$total_items = count($gallery);
-
-// Suddividi la galleria in pagine
-$gallery_page = array_slice($gallery, $offset, $items_per_page);
-
-// Calcola il numero totale di pagine
-$total_pages = ceil($total_items / $items_per_page);
 ?>
-
 <?php if (count($gallery) > 0) { ?>
 	<section id="galleria">
 		<?php if ($nome_sezione) { ?>
@@ -37,51 +20,7 @@ $total_pages = ceil($total_items / $items_per_page);
 			</div>
 		<?php } ?>
 		<div class="section <?= $sfondo_grigio ? 'section-muted' : '' ?> px-0 pt-0 ">
-			<div class="container">
-				<div class="row">
-					<?php
-					// Visualizza solo gli elementi della pagina corrente
-					foreach ($gallery_page as $item) {
-						?>
-						<div class="col-md-4 mb-4"> <!-- Assicurati che ogni immagine sia in una colonna -->
-							<!-- Codice per visualizzare l'immagine -->
-							<?php dci_get_img($item, 'galleria-img'); ?>
-						</div>
-						<?php
-					}
-					?>
-				</div>
-				<?php if ($total_pages > 1) { ?>
-					<!-- Paginazione -->
-					<nav aria-label="Navigazione della galleria">
-						<ul class="pagination justify-content-center">
-							<?php if ($current_page > 1) { ?>
-								<li class="page-item">
-									<a class="page-link" href="?pagina=<?= $current_page - 1 ?>" aria-label="Precedente">
-										<span aria-hidden="true">&laquo;</span>
-										<span class="sr-only">Precedente</span>
-									</a>
-								</li>
-							<?php } ?>
-							
-							<?php for ($i = 1; $i <= $total_pages; $i++) { ?>
-								<li class="page-item <?= ($i == $current_page) ? 'active' : '' ?>">
-									<a class="page-link" href="?pagina=<?= $i ?>"><?= $i ?></a>
-								</li>
-							<?php } ?>
-							
-							<?php if ($current_page < $total_pages) { ?>
-								<li class="page-item">
-									<a class="page-link" href="?pagina=<?= $current_page + 1 ?>" aria-label="Successivo">
-										<span aria-hidden="true">&raquo;</span>
-										<span class="sr-only">Successivo</span>
-									</a>
-								</li>
-							<?php } ?>
-						</ul>
-					</nav>
-				<?php } ?>
-			</div>
+			<?php get_template_part("template-parts/single/gallery"); ?>
 		</div>
 	</section>
 <?php } ?>
