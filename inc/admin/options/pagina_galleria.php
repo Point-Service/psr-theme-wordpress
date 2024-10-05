cfeea9b628a90b24ce3c73433bc7b0ac1b5605c5
Point-Service <tecnico@p-service.it>
2024-10-05 11:55:28 +0200

    Update pagina_galleria.php

--- inc/admin/options/pagina_galleria.php
+++ inc/admin/options/pagina_galleria.php
@@ -15,9 +15,9 @@ function dci_register_pagina_galleria_options(){
         'tab_title'    => __('Galleria', "design_comuni_italia"),	);
 
    
-    $media_options = new_cmb2_box( $args );
+    $gallery_options = new_cmb2_box( $args );
 
-    $media_options->add_field( array(
+    $gallery_options->add_field( array(
         'id' => $prefix . 'messages_istruzioni_g',
         'name'        => __( 'Box in evidenza su Multimedia', 'design_comuni_italia' ),
         'desc' => __( 'Inserisci i video che verrano visualizzati nella pagina multimedia.' , 'design_comuni_italia' ),
@@ -25,12 +25,30 @@ function dci_register_pagina_galleria_options(){
     ) );
 
 
-   $media_options->add_field( array(
+   $gallery_options->add_field( array(
         'id' => $prefix . 'mostra_gallery_vivereilcomune',
         'name' => 'Mostra la Gallery',
         'desc' => 'Mostra la galleria Foto su Vivere il Comune',
         'type' => 'checkbox',
     ) ); 
 
+     $gallery_options->add_field(array(
+        'name' => __('Gallery', 'design_comuni_italia'),
+        'desc' => __('Seleziona le foto da mostrare in fondo alla pagina', 'design_comuni_italia'),
+        'id' => $prefix . 'gallery_items',
+        'type' => 'file_list',
+        'preview_size' => array( 100, 100 ), // Default: array( 50, 50 )
+        'query_args' => array( 'type' => 'image' ), // Only images attachment
+    )
+  );
+
+    $gallery_options->add_field( array(
+        'id' => $prefix . 'gallery_title',
+        'name' => 'Nome gallery',
+        'desc' => 'Scegli il nome della sezione con la galleria.',
+        'type' => 'text',
+        'default' => 'Le nostre foto'
+      ) 
+    );
     
 }
