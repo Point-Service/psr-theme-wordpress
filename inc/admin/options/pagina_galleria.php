fd2306032eb71a2cce0ab01e71de52967e9bb55d
Point-Service <tecnico@p-service.it>
2024-10-05 11:47:58 +0200

    Update pagina_galleria.php

--- inc/admin/options/pagina_galleria.php
+++ inc/admin/options/pagina_galleria.php
@@ -17,31 +17,7 @@ function dci_register_pagina_galleria_options(){
    
 
 
- $options_gallerias->add_field(array(
-        'name' => __('Gallery', 'design_comuni_italia'),
-        'desc' => __('Seleziona le foto da mostrare in fondo alla pagina', 'design_comuni_italia'),
-        'id' => $prefix . 'gallery_items',
-        'type' => 'file_list',
-        'preview_size' => array( 100, 100 ), // Default: array( 50, 50 )
-        'query_args' => array( 'type' => 'image' ), // Only images attachment
-    )
-  );
 
-    $options_galleria->add_field( array(
-        'id' => $prefix . 'gallery_title',
-        'name' => 'Nome gallery',
-        'desc' => 'Scegli il nome della sezione con la galleria.',
-        'type' => 'text',
-        'default' => 'Le nostre foto'
-      ) 
-    );
-
-   $options_galleria->add_field( array(
-        'id' => $prefix . 'mostra_gallery_vivereilcomune',
-        'name' => 'Mostra la Gallery',
-        'desc' => 'Mostra la galleria Foto su Vivere il Comune',
-        'type' => 'checkbox',
-    ) ); 
 
 
 
