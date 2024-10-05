b7d7c42306a7398c8f9e65a213fce23473f42535
Point-Service <tecnico@p-service.it>
2024-10-05 11:42:11 +0200

    Create pagina_galleria.php

---
+++ inc/admin/options/pagina_galleria.php
@@ -0,0 +1,32 @@
+<?php
+
+function dci_register_pagina_galleria_options(){
+    $prefix = '';
+
+ 
+    $args = array(
+        'id'           => 'dci_options_galleriaa',
+        'title'        => esc_html__( 'Galleria', 'design_comuni_italia' ),
+        'object_types' => array( 'options-page' ),
+        'option_key'   => 'Galleria',
+        'capability'    => 'manage_options',
+        'parent_slug'  => 'dci_options',
+        'tab_group'    => 'dci_options',
+        'tab_title'    => __('Galleria', "design_comuni_italia"),	);
+
+   
+    $media_options = new_cmb2_box( $args );
+
+    $media_options->add_field( array(
+        'id' => $prefix . 'messages_istruzioni_g',
+        'name'        => __( 'Box in evidenza su Multimedia', 'design_comuni_italia' ),
+        'desc' => __( 'Inserisci i video che verrano visualizzati nella pagina multimedia.' , 'design_comuni_italia' ),
+        'type' => 'title',
+    ) );
+
+
+
+
+
+
+}
