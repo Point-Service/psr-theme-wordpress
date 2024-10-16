073144329b8bf7b40184409ab7e04a4dff3ee24e
Point-Service <tecnico@p-service.it>
2024-10-16 17:18:31 +0200

    Update opzioni_comune.php

--- inc/admin/options/opzioni_comune.php
+++ inc/admin/options/opzioni_comune.php
@@ -146,6 +146,12 @@ function dci_register_comune_options(){
         'desc' => __( 'Utilizzare questo campo per specificare email principale del sito'),
         'type' => 'text'
     ));
+    $header_options->add_field( array(
+        'id'    => $prefix . 'prenota_appuntamento',
+        'name' => __('Link Prenota Appuntamento', 'design_comuni_italia' ),
+        'desc' => __( 'Utilizzare questo campo per specificare il link alla pagina prenota appuntamento'),
+        'type' => 'text'
+    ));
 
 
 
@@ -161,6 +167,10 @@ function dci_register_comune_options(){
             'image/svg',
         ))
     ));
+
+}
+
+
 
 
 
