ebabc80013e6e91aa2463c386226009986d5eb19
Point-Service <tecnico@p-service.it>
2024-10-18 10:45:34 +0200

    Update card.php

--- template-parts/punto-contatto/card.php
+++ template-parts/punto-contatto/card.php
@@ -15,95 +15,74 @@ $other_contacts = array(
 );
 ?>
 
-<div class="card card-teaser shadow-sm p-4s rounded border border-light flex-nowrap">
-    <div class="card-body pe-3">
+<div class="card card-teaser shadow mt-3 rounded">
+    <?php if (isset($full_contatto['email']) && is_array($full_contatto['email']) && count($full_contatto['email'])): ?>
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#it-mail"></use>
+        </svg>
+    <?php elseif (isset($full_contatto['telefono']) && is_array($full_contatto['telefono']) && count($full_contatto['telefono'])): ?>
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#it-telephone"></use>
+        </svg>
+    <?php else: ?>
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#it-map-marker-circle"></use>
+        </svg>
+    <?php endif; ?>
 
-
-        
-        <h5 class="card-title">
-            <?php echo $contatto->post_title; ?>
-        </h5>
+    <div class="card-body">
+        <h3 class="card-title h5">
+            <span>
+                <?php echo $contatto->post_title; ?>
+            </span>
+        </h3>
         <div class="card-text">
-            <?php if (array_key_exists('indirizzo', $full_contatto) && is_array($full_contatto['indirizzo']) && count ($full_contatto['indirizzo']) ) {
-                echo '<div class="mb-3">';
-                foreach ($full_contatto['indirizzo'] as $value) {
-                    echo '<p>'.$value;;
-                    if($value) { echo $value; }
-                    echo '</p>';
-                } 
-                echo '</div>';
-            } ?>
-            <?php if (array_key_exists('telefono', $full_contatto) && is_array($full_contatto['telefono']) && count ($full_contatto['telefono']) ) {
-                foreach ($full_contatto['telefono'] as $value) {
-                   ?>
+            <!-- Controlla se è un indirizzo -->
+            <?php if (isset($full_contatto['indirizzo']) && is_array($full_contatto['indirizzo']) && count($full_contatto['indirizzo'])): ?>
+                <?php foreach ($full_contatto['indirizzo'] as $value): ?>
+                    <a href="https://www.google.com/maps/place/<?php echo $value; ?>" target="_blank"><?php echo $value; ?></a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <!-- Controlla se è un telefono -->
+            <?php if (isset($full_contatto['telefono']) && is_array($full_contatto['telefono']) && count($full_contatto['telefono'])): ?>
+                <?php foreach ($full_contatto['telefono'] as $value): ?>
+                    <a href="tel:<?php echo $value;?>"><?php echo $value; ?></a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <!-- Controlla se è un url -->
+            <?php if (isset($full_contatto['url']) && is_array($full_contatto['url']) && count($full_contatto['url'])): ?>
+                <?php foreach ($full_contatto['url'] as $value): ?>
                     <p>
-                        Telefono: 
                         <a 
-                        target="_blank" 
-                        aria-label="contatta telefonicamente tramite il numero <?php echo $value; ?>" 
-                        title="chiama <?php echo $value; ?>" 
-                        href="tel:<?php echo $value; ?>">
+                            target="_blank" 
+                            aria-label="scopri di più su <?php echo $value; ?> - link esterno - apertura nuova scheda" 
+                            title="vai sul sito <?php echo $value; ?>" 
+                            href="<?php echo $value; ?>">
                             <?php echo $value; ?>
                         </a>
                     </p>
-                    <?php
-                }
-            } ?>
-            <?php if (array_key_exists('url', $full_contatto) && is_array($full_contatto['url']) && count ($full_contatto['url']) ) {
-                foreach ($full_contatto['url'] as $value) { ?>
-                    <p>
-                        Collegamento web:
-                        <a 
-                        target="_blank" 
-                        aria-label="scopri di pi첫 su   <?php echo $value; ?> - link esterno - apertura nuova scheda" 
-                        title="vai sul sito   <?php echo $value; ?>" 
-                        href="<?php echo $value; ?>">
-                            <?php echo $value; ?>
-                        </a>
-                    </p>
-               <?php }
-            } ?>
-          <?php if (array_key_exists('email', $full_contatto) && is_array($full_contatto['email']) && count ($full_contatto['email']) ) {
-                foreach ($full_contatto['email'] as $value) { ?>
-                    <p>
-                        Email:
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <!-- controlla se è una email -->
+            <?php if (isset($full_contatto['email']) && is_array($full_contatto['email']) && count($full_contatto['email'])): ?>
+                <?php foreach ($full_contatto['email'] as $value): ?>
                         <a  
-                        target="_blank" 
-                        aria-label="invia un'email a <?php echo $value; ?>"
-                        title="invia un'email a <?php echo $value; ?>" 
-                        href="mailto:<?php echo $value; ?>">
+                            target="_blank" 
+                            aria-label="invia un'email a <?php echo $value; ?>"
+                            title="invia un'email a <?php echo $value; ?>" 
+                            href="mailto:<?php echo $value; ?>">
                             <?php echo $value; ?>
                         </a>
-                    </p>
-               <?php }
-            } ?>
-            <?php if (array_key_exists('pec', $full_contatto) && is_array($full_contatto['pec']) && count ($full_contatto['pec']) ) {
-                foreach ($full_contatto['pec'] as $value) { ?>
-                    <p><svg class="icon">
-                            <use xlink:href="#it-mail"></use>
-                        </svg>
-                        <a  
-                        target="_blank" 
-                        aria-label="invia un'email a <?php echo $value; ?>"
-                        title="invia un'email a <?php echo $value; ?>" 
-                        href="mailto:<?php echo $value; ?>">
-                            <?php echo $value; ?>
-                        </a>
-                    </p>
-               <?php }
-            } ?>
-        <?php foreach ($other_contacts as $type) {
-                if (array_key_exists($type, $full_contatto) &&  is_array($full_contatto[$type]) && count ($full_contatto[$type]) ) {
-                    foreach ($full_contatto[$type] as $value) {
-                        echo '<p><svg class="icon">
-                            <use xlink:href="#it-list"></use>
-                            </svg>'.$type.': '.$value.'</p>';
-                    }
-                } 
+                <?php endforeach; ?>
+            <?php endif; ?>
 
-    
-            } ?>
-  
+            <?php foreach ($other_contacts as $type): ?>
+                <?php if (isset($full_contatto[$type]) && is_array($full_contatto[$type]) && count($full_contatto[$type])): ?>
+                    <?php foreach ($full_contatto[$type] as $value): ?>
+                        <p><?php echo $type; ?>: <?php echo $value; ?></p>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            <?php endforeach; ?>
         </div>
     </div>
-</div><p></p>
+</div>
