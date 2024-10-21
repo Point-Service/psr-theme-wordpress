f07344edadaf3e4d0eea64de07a29ea3d9057c4d
Point-Service <tecnico@p-service.it>
2024-10-21 17:34:46 +0200

    Update single-persona_pubblica.php

--- single-persona_pubblica.php
+++ single-persona_pubblica.php
@@ -31,10 +31,23 @@ get_header();
         $responsabile = !empty($responsabili) ? $responsabili[0] : null;
 
         $incarichi = !is_null($responsabile) ? dci_get_meta("incarichi", '_dci_persona_pubblica_', $responsabile) : [];
-        $incarico = !empty($incarichi) ? get_the_title($incarichi[0]) : '';
-
-        $tipo_incarico_terms = get_the_terms(get_post($incarichi[0]), 'tipi_incarico');
-        $tipo_incarico = (!empty($tipo_incarico_terms) && !is_wp_error($tipo_incarico_terms)) ? $tipo_incarico_terms[0]->name : '';
+          // Controllo se ci sono incarichi e se il primo incarico esiste
+            if (!empty($incarichi) && isset($incarichi[0])) {
+                $incarico = get_the_title($incarichi[0]);
+                
+                // Recupero dei termini di tipo incarico
+                $tipo_incarico_terms = get_the_terms(get_post($incarichi[0]), 'tipi_incarico');
+                
+                // Controllo se i termini di tipo incarico esistono e non ci sono errori
+                if (!empty($tipo_incarico_terms) && !is_wp_error($tipo_incarico_terms) && isset($tipo_incarico_terms[0])) {
+                    $tipo_incarico = $tipo_incarico_terms[0]->name;
+                } else {
+                    $tipo_incarico = ''; // Valore di fallback se non ci sono termini
+                }
+            } else {
+                $incarico = ''; // Valore di fallback se non ci sono incarichi
+                $tipo_incarico = ''; // Valore di fallback se non ci sono incarichi
+            }
 
         $compensi = !empty($incarichi) ? dci_get_meta("compensi", '_dci_incarico_', $incarichi[0]) : [];
 
