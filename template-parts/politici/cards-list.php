ff02c890b79ce60c21d84dc618f910701d7ce00d
Point-Service <tecnico@p-service.it>
2024-10-21 16:33:31 +0200

    Update cards-list.php

--- template-parts/politici/cards-list.php
+++ template-parts/politici/cards-list.php
@@ -35,37 +35,35 @@ $img = dci_get_meta('foto');
 
 // Verifica che $tipo sia definito e che il suo nome sia "politico"
 if ($tipo && $tipo->name == "politico") {
-    if ($img) {
-        ?>
-        <div class="col-md-6 col-xl-4">
-            <div class="card-wrapper border border-light rounded shadow-sm cmp-list-card-img cmp-list-card-img-hr">
-                <div class="card no-after rounded">
-                    <div class="row g-2 g-md-0 flex-md-column">
-                        <div class="col-4 order-2 order-md-1">
-                            <?php dci_get_img($img, 'rounded-top img-fluid img-responsive'); ?>
-                        </div>
-                        <div class="col-8 order-1 order-md-2">
-                            <div class="card-body">
-                                <div class="category-top cmp-list-card-img__body">
-                                    <span class="category cmp-list-card-img__body-heading-title underline">
-                                        <?php echo $nome_incarico; ?>
-                                    </span>
-                                    <span class="data"><?php echo $data_formattata; ?></span>
-                                </div>
-                                <a class="text-decoration-none" href="<?php echo get_permalink(); ?>" data-element="administration-element">
-                                    <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
-                                </a>
-                                <p class="card-text d-none d-md-block">
-                                    <?php echo $description; ?>
-                                </p>
+    ?>
+    <div class="col-md-6 col-xl-4">
+        <div class="card-wrapper border border-light rounded shadow-sm cmp-list-card-img cmp-list-card-img-hr">
+            <div class="card no-after rounded">
+                <div class="row g-2 g-md-0 flex-md-column">
+                    <div class="col-4 order-2 order-md-1">
+                        <?php if ($img) { dci_get_img($img, 'rounded-top img-fluid img-responsive'); } ?>
+                    </div>
+                    <div class="col-8 order-1 order-md-2">
+                        <div class="card-body">
+                            <div class="category-top cmp-list-card-img__body">
+                                <span class="category cmp-list-card-img__body-heading-title underline">
+                                    <?php echo $nome_incarico; ?>
+                                </span>
+                                <span class="data"><?php echo $data_formattata; ?></span>
                             </div>
+                            <a class="text-decoration-none" href="<?php echo get_permalink(); ?>" data-element="administration-element">
+                                <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
+                            </a>
+                            <p class="card-text d-none d-md-block">
+                                <?php echo $description; ?>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <?php 
-    } 
+    </div>
+    <?php 
 } else { 
     ?>
     <div class="col-md-6 col-xl-4">
