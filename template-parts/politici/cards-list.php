e9708a20858edb57189f904a09bc0168dc94cd9a
Point-Service <tecnico@p-service.it>
2024-10-21 16:37:02 +0200

    Update cards-list.php

--- template-parts/politici/cards-list.php
+++ template-parts/politici/cards-list.php
@@ -4,21 +4,22 @@ global $posts;
 $description = dci_get_meta('descrizione_breve');
 $incarichi = dci_get_meta('incarichi');
 
-// Verifica che $incarichi sia un array e contenga almeno un elemento
+// Verifica che $incarichi sia un array e che contenga almeno un elemento
 if (is_array($incarichi) && !empty($incarichi[0])) {
     $incarico = $incarichi[0];
     $nome_incarico = get_the_title($incarico);
 } else {
     $incarico = null;
-    $nome_incarico = ''; // Valore predefinito
+    $nome_incarico = 'POLITICO'; // Valore predefinito
 }
 
 // Estrai la data inizio incarico e controlla che abbia almeno tre elementi
 $arrdata = explode('-', dci_get_meta("data_inizio_incarico"));
 if (count($arrdata) >= 3) {
     $monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
+    $data_formattata = $arrdata[0] . ' ' . $monthName . ' ' . $arrdata[2];
 } else {
-    $monthName = ''; // Valore predefinito
+    $data_formattata = ''; // Valore predefinito
 }
 
 // Verifica che get_the_terms() restituisca un array valido con almeno un elemento
@@ -26,7 +27,7 @@ $terms = $incarico ? get_the_terms($incarico, 'tipi_incarico') : null;
 if (is_array($terms) && !empty($terms[0])) {
     $tipo = $terms[0];
 } else {
-    $tipo = null; // Valore predefinito
+    $tipo = null; // Valore predefinito se non ci sono termini validi
 }
 
 $prefix = '_dci_incarico_';
@@ -34,26 +35,21 @@ $img = dci_get_meta('foto');
 
 // Verifica che $tipo sia definito e che il suo nome sia "politico"
 if ($tipo && $tipo->name == "politico") {
-    if ($img) {
-?>
+    ?>
     <div class="col-md-6 col-xl-4">
         <div class="card-wrapper border border-light rounded shadow-sm cmp-list-card-img cmp-list-card-img-hr">
             <div class="card no-after rounded">
                 <div class="row g-2 g-md-0 flex-md-column">
                     <div class="col-4 order-2 order-md-1">
-                        <?php dci_get_img($img, 'rounded-top img-fluid img-responsive'); ?>
+                        <?php if ($img) { dci_get_img($img, 'rounded-top img-fluid img-responsive'); } ?>
                     </div>
                     <div class="col-8 order-1 order-md-2">
                         <div class="card-body">
                             <div class="category-top cmp-list-card-img__body">
-                                <?php if ($tipo) { ?> 
-                                    <span class="category cmp-list-card-img__body-heading-title underline">
-                                        <?php echo $nome_incarico ? $nome_incarico : 'POLITICO'; ?>
-                                    </span>
-                                <?php } ?>                    
-                                <span class="data">
-                                    <?php echo $arrdata[0] . ' ' . $monthName . ' ' . $arrdata[2]; ?>
+                                <span class="category cmp-list-card-img__body-heading-title underline">
+                                    <?php echo $nome_incarico; ?>
                                 </span>
+                                <span class="data"><?php echo $data_formattata; ?></span>
                             </div>
                             <a class="text-decoration-none" href="<?php echo get_permalink(); ?>" data-element="administration-element">
                                 <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
@@ -67,10 +63,9 @@ if ($tipo && $tipo->name == "politico") {
             </div>
         </div>
     </div>
-<?php 
-    } 
+    <?php 
 } else { 
-?>
+    ?>
     <div class="col-md-6 col-xl-4">
         <div class="card-wrapper border border-light rounded shadow-sm cmp-list-card-img cmp-list-card-img-hr">
             <div class="card no-after rounded">
@@ -81,9 +76,7 @@ if ($tipo && $tipo->name == "politico") {
                                 <span class="category cmp-list-card-img__body-heading-title underline">
                                     <?php echo isset($tipo->name) ? strtoupper($tipo->name) : 'POLITICO'; ?>
                                 </span>
-                                <span class="data">
-                                    <?php echo $arrdata[0] . ' ' . strtoupper($monthName) . ' ' . $arrdata[2]; ?>
-                                </span>
+                                <span class="data"><?php echo $data_formattata ? strtoupper($data_formattata) : ''; ?></span>
                             </div>
                             <a class="text-decoration-none" href="<?php echo get_permalink(); ?>" data-element="administration-element">
                                 <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
@@ -97,7 +90,7 @@ if ($tipo && $tipo->name == "politico") {
             </div>
         </div>
     </div>
-<?php 
+    <?php 
 }
 ?>
 
