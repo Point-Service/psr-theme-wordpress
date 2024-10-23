7b31d9c2dab06557b22ae43663d65079f31b8e00
Point-Service <tecnico@p-service.it>
2024-10-23 11:25:35 +0200

    Update accesso-rapido.php

--- template-parts/home/accesso-rapido.php
+++ template-parts/home/accesso-rapido.php
@@ -2,6 +2,8 @@
 global $boxes;
 $box_accesso_rapido = $boxes;
 ?>
+<head><link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"></head>
+
 <div class="container py-5">
     <?php if (!empty($boxes)) { ?>
         <h2 class="title-xxlarge mb-4">Accesso rapido</h2>
@@ -11,8 +13,16 @@ $box_accesso_rapido = $boxes;
         <div class="col-md-6 col-xl-4">
             <div class="cmp-card-simple card-wrapper pb-0 rounded border-none">
                 <div class="card shadow-sm rounded">
-                    <div class="card-body card-bg-blue">
-                        <div class="fas fa-clipboard-list ico" style="font-size:38px; color: #fff;margin-right: 8px;top:10px;" aria-hidden="true"></div>
+                    <div class="card-body card-bg-blue d-flex align-items-center">
+                    <?php 
+                        if(array_key_exists('icona_message', $box)){
+                            if(array_key_exists('icon', $box)) { ?>
+                                <div class="avatar size-lg me-3">
+                                    <i class="fas fa-<?php echo htmlspecialchars($box['icon']); ?>"></i>
+                                </div>
+                            <?php } ?>
+                        <?php }?>
+                        <div class="content">
                         <a class="text-decoration-none card-bg-blue" href="<?php echo $box['link_message']; ?>" data-element="topic-element" target="_blank">
                             <h3 class="card-title t-primary title-xlarge text-white"><?php echo $box['titolo_message']; ?></h3>
                         </a>
@@ -21,6 +31,7 @@ $box_accesso_rapido = $boxes;
                                 <?php echo $box['desc_message']; ?>            
                             </p>
                         <?php } ?>
+                        </div> 
                     </div>
                 </div>
             </div>
@@ -28,7 +39,7 @@ $box_accesso_rapido = $boxes;
         <?php } ?>
     </div>
 </div>
-
+<?php } ?>
 <style>
     .custom-styles .row {
         display: grid;
