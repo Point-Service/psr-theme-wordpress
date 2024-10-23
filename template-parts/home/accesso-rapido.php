2f7a0924bba033399cf67546b18a4c78e5119208
Point-Service <tecnico@p-service.it>
2024-10-23 11:54:06 +0200

    Update accesso-rapido.php

--- template-parts/home/accesso-rapido.php
+++ template-parts/home/accesso-rapido.php
@@ -6,25 +6,31 @@ $box_accesso_rapido = $boxes;
     <?php if (!empty($boxes)) { ?>
         <h2 class="title-xxlarge mb-4">Accesso rapido</h2>
     <?php } ?>
-    <div class="row g-4">
-        <?php foreach($boxes as $box) { ?>
-        <div class="col-md-6 col-xl-4">
-            <div class="cmp-card-simple card-wrapper pb-0 rounded border-none">
-                <div class="card shadow-sm rounded">
-                    <div class="card-body card-bg-blue">
-                        <div class="fas fa-clipboard-list ico" style="font-size:38px; color: #fff;margin-right: 8px;top:10px;" aria-hidden="true"></div>
-                        <a class="text-decoration-none card-bg-blue" href="<?php echo $box['link_message']; ?>" data-element="topic-element" target="_blank">
-                            <h3 class="card-title t-primary title-xlarge text-white"><?php echo $box['titolo_message']; ?></h3>
-                        </a>
-                        <?php if (isset($box['desc_message']) && $box['desc_message']) { ?>
-                            <p class="titillium text-paragraph mb-0 description text-white">
-                                <?php echo $box['desc_message']; ?>            
-                            </p>
-                        <?php } ?>
+    <div class="row g-4 custom-styles">
+        <?php foreach ($boxes as $box) {
+            // Recupero delle variabili dal box
+            $colore_sfondo = $box['colore'] ?? false; // Aggiungi colore se disponibile
+            $sfondo_scuro = $colore_sfondo ? is_this_dark_hex($colore_sfondo) : true; // Controlla se il colore è scuro
+        ?>
+            <div class="col-md-6 col-xl-4">
+                <a href="<?php echo $box['link_message']; ?>" style="<?= ($colore_sfondo) ? 'background-color:' . $colore_sfondo : '' ?>" class="card card-teaser <?= $colore_sfondo ? '' : 'bg-primary' ?> rounded mt-0 p-3 shadow-sm border border-light" target="_blank">
+                    <div class="cmp-card-simple card-wrapper pb-0 rounded border-none">
+                        <div class="card shadow-sm rounded">
+                            <div class="card-body card-bg-blue">
+                                <div class="fas fa-clipboard-list ico" style="font-size:38px; color: #fff;margin-right: 8px;top:10px;" aria-hidden="true"></div>
+                                <h3 class="card-title t-primary title-xlarge text-white <?= $sfondo_scuro ? 'text-white' : 'text-dark' ?>">
+                                    <?php echo $box['titolo_message']; ?>
+                                </h3>
+                                <?php if (isset($box['desc_message']) && $box['desc_message']) { ?>
+                                    <p class="titillium text-paragraph mb-0 description text-white <?= $sfondo_scuro ? 'text-white' : '' ?>">
+                                        <?php echo $box['desc_message']; ?>
+                                    </p>
+                                <?php } ?>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
-        </div>
         <?php } ?>
     </div>
 </div>
