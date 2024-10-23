fad3378614faa5f4fe18e3f01f8873ca630d54a6
Point-Service <tecnico@p-service.it>
2024-10-23 12:27:12 +0200

    Update accesso-rapido.php

--- template-parts/home/accesso-rapido.php
+++ template-parts/home/accesso-rapido.php
@@ -19,8 +19,8 @@ $box_accesso_rapido = $boxes;
                 <a href="<?php echo $box['link_message']; ?>" style="<?= ($colore_sfondo) ? 'background-color:' . $colore_sfondo : '' ?>" class="card card-teaser <?= $colore_sfondo ? '' : 'bg-primary' ?> rounded mt-0 p-3" target="_blank"> <!-- Rimosso 'border border-light' -->
                     <div class="cmp-card-simple card-wrapper pb-0 rounded"> <!-- Rimosso 'border-none' -->
                         <div style="border: none;"> <!-- Aggiunto 'style="border: none;"' -->
-                            <div class="card-body d-flex align-items-center"> <!-- Usato align-items-center per centrare verticalmente -->
-                                <?php if (array_key_exists('icon', $box) && !empty($box['icon'])) { ?>
+                            <div class="card-body d-flex align-items-center"> <!-- Usato align-items-center per centrare verticalmente -->                                    
+                                <?php if (array_key_exists('icona_message', $box) && array_key_exists('icon', $box) && !empty($box['icon'])) { ?>
                                     <div class="avatar size-lg me-3" style="min-width: 50px; width: 50px; height: 50px; display: flex; justify-content: center; align-items: center; background-color: #f0f0f0; border-radius: 50%;">
                                         <i class="fas fa-<?php echo htmlspecialchars($box['icon']); ?>" style="color: #007bff; font-size: 24px;"></i>
                                     </div>
