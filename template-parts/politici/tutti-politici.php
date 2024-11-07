0a049b3371acca30f9be1fde5ec5628663caa066
Point-Service <tecnico@p-service.it>
2024-11-07 11:37:01 +0100

    Update tutti-politici.php

--- template-parts/politici/tutti-politici.php
+++ template-parts/politici/tutti-politici.php
@@ -3,51 +3,56 @@ global $the_query, $load_posts, $load_card_type, $tax_query, $additional_filter,
 
 $query = $_GET['search'] ?? null;
 
+// Identifica la categoria in base alla pagina corrente
 switch ($post->post_name){
-	case 'politici': $tipo_incarico = 'politico'; $descrizione = 'del personale'; break;
-	case 'personale-amministrativo': $tipo_incarico = 'amministrativo'; $descrizione = 'del personale'; break;
-	case 'personale-sanitario': $tipo_incarico = 'sanitario'; $descrizione = 'del personale'; break;
-	case 'personale-socio-assistenziale': $tipo_incarico = 'socio-assistenziale'; $descrizione = 'del personale'; break;
-	case 'altro': $tipo_incarico = 'altro'; $descrizione = 'del personale'; break;
+    case 'politici': $tipo_incarico = 'politico'; $descrizione = 'del personale'; break;
+    case 'personale-amministrativo': $tipo_incarico = 'amministrativo'; $descrizione = 'del personale'; break;
+    case 'personale-sanitario': $tipo_incarico = 'sanitario'; $descrizione = 'del personale'; break;
+    case 'personale-socio-assistenziale': $tipo_incarico = 'socio-assistenziale'; $descrizione = 'del personale'; break;
+    case 'altro': $tipo_incarico = 'altro'; $descrizione = 'del personale'; break;
 }
 
+// Filtra gli incarichi in base al tipo di incarico identificato
 $tax_query = array(
-	array (
-		'taxonomy' => 'tipi_incarico',
-		'field' => 'slug',
-		'terms' => $tipo_incarico
-	));
+    array (
+        'taxonomy' => 'tipi_incarico',
+        'field' => 'slug',
+        'terms' => $tipo_incarico
+    )
+);
 
 $args_incarichi = array(
-	'post_type' => 'incarico',
-	'tax_query' => $tax_query,
+    'post_type' => 'incarico',
+    'tax_query' => $tax_query,
     'posts_per_page' => -1
 );
 
 $incarichi = get_posts($args_incarichi);
 $persone_ids = array();
 
+// Recupera tutti gli ID delle persone associate agli incarichi, senza rimuovere duplicati
 foreach($incarichi as $incarico) {
-	$persone = get_post_meta($incarico->ID, '_dci_incarico_persona');
-	foreach($persone as $persona) {
-		$persone_ids[] = $persona;
-	}
+    $persone = get_post_meta($incarico->ID, '_dci_incarico_persona');
+    foreach($persone as $persona) {
+        $persone_ids[] = $persona; // Aggiungi tutti gli ID senza `array_unique`
+    }
 }
 
-$filter_ids = array_unique($persone_ids);
+// Usa tutti gli ID raccolti per filtrare le persone da visualizzare
+$filter_ids = $persone_ids;
 
 $search_value = isset($_GET['search']) ? $_GET['search'] : null;
 $args = array(
-	's'         => $search_value,
-	'posts_per_page'    => -1,
-	'post_type' => 'persona_pubblica',
-	'post_status' => 'publish',
-	'orderby'        => 'post_title',
-	'order'          => 'ASC',
-    'post__in' => empty($persone_ids) ? [0] : $filter_ids,
+    's'                 => $search_value,
+    'posts_per_page'    => -1,
+    'post_type'         => 'persona_pubblica',
+    'post_status'       => 'publish',
+    'orderby'           => 'post_title',
+    'order'             => 'ASC',
+    'post__in'          => empty($persone_ids) ? [0] : $filter_ids,
 );
 
-$the_query = new WP_Query( $args );
+$the_query = new WP_Query($args);
 $persone = $the_query->posts;
 ?>
 <div class="bg-grey-card py-3">
@@ -62,13 +67,13 @@ $persone = $the_query->posts;
                     <div class="input-group">
                         <label for="autocomplete-two" class="visually-hidden">Cerca una parola chiave</label>
                         <input
-                                type="search"
-                                class="autocomplete form-control"
-                                placeholder="Cerca una parola chiave"
-                                id="autocomplete-two"
-                                name="search"
-                                value="<?php echo $query; ?>"
-                                data-bs-autocomplete="[]"
+                            type="search"
+                            class="autocomplete form-control"
+                            placeholder="Cerca una parola chiave"
+                            id="autocomplete-two"
+                            name="search"
+                            value="<?php echo $query; ?>"
+                            data-bs-autocomplete="[]"
                         />
                         <div class="input-group-append">
                             <button class="btn btn-primary" type="submit" id="button-3">
@@ -77,7 +82,8 @@ $persone = $the_query->posts;
                         </div>
                         <span class="autocomplete-icon" aria-hidden="true">
                             <svg class="icon icon-sm icon-primary" role="img" aria-labelledby="autocomplete-label">
-                            <use href="#it-search"></use></svg>
+                                <use href="#it-search"></use>
+                            </svg>
                         </span>
                     </div>
                 </div>
@@ -85,17 +91,22 @@ $persone = $the_query->posts;
                     <strong><?php echo $the_query->found_posts; ?> </strong>risultati in ordine alfabetico
                 </p>
             </div>
-            <div  class="row g-2" id="load-more">
+            <div class="row g-2" id="load-more">
                 <?php
-				    foreach ($persone as $post) {
-                        get_template_part( 'template-parts/politici/cards-list' );
-				    }
-				?>
+                    // Visualizza i risultati unici, evitando duplicati basati sull'ID della persona
+                    $visti = []; // Array per tracciare le persone già mostrate
+                    foreach ($persone as $post) {
+                        if (!in_array($post->ID, $visti)) {
+                            $visti[] = $post->ID;
+                            get_template_part('template-parts/politici/cards-list');
+                        }
+                    }
+                ?>
             </div>
-			<?php
-				$load_card_type = 'persona_pubblica';
-				get_template_part("template-parts/search/more-results");
-			?>       
+            <?php
+                $load_card_type = 'persona_pubblica';
+                get_template_part("template-parts/search/more-results");
+            ?>       
         </div>
     </form>
 </div>
