c2822c2a55f882dc6814d69e0c3654fd30bb8110
Point-Service <tecnico@p-service.it>
2024-11-07 12:34:19 +0100

    Update cards-list.php

--- template-parts/politici/cards-list.php
+++ template-parts/politici/cards-list.php
@@ -4,7 +4,7 @@ global $posts;
 $description = dci_get_meta('descrizione_breve');
 $incarichi = dci_get_meta('incarichi'); // Assicurati che questo contenga tutti gli incarichi della persona
 
-// Itera sugli incarichi
+// Itera sugli incarichi e visualizzali
 foreach ($incarichi as $incarico) {
     $arrdata = explode( '-', dci_get_meta("data_inizio_incarico") );
     $tipo = get_the_terms($incarico, 'tipi_incarico')[0];
@@ -19,27 +19,27 @@ foreach ($incarichi as $incarico) {
             <div class="col-md-6 col-xl-4">
                 <div class="card-wrapper border border-light rounded shadow-sm cmp-list-card-img cmp-list-card-img-hr">
                     <div class="card no-after rounded">
-                    <div class="row g-2 g-md-0 flex-md-column">
-                        <div class="col-4 order-2 order-md-1">
-                        <?php dci_get_img($img, 'rounded-top img-fluid img-responsive'); ?>
-                        </div>
-                        <div class="col-8 order-1 order-md-2">
-                        <div class="card-body">
-                            <div class="category-top cmp-list-card-img__body">
-                                <?php if ($tipo) { ?> 
-                                    <span class="category cmp-list-card-img__body-heading-title underline"><?php echo $nome_incarico ? $nome_incarico : 'POLITICO'; ?></span>
-                                <?php } ?>                    
-                            <span class="data"><?php echo $arrdata[0].' '.$monthName.' '.$arrdata[2] ?></span>
+                        <div class="row g-2 g-md-0 flex-md-column">
+                            <div class="col-4 order-2 order-md-1">
+                                <?php dci_get_img($img, 'rounded-top img-fluid img-responsive'); ?>
                             </div>
-                            <a class="text-decoration-none" href="<?php echo get_permalink(); ?>" data-element="administration-element">
-                                <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
-                            </a>
-                            <p class="card-text d-none d-md-block">
-                                <?php echo $description; ?>
-                            </p>
+                            <div class="col-8 order-1 order-md-2">
+                                <div class="card-body">
+                                    <div class="category-top cmp-list-card-img__body">
+                                        <?php if ($tipo) { ?> 
+                                            <span class="category cmp-list-card-img__body-heading-title underline"><?php echo $nome_incarico ? $nome_incarico : 'POLITICO'; ?></span>
+                                        <?php } ?>                    
+                                        <span class="data"><?php echo $arrdata[0].' '.$monthName.' '.$arrdata[2] ?></span>
+                                    </div>
+                                    <a class="text-decoration-none" href="<?php echo get_permalink(); ?>" data-element="administration-element">
+                                        <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
+                                    </a>
+                                    <p class="card-text d-none d-md-block">
+                                        <?php echo $description; ?>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
@@ -70,8 +70,9 @@ foreach ($incarichi as $incarico) {
                     </div>
                 </div>
             </div>
-        <?php 
+            <?php 
         }
     }
 }
 ?>
+
