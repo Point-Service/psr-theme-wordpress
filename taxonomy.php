62a423123935a077a8d6b7f29984ed7f60a279d2
Point-Service <tecnico@p-service.it>
2024-11-12 16:39:55 +0100

    Update taxonomy.php

--- taxonomy.php
+++ taxonomy.php
@@ -2,6 +2,7 @@
 global $obj;
 $obj = get_queried_object();
 
+
 if ($obj->taxonomy == "categorie_servizio")
 	get_template_part("archive-servizio");
 else if ( $obj->post_type=="documento_pubblico" ||  $obj->taxonomy == "tipi_documento" || $obj->taxonomy == "tipi_doc_albo_pretorio"){
@@ -10,7 +11,12 @@ else if ( $obj->post_type=="documento_pubblico" ||  $obj->taxonomy == "tipi_docu
 else if ( $obj->post_type=="tipi_notizia" ||  $obj->taxonomy == "tipi_notizia" || $obj->taxonomy == "tipi_notizia"){
 	get_template_part( "taxonomy-tipi_notizia" );
 }
-else if($obj->taxonomy == "argomenti")
+else if($obj->taxonomy == "argomenti"){
 	get_template_part("archive-argomento");
-else
+} else if ($obj->taxonomy == "tipi_luogo") {
+	get_template_part("archive-luogo");
+} 
+else{
 	get_template_part("archive");
+}
+?>
