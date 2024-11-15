198f40d3985dd5c21b2f23146bda7dcc9189c88e
Point-Service <tecnico@p-service.it>
2024-11-15 15:29:14 +0100

    Update single-servizio.php

--- single-servizio.php
+++ single-servizio.php
@@ -109,7 +109,7 @@ get_header();
     }
 }
 </script>
-   <div class="col-12 col-lg-8 offset-lg-1">
+   <div class="col-12">
 
             <div class="container" id="main-container">
                 <div class="row justify-content-center">
