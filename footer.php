f3b0cf5223078efd85ff11291a7ae6ccd998c9e0
Point-Service <tecnico@p-service.it>
2024-11-25 11:09:52 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
   <p>COOKIES - Si usano i cookies e altre tecniche di tracciamento per migliorare la tua esperienza di navigazione nel nostro sito, per mostrarti contenuti personalizzati e annunci mirati, per analizzare il traffico sul nostro sito, e per capire da dove arrivano i nostri visitatori.</p>
   <div class="cookiebar-buttons">
     
-    <button class="cookiebar-btn" href="/privacy/">Preferenze<span class="visually-hidden"> i cookies</span></button>
+    <button class="cookiebar-btn" href="/privacy/">Preferenze   <span class="visually-hidden"> i cookies</span></button>
     <button data-bs-accept="cookiebar" class="cookiebar-btn cookiebar-confirm acceptAllCookie">Accetto<span class="visually-hidden"> i cookies</span></button>
     <button class="cookiebar-btn cookiebar-confirm denyAllCookie">Nega<span class="visually-hidden"> i cookies</span></button>
   </div>
