d23f29edc6553a787005349131f0069f4b467f1f
Point-Service <tecnico@p-service.it>
2025-01-03 11:13:22 +0100

    Update card.php

--- template-parts/unita-organizzativa/card.php
+++ template-parts/unita-organizzativa/card.php
@@ -1,29 +1,25 @@
 <?php
-    global $uo_id, $with_border;
+    global $uo_id, $with_border, $mostra_dettagli_sede;
     $ufficio = get_post( $uo_id );
 
     $prefix = '_dci_unita_organizzativa_';
-    $img = dci_get_meta('immagine', $prefix, $uo_id);
-	$sede_principale = dci_get_meta('sede_principale', $prefix, $uo_id);
-	$prefix = '_dci_luogo_';
-    $indirizzo = dci_get_meta('indirizzo', $prefix, $sede_principale);
+	$descrizione_breve = dci_get_meta("descrizione_breve", $prefix, $ufficio->ID);
 
-    $prefix = '_dci_luogo_';
-    
-    //  Ho rimosso i Contatti perchè non venivano utilizzati
-    
+    $img = get_the_post_thumbnail_url($ufficio->ID);
+
     if($with_border) {
 ?>
-<div class="card card-teaser border rounded shadow p-4 flex-nowrap">
+
+<div class="card card-teaser card-teaser-info rounded shadow-sm p-3 mb-2">
     <div class="card-body pe-3">
-        <h4 class="u-main-black mb-1 title-small-semi-bold-medium">
-            <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>">
+        <h4 class="card-title text-paragraph-regular-medium-semi">
+            <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>" data-element="service-area">
             <?php echo $ufficio->post_title; ?>
             </a>
         </h4>
-        <div class="card-text">
-            <p><?php echo $indirizzo; ?></p>
-        </div>
+	   <?php if ($descrizione_breve && $mostra_dettagli_sede) {
+		        echo '<div class="card-text mt-3"><p class="u-main-black">'.$descrizione_breve.'</p></div>';
+	   } ?>
     </div>
     <?php if ($img) { ?>
     <div class="avatar size-xl">
@@ -36,16 +32,14 @@
 
 <div class="card card-teaser card-teaser-info rounded shadow-sm p-3 flex-nowrap">
     <div class="card-body pe-3">
-        <p class="card-title text-paragraph-regular-medium-semi mb-3">
+        <p class="card-title text-paragraph-regular-medium-semi">
             <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>" data-element="service-area">
             <?php echo $ufficio->post_title; ?>
             </a>
-        </p>
-        <div class="card-text">
-            <p class="u-main-black">
-				<?php echo $indirizzo;  ?>
-			</p>
-        </div>
+        </h4>
+	   <?php if ($descrizione_breve && $mostra_dettagli_sede) {
+		        echo '<div class="card-text mt-3"><p class="u-main-black">'.$descrizione_breve.'</p></div>';
+	   } ?>
     </div>
     <?php if ($img) { ?>
         <div class="avatar size-xl">
