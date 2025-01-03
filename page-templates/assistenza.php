776c5209c83b3366c39c4a3431c6e5e9665cf10d
Point-Service <tecnico@p-service.it>
2025-01-03 16:54:04 +0100

    Update assistenza.php

--- page-templates/assistenza.php
+++ page-templates/assistenza.php
@@ -50,7 +50,12 @@ get_header();
                     </div>
                     <p class="subtitle-small pb-40 mb-0">
                         Hai un’identità digitale SPID o CIE?
-                        <a class="title-small-semi-bold t-primary text-decoration-none" href="#">Accedi</a>
+			    <?php if(dci_get_option("area_riservata")) { ?>
+				<a class="btn btn-primary btn-icon btn-full" title="Accedi all'area personale" href="<?php echo dci_get_option("area_riservata"); ?>" data-element="personal-area-login"></a>					
+			       <?php } else { ?>
+			      <a class="title-small-semi-bold t-primary text-decoration-none" href="#">Accedi</a>
+			    <?php } ?>
+
                     </p>
                 </div>
             </div>
