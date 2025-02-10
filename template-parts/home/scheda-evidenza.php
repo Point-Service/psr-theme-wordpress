077791f96e2ce9fe4c4899ae64efa58b50484da9
Point-Service <tecnico@p-service.it>
2025-02-10 17:59:45 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -31,8 +31,9 @@ $page_macro = get_page_by_path($page_macro_slug);
             <p class="card-title text-paragraph-medium u-grey-light"><?php echo $post->post_title ?></p>
             <p class="text-paragraph-card u-grey-light m-0" style="margin-bottom: 40px!important;"><?php echo $descrizione_breve ?></p>    
             <hr align="left" size="1" width="200" color="red" noshade>
-            <div class="card-body">Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></div>    
-            <hr align="left" size="1" width="200" color="red" noshade>
+            <div class="card-body">Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></div>   
+            <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">
+          
         </div>
         <div class="card-image card-image-rounded pb-5">            
             <?php dci_get_img($img); ?>
