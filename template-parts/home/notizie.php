a00d1ce84ceeb9608dffa9e7328cbc3f7ecd8a17
Point-Service <tecnico@p-service.it>
2025-02-10 18:02:11 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -56,7 +56,8 @@ for ($i = 1; $i <= 20; $i++) {
                                 </a>
                                 <p class="mb-4 font-serif pt-3">
                                     <?php echo $descrizione_breve ?>                       
-                                </p>                               
+                                </p>     
+                                <hr style="margin-bottom: 40px; width: 200px; height: 1px; background-color: grey; border: none;">
                                Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?>
                             </div>
                         </div>
