3fa03a1c73749db1882c332a24e3152c6d802647
Point-Service <tecnico@p-service.it>
2025-02-10 17:09:46 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -13,7 +13,7 @@ $luoghi = dci_get_meta("luoghi", '_dci_notizia_', $post->ID);
 $page_macro_slug = dci_get_group($post->post_type);
 $page_macro = get_page_by_path($page_macro_slug);
 ?>
-
+jjjjjjjjjj
 <?php if ($img) { ?>
 <div class="card card-teaser card-teaser-image card-flex no-after rounded shadow-sm border border-light mb-0">
     <div class="card-image-wrapper with-read-more">
