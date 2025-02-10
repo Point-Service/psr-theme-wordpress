07e81d0a6bdf156a2feef93df93e7b06ce067aaf
Point-Service <tecnico@p-service.it>
2025-02-10 13:02:17 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -57,7 +57,7 @@ for ($i = 1; $i <= 20; $i++) {
                                 </a>
                                 <p class="mb-4 font-serif pt-3">
                                     <?php echo $descrizione_breve ?>
-
+yt
  <?php if(is_array($luoghi) && count($luoghi)) { ?>
    <?php get_template_part("template-parts/single/luoghi"); ?>
  <?php }?>
