ea0d239b44accfed1053bc696fe184f47d3f1fea
Point-Service <tecnico@p-service.it>
2025-02-10 15:18:18 +0100

    Update notizie-evidenza.php

--- template-parts/home/notizie-evidenza.php
+++ template-parts/home/notizie-evidenza.php
@@ -5,9 +5,8 @@ $post = get_post($scheda)??null;
 $img = dci_get_meta('immagine');
 $descrizione_breve = dci_get_meta('descrizione_breve');
 $icon = dci_get_post_type_icon_by_id($post->ID);
-
-$page = get_page_by_path( dci_get_group($post->post_type) );    
-
+$page = get_page_by_path( dci_get_group($post->post_type) ); 
+$argomenti = dci_get_meta("argomenti");
 $page_macro_slug = dci_get_group($post->post_type);
 $page_macro = get_page_by_path($page_macro_slug);
 ?>
