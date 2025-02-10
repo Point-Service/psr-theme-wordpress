2069ab645f57526c98813036d111ccdc035e70f3
Point-Service <tecnico@p-service.it>
2025-02-10 16:02:39 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -24,8 +24,8 @@ $page_macro = get_page_by_path($page_macro_slug);
             <span class="category title-xsmall-semi-bold fw-semibold" ><?php echo $page->post_title ?></span>
             </div>
             <p class="card-title text-paragraph-medium u-grey-light"><?php echo $post->post_title ?></p>
-            <p class="text-paragraph-card u-grey-light m-0" style="margin-bottom: 40px!important;"><?php echo $descrizione_breve ?>
-             <p></p>Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></p>
+            <p class="text-paragraph-card u-grey-light m-0" style="margin-bottom: 40px!important;"><?php echo $descrizione_breve ?></p>
+           <div> <p></p>Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?><p></p></div>
         </div>
         <div class="card-image card-image-rounded pb-5">            
             <?php dci_get_img($img); ?>
@@ -56,9 +56,8 @@ $page_macro = get_page_by_path($page_macro_slug);
             <?php echo $post->post_title ?>
         </p>
         <p class="text-paragraph-card u-grey-light m-0">
-            <?php echo $descrizione_breve ?>
-        
-              <p></p>Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></p>
+            <?php echo $descrizione_breve ?>        
+             <div><p></p>Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?><p></p></div>
         </div>
         <a class="read-more" href="<?php echo get_permalink($post->ID); ?>" aria-label="Vai alla pagina <?php echo $post->post_title ?>" title="Vai alla pagina <?php echo $post->post_title ?>"
         ><span class="text">Vai alla pagina</span>
