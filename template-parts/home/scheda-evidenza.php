0eded8ce1d126b941ae963e8b589cf715dd7a7db
Point-Service <tecnico@p-service.it>
2025-02-10 15:59:13 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -5,8 +5,9 @@ $post = get_post($scheda['scheda_'.$count.'_contenuto'][0]);
 $img = dci_get_meta('immagine');
 $descrizione_breve = dci_get_meta('descrizione_breve');
 $icon = dci_get_post_type_icon_by_id($post->ID);
+$page = get_page_by_path( dci_get_group($post->post_type) ); 
 
-$page = get_page_by_path( dci_get_group($post->post_type) );    
+$argomenti = dci_get_meta("argomenti", '_dci_notizia_', $post->ID);
 
 $page_macro_slug = dci_get_group($post->post_type);
 $page_macro = get_page_by_path($page_macro_slug);
@@ -24,6 +25,7 @@ $page_macro = get_page_by_path($page_macro_slug);
             </div>
             <p class="card-title text-paragraph-medium u-grey-light"><?php echo $post->post_title ?></p>
             <p class="text-paragraph-card u-grey-light m-0" style="margin-bottom: 40px!important;"><?php echo $descrizione_breve ?></p>
+                    Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?>
         </div>
         <div class="card-image card-image-rounded pb-5">            
             <?php dci_get_img($img); ?>
@@ -56,6 +58,7 @@ $page_macro = get_page_by_path($page_macro_slug);
         <p class="text-paragraph-card u-grey-light m-0">
             <?php echo $descrizione_breve ?>
         </p>
+               Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?>
         </div>
         <a class="read-more" href="<?php echo get_permalink($post->ID); ?>" aria-label="Vai alla pagina <?php echo $post->post_title ?>" title="Vai alla pagina <?php echo $post->post_title ?>"
         ><span class="text">Vai alla pagina</span>
