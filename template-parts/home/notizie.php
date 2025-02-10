8e81a97480514df3b22793a6cd8a80f1d7523633
Point-Service <tecnico@p-service.it>
2025-02-10 13:01:07 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -10,13 +10,15 @@ global $count, $scheda;
 // $post = array_shift($posts);
 
 $post_id = dci_get_option('notizia_evidenziata','homepage', true )[0] ?? null;
+$prefix= '_dci_notizia_';
+
 if($post_id) $post = get_post($post_id);
 $img = dci_get_meta("immagine", '_dci_notizia_', $post->ID);
 $arrdata = dci_get_data_pubblicazione_arr("data_pubblicazione", '_dci_notizia_', $post->ID);
 $monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
 $descrizione_breve = dci_get_meta("descrizione_breve", '_dci_notizia_', $post->ID);
 $argomenti = dci_get_meta("argomenti", '_dci_notizia_', $post->ID);
-$luoghi = dci_get_meta("luoghi", '_dci_notizia_', $post->ID);
+$luoghi = dci_get_meta("luoghi", $prefix, $post->ID);
 
 // Popolo schede in evidenza
 //Se non è vuota l'aggiungo
@@ -55,7 +57,7 @@ for ($i = 1; $i <= 20; $i++) {
                                 </a>
                                 <p class="mb-4 font-serif pt-3">
                                     <?php echo $descrizione_breve ?>
-u
+
  <?php if(is_array($luoghi) && count($luoghi)) { ?>
    <?php get_template_part("template-parts/single/luoghi"); ?>
  <?php }?>
