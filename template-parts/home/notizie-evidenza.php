3d85da09ff151ef3718d3a24f8e55faf38c6da30
Point-Service <tecnico@p-service.it>
2025-02-10 15:18:42 +0100

    Update notizie-evidenza.php

--- template-parts/home/notizie-evidenza.php
+++ template-parts/home/notizie-evidenza.php
@@ -55,6 +55,7 @@ $page_macro = get_page_by_path($page_macro_slug);
         <p class="card-text text-secondary">
             <?php echo $descrizione_breve ?>
         </p>
+             Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?>
         </div>
         <a class="read-more" href="<?php echo get_permalink($post->ID); ?>" aria-label="Vai alla pagina <?php echo $post->post_title ?>" title="Vai alla pagina <?php echo $post->post_title ?>"
         ><span class="text">Vai alla pagina</span>
