c250782bb245be070fffb362bd4c3faf17447e59
Point-Service <tecnico@p-service.it>
2025-02-10 18:00:07 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -42,7 +42,7 @@ $page_macro = get_page_by_path($page_macro_slug);
      <a class="read-more ps-3"
         href="<?php echo get_permalink($post->ID); ?>"
         aria-label="Vai alla pagina <?php echo $post->post_title ?>" 
-        title="Vai alla pagina <?php echo $post->post_title ?> style="display: inline-flex; align-items: center; margin-top: 20px;">
+        title="Vai alla pagina <?php echo $post->post_title ?> style="display: inline-flex; align-items: center; margin-top: 30px;">
             <span class="text">Vai alla pagina</span>
             <svg class="icon">
                 <use xlink:href="#it-arrow-right"></use>
