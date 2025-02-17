d03bf3fab0c5ee4e8f9a5ccd0e5524fd1339fe8a
Point-Service <tecnico@p-service.it>
2025-02-17 16:06:45 +0100

    Update filters.php

--- template-parts/search/filters.php
+++ template-parts/search/filters.php
@@ -10,6 +10,8 @@ if(isset($_GET["post_types"]))
     $post_types = $_GET["post_types"];
 
 $post_terms = array();
+
+$voci_escluse=["page", "post","organi_governo","aree_amministrative","uffici"];
 if(isset($_GET["post_terms"]))
     $post_terms = $_GET["post_terms"];
 ?>
@@ -18,37 +20,39 @@ if(isset($_GET["post_terms"]))
     <h2 class="visually-hidden" id="filter">filtri da applicare</h2>
     <fieldset>
         <div class="categoy-list pb-4">
+            </div>
             <legend class="h6 text-uppercase category-list__title">Tipologie</legend>
             <ul>
                 <?php 
                     foreach ($tipologie as $type_slug) {
                         $tipologia = get_term_by('slug', $type_slug);
-                ?>
-                <li>
-                    <div class="form-check">
-                        <div class="checkbox-body border-light py-3">
-                        <input
-                            type="checkbox"
-                            name="post_types[]"
-                            <?php if(in_array($type_slug, $post_types)) echo " checked "; ?>
-                            onChange="this.form.submit()"                            
-                            id="<?php echo $type_slug; ?>" 
-                            value="<?php echo $type_slug; ?>" 
-                        />
-                        <label
-                            for="<?php echo $type_slug; ?>" 
-                            class="subtitle-small_semi-bold mb-0 category-list__list"
-                            ><?php echo COMUNI_TIPOLOGIE[$type_slug]['plural_name']; ?> 
-                        </label>
-                        </div>
-                    </div>
-                </li>
-                <?php } ?>
+                        if(!in_array($type_slug,$voci_escluse)){ ?>
+                            <li>
+                                <div class="form-check">
+                                    <div class="checkbox-body border-light py-3">
+                                        <input
+                                            type="checkbox"
+                                            name="post_types[]"
+                                            <?php if(in_array($type_slug, $post_types)) echo " checked "; ?>
+                                            onChange="this.form.submit()"                            
+                                            id="<?php echo $type_slug; ?>" 
+                                            value="<?php echo $type_slug; ?>" />
+                                            <label
+                                            for="<?php echo $type_slug; ?>" 
+                                            class="subtitle-small_semi-bold mb-0 category-list__list"
+                                            ><?php echo COMUNI_TIPOLOGIE[$type_slug]['plural_name']; ?> 
+                                        </label>                        
+                                    </div>
+                                </div>
+                            </li>
+                        <?php } ?>
+                    <?php }?>
             </ul>
         </div>
     </fieldset>    
     <fieldset>
         <div class="categoy-list pb-4">
+            </div>
             <legend class="h6 text-uppercase category-list__title">Argomenti</legend>
             <ul>
                 <?php 
@@ -81,3 +85,4 @@ if(isset($_GET["post_terms"]))
         </div>
     </fieldset>   
 </div>
+
