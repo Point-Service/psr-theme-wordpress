028787241426346167c2bc1e5e931552038cc5f6
Point-Service <tecnico@p-service.it>
2025-02-17 12:44:30 +0100

    Update eventi.php

--- page-templates/eventi.php
+++ page-templates/eventi.php
@@ -16,6 +16,7 @@ get_header();
 			?>
 			<?php get_template_part("template-parts/hero/hero"); ?>
 			<?php get_template_part("template-parts/vivere-comune/tutti-eventi"); ?>
+			<?php get_template_part("template-parts/evento/tipologie");?>
 			<?php get_template_part("template-parts/common/valuta-servizio"); ?>
 			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
 							
