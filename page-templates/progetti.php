01889784a9bfe805ea27d8fab9e7bec1201e12c2
Point-Service <tecnico@p-service.it>
2025-02-17 16:57:08 +0100

    Update progetti.php

--- page-templates/progetti.php
+++ page-templates/progetti.php
@@ -12,24 +12,24 @@ function info(){?>
 <section class="hero-img mb-20 mb-lg-50">
     <div class="container">
         <div class="row">
-            <h2>Cosâ€™Ã¨ il PNRR</h2>
+            <h2>Cos’è il PNRR</h2>
             <p class="text-justify">
-                Il Piano Nazionale di Ripresa e Resilienza (PNRR) Ã¨ lo strumento che traccia 
-                gli obiettivi, le riforme e gli investimenti che lâ€™Italia intende realizzare 
-                grazie allâ€™utilizzo dei fondi europei di Next Generation EU, per attenuare 
-                lâ€™impatto economico e sociale della pandemia e rendere lâ€™Italia un Paese piÃ¹ 
-                equo, verde e inclusivo, con unâ€™economia piÃ¹ competitiva, dinamica e 
+                Il Piano Nazionale di Ripresa e Resilienza (PNRR) è lo strumento che traccia 
+                gli obiettivi, le riforme e gli investimenti che l’Italia intende realizzare 
+                grazie all’utilizzo dei fondi europei di Next Generation EU, per attenuare 
+                l’impatto economico e sociale della pandemia e rendere l’Italia un Paese più 
+                equo, verde e inclusivo, con un’economia più competitiva, dinamica e 
                 innovativa.
             </p>
             <p class="text-justify">
-                Il PNRR annovera tre prioritÃ  trasversali condivise a livello europeo 
+                Il PNRR annovera tre priorità trasversali condivise a livello europeo 
                 (digitalizzazione e innovazione, transizione ecologica e inclusione sociale) 
                 e si sviluppa lungo 16 Componenti, raggruppate in 6 missioni:
             </p>
             <ul class="text-justify" style="list-style-type: disc; margin-left:30px; padding:10px">
-                <li>Digitalizzazione, innovazione, competitivitÃ , cultura e turismo (M1)</li>
+                <li>Digitalizzazione, innovazione, competitività, cultura e turismo (M1)</li>
                 <li>Rivoluzione verde e transizione ecologica (M2)</li>
-                <li>Infrastrutture per una mobilitÃ  sostenibile (M3)</li>
+                <li>Infrastrutture per una mobilità sostenibile (M3)</li>
                 <li>Istruzione e ricerca (M4)</li>
                 <li>Inclusione e coesione (M5)</li>
                 <li>Salute (M6)</li>
