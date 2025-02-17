b91665e9f0bb04e8ee55742055fff1581b0448bf
Point-Service <tecnico@p-service.it>
2025-02-17 15:46:13 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -17,9 +17,9 @@ get_header();
                     <div class="cmp-hero">
                         <section class="it-hero-wrapper bg-white align-items-start">
                             <div class="it-hero-text-wrapper pt-0 ps-0 pb-4 pb-lg-60">
-                                <h1 class="text-black title-xxxlarge mb-2"><?php esc_html_e( '404', 'design_comuni_italia' ); ?></h1>                               
-                                <h2><?php esc_html_e( 'La pagina o categoria che stavi cercando non è stata trovata.', 'design_comuni_italia' ); ?></h2>
-                                <p class="text-black titillium text-paragraph"><?php _e( 'Elemento non trovato, si consiglia di ritornare indietro e riprovare più tardi.,<br> <a href="javascript:history.back();" title="Torna alla pagina precedente">torna indietro</a> o utilizza il menu per continuare la navigazione.', 'design_comuni_italia' ); ?></p>
+                                <h1 class="text-black title-xxxlarge mb-2"><?php esc_html_e( '404', 'design_comuni_italia' ); ?></h1>
+                                <h2><?php esc_html_e( 'Pagina non trovata', 'design_comuni_italia' ); ?></h2>                              
+                                <p class="text-black titillium text-paragraph"><?php _e( '<b>La pagina o categoria che stavi cercando non è stata trovata.</b><br><br>Elemento non trovato, si consiglia di ritornare indietro e riprovare più tardi.,<br> <a href="javascript:history.back();" title="Torna alla pagina precedente">torna indietro</a> o utilizza il menu per continuare la navigazione.', 'design_comuni_italia' ); ?></p>
                             </div>
                         </section>
                     </div>
