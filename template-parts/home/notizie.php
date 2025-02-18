11320c60ce1c6ff13562daedcb6e6ed24f3303e9
Point-Service <tecnico@p-service.it>
2025-02-18 18:00:05 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -1,30 +1,44 @@
 <?php
-global $count, $scheda;
-
+global $scheda;
+// Per mostrare la notizia più recente
 $post_id = dci_get_option('notizia_evidenziata', 'homepage', true)[0] ?? null;
-$prefix = '_dci_notizia_';
-
 if ($post_id) {
     $post = get_post($post_id);
-    $img = dci_get_meta("immagine", $prefix, $post->ID);
-    $arrdata = dci_get_data_pubblicazione_arr("data_pubblicazione", $prefix, $post->ID);
-    $monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
-    $descrizione_breve = dci_get_meta("descrizione_breve", $prefix, $post->ID);
-    $argomenti = dci_get_meta("argomenti", $prefix, $post->ID);
-    $luogo_notizia = dci_get_meta("luoghi", $prefix, $post->ID);
 }
 
-$schede = [];
-for ($i = 1; $i <= 20; $i++) {
-    $schede[] = dci_get_option("schede_evidenziate_$i", 'homepage', true)[0] ?? null;
+//Notizie in homepage
+$posts = null;
+$notizie_in_home = dci_get_option('notizie_in_home', 'homepage');
+if ($notizie_in_home && $notizie_in_home > 0) {
+    $args  = array(
+        'post_type'      => 'notizia',
+        'post_status'    => 'publish',
+        'posts_per_page' => $notizie_in_home,
+        'orderby'        => 'date',
+        'order'          => 'DESC'
+    );
+    $posts = get_posts($args);
+    //$post  = array_shift( $posts  );
 }
+
+$img               = dci_get_meta("immagine", '_dci_notizia_', $post->ID);
+$arrdata           = dci_get_data_pubblicazione_arr("data_pubblicazione", '_dci_notizia_', $post->ID);
+$monthName         = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
+$descrizione_breve = dci_get_meta("descrizione_breve", '_dci_notizia_', $post->ID);
+$argomenti         = dci_get_meta("argomenti", '_dci_notizia_', $post->ID);
+
+$schede = dci_get_option('schede_evidenziate', 'homepage') ?? null;
+
+$overlapping = "";
 ?>
-
+<!-- Tag section is opened in home.php -->
 <section id="notizie" aria-describedby="novita-in-evidenza">
     <div class="section-content">
         <div class="container">
             <h2 id="novita-in-evidenza" class="visually-hidden">Novità in evidenza</h2>
-            <?php if ($post_id) { ?>
+            <?php if ($post_id) {
+                $overlapping = "card-overlapping";
+            ?>
                 <div class="row">
                     <div class="col-lg-5 order-2 order-lg-1">
                         <div class="card mb-1">
@@ -34,42 +48,19 @@ for ($i = 1; $i <= 20; $i++) {
                                         <use xlink:href="#it-calendar"></use>
                                     </svg>
                                     <span class="title-xsmall-semi-bold fw-semibold"><?php echo $post->post_type ?></span>
+                                    <?php if (is_array($arrdata) && count($arrdata)) { ?>
+                                        <span class="data fw-normal"><?php echo $arrdata[0] . ' ' . $monthName . ' ' . $arrdata[2]; ?></span>
+                                    <?php } ?>
                                 </div>
                                 <a href="<?php echo get_permalink($post->ID); ?>" class="text-decoration-none">
-                                    <h3 class="card-title"><?php echo $post->post_title ?></h3>
+                                    <h3 class="card-title">
+                                        <?php echo $post->post_title ?>
+                                    </h3>
                                 </a>
-                                <p class="mb-4 font-serif pt-3"><?php echo $descrizione_breve ?></p>
-                                        <?php if (is_array($luogo_notizia) && count($luogo_notizia)) { ?>
-                                            <span class="data fw-normal">📍 
-                                                <?php 
-                                                foreach ($luogo_notizia as $luogo_id) {
-                                                    // Ottieni i dettagli del luogo
-                                                    $luogo_post = get_post($luogo_id);
-                                                    
-                                                    if ($luogo_post && !is_wp_error($luogo_post)) {
-                                                        // Stampa il nome del luogo come link
-                                                        echo '<a href="' . esc_url(get_permalink($luogo_post->ID)) . '" title="' . esc_attr($luogo_post->post_title) . '" class="card-text text-secondary text-uppercase pb-3">' . esc_html($luogo_post->post_title) . '</a> ';
-                                                    }
-                                                }
-                                                ?>
-                                            </span>
-                                        <?php } elseif (!empty($luogo_notizia)) { ?>
-                                            <span class="data fw-normal"> | 📍 
-                                                <?php echo esc_html($luogo_notizia); ?>
-                                            </span>
-                                        <?php } ?>
-                                      <div class="row mt-5 mb-4">
-                                        <div class="col-6">
-                                            <small>Data:</small>
-                                            <p class="fw-semibold font-monospace">
-                                                         <?php if (is_array($arrdata) && count($arrdata)) { ?>
-                                                    <span class="data fw-normal"><?php echo $arrdata[0] . ' ' . $monthName . ' ' . $arrdata[2]; ?></b></span>
-                                                <?php } ?>
-                                            </p>
-                                        </div>
-                                    </div>
-                                  <small>Argomenti: </small><?php get_template_part("template-parts/common/badges-argomenti"); ?>
-                                
+                                <p class="mb-4 font-serif pt-3">
+                                    <?php echo $descrizione_breve ?>
+                                </p>
+                                <?php get_template_part("template-parts/common/badges-argomenti"); ?>
                             </div>
                         </div>
                     </div>
@@ -79,39 +70,64 @@ for ($i = 1; $i <= 20; $i++) {
                         } ?>
                     </div>
                 </div>
+
+            <?php }
+            if ($posts && is_array($posts) && count($posts) > 0) { ?>
+                <?php if (!$post_id) { ?>
+                    <div class="row row-title pt-lg-60 pb-3">
+                        <div class="col-12 d-lg-flex justify-content-between">
+                            <h2 id="ultime-news" class="mb-lg-0">Ultime news</h2>
+                        </div>
+                    </div>
+                <?php } ?>
+                <div class="row mb-2">
+                    <div class="card-wrapper px-0 <?php echo $overlapping; ?> card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                        <?php
+                        foreach ($posts as $post) {
+                            if ($post) {
+                                get_template_part("template-parts/home/notizia-evidenza");
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="row my-4 justify-content-md-center">
+                    <a class="read-more pb-3" href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>">
+                        <button type="button" class="btn btn-outline-primary">Tutte le novità
+                            <svg class="icon">
+                                <use xlink:href="#it-arrow-right"></use>
+                            </svg>
+                        </button>
+                    </a>
+                </div>
             <?php } ?>
         </div>
     </div>
 </section>
-
-    <div class="container">
-      <div class="row mb-2">
-        <div class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
-                <div class="row my-4 justify-content-md-center">                 
-                       <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary-outline mt-40" >   
-                         <svg class="icon icon-sm" aria-hidden="true">
-                           <use xlink:href="#it-calendar"></use>
-                          </svg>
-                          Visualizza tutte le novità
-                     </a>                 
-              </div>
+<?php
+if ($schede && count($schede) > 0) { ?>
+    <section aria-describedby="contenuti_evidenza">
+        <div class="section-content">
+            <div class="section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
+                <div class="container">
+                    <div class="row row-title pt-5 pt-lg-60 pb-3">
+                        <div class="col-12 d-lg-flex justify-content-between">
+                            <h2 id="contenuti_evidenza" class="mb-lg-0">Contenuti in evidenza</h2>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="card-wrapper px-0 card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                            <?php $count = 1;
+                            foreach ($schede as $scheda) {
+                                if ($scheda) {
+                                    get_template_part("template-parts/home/scheda-evidenza");
+                                }
+                                ++$count;
+                            } ?>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-         </div>
-
-<p></p>
-<section id="calendario">
-  <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
-    <div class="container">
-      <div class="row mb-2">
-        <div class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
-        <?php $count=1;
-        foreach ($schede as $scheda) {
-          if ($scheda) 
-            get_template_part("template-parts/home/scheda-evidenza");
-          ++$count;
-        } ?>
         </div>
-      </div>
-    </div>
-<!-- Tag section is closed in home.php -->
+    </section>
+<?php } ?>
