9a55e97045961e87bae8ae817e29d3c4fdb6b651
Point-Service <tecnico@p-service.it>
2025-02-18 18:15:51 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -69,13 +69,19 @@ for ($i = 1; $i <= 20; $i++) {
                                         </div>
                                     </div>
                                   <small>Argomenti: </small><?php get_template_part("template-parts/common/badges-argomenti"); ?>
-                                  <div class="row my-4 justify-content-md-center">
-                                    <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary-outline mt-40" >   
+                                    <div class="container">
+                                      <div class="row mb-2">
+                                        <div class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                                                <div class="row my-4 justify-content-md-center">                 
+                                                       <a href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>" class="btn btn-primary-outline mt-40" >   
                                                          <svg class="icon icon-sm" aria-hidden="true">
                                                            <use xlink:href="#it-calendar"></use>
                                                           </svg>
                                                           Visualizza tutte le novità
-                                                     </a>  
+                                                     </a>                 
+                                              </div>
+                                            </div>
+                                          </div>
                                     </div>
 
                             </div>
