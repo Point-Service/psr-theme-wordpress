40325a140bb9e90bea3933fe77ad8efc46b8e2ef
Point-Service <tecnico@p-service.it>
2025-02-18 10:26:18 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -74,10 +74,7 @@ $page_macro = get_page_by_path($page_macro_slug);
             <?php } ?>
         </div>
         <p class="card-title text-paragraph-medium u-grey-light"><?php echo esc_html($post->post_title); ?></p>
-        <p class="text-paragraph-card u-grey-light m-0"><?php echo esc_html($descrizione_breve); ?></p>    
-        <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">
-        <div class="card-body">Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></div>            
-        <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">
+        <p class="text-paragraph-card u-grey-light m-0"><?php echo esc_html($descrizione_breve); ?></p>  
         <?php if (is_array($luogo_notizia) && count($luogo_notizia)) { ?>
             <span class="data fw-normal"> | 📍 
                 <?php 
@@ -97,10 +94,9 @@ $page_macro = get_page_by_path($page_macro_slug);
                 <?php echo esc_html($luogo_notizia); ?>
             </span>
         <?php } ?>
-
-
-
-        
+        <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">
+        <div class="card-body">Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></div>            
+        <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">        
         <a class="read-more" href="<?php echo esc_url(get_permalink($post->ID)); ?>" 
            aria-label="Vai alla pagina <?php echo esc_attr($post->post_title); ?>" 
            title="Vai alla pagina <?php echo esc_attr($post->post_title); ?>">
