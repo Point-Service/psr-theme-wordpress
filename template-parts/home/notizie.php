63733fca98000cc7c0eebba6d88a606fe62fce95
Point-Service <tecnico@p-service.it>
2025-02-18 10:45:20 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -18,7 +18,7 @@ for ($i = 1; $i <= 20; $i++) {
     $schede[] = dci_get_option("schede_evidenziate_$i", 'homepage', true)[0] ?? null;
 }
 ?>
-
+jjjjjj
 <section id="notizie" aria-describedby="novita-in-evidenza">
     <div class="section-content">
         <div class="container">
