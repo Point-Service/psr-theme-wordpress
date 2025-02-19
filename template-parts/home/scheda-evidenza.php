958107008a7ab1482721b5d82f43ecb84d21f632
Point-Service <tecnico@p-service.it>
2025-02-19 16:14:40 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -1,67 +1,118 @@
 <?php
-global $argomento_full, $count, $sito_tematico_id;
+global $scheda, $count;
 
-$argomento = get_term_by('slug', $argomento_full['argomento_'.$count.'_argomento'], 'argomenti');
+$post = get_post($scheda['scheda_'.$count.'_contenuto'][0]);
+$img = dci_get_meta('immagine');
+$descrizione_breve = dci_get_meta('descrizione_breve');
+$icon = dci_get_post_type_icon_by_id($post->ID);
+$page = get_page_by_path( dci_get_group($post->post_type) ); 
+$argomenti = dci_get_meta("argomenti", '_dci_notizia_', $post->ID);
+$luogo_notizia = dci_get_meta("luoghi", '_dci_notizia_', $post->ID); // Recupera il luogo della notizia
 
-$icon = dci_get_term_meta('icona', "dci_term_", $argomento->term_id);
+$arrdata = dci_get_data_pubblicazione_arr("data_pubblicazione", '_dci_notizia_', $post->ID);
+$monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
 
-if (isset($argomento_full['argomento_'.$count.'_siti_tematici']))
-$sito_tematico_id = $argomento_full['argomento_'.$count.'_siti_tematici'];
-if (isset($argomento_full['argomento_'.$count.'_contenuti']))
-$links = $argomento_full['argomento_'.$count.'_contenuti'];
+$page_macro_slug = dci_get_group($post->post_type);
+$page_macro = get_page_by_path($page_macro_slug);
 ?>
 
-<div class="card card-teaser no-after rounded shadow-sm border border-light">
-    <div class="card-body pb-5">
-        <!-- card head -->
-        <!-- <div class="category-top">
-            <svg class="icon">
-            <use
-                xlink:href="#<?php #echo $icon ? $icon : 'it-info-circle'; ?>"
-            ></use>
-            </svg>
-        </div> -->
-        <h3 class="card-title title-xlarge-card"><?php echo $argomento->name?></h3>
-        <p class="card-text">
-            <?php echo $argomento->description?>
-        </p>
-
-        <!-- sito tematico -->
-        <?php if($sito_tematico_id) { ?>
-        <p class="card-text pb-3">Visita il sito:</p>
-        <?php 
-            $custom_class = "no-after mt-0";
-            get_template_part("template-parts/sito-tematico/card");
-        ?>
-        <?php } ?>
-
-        <!-- links -->
-        <?php if(isset($links) && is_array($links) && count($links)) { ?>
-        <div class="link-list-wrapper mt-4">
-            <ul class="link-list">
-                <?php foreach ($links as $link_id) { 
-                    $link_obj = get_post($link_id);
-                ?>
-                <li>
-                    <a class="list-item active icon-left mb-2" href="<?php echo get_permalink(intval($link_id)); ?>">
-                    <span class="list-item-title-icon-wrapper">
-                        <span><?php echo $link_obj->post_title; ?></span>
-                    </span>
-                    </a>
-                </li>
+<?php if ($img) { ?>
+<div class="card card-teaser card-teaser-image card-flex no-after rounded shadow-sm border border-light mb-0">
+    <div class="card-image-wrapper with-read-more">
+        <div class="card-body p-3 u-grey-light">
+            <div class="category-top">
+                <span class="category title-xsmall-semi-bold fw-semibold"><?php echo esc_html($page->post_title); ?></span>
+                <?php if (is_array($arrdata) && count($arrdata)) { ?>
+                    <span class="data fw-normal">
+                        <?php echo esc_html($arrdata[0] . ' ' . $monthName . ' ' . $arrdata[2]); ?>
+                    </span>          
                 <?php } ?>
-            </ul>
-        </div>
+            </div>
+            <p class="card-title text-paragraph-medium u-grey-light"><?php echo esc_html($post->post_title); ?></p>
+            <p class="text-paragraph-card u-grey-light m-0" style="margin-bottom: 40px!important;"><?php echo esc_html($descrizione_breve); ?></p> 
+            <?php if (is_array($luogo_notizia) && count($luogo_notizia)) { ?><br><br>
+            <span class="data fw-normal">📍 
+                <?php 
+                foreach ($luogo_notizia as $luogo_id) {
+                    // Ottieni i dettagli del luogo
+                    $luogo_post = get_post($luogo_id);
+                    
+                    if ($luogo_post && !is_wp_error($luogo_post)) {
+                        // Stampa il nome del luogo come link
+                        echo '<a href="' . esc_url(get_permalink($luogo_post->ID)) . '" title="' . esc_attr($luogo_post->post_title) . '" class="card-text text-secondary text-uppercase pb-3">' . esc_html($luogo_post->post_title) . '</a> ';
+                    }
+                }
+                ?>
+            </span>
+          <?php } elseif (!empty($luogo_notizia)) { ?>
+            <span class="data fw-normal"> | 📍 
+                <?php echo esc_html($luogo_notizia); ?>
+            </span>
         <?php } ?>
-    </div>
-    <a class="read-more pt-0" href="<?php echo get_term_link(intval($argomento->term_id),'argomenti'); ?>">
-        <span class="list-item-title-icon-wrapper">
-            <span class="text">Esplora argomento</span>
+            <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">
+            <div class="card-body">Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></div>   
+            <hr style="margin-bottom: 40px; width: 200px; height: 1px; background-color: grey; border: none;">
+        </div>
+        <div class="card-image card-image-rounded pb-5">            
+            <?php dci_get_img($img); ?>
+        </div>
+
+        <a class="read-more ps-3"
+           href="<?php echo esc_url(get_permalink($post->ID)); ?>"
+           aria-label="Vai alla pagina <?php echo esc_attr($post->post_title); ?>" 
+           title="Vai alla pagina <?php echo esc_attr($post->post_title); ?>" 
+           style="display: inline-flex; align-items: center; margin-top: 30px;">
+            <span class="text">Vai alla pagina</span>
             <svg class="icon">
                 <use xlink:href="#it-arrow-right"></use>
             </svg>
-        </span>
-    </a>
+        </a>
+    </div>
 </div>
-<?php
-$sito_tematico_id = null;
+
+<?php } else { ?>
+<div class="card card-teaser no-after rounded shadow-sm mb-0 border border-light">
+    <div class="card-body pb-5">
+        <div class="category-top">
+            <span class="category title-xsmall-semi-bold fw-semibold"><?php echo esc_html($page->post_title); ?></span>
+            <?php if (is_array($arrdata) && count($arrdata)) { ?>
+                <span class="data fw-normal">
+                    <?php echo esc_html($arrdata[0] . ' ' . $monthName . ' ' . $arrdata[2]); ?>
+                </span>
+            <?php } ?>
+        </div>
+        <p class="card-title text-paragraph-medium u-grey-light"><?php echo esc_html($post->post_title); ?></p>
+        <p class="text-paragraph-card u-grey-light m-0"><?php echo esc_html($descrizione_breve); ?></p>  
+        <?php if (is_array($luogo_notizia) && count($luogo_notizia)) { ?><br><br>
+            <span class="data fw-normal">📍 
+                <?php 
+                foreach ($luogo_notizia as $luogo_id) {
+                    // Ottieni i dettagli del luogo
+                    $luogo_post = get_post($luogo_id);
+                    
+                    if ($luogo_post && !is_wp_error($luogo_post)) {
+                        // Stampa il nome del luogo come link
+                        echo '<a href="' . esc_url(get_permalink($luogo_post->ID)) . '" title="' . esc_attr($luogo_post->post_title) . '" class="card-text text-secondary text-uppercase pb-3">' . esc_html($luogo_post->post_title) . '</a> ';
+                    }
+                }
+                ?>
+            </span>
+        <?php } elseif (!empty($luogo_notizia)) { ?>
+            <span class="data fw-normal"> | 📍 
+                <?php echo esc_html($luogo_notizia); ?>
+            </span>
+        <?php } ?>
+        <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">
+        <div class="card-body">Argomenti: <?php get_template_part("template-parts/common/badges-argomenti"); ?></div>            
+        <hr style="margin-bottom: 20px; width: 200px; height: 1px; background-color: grey; border: none;">        
+        <a class="read-more" href="<?php echo esc_url(get_permalink($post->ID)); ?>" 
+           aria-label="Vai alla pagina <?php echo esc_attr($post->post_title); ?>" 
+           title="Vai alla pagina <?php echo esc_attr($post->post_title); ?>">
+            <span class="text">Vai alla pagina</span>
+            <svg class="icon ms-0">
+                <use xlink:href="#it-arrow-right"></use>
+            </svg>
+        </a>
+    </div>
+</div>
+<?php } ?>
