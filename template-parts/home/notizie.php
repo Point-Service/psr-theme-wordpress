486cb66003af8df1389840142fd07eced26a6166
Point-Service <tecnico@p-service.it>
2025-02-19 16:26:33 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -91,28 +91,31 @@ for ($i = 1; $i <= 20; $i++) {
     </div>
 </section>
 
-  <section aria-describedby="contenuti_evidenza">
-        <div class="section-content">
-            <div class="section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
-                <div class="container">
-                    <div class="row row-title pt-5 pt-lg-60 pb-3">
-                        <div class="col-12 d-lg-flex justify-content-between">
-                            <h2 id="contenuti_evidenza" class="mb-lg-0">Contenuti in evidenza</h2>
-                        </div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="card-wrapper px-0 card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
-                            <?php $count = 1;
-                            foreach ($schede as $scheda) {
-                                if ($scheda) {
-                                    get_template_part("template-parts/home/scheda-evidenza");
-                                }
-                                ++$count;
-                            } ?>
-                        </div>
-                    </div>
+<section aria-describedby="contenuti_evidenza">
+    <div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
+        <div class="container">
+            <div class="row mb-2">
+                <div class="card-wrapper px-0 card-overlapping card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                    <?php 
+                    $count = 1;
+                    foreach ($schede as $scheda) {
+                        if ($scheda) {
+                            get_template_part("template-parts/home/scheda-evidenza");
+                        }
+                        ++$count;
+                    } 
+                    ?>
                 </div>
             </div>
+              <div class="row my-4 justify-content-md-center">
+                    <a class="read-more pb-3" href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>">
+                        <button type="button" class="btn btn-outline-primary">Tutte le novità
+                            <svg class="icon">
+                                <use xlink:href="#it-arrow-right"></use>
+                            </svg>
+                        </button>
+                    </a>
+                </div>
         </div>
-    </section>
-
+    </div>
+</section>
