1ba333f18dadc03b7aebf525151c794947ec1ef2
Point-Service <tecnico@p-service.it>
2025-02-19 17:18:57 +0100

    Update hero.php

--- template-parts/hero/hero.php
+++ template-parts/hero/hero.php
@@ -9,7 +9,7 @@
 <div class="container" id="main-container">
     <div class="row justify-content-center">
         <div class="col-12 col-lg-10">
-            <?php get_template_part("template-parts/common/breadcrumb"); ?>
+            
         </div>
     </div>
 </div>
