f90e203e40537fc7e90fb97fd9a58aeef310d1f4
Point-Service <tecnico@p-service.it>
2025-02-20 11:30:04 +0100

    Update cards-list.php

--- template-parts/novita/cards-list.php
+++ template-parts/novita/cards-list.php
@@ -88,6 +88,7 @@ if ($img) {
                                     </ul>
                                 </div>
                             <?php } ?>
+                   <br>
                       <a class="read-more ps-3"
                        href="<?php echo esc_url(get_permalink($post->ID)); ?>"
                        aria-label="Vai alla pagina <?php echo esc_attr($post->post_title); ?>" 
@@ -98,7 +99,7 @@ if ($img) {
                             <use xlink:href="#it-arrow-right"></use>
                         </svg>
                     </a>
-        </div>
+                 </div>
                      </div>                  
                 </div>                 
               </div>
