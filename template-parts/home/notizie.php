0b8d25621a223e1fbfeda652153a1d9321774f0c
Point-Service <tecnico@p-service.it>
2025-02-20 10:04:03 +0100

    Update notizie.php

--- template-parts/home/notizie.php
+++ template-parts/home/notizie.php
@@ -130,17 +130,11 @@ for ($i = 1; $i <= 20; $i++) {
                             ++$count;
                         } 
                         ?>                       
-                        <!-- Pulsante "Tutte le novità" -->
-                        <div class="row my-3 justify-content-md-center">
-                        <p></p>
-                            <a class="read-more pb-2" href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>">
-                                <button type="button" class="btn btn-outline-primary">Tutte le novità
-                                    <svg class="icon">
-                                        <use xlink:href="#it-arrow-right"></use>
-                                    </svg>
-                                </button>
-                            </a>
-                        </div>                        
+                         <button type="button" class="btn btn-outline-primary">Tutte le novità
+                            <svg class="icon">
+                                <use xlink:href="#it-arrow-right"></use>
+                            </svg>
+                        </button>           
                     </div>                  
                 </div>
 
