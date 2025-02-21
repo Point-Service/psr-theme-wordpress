3db5348e766582afbc70d927edd4d36cee086ccd
Point-Service <tecnico@p-service.it>
2025-02-21 18:27:24 +0100

    Update single-unita_organizzativa.php

--- single-unita_organizzativa.php
+++ single-unita_organizzativa.php
@@ -270,8 +270,7 @@ get_header();
                             </section>
                             <?php } ?>
 										
-				<?php if ($area_riferimento && is_array($area_riferimento) && count($area_riferimento) > 0) { ?>	
-				jjj
+				<?php if ($area_riferimento && is_array($area_riferimento) && count($area_riferimento) > 0) { ?>
 				    <section class="it-page-section mb-30">
 				        <h2 class="title-xxlarge mb-3" id="costs">Area di Riferimento</h2>
 				        <div class="richtext-wrapper lora">
