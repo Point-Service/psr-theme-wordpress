6b15d9f7e1641582def1f7d7567a8ee7c0260470
Point-Service <tecnico@p-service.it>
2025-02-26 16:56:24 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -465,14 +465,16 @@ class Breadcrumb_Trail {
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
                     }
-                    else if (is_tax(array("tipi_notizia"))){
-                        $this->items[] = "<a href='".home_url("novita")."'>".__("Novità", "design_comuni_italia")."</a>";
-                        $term_name = single_term_title( '', false );
-                        $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
                     else if (is_tax(array("tipi_luogo"))){
                         $this->items[] = "<a href='".home_url("luoghi")."'>".__("luoghi", "design_comuni_italia")."</a>";
                         $term_name = single_term_title( '', false );
-                        $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");			    
+                        $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
+		    }
+			    
+                    else if (is_tax(array("tipi_notizia"))){
+                        $this->items[] = "<a href='".home_url("novita")."'>".__("Novità", "design_comuni_italia")."</a>";
+                        $term_name = single_term_title( '', false );
+                        $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");		    
                     }else if (is_tax(array("tipi_progetto"))){
                         $this->items[] = "<a href='".home_url("progetti")."'>".__("Progetti PNRR", "design_comuni_italia")."</a>";
                         $term_name = single_term_title( '', false );
