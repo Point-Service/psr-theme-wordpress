b38ecc6fbca42ea8284cba1242f988c497fa47b1
Point-Service <tecnico@p-service.it>
2025-02-26 17:47:49 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -389,7 +389,7 @@ class Breadcrumb_Trail {
 			    switch ($group_name) {
                     case 'Vivere il comune' :
                         $this->items[] =  "<a href='".home_url("vivere-il-comune")."'>".__("Vivere il Comune", "design_comuni_italia")."</a>";
-				    
+				    echo 'dd' ;
 	    		   // Ottieni i termini associati al post corrente nella tassonomia
 				$terms = get_the_terms(get_the_ID(), 'tipi-luogo');	
 					    
@@ -399,7 +399,9 @@ class Breadcrumb_Trail {
 			        
 			        // Ottieni il link del termine
 			        $term_link = get_term_link($terms[0]);
-			
+					
+			 
+					
 			        // Verifica che il link del termine non contenga errori
 			        if (!is_wp_error($term_link)) {
 			            // Aggiungi il termine come link
@@ -407,27 +409,6 @@ class Breadcrumb_Trail {
 			        }	  
 			    }	
 				    
-                        $this->items[] = get_the_title();
-                        return;
-                        break;
-		   case 'Luoghi':
-                        $this->items[] =  "<a href='".home_url("vivere-il-comune")."'>".__("vivere-il-comune", "design_comuni_italia")."</a>";
-				    // Ottieni i termini associati al post corrente nella tassonomia
-				$terms = get_the_terms(get_the_ID(), 'tipi-luogo');	
-					    
-			        if ($terms && !is_wp_error($terms)) {
-			        // Se ci sono termini, prendi il nome del primo termine
-			        $term_name = $terms[0]->name;
-			        
-			        // Ottieni il link del termine
-			        $term_link = get_term_link($terms[0]);
-			
-			        // Verifica che il link del termine non contenga errori
-			        if (!is_wp_error($term_link)) {
-			            // Aggiungi il termine come link
-			            $this->items[] = "<a href='" . esc_url($term_link) . "'>" . __(dci_get_breadcrumb_label($term_name), "design_comuni_italia") . "</a>";				
-			        }	  
-			    }	
                         $this->items[] = get_the_title();
                         return;
                         break;
