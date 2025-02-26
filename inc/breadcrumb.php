84b3faa582f029157de370047ea327eb8ed7a647
Point-Service <tecnico@p-service.it>
2025-02-26 18:34:38 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -485,8 +485,8 @@ class Breadcrumb_Trail {
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
                     }
 			    
-                    else if (is_tax(array("tipi_luogo1"))){		
-                        $this->items[] = "<a href='".home_url("vivere-il-comune")."'>".__("vivere-il-comune", "design_comuni_italia")."</a>";
+                    else if (is_tax(array("tipi_luogo"))){		
+                        $this->items[] = "<a href='".home_url("vivere-il-comune")."'>".__("vivere-il-comune", "design_comuni_italia")."</a>";			    
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
 		    }
