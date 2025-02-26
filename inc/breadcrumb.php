a29607e81d124623f8490c341a78f1d19d90a93f
Point-Service <tecnico@p-service.it>
2025-02-26 16:55:17 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -401,40 +401,39 @@ class Breadcrumb_Trail {
                         $this->items[] = get_the_title();
                         return;
                         break;
-             case 'Novità':
-		    $this->items[] = "<a href='".home_url("novita")."'>".__("Novità", "design_comuni_italia")."</a>";
-		    
-	                // Ottieni i termini associati al post corrente nella tassonomia 'tipi_notizia'
-			$terms = get_the_terms(get_the_ID(), 'tipi_notizia');
-
-
-				    
-		        if ($terms && !is_wp_error($terms)) {
-		        // Se ci sono termini, prendi il nome del primo termine
-		        $term_name = $terms[0]->name;
-		        
-		        // Ottieni il link del termine
-		        $term_link = get_term_link($terms[0]);
-		
-		        // Verifica che il link del termine non contenga errori
-		        if (!is_wp_error($term_link)) {
-		            // Aggiungi il termine come link
-		            $this->items[] = "<a href='" . esc_url($term_link) . "'>" . __(dci_get_breadcrumb_label($term_name), "design_comuni_italia") . "</a>";				
-		        }
-
-				
-		
+	             case 'Novità':
+			    $this->items[] = "<a href='".home_url("novita")."'>".__("Novità", "design_comuni_italia")."</a>";
 			    
-		    }		
-		    
-		    // Aggiungi il titolo del post
-		    $this->items[] .= get_the_title();
-		    return;
-		    break;
-			    }
-                $this->add_singular_items();
-                //console_log( $this->items);
-            }
+		                // Ottieni i termini associati al post corrente nella tassonomia 'tipi_notizia'
+				$terms = get_the_terms(get_the_ID(), 'tipi_notizia');	
+	
+					    
+			        if ($terms && !is_wp_error($terms)) {
+			        // Se ci sono termini, prendi il nome del primo termine
+			        $term_name = $terms[0]->name;
+			        
+			        // Ottieni il link del termine
+			        $term_link = get_term_link($terms[0]);
+			
+			        // Verifica che il link del termine non contenga errori
+			        if (!is_wp_error($term_link)) {
+			            // Aggiungi il termine come link
+			            $this->items[] = "<a href='" . esc_url($term_link) . "'>" . __(dci_get_breadcrumb_label($term_name), "design_comuni_italia") . "</a>";				
+			        }
+	
+					
+			
+				    
+			    }		
+			    
+			    // Aggiungi il titolo del post
+			    $this->items[] .= get_the_title();
+			    return;
+			    break;
+				    }
+	                $this->add_singular_items();
+	                //console_log( $this->items);
+	            }
 
 			// If viewing a single post.
 			elseif ( is_singular() ) {
@@ -470,10 +469,15 @@ class Breadcrumb_Trail {
                         $this->items[] = "<a href='".home_url("novita")."'>".__("Novità", "design_comuni_italia")."</a>";
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
+                    else if (is_tax(array("tipi_luogo"))){
+                        $this->items[] = "<a href='".home_url("luoghi")."'>".__("luoghi", "design_comuni_italia")."</a>";
+                        $term_name = single_term_title( '', false );
+                        $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");			    
                     }else if (is_tax(array("tipi_progetto"))){
                         $this->items[] = "<a href='".home_url("progetti")."'>".__("Progetti PNRR", "design_comuni_italia")."</a>";
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
+			    
                     }
                     else {
                         $this->add_term_archive_items();
