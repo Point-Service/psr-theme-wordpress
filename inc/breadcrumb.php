573f38f21cce93951e6a477bd0e078ba66e4aeaf
Point-Service <tecnico@p-service.it>
2025-02-27 09:20:20 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -390,8 +390,12 @@ class Breadcrumb_Trail {
 case 'Vivere il comune' :
     $this->items[] = "<a href='".home_url("vivere-il-comune")."'>".__("Vivere il Comune", "design_comuni_italia")."</a>";	
 
-    // Ottieni i termini associati al post corrente nella tassonomia 'argomenti'
-    $terms = get_the_terms(get_the_ID(), 'argomenti');
+    // Aggiungi la categoria "Luoghi" come sottocategoria di "Vivere il Comune"
+    $luoghi_link = home_url("vivere-il-comune/luoghi");
+    $this->items[] = "<a href='" . esc_url($luoghi_link) . "'>" . __("Luoghi", "design_comuni_italia") . "</a>"; 
+
+    // Ottieni i termini associati al post corrente nella tassonomia
+    $terms = get_the_terms(get_the_ID(), 'tipi_luogo');	
 
     if ($terms && !is_wp_error($terms)) {
         // Prendi il primo termine disponibile
@@ -402,12 +406,11 @@ case 'Vivere il comune' :
 
         // Verifica che il link del termine non contenga errori
         if (!is_wp_error($term_link)) {
-            // Aggiungi il termine come link (es. "Eventi", "Luoghi", "Storia")
-            $this->items[] = "<a href='" . esc_url($term_link) . "'>" . esc_html($term->name) . "</a>";
-        }
-    }
+            // Aggiungi il termine come link (es. "Chiesa")
+            $this->items[] = "<a href='" . esc_url($term_link) . "'>" . __(dci_get_breadcrumb_label($term->name), "design_comuni_italia") . "</a>";				
+        }	  
+    }        
 
-    // Aggiungi il titolo del post corrente
     $this->items[] = get_the_title();
     return;
     break;
