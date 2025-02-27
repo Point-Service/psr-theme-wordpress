49c05d3b5ef3abb9be52cfb1da3d1f6bb5d39f10
Point-Service <tecnico@p-service.it>
2025-02-27 11:03:50 +0100

    Update scheda-evidenza.php

--- template-parts/home/scheda-evidenza.php
+++ template-parts/home/scheda-evidenza.php
@@ -26,7 +26,7 @@ $page_macro = get_page_by_path($page_macro_slug);
                     </span>          
                 <?php } ?>
             </div>
-            <p class="card-title text-paragraph-medium u-grey-light"><?php echo esc_html($post->post_title); ?></p>
+            <p class="card-title text-paragraph-medium u-grey-light"><?php echo ucfirst(strtolower(esc_html($post->post_title))); ?></p>
             <p class="text-paragraph-card u-grey-light m-0" style="margin-bottom: 40px!important;"><?php echo esc_html($descrizione_breve); ?></p> 
             <?php if (is_array($luogo_notizia) && count($luogo_notizia)) { ?><br><br>
             <span class="data fw-normal">📍 
@@ -79,7 +79,7 @@ $page_macro = get_page_by_path($page_macro_slug);
                 </span>
             <?php } ?>
         </div>
-        <p class="card-title text-paragraph-medium u-grey-light"><?php echo esc_html($post->post_title); ?></p>
+        <p class="card-title text-paragraph-medium u-grey-light"><?php echo ucfirst(strtolower(esc_html($post->post_title))); ?></p>
         <p class="text-paragraph-card u-grey-light m-0"><?php echo esc_html($descrizione_breve); ?></p>  
         <?php if (is_array($luogo_notizia) && count($luogo_notizia)) { ?><br><br>
             <span class="data fw-normal">📍 
