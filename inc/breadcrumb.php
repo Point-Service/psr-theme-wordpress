0adc2b1827e527efd75a0afa30a31e8e58868748
Point-Service <tecnico@p-service.it>
2025-02-27 08:52:49 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -387,33 +387,35 @@ class Breadcrumb_Trail {
 			    $group_name = dci_get_group_name(get_post_type());
 			    //console_log($group_name);
 			    switch ($group_name) {
-                    case 'Vivere il comune' :
-                        $this->items[] =  "<a href='".home_url("vivere-il-comune")."'>".__("Vivere il Comune", "design_comuni_italia")."</a>";	
-                       echo 'd';
+case 'Vivere il comune' :
+    $this->items[] = "<a href='".home_url("vivere-il-comune")."'>".__("Vivere il Comune", "design_comuni_italia")."</a>";	
 
+    // Ottieni i termini associati al post corrente nella tassonomia
+    $terms = get_the_terms(get_the_ID(), 'tipi_luogo');	
 
-                              // Ottieni i termini associati al post corrente nella tassonomia
-				$terms = get_the_terms(get_the_ID(), 'tipi_luogo');	
-	
-					    
-			        if ($terms && !is_wp_error($terms)) {
-			        // Se ci sono termini, prendi il nome del primo termine
-			        $term_name = $terms[0]->name;
-			        
-			        // Ottieni il link del termine
-			        $term_link = get_term_link($terms[0]);
-			
-			        // Verifica che il link del termine non contenga errori
-			        if (!is_wp_error($term_link)) {
-			            // Aggiungi il termine come link
-			            $this->items[] = "<a href='" . esc_url($term_link) . "'>" . __(dci_get_breadcrumb_label($term_name), "design_comuni_italia") . "</a>";				
-			        }	  
-			    }		
-  
-				    
-                        $this->items[] = get_the_title();
-                        return;
-                        break;
+    if ($terms && !is_wp_error($terms)) {
+        // Controlla se esiste un termine "Luoghi"
+        foreach ($terms as $term) {
+            if (strtolower($term->name) == "chiesa") { 
+                // Inserisci "Luoghi" come categoria principale prima di "Chiesa"
+                $luoghi_link = get_term_link('luoghi', 'tipi_luogo');
+                if (!is_wp_error($luoghi_link)) {
+                    $this->items[] = "<a href='" . esc_url($luoghi_link) . "'>" . __("Luoghi", "design_comuni_italia") . "</a>"; 
+                }
+
+                // Inserisci il termine effettivo "Chiesa"
+                $term_link = get_term_link($term);
+                if (!is_wp_error($term_link)) {
+                    $this->items[] = "<a href='" . esc_url($term_link) . "'>" . __(dci_get_breadcrumb_label($term->name), "design_comuni_italia") . "</a>"; 
+                }
+            }
+        }
+    }        
+
+    $this->items[] = get_the_title();
+    return;
+    break;
+
                     case 'Amministrazione':
                         $this->items[] =  "<a href='".home_url("amministrazione")."'>".__("Amministrazione", "design_comuni_italia")."</a>";
                         $this->items[] = get_the_title();
