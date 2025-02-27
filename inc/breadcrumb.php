3cb35a0a4f81d31c977140ee2171c62542cf2000
Point-Service <tecnico@p-service.it>
2025-02-27 13:01:20 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -408,13 +408,14 @@ class Breadcrumb_Trail {
 				        $category_link = home_url("vivere-il-comune/luoghi");
 				        $this->items[] = "<a href='" . esc_url($category_link) . "'>" . __("Luoghi", "design_comuni_italia") . "</a>"; 
 				        $terms = get_the_terms(get_the_ID(),'categorie_servizio');
+					
+			$terms = get_the_terms(get_the_ID(),'categorie_servizio');
 					if($terms){
 					  foreach ($terms as $term) {
-						  $this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_term_link( $term, 'tipi_luogo' ) ), $term->name );
+						  $this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_term_link( $term, 'categorie_servizio' ) ), $term->name );
 					  }
 					}
-			
-				        }
+				        
 				    } elseif ($category === 'eventi') {
 				        $category_link = home_url("vivere-il-comune/eventi");
 				        $this->items[] = "<a href='" . esc_url($category_link) . "'>" . __("Eventi", "design_comuni_italia") . "</a>";
