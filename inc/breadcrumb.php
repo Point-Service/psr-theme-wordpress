d6000eac82c6e5b9659f01178e735b2bdea22a56
Point-Service <tecnico@p-service.it>
2025-02-27 08:35:21 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -390,11 +390,11 @@ class Breadcrumb_Trail {
                     case 'Vivere il comune' :
                         $this->items[] =  "<a href='".home_url("vivere-il-comune")."'>".__("Vivere il Comune", "design_comuni_italia")."</a>";	
 
-kk
+
 
                               // Ottieni i termini associati al post corrente nella tassonomia
 				$terms = get_the_terms(get_the_ID(), 'tipi_luogo');	
-	
+	echo 'd';
 					    
 			        if ($terms && !is_wp_error($terms)) {
 			        // Se ci sono termini, prendi il nome del primo termine
