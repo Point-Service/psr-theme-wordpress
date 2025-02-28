029905ef1e5809163679c24d29d3e7d286a5dc35
Point-Service <tecnico@p-service.it>
2025-02-28 15:57:15 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -404,7 +404,7 @@ class Breadcrumb_Trail {
 					        $term_name = ($term->slug === 'area') ? __("Aree Amministrative", "design_comuni_italia") : $term->name;
 					
 					        $this->items[] = sprintf(
-					            '<a href="amministrazione/aree-amministrative">%s</a>',
+					            '<a href="amministrazione/aree-amministrative">Aree Amministrative</a>',
 					            esc_url(get_term_link($term, 'tipi_unita_organizzativa')),
 					            esc_html($term_name)
 					        );
