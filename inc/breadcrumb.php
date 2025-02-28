93fa46423a75b2576ae1623463a8468edcfa6dee
Point-Service <tecnico@p-service.it>
2025-02-28 17:33:11 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -402,7 +402,7 @@ class Breadcrumb_Trail {
 				        foreach ($terms as $term) {
 						
 				            // Se trovi il termine "struttura politica", crea un link alla pagina "politici"
-				           if (esc_html($term->name) == 'Struttura Politica' || esc_html($term->name) == 'Consiglio Comunale' || esc_html($term->name) == 'giunta comunale') {
+				           if (esc_html($term->name) == 'Struttura Politica' || esc_html($term->name) == 'consiglio comunale' || esc_html($term->name) == 'giunta comunale') {
 				                // Link Politici
 				                $politici_link = home_url("amministrazione/politici");
 				                $this->items[] = "<a href='" . esc_url($politici_link) . "'>Politici</a>"; 
