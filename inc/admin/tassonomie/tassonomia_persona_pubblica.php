556f614d156a5c69f2156c95a6b43904c3246e3a
Point-Service <tecnico@p-service.it>
2025-03-03 10:46:07 +0100

    Update tassonomia_persona_pubblica.php

--- inc/admin/tassonomie/tassonomia_persona_pubblica.php
+++ inc/admin/tassonomie/tassonomia_persona_pubblica.php
@@ -1,17 +1,1 @@
-<?php
-function register_taxonomy_persona_pubblica() {
-    register_taxonomy(
-        'ruolo_persona_pubblica',
-        'persona_pubblica',
-        array(
-            'label'             => __('Ruoli Persona Pubblica', 'textdomain'),
-            'rewrite'           => array('slug' => 'ruolo-persona-pubblica'),
-            'hierarchical'      => true,
-            'show_admin_column' => true,
-            'public'            => true,
-            'show_ui'           => true
-        )
-    );
-}
-add_action('init', 'register_taxonomy_persona_pubblica');
 
