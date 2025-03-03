e9063fa4918e16b7a691faec04d34377086746af
Point-Service <tecnico@p-service.it>
2025-03-03 17:20:58 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -236,7 +236,7 @@
 						<a id="area_personale_admin" href="<?php echo get_admin_url(); ?>">Area Riservata</a>
 						
 			                      
-						
+						<?php echo dci_get_option("firma_nostra",'header'); ?>
 			  <ul class="it-footer-small-prints-list list-inline mb-0 d-flex flex-column flex-md-row" style="float: right;">
 	                            <li class="list-inline-item d-flex">
 	                                <small>  © <?php echo dci_get_option("nome_comune"); ?>
