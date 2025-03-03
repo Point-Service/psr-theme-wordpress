747562a7bc374638ea900d6116175373495c357a
Point-Service <tecnico@p-service.it>
2025-03-03 17:08:22 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -240,17 +240,18 @@
 			 <ul class="it-footer-small-prints-list list-inline mb-0 d-flex flex-column flex-md-row" style="float: right;">
 	                            <li class="list-inline-item d-flex">
 	                                <small>  © <?php echo dci_get_option("nome_comune"); ?>
-					    <?php
+					   <?php
 						// Recupera il valore dell'opzione 'firma_nostra'
 						$firma_nostra = get_option( $prefix . 'firma_nostra', 'false' );
-						// Se l'opzione è impostata su 'true', nascondi la firma
-						if ( $firma_nostra == 'true' ) : ?>
-						    <&nbsp;&nbsp;-&nbsp;&nbsp;Sviluppato da 
+						
+						// Se l'opzione è impostata su 'false', mostra la firma
+						if ( $firma_nostra == 'false' ) : ?>
+						    &nbsp;&nbsp;-&nbsp;&nbsp;Sviluppato da 
 						        <a class="text-primary" style="text-decoration:none;" target="_blank" href="https://www.p-service.it/" title="Point Service S.r.l" aria-label="Point Service S.r.l" aria-labelledby="footerCompanyLabel">
 						            <span id="footerCompanyLabel" style="color: #fff">
 						                &nbsp;Point Service S.r.l
 						            </span>
-						        </a> 
+						        </a>  						   
 						<?php endif; ?>
 				      </small>
 	                            </li>
