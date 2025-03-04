b067c045acedf31ca193faa298e635932a4230b5
Point-Service <tecnico@p-service.it>
2025-03-04 09:37:55 +0100

    Update tutto-personale.php

--- template-parts/personale-amministrativo/tutto-personale.php
+++ template-parts/personale-amministrativo/tutto-personale.php
@@ -14,7 +14,7 @@ $args = array(
     'order'          => 'ASC',
     'meta_query'     => array(
         array(
-            'key'     => '_dci_incarico', // Assicurati che sia il nome corretto del meta field
+            'key'     => '_dci_incarico_', // Assicurati che sia il nome corretto del meta field
             'value'   => 'personale_amministrativo',
             'compare' => '='
         )
