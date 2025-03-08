93322cd66aff90e11829b783ef16c7ec44197700
Point-Service <tecnico@p-service.it>
2025-03-08 09:44:42 +0100

    Update single-unita_organizzativa.php

--- single-unita_organizzativa.php
+++ single-unita_organizzativa.php
@@ -110,12 +110,19 @@ get_header();
     </div>
 </div>
 
+<div class="container" id="main-container">
+    <div class="row">
+        <div class="col-12 col-lg-10">
+            <?php get_template_part("template-parts/common/breadcrumb"); ?>
+        </div>
+    </div>
+</div>
+
 <div class="container">
     <div class="row">
         <div class="col-lg-8 px-lg-4 py-lg-2">
             <div class="cmp-heading pb-3 pb-lg-4">
                 <div class="row">
-                    <!-- Modifica la colonna per rimuovere il centramento e far partire il contenuto a sinistra -->
                     <div class="col-12">
                         <h1 class="title-xxxlarge text-start" data-element="service-title">
                             <?php the_title(); ?>
@@ -137,19 +144,21 @@ get_header();
                             </button>
                         <?php } ?>
                     </div>
-                    <!-- Colonna delle azioni a destra (rimane separata dalla parte del testo principale) -->
-                    <div class="col-lg-3 offset-lg-1 mt-5 mt-lg-0">
-                        <?php
-                            $hide_arguments = true;
-                            get_template_part('template-parts/single/actions');
-                        ?>
-                    </div>
                 </div>
             </div>
         </div>
-        <hr class="d-none d-lg-block mt-2"/>
+
+        <!-- Colonna per le azioni -->
+        <div class="col-lg-3 offset-lg-1 mt-5 mt-lg-0">
+            <?php
+                $hide_arguments = true;
+                get_template_part('template-parts/single/actions');
+            ?>
+        </div>
     </div>
+    <hr class="d-none d-lg-block mt-2"/>
 </div>
+
 
             <div class="container">
                 <div class="row row-column-menu-left mt-4 mt-lg-80 pb-lg-80 pb-40">
