32e5128f04bb6cf5e9db16c59c72a7eb2e00417d
Point-Service <tecnico@p-service.it>
2025-03-08 10:17:55 +0100

    Update card-full.php

--- template-parts/evento/card-full.php
+++ template-parts/evento/card-full.php
@@ -54,9 +54,9 @@ if ($luogo_evento_id) $luogo_evento = get_post($luogo_evento_id);
                 
                     <?php if (!empty($luogo_evento)) { ?>
                         <span class="data fw-normal"><!-- SVG per l'icona fa-map-marker-alt -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map-marker-alt" viewBox="0 0 16 16">
-            <path d="M8 0C4.686 0 2 3.582 2 7c0 4.418 6 9 6 9s6-4.582 6-9c0-3.418-2.686-7-6-7zm0 11.5s-3-3.736-3-4.5c0-1.314 1.343-2.5 3-2.5s3 1.186 3 2.5c0 .764-3 4.5-3 4.5z"/>
-        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map-marker-alt" viewBox="0 0 16 16">
+                                <path d="M8 0C4.686 0 2 3.582 2 7c0 4.418 6 9 6 9s6-4.582 6-9c0-3.418-2.686-7-6-7zm0 11.5s-3-3.736-3-4.5c0-1.314 1.343-2.5 3-2.5s3 1.186 3 2.5c0 .764-3 4.5-3 4.5z"/>
+                            </svg>
                             <?php 
                             // Ottieni i dettagli del luogo
                             $luogo_post = get_post($luogo_evento);
@@ -70,9 +70,9 @@ if ($luogo_evento_id) $luogo_evento = get_post($luogo_evento_id);
                     <?php } elseif (!empty($luogo_notizia)) { ?>
                         <span class="data fw-normal"> | 
                            <!-- SVG per l'icona fa-map-marker-alt -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map-marker-alt" viewBox="0 0 16 16">
-            <path d="M8 0C4.686 0 2 3.582 2 7c0 4.418 6 9 6 9s6-4.582 6-9c0-3.418-2.686-7-6-7zm0 11.5s-3-3.736-3-4.5c0-1.314 1.343-2.5 3-2.5s3 1.186 3 2.5c0 .764-3 4.5-3 4.5z"/>
-        </svg> <?php echo esc_html($luogo_notizia); ?>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map-marker-alt" viewBox="0 0 16 16">
+                                <path d="M8 0C4.686 0 2 3.582 2 7c0 4.418 6 9 6 9s6-4.582 6-9c0-3.418-2.686-7-6-7zm0 11.5s-3-3.736-3-4.5c0-1.314 1.343-2.5 3-2.5s3 1.186 3 2.5c0 .764-3 4.5-3 4.5z"/>
+                            </svg> <?php echo esc_html($luogo_notizia); ?>
                         </span>
                     <?php } ?>
                         <?php if ($start_timestamp && $end_timestamp && $start_date != $end_date) { ?>
@@ -83,7 +83,7 @@ if ($luogo_evento_id) $luogo_evento = get_post($luogo_evento_id);
                             </blockquote>
                       <?php } ?>
     
-                     <hr style="margin-bottom: 25px; width: 200px; height: 1px; background-color: grey; border: none;">
+                     <hr style="margin-bottom: 35px; width: 200px; height: 1px; background-color: grey; border: none;">
 
 
                 <a class="read-more t-primary text-uppercase"
