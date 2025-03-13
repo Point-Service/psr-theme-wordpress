55bf25eba9e854a169763e845eb648ff508a2edf
Point-Service <tecnico@p-service.it>
2025-03-13 16:20:48 +0100

    Update load_more.php

--- inc/load_more.php
+++ inc/load_more.php
@@ -106,15 +106,14 @@ function load_more(){
 		if ($load_card_type == "global-search"){
 			$out .= load_template_part("template-parts/search/item");  
 		}	
-		if ($load_card_type == "personale-amministrativo"){
-			$out .= load_template_part("template-parts/personale-amministrativo/cards-list");  
-		}
 		if ($load_card_type == "domanda-frequente"){
 			$out .= load_template_part("template-parts/domanda-frequente/item");  
-		}	
+		}
 		if ($load_card_type == "luogo"){
 			$out .= load_template_part("template-parts/luogo/card-full");  
-		}	 
+		}	
+ 
+ 
 		endwhile;
  
 	endif;
