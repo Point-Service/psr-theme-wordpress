a6ddb73974e084867e967bfd1f7df2befd47cb4e
Point-Service <tecnico@p-service.it>
2025-03-17 11:56:27 +0100

    Update cards-list.php

--- template-parts/documento/cards-list.php
+++ template-parts/documento/cards-list.php
@@ -80,7 +80,14 @@
                                        ?></h3>
                             </a>
                             <p class="card-text d-none d-md-block">
-                                <?php echo $description; ?>
+                           <?php                            
+                           $description1 = $description;			
+                           if (preg_match('/[A-Z]{5,}/', $description1)) {
+                               // Se sì, lo trasforma in minuscolo con la prima lettera maiuscola
+                               $description1 = ucfirst(strtolower($description1));
+                           }					
+                           // Aggiunge il titolo alla lista degli elementi
+                            echo $description1; ?>
                             </p>
                             <hr style="margin-bottom: 40px; width: 200px; height: 1px; background-color: grey; border: none;">
                             <a class="read-more ps-3"
@@ -114,7 +121,21 @@
                                 <span class="data"><?php echo $arrdata[0].' '.strtoupper($monthName).' '.$arrdata[2] ?></span>
                             </div>
                             <a class="text-decoration-none" href="<?php echo get_permalink(); ?>">
-                                <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
+                                <h3 class="h5 card-title"><?php
+                                        // Recupera il titolo della pagina
+                    					$title = get_the_title();					
+                    					// Se il titolo supera i 100 caratteri, lo tronca e aggiunge "..."
+                    					if (strlen($title) > 100) {
+                    					    $title = substr($title, 0, 97) . '...';
+                    					}					
+                    					// Controlla se il titolo contiene almeno 5 lettere maiuscole consecutive
+                    					if (preg_match('/[A-Z]{5,}/', $title)) {
+                    					    // Se sì, lo trasforma in minuscolo con la prima lettera maiuscola
+                    					    $title = ucfirst(strtolower($title));
+                    					}				
+
+                                        echo $title;
+                                       ?></h3>
                             </a>
                             <p class="card-text d-none d-md-block">
                                 <?php echo $description; ?>
