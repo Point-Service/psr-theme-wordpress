0cb1590464c12b41c2887e230fb83b371f244e8f
Point-Service <tecnico@p-service.it>
2025-03-17 16:17:58 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -735,7 +735,7 @@ class Breadcrumb_Trail {
                     }
                     else if (is_tax(array("tipi_documento"))){
 			$this->items[] =  "<a href='".home_url("amministrazione")."'>".__("Amministrazione", "design_comuni_italia")."</a>";
-                        $this->items[] = "<a href='" . home_url("amministrazione") . "/" . home_url("documenti-e-dati") . "'>" . __("Documenti e dati", "design_comuni_italia") . "</a>";
+                        $this->items[] =  "<a href='" . home_url("amministrazione/documenti-e-dati") . "'>" . __("Documenti e dati", "design_comuni_italia") . "</a>";
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
                     }		    
