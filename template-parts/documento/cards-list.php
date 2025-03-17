42b9583d1c97a874ddd5a5110d6ec886df1b0df7
Point-Service <tecnico@p-service.it>
2025-03-17 11:53:55 +0100

    Update cards-list.php

--- template-parts/documento/cards-list.php
+++ template-parts/documento/cards-list.php
@@ -63,7 +63,21 @@
                                 <span class="data"><?php echo $arrdata[0].' '.$monthName.' '.$arrdata[2] ?></span>
                             </div>
                             <a class="text-decoration-none" href="<?php echo get_permalink(); ?>">
-                                <h3 class="h5 card-title"><?php echo the_title(); ?></h3>
+                                <h3 class="h5 card-title"><?php
+                                        // Recupera il titolo della pagina
+                    					$title = get_the_title();					
+                    					// Se il titolo supera i 100 caratteri, lo tronca e aggiunge "..."
+                    					if (strlen($title) > 100) {
+                    					    $title = substr($title, 0, 97) . '...';
+                    					}					
+                    					// Controlla se il titolo contiene almeno 5 lettere maiuscole consecutive
+                    					if (preg_match('/[A-Z]{5,}/', $title)) {
+                    					    // Se sì, lo trasforma in minuscolo con la prima lettera maiuscola
+                    					    $title = ucfirst(strtolower($title));
+                    					}				
+
+                                        echo $title;
+                                       ?></h3>
                             </a>
                             <p class="card-text d-none d-md-block">
                                 <?php echo $description; ?>
