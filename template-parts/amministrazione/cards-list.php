b0642f141aaf9e116d9fb9e37480b4332054b667
Point-Service <tecnico@p-service.it>
2025-03-27 18:29:22 +0100

    Update cards-list.php

--- template-parts/amministrazione/cards-list.php
+++ template-parts/amministrazione/cards-list.php
@@ -27,7 +27,7 @@
             <?php
 
 		// Recupera il valore dell'opzione 'ck_dataset'
-		$ck_dataset = get_option( 'amministrazione_ck_dataset', 'true' ); 
+		$ck_dataset = get_option( 'ck_dataset', 'true' ); 
 
 		echo $ck_dataset_value;
             
