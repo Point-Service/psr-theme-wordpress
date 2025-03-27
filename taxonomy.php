5434286c6e66995522a1ff49efd23d91a2f1397a
Point-Service <tecnico@p-service.it>
2025-03-27 12:29:01 +0100

    Update taxonomy.php

--- taxonomy.php
+++ taxonomy.php
@@ -4,7 +4,7 @@ $obj = get_queried_object();
 
 if ($obj->taxonomy == "categorie_servizio")
 	get_template_part("archive-servizio");
-else if ( $obj->post_type=="documento_pubblico" ||  $obj->taxonomy == "tipi_documento" || $obj->taxonomy == "tipi_doc_albo_pretorio" || $obj->taxonomy == "dataset"){
+else if ( $obj->post_type=="documento_pubblico" ||  $obj->taxonomy == "tipi_documento" || $obj->taxonomy == "tipi_doc_albo_pretorio"){
 	get_template_part( "archive-documento" );
 }
 else if ( $obj->post_type=="tipi_notizia" ||  $obj->taxonomy == "tipi_notizia" || $obj->taxonomy == "tipi_notizia"){
