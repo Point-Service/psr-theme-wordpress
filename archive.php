202a57b91da96bb96e18dd80587fd3fa64cbf8ac
Point-Service <tecnico@p-service.it>
2025-03-27 16:22:21 +0100

    Update archive.php

--- archive.php
+++ archive.php
@@ -14,14 +14,9 @@ get_header();
             <div class="row">
                 <div class="col px-lg-4">
                                 <?php
-                                        $archive_title = the_archive_title();
 
-                                    if ($archive_title !== 'Dataset') {
                                        get_template_part('template-parts/common/breadcrumb');
-                                    } else {
-                                        echo 'sss';
-                                    }
-                        
+           
                                 ?>    
                 </div>
             </div>
