dcf0447a33009f43e3b1a0a50fe8864580b86b5a
Point-Service <tecnico@p-service.it>
2025-03-27 17:30:45 +0100

    Update cards-list.php

--- template-parts/amministrazione/cards-list.php
+++ template-parts/amministrazione/cards-list.php
@@ -30,7 +30,7 @@
                 <div class="card-body">
                 <a class="text-decoration-none" href="/dataset"><h3 class="card-title t-primary title-xlarge">Dataset</h3></a>
                 <p class="text-paragraph mb-0">
-                    Adempimento ANAC -  Legge 190/2012 art. 1, comma 32
+                    Adempimento ANAC<br>Legge 190/2012 art. 1, comma 32
                 </p>
                 </div>
             </div>
