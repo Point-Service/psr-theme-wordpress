936ddf5282e820ba79a368cecfea64477084044a
Point-Service <tecnico@p-service.it>
2025-03-27 11:44:44 +0100

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive
+ * The template for displaying archive with modern design
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#archive
  *
@@ -8,71 +8,92 @@
  */
 
 $class = "petrol";
-
 get_header();
 ?>
 
-    <main id="main-container" class="main-container <?php echo $class; ?>">
-		<?php get_template_part("template-parts/common/breadcrumb"); ?>
+<main id="main-container" class="main-container <?php echo $class; ?>">
+  <?php get_template_part("template-parts/common/breadcrumb"); ?>
 
-        <section class="section bg-white py-2 py-lg-3 py-xl-5">
-            <div class="container">
-                <div class="row variable-gutters">
-                    <div class="col-lg-5 col-md-8 offset-lg-3">
-                        <div class="section-title">
-							<?php the_archive_title( '<h2 class="mb-0">', '</h2>' ); ?>
-							<?php the_archive_description("<p>","</p>"); ?>
-                        </div><!-- /title-section -->
-                    </div><!-- /col-lg-5 col-md-8 offset-lg-2 -->
+  <section class="section bg-white py-2 py-lg-3 py-xl-5">
+    <div class="container">
+      <div class="row variable-gutters">
+        <div class="col-lg-8 col-md-8">
+          <div class="section-title">
+            <?php the_archive_title( '<h2 class="mb-0">', '</h2>' ); ?>
+            <?php the_archive_description("<p>","</p>"); ?>
+          </div><!-- /title-section -->
+        </div><!-- /col-lg-8 col-md-8 -->
 
-                    <div class="col-lg-3 col-md-4 offset-lg-1">
-						<?php get_template_part("template-parts/single/actions"); ?>
-                    </div><!-- /col-lg-3 col-md-4 offset-lg-1 -->
-                </div><!-- /row -->
-            </div><!-- /container -->
-        </section><!-- /section -->
+        <div class="col-lg-4 col-md-4">
+          <?php get_template_part("template-parts/single/actions"); ?>
+        </div><!-- /col-lg-4 col-md-4 -->
+      </div><!-- /row -->
+    </div><!-- /container -->
+  </section><!-- /section -->
 
-
-
-        <section class="section bg-white border-top border-bottom d-block d-lg-none">
-            <div class="container d-flex justify-content-between align-items-center py-3">
-                <h3 class="h6 text-uppercase mb-0 label-filter"><strong><?php _e("Filtri", "design_comuni_italia"); ?></strong></h3>
-                <a class="toggle-search-results-mobile toggle-menu menu-search push-body mb-0" href="#" aria-label="filtri">
-                    <svg class="svg-filters"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-filters"></use></svg>
-                </a>
+  <!-- Search bar with autocomplete -->
+  <section class="section bg-grey-card">
+    <div class="container">
+      <form role="search" id="search-form" method="get" class="search-form">
+        <div class="row">
+          <div class="col-12 pt-30 pt-lg-50 pb-lg-50">
+            <div class="cmp-input-search">
+              <div class="form-group autocomplete-wrapper mb-2 mb-lg-4">
+                <div class="input-group">
+                  <label for="autocomplete-search" class="visually-hidden">Cerca una parola chiave</label>
+                  <input type="search" 
+                         class="autocomplete form-control" 
+                         placeholder="Cerca una parola chiave"
+                         id="autocomplete-search"
+                         name="search"
+                         value="<?php echo get_search_query(); ?>"
+                         data-bs-autocomplete="[]">
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit" id="button-search">
+                      Invio
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <p id="autocomplete-label" class="mb-4"><strong><?php echo $wp_query->found_posts; ?> </strong>documenti trovati</p>
             </div>
-        </section>
-        <section class="section bg-gray-light">
-            <div class="container">
-                <div class="row variable-gutters sticky-sidebar-container">
-                    <div class="col-lg-3 bg-white bg-white-left">
-						<?php get_template_part("template-parts/search/filters", "argomento"); ?>
-                    </div>
-                    <div class="col-lg-7 offset-lg-1 pt84">
-						<?php if ( have_posts() ) : ?>
-							<?php
-							/* Start the Loop */
-							while ( have_posts() ) :
-								the_post();
-								get_template_part( 'template-parts/list/article', get_post_type() );
+          </div>
+        </div>
+      </form>
+    </div>
+  </section>
 
-							endwhile;
-							?>
-                            <nav class="pagination-wrapper justify-content-center col-12">
-								<?php echo dci_bootstrap_pagination(); ?>
-                            </nav>
-						<?php
-						else :
+  <!-- Content Section with Grid Layout -->
+  <section class="section bg-gray-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3">
+          <?php get_template_part("template-parts/search/filters", "argomento"); ?>
+        </div><!-- /col-lg-3 -->
 
-							get_template_part( 'template-parts/content', 'none' );
+        <div class="col-lg-9">
+          <div class="row g-4" id="load-more">
+            <?php
+            if ( have_posts() ) :
+              while ( have_posts() ) :
+                the_post();
+                get_template_part( 'template-parts/list/article', get_post_type() );
+              endwhile;
+            else :
+              get_template_part( 'template-parts/content', 'none' );
+            endif;
+            ?>
+          </div><!-- /row -->
 
-						endif;
-						?>
-                    </div><!-- /col-lg-8 -->
-                </div><!-- /row -->
-            </div><!-- /container -->
-        </section>
-    </main>
+          <nav class="pagination-wrapper justify-content-center col-12">
+            <?php echo dci_bootstrap_pagination(); ?>
+          </nav>
+        </div><!-- /col-lg-9 -->
+      </div><!-- /row -->
+    </div><!-- /container -->
+  </section><!-- /section -->
 
-<?php
-get_footer();
+</main><!-- /main -->
+
+<?php get_footer(); ?>
+
