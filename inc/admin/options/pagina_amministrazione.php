328234ca959d13c17edae31a87c239a58053a20f
Point-Service <tecnico@p-service.it>
2025-03-27 18:18:37 +0100

    Update pagina_amministrazione.php

--- inc/admin/options/pagina_amministrazione.php
+++ inc/admin/options/pagina_amministrazione.php
@@ -56,14 +56,15 @@ function dci_register_pagina_amministrazione_options(){
         )
     );
 
+    
         $amministrazione_options->add_field(array(
             'id'      => $prefix . 'ck_dataset',
             'name'    => __('Visualizza nella sezione Amministrazione il pulsante Dataset.', 'design_comuni_italia'),
             'desc'    => __('Dataset fornisce l\'accesso ai dati aperti pubblicati dall\'Autorità Nazionale Anticorruzione (ANAC) riguardanti i contratti pubblici in Italia. Questi dataset, disponibili in formato aperto, comprendono informazioni dettagliate sulle procedure di appalto, le stazioni appaltanti e altri elementi chiave relativi ai contratti pubblici, permettendo un\'analisi approfondita e promuovendo la trasparenza nel settore degli appalti pubblici.', 'design_comuni_italia'),
             'type'    => 'radio_inline',
-            'default' => 'false',
+            'default' => 'true', // Imposta il valore predefinito su 'Sì'
             'options' => array(
-                'true'  => __('Si', 'design_comuni_italia'),
+                'true'  => __('Sì', 'design_comuni_italia'),
                 'false' => __('No', 'design_comuni_italia'),
             ),
             'attributes' => array(
