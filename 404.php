26d1e4fff4e303066ea1a470f1bcde7bc1a03348
Point-Service <tecnico@p-service.it>
2025-03-28 16:16:50 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -37,17 +37,16 @@ get_header();
                                 <ul class="list-unstyled">
                                     <li><a href="/luoghi" class="text-decoration-none text-primary"><?php esc_html_e( 'Luoghi', 'design_comuni_italia' ); ?></a></li>
                                     <li><a href="/eventi" class="text-decoration-none text-primary"><?php esc_html_e( 'Eventi', 'design_comuni_italia' ); ?></a></li>
-                                    <li><a href="/notizie" class="text-decoration-none text-primary"><?php esc_html_e( 'Notizie', 'design_comuni_italia' ); ?></a></li>
-                                    <li><a href="/comunicati" class="text-decoration-none text-primary"><?php esc_html_e( 'Comunicati', 'design_comuni_italia' ); ?></a></li>
-                                    <li><a href="/avvisi" class="text-decoration-none text-primary"><?php esc_html_e( 'Avvisi', 'design_comuni_italia' ); ?></a></li>
+                                    <li><a href="/tipi_notizia/notizie" class="text-decoration-none text-primary"><?php esc_html_e( 'Notizie', 'design_comuni_italia' ); ?></a></li>
+                                    <li><a href="/tipi_notizia/comunicati" class="text-decoration-none text-primary"><?php esc_html_e( 'Comunicati', 'design_comuni_italia' ); ?></a></li>
+                                    <li><a href="/tipi_notizia/avvisi" class="text-decoration-none text-primary"><?php esc_html_e( 'Avvisi', 'design_comuni_italia' ); ?></a></li>
                                     <li><a href="/servizi" class="text-decoration-none text-primary"><?php esc_html_e( 'Servizi', 'design_comuni_italia' ); ?></a></li>
                                 </ul>
                             </div>
                             <!-- Fine link suggeriti -->
-
                         </div>
                     </section>
-                    <br><br><br>
+                    <br>
                 </div>
             </div>
         </div>
