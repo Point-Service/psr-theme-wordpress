cbea1e53be758f33ed03f24f468f70a0e748f637
Point-Service <tecnico@p-service.it>
2025-03-28 16:46:22 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -56,6 +56,7 @@ get_header();
         </div>
     </div>
 </main>
+<?php get_template_part("template-parts/home/ricerca"); ?>
 <?php get_template_part("template-parts/common/valuta-servizio"); ?>
 <?php
 get_footer();  
