05f46d6648c204c638e2206f6e139b4d62184d50
Point-Service <tecnico@p-service.it>
2025-03-28 16:24:42 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -44,7 +44,7 @@ get_header();
                                     <a href="/amministrazione/documenti-e-dati" class="text-decoration-none text-primary"><?php esc_html_e( 'Documenti e Dati', 'design_comuni_italia' ); ?></a>, 
                                     <a href="/amministrazione/uffici" class="text-decoration-none text-primary"><?php esc_html_e( 'Uffici', 'design_comuni_italia' ); ?></a>, 
                                     <a href="/tipi_documento/modulistica" class="text-decoration-none text-primary"><?php esc_html_e( 'Modulistica', 'design_comuni_italia' ); ?></a>, 
-                                    
+                                    <a href="/amministrazione/personale-amministrativo" class="text-decoration-none text-primary"><?php esc_html_e( 'Personale Amministrativo', 'design_comuni_italia' ); ?></a>
                                 </ul>
                             </div>
                             <!-- Fine link suggeriti -->
