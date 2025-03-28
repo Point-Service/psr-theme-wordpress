612d8b4adfba08a58d86e8c6c2763412524b63f2
Point-Service <tecnico@p-service.it>
2025-03-28 16:03:33 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -9,27 +9,35 @@
 
 get_header();
 ?>
-    <main>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12 col-lg-10">
-                    <?php get_template_part("template-parts/common/breadcrumb"); ?>
-                    <div class="cmp-hero">
-                        <section class="it-hero-wrapper bg-white align-items-start">
-                            <div class="it-hero-text-wrapper pt-0 ps-0 pb-4 pb-lg-60">
-                                  <?php //<h1 class="text-black title-xxxlarge mb-2"><?php esc_html_e( '404', 'design_comuni_italia' ); </h1> ?>
-                                <h2><?php esc_html_e( 'Pagina non trovata', 'design_comuni_italia' ); ?></h2>
-                                <p class="text-black titillium text-paragraph"><?php _e( '<b><h1 class="text-black title-xxxlarge mb-2">La pagina o categoria che stavi cercando non è stata trovata.</h1></b><br><br><br><br>Si consiglia di ritornare indietro e riprovare più tardi.<br> <a href="javascript:history.back();" title="Torna alla pagina precedente">Torna indietro</a> o utilizza il menu per continuare la navigazione.', 'design_comuni_italia' ); ?></p>
+<main>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <?php get_template_part("template-parts/common/breadcrumb"); ?>
+                <div class="cmp-hero">
+                    <section class="it-hero-wrapper bg-light align-items-start">
+                        <div class="it-hero-text-wrapper pt-0 ps-0 pb-4 pb-lg-60">
+                            <h2 class="text-center text-primary mb-4"><?php esc_html_e( 'Pagina non trovata', 'design_comuni_italia' ); ?></h2>
+                            <p class="text-center text-muted mb-5 fs-5">
+                                <?php _e( 
+                                    'Siamo spiacenti, ma non siamo riusciti a trovare la pagina o la categoria che stavi cercando. <br><br> 
+                                    Ti consigliamo di tornare indietro o esplorare il nostro sito tramite il menu. Puoi anche cliccare il link qui sotto per tornare facilmente alla pagina precedente.',
+                                    'design_comuni_italia'
+                                ); ?>
+                            </p>
+                            <div class="text-center">
+                                <a href="javascript:history.back();" title="Torna alla pagina precedente" class="btn btn-primary btn-lg">
+                                    <?php esc_html_e( 'Torna indietro', 'design_comuni_italia' ); ?>
+                                </a>
                             </div>
-                        </section>
-                    </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
-
-    </main>
+    </div>
+</main>
 
 <?php
 get_footer();
-
 
