d5606eec5e158e206f5fb4afee79cfb713705c1e
Point-Service <tecnico@p-service.it>
2025-04-02 18:01:57 +0200

    Update pagina_amministrazione.php

--- inc/admin/options/pagina_amministrazione.php
+++ inc/admin/options/pagina_amministrazione.php
@@ -72,10 +72,5 @@ function dci_register_pagina_amministrazione_options(){
             ),
         ));
 
-          $amministrazione_options->add_field(array(
-             'name'    => __('Visualizza e attiva la sezione Commissario OSL.', 'design_comuni_italia'),
-             'desc'    => __('Con questa funzione puoi attivare la parte aggiuntiva <strong><span style="color: red;">Commissario OSL</span></strong>. Si ricorda che bisogna creare un Ufficio con il nome "<strong><span style="color: red;">Commissario OSL</span></strong>" e in Novità la tipologia "<strong><span style="color: red;">Avvisi OSL</span></strong>" e in Documenti e dati creare la Tipologia "<strong><span style="color: red;">Documenti OSL</span></strong>" ', 'design_comuni_italia'),
-             ),
-         ));  
 
 }
