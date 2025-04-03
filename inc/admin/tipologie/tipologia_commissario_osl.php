cf549fad34487d71165ee9b512d4261742ae2f89
Point-Service <tecnico@p-service.it>
2025-04-03 17:26:50 +0200

    Update tipologia_commissario_osl.php

--- inc/admin/tipologie/tipologia_commissario_osl.php
+++ inc/admin/tipologie/tipologia_commissario_osl.php
@@ -42,40 +42,5 @@ function dci_Progetto_add_content_after_title($post) {
         _e('<span><i>il <b>Titolo</b> è il <b>Titolo di un documento per la sezione Commissario OSL</b>.</i></span><br><br>', 'design_comuni_italia' );
 }
 
-add_action( 'cmb2_init', 'dci_add_commissario_metaboxes' );
-function dci_add_commissario_metaboxes() {
-    $prefix = '_dci_commissario_';
-
-    //APERTURA
-    $cmb_apertura = new_cmb2_box( array(
-        'id'           => $prefix . 'box_apertura',
-        'title'        => __( 'Apertura', 'design_comuni_italia' ),
-        'object_types' => array( 'commissario' ),
-        'context'      => 'normal',
-        'priority'     => 'high',
-    ) );
-
-
-}
-
-
-/**
- * aggiungo js per controllo compilazione campi
- */
-
-add_action( 'admin_print_scripts-post-new.php', 'dci_commissario_admin_script', 11 );
-add_action( 'admin_print_scripts-post.php', 'dci_commissario_admin_script', 11 );
-
-function dci_commissario_admin_script() {
-    global $post_type;
-    if( 'commissario' == $post_type )
-        // wp_enqueue_script( 'progetto-admin-script', get_template_directory_uri() . '/inc/admin-js/progetto.js' );
-}
-
-/**
- * Valorizzo il post content in base al contenuto dei campi custom
- * @param $data
- * @return mixed
- */
 
 add_filter( 'wp_insert_post_data' , 'dci_commissario_set_post_content' , '99', 1 );
