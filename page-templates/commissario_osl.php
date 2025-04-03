1b1184399c4e465470018ff974c41553962ca402
Point-Service <tecnico@p-service.it>
2025-04-03 16:59:46 +0200

    Update commissario_osl.php

--- page-templates/commissario_osl.php
+++ page-templates/commissario_osl.php
@@ -9,25 +9,6 @@ global $post, $with_shadow;
 $search_url = esc_url( home_url( '/' ));
 
 function info(){?>
-<section class="hero-img mb-20 mb-lg-50">
-    <div class="container">
-        <div class="row">
-		<article class="commissario-osl">
-		    <h1>Commissario OSL</h1>		    
-		    <p>Il <strong>Commissario OSL (Organo Straordinario di Liquidazione)</strong> è una figura istituzionale nominata per gestire il risanamento finanziario degli enti locali in dissesto. Il suo compito principale è quello di amministrare la massa passiva dell’ente, provvedendo alla ricognizione dei debiti e alla loro estinzione secondo le normative vigenti.</p>
-
-            </p>
-                 <h3>Compiti e Funzioni</h3>
-		    <ul>
-		        <li>Verifica e certificazione della situazione economico-finanziaria dell’ente</li>
-		        <li>Gestione della liquidazione dei debiti pregressi</li>
-		        <li>Adozione di misure per il riequilibrio del bilancio</li>
-		        <li>Interazione con creditori e soggetti coinvolti nel processo di risanamento</li>
-		    </ul>
-            </ul>
-        </div>
-    </div>
-</section>
 
 
 <?php }
