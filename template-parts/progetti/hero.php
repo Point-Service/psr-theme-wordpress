878de23b575f93971ad313fbc418ee12ed7ebdea
Point-Service <tecnico@p-service.it>
2025-04-03 10:48:28 +0200

    Update hero.php

--- template-parts/progetti/hero.php
+++ template-parts/progetti/hero.php
@@ -10,7 +10,7 @@
         <div class="img-responsive">
           <div class="img-wrapper">
             <!-- <?php dci_get_img($img); ?> -->
-            <img src="assets/img/pnnr_sfo.png">
+            <img src="wp-content/themes/design-comuni-wordpress-theme/assets/img/pnnr_sfo.png">
           </div>
         </div>
       </div>
