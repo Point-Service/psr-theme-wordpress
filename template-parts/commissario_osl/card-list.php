c3d9774d0a0fe44bd16fd756327bdf07c0c12816
Point-Service <tecnico@p-service.it>
2025-04-03 17:58:46 +0200

    Update card-list.php

--- template-parts/commissario_osl/card-list.php
+++ template-parts/commissario_osl/card-list.php
@@ -1,8 +1,6 @@
 <?php 
 global $post;
 
-
-
 $description = dci_get_meta('descrizione_breve','_dci_commissario', $post->ID);
 $arrdata = dci_get_data_pubblicazione_arr("data_pubblicazione", '_dci_commissario_', $post->ID);
 $monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
