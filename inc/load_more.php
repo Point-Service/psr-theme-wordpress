5db07ceca80d7380e63e124c6185fa14336890f4
Point-Service <tecnico@p-service.it>
2025-04-04 17:22:37 +0200

    Update load_more.php

--- inc/load_more.php
+++ inc/load_more.php
@@ -110,6 +110,9 @@ function load_more(){
 		if ($load_card_type == "personale-amministrativo"){
 			$out .= load_template_part("template-parts/personale-amministrativo/cards-list");  
 		}
+		if ($load_card_type == "commissario"){
+			$out .= load_template_part("template-parts/commissario_osl/cards-list");  
+		}
 		if ($load_card_type == "domanda-frequente"){
 			$out .= load_template_part("template-parts/domanda-frequente/item");  
 		}	
