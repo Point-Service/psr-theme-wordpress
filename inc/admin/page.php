6dc3d41c612ad335001dcbcb6bc41a504e0d3307
Point-Service <tecnico@p-service.it>
2025-04-04 11:22:45 +0200

    Update page.php

--- inc/admin/page.php
+++ inc/admin/page.php
@@ -10,43 +10,42 @@ function dci_page_add_content_after_title($post) {
 }
 
 /**
-* Crea i metabox del post type page
-*/
+ * Crea i metabox del post type page
+ */
 add_action( 'cmb2_init', 'dci_add_page_metaboxes' );
 function dci_add_page_metaboxes() {
     $prefix = '_dci_page_';
 
     $cmb_descrizione = new_cmb2_box( array(
-    'id'           => $prefix . 'box_descrizione',
-    'object_types' => array( 'page' ),
-    'context'      => 'after_title',
-    'priority'     => 'high',
+        'id'           => $prefix . 'box_descrizione',
+        'object_types' => array( 'page' ),
+        'context'      => 'after_title',
+        'priority'     => 'high',
     ) );
 
     $args =  array(
         'id' => $prefix . 'descrizione',
         'name'        => __( 'Descrizione *', 'design_comuni_italia' ),
         'desc'        => __( 'Una breve descrizione compare anche nella card di presentazione della pagina', 'design_comuni_italia' ),
-        'type'             => 'textarea',
-        'attributes' => array(
-            'required' => 'required',
-            'maxlength' => 255
+        'type'        => 'textarea',
+        'attributes'  => array(
+            'required'   => 'required',
+            'maxlength'  => 255
         ),
     );
 
     /**
-     * disabilito editor body e title per le pagine del Sito dei Comuni
-     * rendo il campo descrivione_breve readonly
+     * Disabilito editor body e title per le pagine del Sito dei Comuni
+     * Rendo il campo descrizione_breve readonly
      */
     global $pagenow;
-    if (( $pagenow == 'post.php' ) || (get_post_type() == 'post')) {
+    if (( $pagenow == 'post.php' ) || ($pagenow == 'post-new.php')) {
 
-        if(isset($_GET['post']))
+        if ( isset($_GET['post']) ) {
             $curr_page_id = $_GET['post'];
-        else if(isset($_POST['post_ID']))
+        } elseif ( isset($_POST['post_ID']) ) {
             $curr_page_id = $_POST['post_ID'];
-
-        if ( ! isset( $curr_page_id ) ) {
+        } else {
             return;
         }
 
@@ -56,40 +55,47 @@ function dci_add_page_metaboxes() {
         $template_file = get_post_meta( $curr_page_id, '_wp_page_template', true );
         $template_name = basename($template_file, ".php");
 
-        //se la pagina utilizza un template del Sito dei Comuni
-        if (in_array($template_name, dci_get_pagine_template_names())) {
+        // Se la pagina utilizza un template del Sito dei Comuni
+        if ( in_array($template_name, dci_get_pagine_template_names()) ) {
+
+            // Aggiungi il controllo per non disabilitare il titolo se la pagina è già pubblicata
+            $post_status = get_post_status( $curr_page_id );
+            if ( 'publish' !== $post_status ) {
+                remove_post_type_support( 'page', 'title' );
+            }
 
             remove_post_type_support( 'page', 'editor' );
 
-            remove_post_type_support( 'page', 'title' );
-
+            // Rendo readonly il campo descrizione_breve
             $args['attributes'] = array(
                 'required' => 'required',
                 'maxlength' => 255,
                 'readonly' => true
             );
         }
-
     }
 
+    // Aggiungi il campo "Descrizione" al metabox
     $cmb_descrizione->add_field($args);
 
 }
 
 /**
- * disabilito quick edit del titolo per le pagine del Sito dei Comuni
+ * Disabilito quick edit del titolo per le pagine del Sito dei Comuni
  * @param $actions
  * @param $post
  * @return mixed
  */
 function dci_page_row_actions( $actions, $post ) {
 
-    //se la pagina ha slug tra le pagine create all'attivazione del tema
+    // Se la pagina ha slug tra le pagine create all'attivazione del tema
     if ( 'page' === $post->post_type && in_array ($post->post_name, dci_get_pagine_slugs())) {
 
-        // Removes the "Quick Edit" action.
+        // Rimuove l'azione "Quick Edit"
         unset( $actions['inline hide-if-no-js'] );
     }
     return $actions;
 }
 add_filter( 'page_row_actions', 'dci_page_row_actions', 10, 2 );
+
+?>
