cc746c08098b048e2ea1a4bef7a18db708e818ba
Point-Service <tecnico@p-service.it>
2025-04-04 17:29:11 +0200

    Update tutti.php

--- template-parts/commissario_osl/tutti.php
+++ template-parts/commissario_osl/tutti.php
@@ -1,7 +1,7 @@
 <?php
 global $the_query, $load_posts, $load_card_type;
 
-    $max_posts = isset($_GET['max_posts']) ? $_GET['max_posts'] : 6;
+    $max_posts = isset($_GET['max_posts']) ? $_GET['max_posts'] : 4;
     $load_posts = 12;
     $query = isset($_GET['search']) ? dci_removeslashes($_GET['search']) : null;
     $args = array(
