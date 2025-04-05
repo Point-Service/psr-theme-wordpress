d8b7c6f6dece3495ee8e8b97e26602b45fe472a4
Point-Service <tecnico@p-service.it>
2025-04-05 09:32:11 +0200

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -786,6 +786,16 @@ class Breadcrumb_Trail {
                     }else if (is_tax(array("tipi_progetto"))){
                        // $this->items[] = "<a href='".home_url("progetti")."'>".__("Progetti PNRR", "design_comuni_italia")."</a>";
 			$this->items[] = "<a href='index.php/".home_url("progetti")."'>".__("Progetti PNRR", "design_comuni_italia")."</a>";
+			    
+			// Ottieni i termini associati al post corrente nella tassonomia 'tipi_notizia'
+			$terms = get_the_terms(get_the_ID(), 'tipi_progetto');	    			    
+			// Ottieni l'oggetto del termine corrente
+			$term = get_queried_object();			
+			if ($term instanceof WP_Term) {
+			    $term_name = $term->name; // Nome del termine
+			    $this->items[] = dci_get_breadcrumb_label($term_name); // Senza __() perché è una variabile dinamica
+			}
+			    
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
 			    
