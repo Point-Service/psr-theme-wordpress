e7fb68949e1788c71e8b0a9e5d671a1ea2465f3d
Point-Service <tecnico@p-service.it>
2025-04-05 10:22:01 +0200

    Update cards-list.php

--- template-parts/amministrazione/cards-list.php
+++ template-parts/amministrazione/cards-list.php
@@ -1,6 +1,6 @@
- <?php
-    $pages = dci_get_children_pages('amministrazione');
-    $arr_pages = array_keys((array)$pages);
+<?php
+$pages = dci_get_children_pages('amministrazione');
+$arr_pages = array_keys((array)$pages);
 ?>
 <div class="container py-5">
     <h2 class="title-xxlarge mb-4">
@@ -9,13 +9,20 @@
     <div class="row g-4">
         <?php foreach ($arr_pages as $key => $page_name) { 
             $page = $pages[$page_name]; 
-
-	?>
+            // Ottieni la tipologia del post per ogni pagina
+            $post_type = get_post_type($page['ID']);
+	echo $post_type;
+	echo'<br>'
+	 echo esc_attr($post_type); 
+        ?>
         <div class="col-12 col-md-6 col-lg-4">
             <div class="cmp-card-simple card-wrapper pb-0 rounded border border-light">
             <div class="card shadow-sm rounded">
                 <div class="card-body">
-                <a class="text-decoration-none" href="<?php echo $page['link']; ?>" data-element="management-category-link"><h3 class="card-title t-primary title-xlarge"><?php echo $page_name; ?></h3></a>
+                <!-- Aggiungi la tipologia come classe CSS -->
+                <a class="text-decoration-none <?php echo esc_attr($post_type); ?>" href="<?php echo $page['link']; ?>" data-element="management-category-link">
+                    <h3 class="card-title t-primary title-xlarge"><?php echo $page_name; ?></h3>
+                </a>
                 <p class="text-paragraph mb-0">
                     <?php echo $page['description']; ?>
                 </p>
@@ -25,30 +32,28 @@
         </div>
         <?php } ?>
 
-            <?php              
-                $ck_dataset = dci_get_option('ck_dataset', 'Amministrazione');
-		// Verifica se l'opzione non è impostata su 'false' entra.
-		if ( 'false' !== $ck_dataset ) :
-                 ?>
-                <!-- Blocco HTML da visualizzare -->
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="cmp-card-simple card-wrapper pb-0 rounded border border-light">
-                        <div class="card shadow-sm rounded">
-                            <div class="card-body">
-                                <a class="text-decoration-none" href="/dataset">
-                                    <h3 class="card-title t-primary title-xlarge">Dataset</h3>
-                                </a>
-                                <p class="text-paragraph mb-0">
-                                    "Dataset" fornisce l'accesso ai dati aperti pubblicati dall'Autorità Nazionale Anticorruzione (ANAC) riguardanti i contratti pubblici in Italia. Questi dataset, disponibili in formato aperto, comprendono informazioni dettagliate sulle procedure di appalto, le stazioni appaltanti e altri elementi chiave relativi ai contratti pubblici, permettendo un'analisi approfondita e promuovendo la trasparenza nel settore degli appalti pubblici.
-                                </p>
-                            </div>
+        <?php              
+            $ck_dataset = dci_get_option('ck_dataset', 'Amministrazione');
+            // Verifica se l'opzione non è impostata su 'false'
+            if ( 'false' !== $ck_dataset ) :
+        ?>
+            <!-- Blocco HTML da visualizzare -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="cmp-card-simple card-wrapper pb-0 rounded border border-light">
+                    <div class="card shadow-sm rounded">
+                        <div class="card-body">
+                            <a class="text-decoration-none" href="/dataset">
+                                <h3 class="card-title t-primary title-xlarge">Dataset</h3>
+                            </a>
+                            <p class="text-paragraph mb-0">
+                                "Dataset" fornisce l'accesso ai dati aperti pubblicati dall'Autorità Nazionale Anticorruzione (ANAC) riguardanti i contratti pubblici in Italia. Questi dataset, disponibili in formato aperto, comprendono informazioni dettagliate sulle procedure di appalto, le stazioni appaltanti e altri elementi chiave relativi ai contratti pubblici, permettendo un'analisi approfondita e promuovendo la trasparenza nel settore degli appalti pubblici.
+                            </p>
                         </div>
                     </div>
                 </div>
-            <?php
-            endif;
-            ?>
-      
+            </div>
+        <?php endif; ?>
     </div>
 </div>
+
 
