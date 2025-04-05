2d6398f97a86414ddc2b2523ddab98038ea10489
Point-Service <tecnico@p-service.it>
2025-04-05 10:50:27 +0200

    Update pagina_amministrazione.php

--- inc/admin/options/pagina_amministrazione.php
+++ inc/admin/options/pagina_amministrazione.php
@@ -78,7 +78,7 @@ function dci_register_pagina_amministrazione_options(){
      $amministrazione_options->add_field( array(
          'id' => $prefix . 'ck_osl',
          'name'        => __( 'Attiva la Sezione OSL', 'design_comuni_italia' ),
-         'desc'    => __('Visualizza la Sezione OSL sulla parte amministrativa.', 'design_comuni_italia'),
+         'desc'    => __('Con questa funzione attivi la visualizzazione della Sezione OSL (Tutte le tipologie OSL) sulla parte amministrativa.', 'design_comuni_italia'),
              'type'    => 'radio_inline',
              'default' => 'false', 
              'options' => array(
@@ -90,18 +90,5 @@ function dci_register_pagina_amministrazione_options(){
              ),
      ) );
     
-     $amministrazione_options->add_field( array(
-         'id' => $prefix . 'ck_osl1',
-         'name'        => __( 'Attiva la Sezione OSL', 'design_comuni_italia' ),
-         'desc'    => __('Visualizza la Sezione OSL sulla parte amministrativa.', 'design_comuni_italia'),
-             'type'    => 'radio_inline',
-             'default' => 'false', 
-             'options' => array(
-                 'true'  => __('Sì', 'design_comuni_italia'),
-                 'false' => __('No', 'design_comuni_italia'),
-             ),
-             'attributes' => array(
-                 'data-conditional-value' => 'true',
-             ),
-     ) );
+
 }
