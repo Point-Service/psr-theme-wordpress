bbc95c395b58cec84e1e0f178fee2884dfebfb3a
Point-Service <tecnico@p-service.it>
2025-04-16 12:27:41 +0200

    Update progetto.js

--- inc/admin-js/progetto.js
+++ inc/admin-js/progetto.js
@@ -1,37 +1,33 @@
-jQuery( document ).ready(function() {
-
-    let input = jQuery('input[name^="_dci_progetto_argomenti"]');
+jQuery(document).ready(function($) {
+    
+    let input = $('input[name^="_dci_progetto_tipo_progetto"]');
     input.each(function() {
-        jQuery(this).click(function(){
-            dci_remove_highlight_missing_field('.cmb2-id--dci-progetto-argomenti');
+        $(this).click(function(){
+            dci_remove_highlight_missing_field('.cmb2-id--dci-progetto-tipo_progetto');
         });
     });
 
-    jQuery( 'form[name="post"]' ).on('submit', function(e) {
+    $('form[name="post"]').on('submit', function(e) {
 
         /**
-         * controllo compilazione campo Argomenti
+         * Controllo compilazione campo tipo progetto
          */
-        if(document.activeElement.id === 'publish' && jQuery('input[name^="_dci_progetto_argomenti"]:checked').length == 0){
-            dci_highlight_missing_field('.cmb2-id--dci-progetto-argomenti');
+        if(document.activeElement.id === 'publish' && $('input[name^="_dci_progetto_tipo_progetto"]:checked').length === 0) {
+            dci_highlight_missing_field('.cmb2-id--dci-progetto-tipo_progetto');
             return false;
         }
         return true;
     });
 });
 
-
-
 function dci_highlight_missing_field(fieldClass) {
     jQuery(fieldClass).addClass("highlighted_missing_field")
-        .append('<div id ="field-required-msg" class="field-required-msg"><em>Campo obbligatorio</em></div>')
-    ;
+        .append('<div id="field-required-msg" class="field-required-msg"><em>Campo obbligatorio</em></div>');
+    
     jQuery('html,body').animate({
         scrollTop: jQuery("#field-required-msg").parent().offset().top - 100
     }, 'slow');
-
 }
-
 
 function dci_remove_highlight_missing_field(fieldClass) {
     jQuery(fieldClass).removeClass("highlighted_missing_field");
