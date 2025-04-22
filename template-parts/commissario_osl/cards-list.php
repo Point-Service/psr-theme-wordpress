fa6f5e34231a5e7d9219dce2cd186241f3e05760
Point-Service <tecnico@p-service.it>
2025-04-22 16:45:07 +0200

    Update cards-list.php

--- template-parts/commissario_osl/cards-list.php
+++ template-parts/commissario_osl/cards-list.php
@@ -81,9 +81,7 @@ if ($img) {
                                         <use xlink:href="<?php echo $custom_icon ? $custom_icon : $default_icon; ?>" />
                                     </svg>
                             <a class="category text-decoration-none" href="<?php echo get_term_link($tipo->term_id); ?>">
-                               <h3 class="card-title t-primary title-xlarge">
                                 <?php echo strtoupper($tipo->name); ?>
-                              </h3>                
                             </a>
                     <?php } ?>
                     <span class="data"><?php echo $arrdata[0].' '.strtoupper($monthName).' '.$arrdata[2] ?></span>
@@ -125,9 +123,7 @@ if ($img) {
                                     </svg>
   
                                 <a class="category text-decoration-none" href="<?php echo get_term_link($tipo->term_id); ?>">
-                              <h3 class="card-title t-primary title-xlarge">
-                                <?php echo strtoupper($tipo->name); ?>
-                              </h3>
+                                 <?php echo strtoupper($tipo->name); ?>
                              </a>
                             <?php } ?>
                                 <span class="data"><?php echo $arrdata[0].' '.strtoupper($monthName).' '.$arrdata[2] ?></span>
