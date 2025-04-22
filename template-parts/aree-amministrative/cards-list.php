25e9d21ee95d67fc8d914a27df711e4f193caf3c
Point-Service <tecnico@p-service.it>
2025-04-22 12:15:05 +0200

    Update cards-list.php

--- template-parts/aree-amministrative/cards-list.php
+++ template-parts/aree-amministrative/cards-list.php
@@ -56,7 +56,7 @@
                         <div class="card-body card-img-none rounded-top">
                             <a class="text-decoration-none" href="<?php echo get_permalink(); ?>" data-element="administration-element">
                               <svg class="icon icon-primary icon-sm me-2" aria-hidden="true">
-                                <use href="#it-folder"></use>
+                                <use href="#it-pa"></use>
                             </svg><h3 class="h5 card-title"> <?php echo the_title(); ?></h3>
                             </a>
                             <p class="card-text d-none d-md-block">
