ed18971a4dd1c8dc298dc140f20af10b2399fb6d
Point-Service <tecnico@p-service.it>
2025-04-23 09:38:40 +0200

    Update opzioni_comune.php

--- inc/admin/options/opzioni_comune.php
+++ inc/admin/options/opzioni_comune.php
@@ -1,5 +1,6 @@
 <?php
- function dci_register_comune_options() {
+
+function dci_register_comune_options(){
     $prefix = '';
 
     /**
@@ -25,16 +26,108 @@
 
     $header_options = new_cmb2_box( $args );
 
-    // Resto delle tue opzioni di configurazione già esistenti
+    // Configurazione delle informazioni di base
     $header_options->add_field( array(
         'id' => $prefix . 'home_istruzioni',
         'name'        => __( 'Configurazione Comune', 'design_comuni_italia' ),
         'desc' => __( 'Area di configurazione delle informazioni di base' , 'design_comuni_italia' ),
         'type' => 'title',
-    ) );
+    ));
 
+    // Altri campi (quelli che hai già nel codice)
+    $header_options->add_field( array(
+        'id'    => $prefix . 'area_riservata',
+        'name'  => __('Area Riservata', 'design_comuni_italia' ),
+        'desc'  => __( 'Url per agganciare il tasto ad uno sportello esterno di autenticazione con CNS o SPID (Se aggiunto un url il tasto verrà automaticamente indirizzato al link da te inserito.)'),
+        'type'  => 'text'
+    ));   
 
-    // Aggiungi il campo per visualizzare il percorso attuale
+    $header_options->add_field( array(
+        'id'    => $prefix . 'nome_comune',
+        'name'  => __( 'Nome del Comune *', 'design_comuni_italia' ),
+        'desc'  => __( 'Il Nome del Comune' , 'design_comuni_italia' ),
+        'type'  => 'text',
+        'attributes' => array(
+            'required' => 'required'
+        ),
+    ));
+
+    $header_options->add_field( array(
+        'id'    => $prefix . 'nome_regione',
+        'name'  => __( 'Nome Regione *', 'design_comuni_italia' ),
+        'desc'  => __( 'La Regione di appartenenza del Comune' , 'design_comuni_italia' ),
+        'type'  => 'text',
+        'attributes' => array(
+            'required' => 'required'
+        ),
+    ));
+
+    $header_options->add_field( array(
+        'id'    => $prefix . 'url_sito_regione',
+        'name'  => __( 'Sito Regione', 'design_comuni_italia' ),
+        'desc'  => __( 'Link al sito della Regione di Appartenenza' , 'design_comuni_italia' ),
+        'type'  => 'text_url',
+        'attributes' => array(
+            'required' => 'required'
+        ),
+    ));
+
+    $header_options->add_field( array(
+        'id'    => $prefix . 'motto_comune',
+        'name'  => __( 'Motto del Comune ', 'design_comuni_italia' ),
+        'desc'  => __( 'Il Motto del Comune, viene visualizzato sotto il nome del Comune' , 'design_comuni_italia' ),
+        'type'  => 'text',
+    ));
+
+    $header_options->add_field( array(
+        'id'    => $prefix . 'stemma_comune',
+        'name'  => __('Stemma', 'design_comuni_italia' ),
+        'desc'  => __( 'Lo stemma del Comune. Si raccomanda di caricare un\'immagine in formato svg' , 'design_comuni_italia' ),
+        'type'  => 'file',
+        'query_args' => array(
+            'type' => array(
+                'image/svg',
+            )
+        )
+    ));
+
+    $header_options->add_field( array(
+        'id'    => $prefix . 'stemma_comune_mobile',
+        'name'  => __('Stemma per mobile', 'design_comuni_italia' ),
+        'desc'  => __( 'Utilizzare questo campo per caricare un\'immagine alternativa dello stemma del Comune visibile dal menu hamburger (mobile). Si raccomanda di caricare un\'immagine in formato svg' , 'design_comuni_italia' ),
+        'type'  => 'file',
+        'query_args' => array(
+            'type' => array(
+                'image/svg',
+            )
+        )
+    ));
+
+    // Altri campi per i link
+    $header_options->add_field( array(
+        'id'    => $prefix . 'link_albopretorio',
+        'name'  => __('Link Albo pretorio', 'design_comuni_italia' ),
+        'desc'  => __( 'Utilizzare questo campo per inserire il link esterno ad albo pretorio (Lasciare vuoto se interno)'),
+        'type'  => 'text_url'
+    ));
+
+    $header_options->add_field( array(
+        'id'    => $prefix . 'link_ammtrasparente',
+        'name'  => __('Link Amministrazione Trasparente', 'design_comuni_italia' ),
+        'desc'  => __( 'Utilizzare questo campo per inserire il link esterno ad amministrazione trasparente (Lasciare vuoto se interna)'),
+        'type'  => 'text_url'
+    ));
+
+    $header_options->add_field( array(
+        'id'    => $prefix . 'link_pagopa',
+        'name'  => __('Link PagoPA', 'design_comuni_italia' ),
+        'desc'  => __( 'Utilizzare questo campo per inserire il link esterno a pagoPA (Lasciare vuoto se interna)'),
+        'type'  => 'text_url'
+    ));
+
+    // ... Altri campi simili
+
+    // Aggiungi il campo per visualizzare il percorso attuale del template
     $header_options->add_field( array(
         'id'    => $prefix . 'percorso_template',
         'name'  => __( 'Percorso Template Attuale', 'design_comuni_italia' ),
@@ -44,37 +137,32 @@
         'attributes' => array(
             'readonly' => 'readonly', // Rendi il campo di sola lettura
         ),
-    ) );
+    ));
 
-    // Aggiungi un campo per un pulsante che aggiorna il percorso
+    // Continuazione degli altri campi che hai nel codice precedente...
+
     $header_options->add_field( array(
-        'id'   => $prefix . 'aggiorna_percorso_button',
-        'name' => __('Aggiorna Percorso', 'design_comuni_italia'),
-        'desc' => __('Clicca per aggiornare il percorso del template', 'design_comuni_italia'),
-        'type' => 'button',
-        'attributes' => array(
-            'onclick' => 'updateTemplatePath();', // Funzione JavaScript da richiamare
+        'id'    => $prefix . 'dichiarazioneaccessibilita',
+        'name'  => __( 'Dichiarazione Accessibilità', 'design_comuni_italia' ),
+        'desc'  => __( 'Inserisci qui il link della dichiarazione di accessibilità', 'design_comuni_italia' ),
+        'type'  => 'text'
+    ));
+
+    $header_options->add_field(array(
+        'id'    => $prefix . 'firma_nostra',
+        'name'  => __('Nascondi la nostra firma.', 'design_comuni_italia'),
+        'desc'  => __('Opzione per nascondere dal footer la nostra firma.', 'design_comuni_italia'),
+        'type'  => 'radio_inline',
+        'default' => 'false',
+        'options' => array(
+            'true' => __('Si', 'design_comuni_italia'),
+            'false' => __('No', 'design_comuni_italia'),
         ),
-    ) );
-
-    // Funzione JavaScript da utilizzare per l'aggiornamento del percorso
-    add_action( 'admin_footer', function() {
-        ?>
-        <script type="text/javascript">
-            function updateTemplatePath() {
-                // Aggiungi qui il percorso aggiornato
-                var nuovoPercorso = '<?php echo get_template_directory_uri(); ?>';
-
-                // Se il campo esiste, aggiornalo
-                var campoPercorso = document.querySelector('[name="dci_options[percorso_template]"]');
-                if (campoPercorso) {
-                    campoPercorso.value = nuovoPercorso;
-                }
-
-                alert('Percorso template aggiornato a: ' + nuovoPercorso);
-            }
-        </script>
-        <?php
-    });
+        'attributes' => array(
+            'data-conditional-value' => "false",
+        ),
+    ));
+  
 }
 add_action('cmb2_admin_init', 'dci_register_comune_options');
+
