d07e99d54f1ed12604b849014905e2950cf048ca
Point-Service <tecnico@p-service.it>
2025-04-30 10:09:04 +0200

    Update taxonomy-categorie_servizio.php

--- taxonomy-categorie_servizio.php
+++ taxonomy-categorie_servizio.php
@@ -32,27 +32,22 @@ $tax_query = array(
     'terms' => $obj->slug
   )
 );
-
-$amministrazione = dci_get_related_unita_amministrative();
-$bandi = dci_get_related_bandi();
-
 get_header();
 ?>
  <main>
-    <?php 
+ <?php 
       $title = $obj->name;
       $description = $obj->description;
       $data_element = 'data-element="page-name"';
       get_template_part("template-parts/hero/hero"); 
     ?>
-  
-    <div class="bg-grey-card">
+  <div class="bg-grey-card">
       <form role="search" id="search-form" method="get" class="search-form">
           <button type="submit" class="d-none"></button>
           <div class="container">
             <div class="row ">
-              <h2 class="visually-hidden">Esplora tutti i servizi</h2>
-              <div class="col-12 col-lg-8 pt-30 pt-lg-50 pb-lg-50">
+              <!-- <h2 class="visually-hidden">Esplora i servizi</h2> -->
+              <div class="col-12 pt-30 pt-lg-50 pb-lg-50">
                 <div class="cmp-input-search">
                   <div class="form-group autocomplete-wrapper mb-2 mb-lg-4">
                   <div class="input-group">
@@ -74,18 +69,16 @@ get_header();
                   </span>
                   </div>
                   </div>
-                     <?php     
-                        if (strlen(dci_get_option('servizi_maggioli_url', 'servizi')) < 5) { ?>
-                            <p id="autocomplete-label" class="mb-4"><strong><?php echo $the_query->found_posts; ?> </strong>servizi trovati in ordine alfabetico</p>
-                     <?php } ?>    
-
+                  <p id="autocomplete-label" class="mb-4"><strong><?php echo $the_query->found_posts; ?> </strong>documenti trovati in ordine alfabetico</p>
                 </div>
                 <div class="row g-4" id="load-more">
-                    <?php foreach ($servizi as $servizio) { 
+                <?php foreach ($servizi as $servizio) { 
                         $load_card_type = "categoria_servizio";
-                        $hide_categories = true;
-                        get_template_part("template-parts/servizio/card");    
-                    } ?>
+                        $hide_categories = false;?>
+                        <div class="col-12 col-sm-6 col-lg-4"> 
+                        <?php get_template_part("template-parts/servizio/card");  ?>
+                        </div>  
+                    <?php   } ?>
 
                      <?php     
                         if (strlen(dci_get_option('servizi_maggioli_url', 'servizi')) >5 ) { ?>
@@ -95,62 +88,14 @@ get_header();
                  </div>
                        <?php get_template_part("template-parts/search/more-results"); ?>
               </div>
-              
-              <?php if ( is_array($amministrazione) && count($amministrazione) ) { ?>
-                <div class="col-12 col-lg-4 pt-50 pb-30 pt-lg-5 ps-lg-5">
-                  <div class="link-list-wrap">
-                    <h2 class="title-xsmall-semi-bold"><span>UFFICI</span></h2>
-                    <ul class="link-list t-primary">
-                      <?php foreach ($amministrazione as $item) { ?>
-                        <li class="mb-3 mt-3">
-                          <a class="list-item ps-0 title-medium underline" href="<?php echo $item['link']; ?>">
-                            <span><?php echo $item['title']; ?></span>
-                          </a>
-                        </li>
-                      <?php } ?>                      
-                      <li>
-                        <a class="list-item ps-0 text-button-xs-bold d-flex align-items-center text-decoration-none" href="<?php echo get_permalink( get_page_by_path( 'amministrazione' ) ); ?>">
-                          <span class="mr-10">VAI ALL’AREA AMMINISTRATIVA</span>
-                          <svg class="icon icon-xs icon-primary">
-                            <use href="#it-arrow-right"></use>
-                          </svg>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              <?php } ?>
             </div>
           </div>
       </form>
     </div>
-           
-    <?php if ( is_array($bandi) && count($bandi) ) { ?>
-      <div class="container">
-        <h2 class="title-xxlarge mt-40 mb-40 mt-lg-60 mb-lg-40">Bandi</h2>
-        <div class="row flex-wrap justify-content-between gy-4 gy-lg-5 gx-lg-5 pb-3 pb-lg-60 align-items-stretch">
-          <div class="col-12 col-lg-4">
-            <?php foreach ($bandi as $item) { ?>
-              <div class="cmp-card-simple card-wrapper pb-0">
-                <div class="card shadow rounded">
-                  <div class="card-body">
-                    <a class="text-decoration-none" href="<?php echo $item['link']; ?>">
-                      <h3 class="card-title t-primary title-xlarge">
-                        <?php echo $item['titolo']; ?>
-                      </h3>
-                    </a>
-                    <p class="titillium text-paragraph "><?php echo $item['description']; ?></p>
-                  </div>
-                </div>
-              </div>
-            <?php }?>
-          </div>
-        </div>
-      </div>  
-    <?php } ?>
-    
-    <?php echo get_template_part( 'template-parts/common/valuta-servizio'); ?>
+    <?= get_template_part("template-parts/servizio/uffici");?>
+</main>
+  <!-- <?php echo get_template_part( 'template-parts/servizio/categorie'); ?> -->
+  <?php echo get_template_part( 'template-parts/common/valuta-servizio'); ?>
     <?php echo get_template_part( 'template-parts/common/assistenza-contatti'); ?>
-  </main>
-<?php
+<?php 
 get_footer();
