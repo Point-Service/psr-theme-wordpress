52a1bd92ce3be7a918e08e28682faeb1b17dfae4
Point-Service <tecnico@p-service.it>
2025-05-03 11:06:36 +0200

    Update carosello.php

--- template-parts/home/carosello.php
+++ template-parts/home/carosello.php
@@ -1,40 +1,229 @@
-   <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<style id='safe-svg-svg-icon-style-inline-css' type='text/css'>
+.safe-svg-cover{text-align:center}.safe-svg-cover .safe-svg-inside{display:inline-block;max-width:100%}.safe-svg-cover svg{height:100%;max-height:100%;max-width:100%;width:100%}
+</style>
+
+<link rel='stylesheet' id='bootstrap-italia-custom-css' href='https://www.comune.lucca.it/app/themes/net7-comuni/dist/css/bootstrapItaliaCss.css?ver=6.7.2' type='text/css' media='all' />
+<link rel="preload" href="https://www.comune.lucca.it/app/themes/design-comuni-wordpress-theme/assets/css/comuni.css?ver=6.7.2" as="style" onload="this.onload=null;this.rel='stylesheet'"><noscript><link rel="stylesheet" href="https://www.comune.lucca.it/app/themes/design-comuni-wordpress-theme/assets/css/comuni.css?ver=6.7.2"></noscript><link rel='stylesheet' id='dci-print-style-css' href='https://www.comune.lucca.it/app/themes/design-comuni-wordpress-theme/print.css?ver=20190912' type='text/css' media='print' />
+<link rel='stylesheet' id='net7-comuni-style-css' href='https://www.comune.lucca.it/app/themes/net7-comuni/style.css?ver=6.7.2' type='text/css' media='all' />
+
+<style type="text/css" id="wp-custom-css">
+			
+.it-header-center-wrapper, .it-header-navbar-wrapper {
+	background:#F6F6F6
+}
+.it-header-slim-wrapper {
+	background:#AB040A
+}
+.navbar {
+	background:transparent
+}
+.cmp-breadcrumbs .breadcrumb-container .breadcrumb .breadcrumb-item a, .t-primary {
+	color:#BE040B
+}
+
+.custom-navbar-toggler svg {
+	fill:#BE040B!important
+}
+.it-calendar-wrapper .it-header-block-title, .btn-primary, .bg-primary {
+	background-color:#BE040B!important
+}
+.it-header-center-wrapper .it-header-center-content-wrapper .it-brand-wrapper a, .navbar .navbar-collapsable .navbar-nav li a.nav-link {
+	color:#191919
+}
+.navbar-nav li a.nav-link.active {border-color:#BE040B!important}		</style>
+
+  <main id="main-container" class="main-container redbrown">
+
+        <section id="head-section">
     
-    <!-- Aggiungi uno stile di base se necessario -->
-    <style>
-        .carousel-item img {
-            max-height: 500px; /* Impostazione opzionale per la massima altezza delle immagini */
-            object-fit: cover;
-        }
-    </style>
-<div id="caroselloHome" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#caroselloHome" data-slide-to="0" class="active"></li>
-    <li data-target="#caroselloHome" data-slide-to="1"></li>
-    <li data-target="#caroselloHome" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://www.servizipa.cloud/comuni/img/comune/album_pd/3075_142.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://www.servizipa.cloud/comuni/img/comune/album_pd/3075_142.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://www.servizipa.cloud/comuni/img/comune/album_pd/3075_142.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#caroselloHome" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#caroselloHome" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+                            <div class="it-carousel-wrapper it-carousel-landscape-abstract splide" data-bs-carousel-splide data-splide='{"type":"loop", "autoplay":true, "interval":5000, "arrows": true, "breakpoints": {"768": {"arrows": false}}}'>
+	<div class="splide__track">
+		<ul class="splide__list">
+							<li class="splide__slide">
+					<div class="it-single-slide-wrapper">
+
+						<div class="it-text-slider-wrapper-outside">
+							<div class="card-wrapper">
+								<div class="card">
+									<div class="card-body">
+										<div class="category-top">
+											<svg class="icon icon-sm" aria-hidden="true">
+												<use xlink:href="#it-calendar"></use>
+											</svg>
+											<span class="title-xsmall-semi-bold fw-semibold">notizia</span>
+																							<span class="data">08 aprile 2025</span>
+																					</div>
+										<h3 class="card-title big-heading">
+											<a href="https://www.comune.lucca.it/notizia/a-s-2025-2026-iscrizione-al-servizio-di-trasporto-scolastico-e-domanda-di-agevolazione-esenzione-tariffaria-per-il-servizio-di-ristorazione-e-trasporto-scolastico/" class="text-decoration-none">
+												A.S. 2025-2026: Iscrizione al servizio di trasporto scolastico e domanda di agevolazione/esenzione tariffaria per il servizio di ristorazione e trasporto scolastico											</a>
+										</h3>
+										<p class="mb-4 subtitle-small pt-3 lora">
+											 Domande di iscrizione al trasporto scolastico a.s. 2025-2026 
+										</p>
+										
+<ul class="d-flex flex-wrap gap-1">
+            <li>
+            <a class="chip chip-simple"
+            href="https://www.comune.lucca.it/argomento/istruzione/"
+            >
+                <span class="chip-label"> 
+                    Istruzione                </span>
+            </a>
+        </li>
+    </ul>
+
+										<a class="read-more pb-3 mt-4 float-none d-block" href="https://www.comune.lucca.it/notizia/a-s-2025-2026-iscrizione-al-servizio-di-trasporto-scolastico-e-domanda-di-agevolazione-esenzione-tariffaria-per-il-servizio-di-ristorazione-e-trasporto-scolastico/">
+											<span class="text">Vai alla novità</span>
+											<svg class="icon">
+												<use xlink:href="#it-arrow-right"></use>
+											</svg>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<a href="https://www.comune.lucca.it/notizia/a-s-2025-2026-iscrizione-al-servizio-di-trasporto-scolastico-e-domanda-di-agevolazione-esenzione-tariffaria-per-il-servizio-di-ristorazione-e-trasporto-scolastico/">
+							<div class="">
+								<div class="">
+									<div class="">
+										<img width="491" height="371" src="https://www.comune.lucca.it/app/uploads/2024/03/19-scuolabus.jpg" class="attachment-slide size-slide" alt="" loading="lazy" decoding="async" srcset="https://www.comune.lucca.it/app/uploads/2024/03/19-scuolabus.jpg 491w, https://www.comune.lucca.it/app/uploads/2024/03/19-scuolabus-300x227.jpg 300w, https://www.comune.lucca.it/app/uploads/2024/03/19-scuolabus-331x250.jpg 331w" sizes="auto, (max-width: 491px) 100vw, 491px" />									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</li>
+							<li class="splide__slide">
+					<div class="it-single-slide-wrapper">
+
+						<div class="it-text-slider-wrapper-outside">
+							<div class="card-wrapper">
+								<div class="card">
+									<div class="card-body">
+										<div class="category-top">
+											<svg class="icon icon-sm" aria-hidden="true">
+												<use xlink:href="#it-calendar"></use>
+											</svg>
+											<span class="title-xsmall-semi-bold fw-semibold">notizia</span>
+																							<span class="data">23 aprile 2025</span>
+																					</div>
+										<h3 class="card-title big-heading">
+											<a href="https://www.comune.lucca.it/notizia/graduatorie-provvisorie-dei-nidi-dinfanzia-e-spazi-gioco-sez-medi-e-grandi/" class="text-decoration-none">
+												Graduatorie provvisorie dei nidi d'infanzia e spazi gioco (sez. medi e grandi)											</a>
+										</h3>
+										<p class="mb-4 subtitle-small pt-3 lora">
+											Graduatorie provvisorie per l’Anno Educativo 2025-2026										</p>
+										
+<ul class="d-flex flex-wrap gap-1">
+            <li>
+            <a class="chip chip-simple"
+            href="https://www.comune.lucca.it/argomento/istruzione/"
+            >
+                <span class="chip-label"> 
+                    Istruzione                </span>
+            </a>
+        </li>
+    </ul>
+
+										<a class="read-more pb-3 mt-4 float-none d-block" href="https://www.comune.lucca.it/notizia/graduatorie-provvisorie-dei-nidi-dinfanzia-e-spazi-gioco-sez-medi-e-grandi/">
+											<span class="text">Vai alla novità</span>
+											<svg class="icon">
+												<use xlink:href="#it-arrow-right"></use>
+											</svg>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<a href="https://www.comune.lucca.it/notizia/graduatorie-provvisorie-dei-nidi-dinfanzia-e-spazi-gioco-sez-medi-e-grandi/">
+							<div class="">
+								<div class="">
+									<div class="">
+										<img width="600" height="400" src="https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-600x400.jpg" class="attachment-slide size-slide" alt="nidi infanzia iscrizioni" loading="lazy" decoding="async" srcset="https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-600x400.jpg 600w, https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-300x200.jpg 300w, https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-1024x682.jpg 1024w, https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-768x512.jpg 768w, https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-730x485.jpg 730w, https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-375x250.jpg 375w, https://www.comune.lucca.it/app/uploads/2025/02/nidi-infanzia-e1740652632715.jpg 900w" sizes="auto, (max-width: 600px) 100vw, 600px" />									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</li>
+							<li class="splide__slide">
+					<div class="it-single-slide-wrapper">
+
+						<div class="it-text-slider-wrapper-outside">
+							<div class="card-wrapper">
+								<div class="card">
+									<div class="card-body">
+										<div class="category-top">
+											<svg class="icon icon-sm" aria-hidden="true">
+												<use xlink:href="#it-calendar"></use>
+											</svg>
+											<span class="title-xsmall-semi-bold fw-semibold">notizia</span>
+																							<span class="data">26 luglio 2023</span>
+																					</div>
+										<h3 class="card-title big-heading">
+											<a href="https://www.comune.lucca.it/notizia/cosa_fa_il_comune_e_con_quali_risorse/" class="text-decoration-none">
+												Cosa fa il Comune e con quali risorse											</a>
+										</h3>
+										<p class="mb-4 subtitle-small pt-3 lora">
+											Una lettura agile del Dup e del Bilancio 										</p>
+										
+<ul class="d-flex flex-wrap gap-1">
+            <li>
+            <a class="chip chip-simple"
+            href="https://www.comune.lucca.it/argomento/bilancio/"
+            >
+                <span class="chip-label"> 
+                    Bilancio                </span>
+            </a>
+        </li>
+            <li>
+            <a class="chip chip-simple"
+            href="https://www.comune.lucca.it/argomento/comunicazione-istituzionale/"
+            >
+                <span class="chip-label"> 
+                    Comunicazione istituzionale                </span>
+            </a>
+        </li>
+            <li>
+            <a class="chip chip-simple"
+            href="https://www.comune.lucca.it/argomento/trasparenza-amministrativa/"
+            >
+                <span class="chip-label"> 
+                    Trasparenza amministrativa                </span>
+            </a>
+        </li>
+    </ul>
+
+										<a class="read-more pb-3 mt-4 float-none d-block" href="https://www.comune.lucca.it/notizia/cosa_fa_il_comune_e_con_quali_risorse/">
+											<span class="text">Vai alla novità</span>
+											<svg class="icon">
+												<use xlink:href="#it-arrow-right"></use>
+											</svg>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<a href="https://www.comune.lucca.it/notizia/cosa_fa_il_comune_e_con_quali_risorse/">
+							<div class="">
+								<div class="">
+									<div class="">
+										<img width="600" height="398" src="https://www.comune.lucca.it/app/uploads/2023/07/LUCCA-ALTO-004_rid-600x398.jpg" class="attachment-slide size-slide" alt="Lucca dall&#039;alto" loading="lazy" decoding="async" srcset="https://www.comune.lucca.it/app/uploads/2023/07/LUCCA-ALTO-004_rid-600x398.jpg 600w, https://www.comune.lucca.it/app/uploads/2023/07/LUCCA-ALTO-004_rid-300x199.jpg 300w, https://www.comune.lucca.it/app/uploads/2023/07/LUCCA-ALTO-004_rid-768x510.jpg 768w, https://www.comune.lucca.it/app/uploads/2023/07/LUCCA-ALTO-004_rid-730x485.jpg 730w, https://www.comune.lucca.it/app/uploads/2023/07/LUCCA-ALTO-004_rid-377x250.jpg 377w, https://www.comune.lucca.it/app/uploads/2023/07/LUCCA-ALTO-004_rid.jpg 1000w" sizes="auto, (max-width: 600px) 100vw, 600px" />									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</li>
+					</ul>
+	</div>
+	<div class="splide__arrows">
+		<button class="splide__arrow splide__arrow--prev">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" width="40" height="40">
+				<path d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path>
+			</svg> </button>
+		<button class="splide__arrow splide__arrow--next">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" width="40" height="40">
+				<path d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path>
+			</svg> </button>
+	</div>
 </div>
-<!-- Bootstrap JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script type="text/javascript" src="https://www.comune.lucca.it/app/themes/net7-comuni/dist/js/bootstrapItaliaJs.bundle.js?ver=6.7.2" id="nc-bootstrap-italia-js-js"></script>
