27977cb8cc08dfc324d4ff208adc64e1abfad8fb
Point-Service <tecnico@p-service.it>
2025-05-03 11:03:06 +0200

    Update carosello.php

--- template-parts/home/carosello.php
+++ template-parts/home/carosello.php
@@ -1,10 +1,8 @@
-
-
 <div id="caroselloHome" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#caroselloControlli" data-slide-to="0" class="active"></li>
-    <li data-target="#caroselloControlli" data-slide-to="1"></li>
-    <li data-target="#caroselloControlli" data-slide-to="2"></li>
+    <li data-target="#caroselloHome" data-slide-to="0" class="active"></li>
+    <li data-target="#caroselloHome" data-slide-to="1"></li>
+    <li data-target="#caroselloHome" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -17,11 +15,11 @@
       <img class="d-block w-100" src="https://www.servizipa.cloud/comuni/img/comune/album_pd/3075_142.jpg" alt="Third slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#caroselloControlli" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#caroselloHome" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#caroselloControlli" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#caroselloHome" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
