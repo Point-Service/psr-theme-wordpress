8add1f9a4f796a878648e762457a704c7da48762
Point-Service <tecnico@p-service.it>
2025-05-03 12:27:42 +0200

    Update cards-list.php

--- template-parts/novita/cards-list.php
+++ template-parts/novita/cards-list.php
@@ -15,6 +15,8 @@ if ($tipo_terms && !is_wp_error($tipo_terms)) {
 } else {
     $tipo = null;
 }
+
+
 if ($img) {
 ?>
     <div class="col-12 col-md-6 col-xl-4">
@@ -27,24 +29,7 @@ if ($img) {
                 <div class="col-12 order-1 order-md-2">
                 <div class="card-body">
                     <div class="category-top cmp-list-card-img__body">
-                    <?php if ($tipo){
-                        if($tipo->name=="Avvisi"){?>
-                                    <svg class="icon">
-                                        <use xlink:href="#it-info-circle"/>
-                                    </svg>
-                                <?php } elseif($tipo->name=="Comunicati"){ ?>
-                                    <svg class="icon">
-                                        <use xlink:href="#it-horn"/>
-                                    </svg>
-                                <?php } elseif($tipo->name=="Notizie"){ ?>
-                                     <svg class="icon">
-                                     <use xlink:href="#it-copy"/>
-                                 </svg>
-                                <?php } else{?>
-                                    <svg class="icon">
-                                        <use xlink:href="#it-note"/>
-                                    </svg>
-                                <?php }?>      
+
                                 <a class="category text-decoration-none" href="<?php echo get_term_link($tipo->term_id); ?>">
                                  <?php echo strtoupper($tipo->name); ?>
                              </a>
