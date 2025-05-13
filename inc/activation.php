b9665b0d8af9a2b2552100f7793e8e1952ac29b2
Point-Service <tecnico@p-service.it>
2025-05-13 12:21:29 +0200

    Update activation.php

--- inc/activation.php
+++ inc/activation.php
@@ -353,8 +353,14 @@ function createMenu()
 
 
 
-
-
+    //voci menu Argomenti (in alto a destra)
+    dci_create_term_menu_item('Agricoltura','argomenti',$menu_argomenti); //voce tassonomia argomenti come placeholder
+    dci_create_term_menu_item('Tempo libero','argomenti',$menu_argomenti); //voce tassonomia argomenti come placeholder
+    dci_create_term_menu_item('Istruzione','argomenti',$menu_argomenti); //voce tassonomia argomenti come placeholder
+    dci_create_page_menu_item(__('Argomenti', 'design_comuni_italia'), $menu_argomenti, __('Tutti gli argomenti','design_comuni_italia'));
+    //assegna menu a posizione topright
+    dci_add_menu_to_location($menu_argomenti,'menu-header-right');
+    
 
     
     //Main menu
@@ -377,8 +383,7 @@ function createMenu()
     dci_add_menu_to_location($menu_amministrazione,'menu-footer-col-1');
 
 
-  //voci menu Vivere il Comune
-    //placeholder
+    //voci menu Vivere il Comune
     dci_create_custom_menu_item(__( 'Luoghi', 'design_comuni_italia'),$menu_vivere_comune, '/vivere-il-comune');
     dci_create_custom_menu_item(__( 'Eventi', 'design_comuni_italia'),$menu_vivere_comune, '/vivere-il-comune');
     //PAGINE DI SECONDO LIVELLO
