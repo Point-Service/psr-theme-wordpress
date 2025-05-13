c87261d6db48d6c17bb12709ce2ad823263c0485
Point-Service <tecnico@p-service.it>
2025-05-13 09:30:40 +0200

    Update activation.php

--- inc/activation.php
+++ inc/activation.php
@@ -379,22 +379,22 @@ function createMenu()
     //dci_create_custom_menu_item(__( 'Leggi le FAQ', 'design_comuni_italia'),$menu_info_1);
     //dci_create_archive_menu_item('domanda_frequente', $menu_info_1, __( 'Leggi le FAQ', 'design_comuni_italia'));
     
-   //  dci_create_page_menu_item(__('Domande frequenti', 'design_comuni_italia'), $menu_info_1, __('Leggi le FAQ','design_comuni_italia'));
-  //   dci_create_page_menu_item(__('Prenotazioni', 'design_comuni_italia'), $menu_info_1, __( 'Prenotazione appuntamento', 'design_comuni_italia'));
-  //   dci_create_custom_menu_item(__( 'Segnalazione disservizio', 'design_comuni_italia'),$menu_info_1);
-  //   dci_create_page_menu_item(__( 'Assistenza', 'design_comuni_italia'), $menu_info_1, __( 'Richiesta assistenza', 'design_comuni_italia'));
+      dci_create_page_menu_item(__('Domande frequenti', 'design_comuni_italia'), $menu_info_1, __('Leggi le FAQ','design_comuni_italia'));
+      dci_create_page_menu_item(__('Prenotazioni', 'design_comuni_italia'), $menu_info_1, __( 'Prenotazione appuntamento', 'design_comuni_italia'));
+      dci_create_custom_menu_item(__( 'Segnalazione disservizio', 'design_comuni_italia'),$menu_info_1);
+      dci_create_page_menu_item(__( 'Assistenza', 'design_comuni_italia'), $menu_info_1, __( 'Richiesta assistenza', 'design_comuni_italia'));
     //assegno menu a location
-    dci_add_menu_to_location($menu_info_1,'menu-footer-info-1');
+   // dci_add_menu_to_location($menu_info_1,'menu-footer-info-1');
 
     
 
     //menu info colonna 2
-  //  dci_create_custom_menu_item(__( 'Amministrazione trasparente', 'design_comuni_italia'),$menu_info_2);
-   // dci_create_custom_menu_item(__( 'Informativa privacy', 'design_comuni_italia'),$menu_info_2);
-  //  dci_create_custom_menu_item(__( 'Note legali', 'design_comuni_italia'),$menu_info_2);
-  //  dci_create_custom_menu_item(__( 'Dichiarazione di accessibilità', 'design_comuni_italia'),$menu_info_2);
+     dci_create_custom_menu_item(__( 'Amministrazione trasparente', 'design_comuni_italia'),$menu_info_2);
+     dci_create_custom_menu_item(__( 'Informativa privacy', 'design_comuni_italia'),$menu_info_2);
+     dci_create_custom_menu_item(__( 'Note legali', 'design_comuni_italia'),$menu_info_2);
+     dci_create_custom_menu_item(__( 'Dichiarazione di accessibilità', 'design_comuni_italia'),$menu_info_2);
     //assegno menu a location
-    dci_add_menu_to_location($menu_info_2,'menu-footer-info-2');
+   // dci_add_menu_to_location($menu_info_2,'menu-footer-info-2');
 
     
     
