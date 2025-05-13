c05744cc9b57849dd8bb2c588912f56d4880c5c0
Point-Service <tecnico@p-service.it>
2025-05-13 18:03:52 +0200

    Update activation.php

--- inc/activation.php
+++ inc/activation.php
@@ -5,7 +5,7 @@
  */
 function dci_theme_activation() {
 
-    set_time_limit(190);  // Imposta il limite a 190 secondi per questa funzione
+    set_time_limit(300);  // Imposta il limite a 300 secondi per questa funzione
     
     // inserisco i termini di tassonomia
     insertCustomTaxonomyTerms();
