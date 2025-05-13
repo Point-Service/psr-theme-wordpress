d58c9cb61ef056ea53185da5075d66839ce090ca
Point-Service <tecnico@p-service.it>
2025-05-13 09:35:06 +0200

    Update activation.php

--- inc/activation.php
+++ inc/activation.php
@@ -391,6 +391,16 @@ function createMenu()
 
 
 
+
+    
+
+    //menu info colonna 2
+    dci_create_custom_menu_item(__( 'Amministrazione trasparente', 'design_comuni_italia'),$menu_info_2);
+  //  dci_create_custom_menu_item(__( 'Informativa privacy', 'design_comuni_italia'),$menu_info_2);
+ //   dci_create_custom_menu_item(__( 'Note legali', 'design_comuni_italia'),$menu_info_2);
+//    dci_create_custom_menu_item(__( 'Dichiarazione di accessibilità', 'design_comuni_italia'),$menu_info_2);
+    //assegno menu a location
+    dci_add_menu_to_location($menu_info_2,'menu-footer-info-2');
     
 }
 
