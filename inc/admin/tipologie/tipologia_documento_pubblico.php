59febfe1fb3637bf0031ed895e5954721756387b
Point-Service <tecnico@p-service.it>
2025-06-09 11:21:01 +0200

    Update tipologia_documento_pubblico.php

--- inc/admin/tipologie/tipologia_documento_pubblico.php
+++ inc/admin/tipologie/tipologia_documento_pubblico.php
@@ -191,7 +191,7 @@ $cmb_documento->add_field(array(
     'desc' => __('Non usare più. Usare il campo sottostante per caricare più documenti.', 'design_comuni_italia'),
     'type' => 'file',
     'attributes' => array(
-        'disabled' => false // puoi metterlo true se vuoi renderlo non modificabile
+        'disabled' => true // puoi metterlo true se vuoi renderlo non modificabile
     ),
 ));
 
