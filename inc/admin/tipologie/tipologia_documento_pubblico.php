6ca1c2dafced3ad6516b7e4a073eae8771a38261
Point-Service <tecnico@p-service.it>
2025-06-09 18:22:43 +0200

    Update tipologia_documento_pubblico.php

--- inc/admin/tipologie/tipologia_documento_pubblico.php
+++ inc/admin/tipologie/tipologia_documento_pubblico.php
@@ -178,14 +178,20 @@ function dci_add_documento_pubblico_metaboxes()
         'priority' => 'high',
     ));
 
-   /**
+
+
+
+    
     $cmb_documento->add_field(array(
         'id' => $prefix . 'url_documento',
         'name' => __('Documento: URL', 'design_comuni_italia'),
         'desc' => __('Link al documento vero e proprio', 'design_comuni_italia'),
         'type' => 'text_url'
     ));
-    */
+
+
+
+    
 
   add_action('cmb2_after_init', function () {
     if (!is_admin()) return;
