23b9ae618fd7e7a2a0acabb6bf0a80762c3f79c1
Point-Service <tecnico@p-service.it>
2025-06-09 12:44:19 +0200

    Update tipologia_documento_pubblico.php

--- inc/admin/tipologie/tipologia_documento_pubblico.php
+++ inc/admin/tipologie/tipologia_documento_pubblico.php
@@ -187,7 +187,7 @@ function dci_add_documento_pubblico_metaboxes()
     
     // CAMPO VECCHIO - PER COMPATIBILITÀ
     $cmb_documento->add_field(array(
-        'id' => $prefix . 'file_documento_deprecated',
+        'id' => $prefix . 'file_documento',
         'name' => __('[OBSOLETO] Documento singolo', 'design_comuni_italia'),
         'desc' => __('Non usare più. Usare il campo sottostante per caricare più documenti.', 'design_comuni_italia'),
         'type' => 'file',
