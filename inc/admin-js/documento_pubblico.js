060a9bcf8f755011753c8943fb88df1fcf35f997
Point-Service <tecnico@p-service.it>
2025-06-09 11:44:13 +0200

    Update documento_pubblico.js

--- inc/admin-js/documento_pubblico.js
+++ inc/admin-js/documento_pubblico.js
@@ -46,7 +46,7 @@ jQuery( document ).ready(function() {
         /**
          * controllo compilazione alternativa url documento - file documento
          */
-        if(document.activeElement.id === 'publish' &&(!jQuery('input[name^="_dci_documento_pubblico_url_documento"]').val() && jQuery('#_dci_documento_pubblico_file_documento-status').children().length == 0 && !jQuery('input[name^="_dci_documento_pubblico_file_documento"]').val())){
+        if(document.activeElement.id === 'publish' &&(!jQuery('input[name^="_dci_documento_pubblico_url_documento"]').val() && jQuery('#_dci_documento_pubblico_file_documento_lista-status').children().length == 0 && jQuery('#_dci_documento_pubblico_file_documento-status').children().length == 0 && !jQuery('input[name^="_dci_documento_pubblico_file_documento"]').val())){
             dci_highlight_alternative_field('#_dci_documento_pubblico_box_documento', 'Campo obbligatorio');
             return false;
         }
