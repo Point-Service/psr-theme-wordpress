75d6e33f5ff0f3a7ae43bcaf86b7ddc727be2d78
Point-Service <tecnico@p-service.it>
2025-06-09 11:13:22 +0200

    Update single-documento_pubblico.php

--- single-documento_pubblico.php
+++ single-documento_pubblico.php
@@ -18,7 +18,7 @@ get_header();
         the_post();
         $user_can_view_post = dci_members_can_user_view_post(get_current_user_id(), $post->ID);
 
-        //$prefix= '_dci_documento_pubblico_';			
+        $prefix= '_dci_documento_pubblico_';			
         $identificativo = dci_get_meta("identificativo");
         $numero_protocollo = dci_get_meta("numero_protocollo");
         $data_protocollo = dci_get_meta("data_protocollo");
