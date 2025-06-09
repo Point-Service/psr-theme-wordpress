062fe7d58b9f54ab5af09b9a5dc46d90ac668183
Point-Service <tecnico@p-service.it>
2025-06-09 17:09:31 +0200

    Update single-documento_pubblico.php

--- single-documento_pubblico.php
+++ single-documento_pubblico.php
@@ -298,40 +298,40 @@ get_header();
 
 
 
-<?php
-// Mostra i link multipli (url_documento_group)
-if (!empty($url_documento_group) && is_array($url_documento_group)) {
-    foreach ($url_documento_group as $link_item) {
-        // Supporta sia ACF repeater che array associativi
-        $url = '';
-        $titolo = '';
-
-        if (is_array($link_item)) {
-            $url = !empty($link_item['url_documento']) ? esc_url($link_item['url_documento']) : '';
-            $titolo = !empty($link_item['titolo']) ? esc_html($link_item['titolo']) : basename($url);
-        }
-
-        if ($url) {
-            ?>
-            <div class="card card-teaser shadow-sm p-4 mt-3 rounded border border-light flex-nowrap">
-                <svg class="icon" aria-hidden="true">
-                    <use xlink:href="#it-clip"></use>
-                </svg>
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a class="text-decoration-none" href="<?= $url ?>"
-                           aria-label="Scarica il documento <?= $titolo ?>"
-                           title="Scarica il documento <?= $titolo ?>">
-                            <?= $titolo ?>
-                        </a>
-                    </h5>
-                </div>
-            </div>
-            <?php
-        }
-    }
-}
-?>
+                                    <?php
+                                    // Mostra i link multipli (url_documento_group)
+                                    if (!empty($url_documento_group) && is_array($url_documento_group)) {
+                                        foreach ($url_documento_group as $link_item) {
+                                            // Supporta sia ACF repeater che array associativi
+                                            $url = '';
+                                            $titolo = '';
+                                    
+                                            if (is_array($link_item)) {
+                                                $url = !empty($link_item['url_documento']) ? esc_url($link_item['url_documento']) : '';
+                                                $titolo = !empty($link_item['titolo']) ? esc_html($link_item['titolo']) : basename($url);
+                                            }
+                                    
+                                            if ($url) {
+                                                ?>
+                                                <div class="card card-teaser shadow-sm p-4 mt-3 rounded border border-light flex-nowrap">
+                                                    <svg class="icon" aria-hidden="true">
+                                                        <use xlink:href="#it-clip"></use>
+                                                    </svg>
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">
+                                                            <a class="text-decoration-none" href="<?= $url ?>"
+                                                               aria-label="Scarica il documento <?= $titolo ?>"
+                                                               title="Scarica il documento <?= $titolo ?>">
+                                                                <?= $titolo ?>
+                                                            </a>
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                                <?php
+                                            }
+                                        }
+                                    }
+                                    ?>
 
                                 
 
