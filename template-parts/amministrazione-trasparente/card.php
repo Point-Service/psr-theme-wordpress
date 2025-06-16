cc6940d327832ef2031d01f4bbae35341a44050d
Point-Service <tecnico@p-service.it>
2025-06-16 15:32:38 +0200

    Add files via upload

--- template-parts/amministrazione-trasparente/card.php
+++ template-parts/amministrazione-trasparente/card.php
@@ -8,20 +8,23 @@ $descrizione_breve = dci_get_meta('descrizione_breve', $prefix, $elemento->ID);
 $ck_target         = dci_get_meta('open_in_new_tab', $prefix, $elemento->ID) === 'on';
 $ck_link           = dci_get_meta('open_direct', $prefix, $elemento->ID) === 'on';
 $url               = dci_get_meta('url', $prefix, $elemento->ID);
-$documento         = dci_get_meta('file', $prefix, $elemento->ID);
 
-// Inizializzazione del link
-if ($ck_link) {
-    // Link diretto (URL o file)
-    if (!empty($url)) {
-        $link = esc_url($url); // URL assoluto
-    } elseif (!empty($documento)) {
-        $link = esc_url($documento); // Es. URL file
-    } else {
-        $link = '#'; // fallback se mancano entrambi
-    }
-} else {
-    // Link alla scheda del post
+$documenti         = dci_get_meta('file', $prefix, $elemento->ID);
+$documento = is_array($documenti) && !empty($documenti) ? get_permalink($elemento->ID) : $documenti;
+
+$arrayDataPubblicazione = dci_get_data_pubblicazione_arr("data_pubblicazione", $prefix, $elemento->ID); 
+$monthNamePubblicazione = date_i18n('M', mktime(0, 0, 0, $arrayDataPubblicazione[1], 10));
+
+$arrayDataScadenza = dci_get_data_pubblicazione_arr("data_scadenza", $prefix, $elemento->ID); 
+$monthNameScadenza = date_i18n('M', mktime(0, 0, 0, $arrayDataScadenza[1], 10));
+
+$ck_sowh_section = dci_get_option("ck_show_section", "Trasparenza");
+
+if($ck_link && !empty($url)){
+     $link = esc_url($url);
+}else if($ck_link && !empty($documento)){
+    $link = esc_url($documento);
+}else{
     $link = get_permalink($elemento->ID);
 }
 
@@ -30,22 +33,30 @@ if ($elemento->post_status === "publish") :
     <div class="cmp-card-latest-messages card-wrapper" data-bs-toggle="modal" data-bs-target="#">
         <div class="card shadow-sm px-4 pt-4 pb-4 rounded border border-light">
             <span class="visually-hidden">Categoria:</span>
-            <div class="card-header border-0 p-0">
-                <?php
-                $categorie = get_the_terms($elemento->ID, 'tipi_cat_amm_trasp');
-                if ($categorie && !is_wp_error($categorie)) {
-                    foreach ($categorie as $cat) {
-                        echo '<span class="badge bg-primary me-2">' . esc_html($cat->name) . '</span>';
+             <div class="card-header border-0 p-0">
+                <?php if ($ck_sowh_section === 'true') {?>
+                    <?php
+                    $categorie = get_the_terms($elemento->ID, 'tipi_cat_amm_trasp');
+                    if ($categorie && !is_wp_error($categorie)) {
+                        foreach ($categorie as $cat) {
+                            echo '<span class="badge bg-secondary me-2">' . esc_html($cat->name) . '</span> -';
+                        }
                     }
-                }
-                ?>
-            </div>
+                }?>
+                    
+                    <span class="data"><?php echo $arrayDataPubblicazione[0].' '.strtoupper($monthNamePubblicazione).' '.$arrayDataPubblicazione[2] ?> </span>
+                    <?php if($arrayDataPubblicazione[0]!=$arrayDataScadenza[0]) {?>
+                     - <span class="data"><?php echo $arrayDataScadenza[0].' '.strtoupper($monthNameScadenza).' '.$arrayDataScadenza[2] ?></span>
+                     <?php }?>
+                </div>
+       
+            
             <div class="card-body p-0 my-2">
                 <h3 class="green-title-big t-primary mb-8">
                     <a class="text-decoration-none"
-                       href="<?php echo esc_url($link); ?>"
-                       <?php echo $ck_target ? 'target="_blank" rel="noopener noreferrer"' : ''; ?>
-                       data-element="service-link">
+                        href="<?php echo esc_url($link); ?>"
+                        <?php echo $ck_target ? 'target="_blank" rel="noopener noreferrer"' : ''; ?>
+                        data-element="service-link">
                         <?php echo esc_html($elemento->post_title); ?>
                     </a>
                 </h3>
