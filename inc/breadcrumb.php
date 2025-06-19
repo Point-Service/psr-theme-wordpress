a9d1b072b4802a1055aa2da3da2dfdf2b68bdd6f
Point-Service <tecnico@p-service.it>
2025-06-19 10:59:51 +0200

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -406,53 +406,13 @@ class Breadcrumb_Trail {
 
 		    
 
-				else if (is_tax(array("elemento_trasparenza"))) {
-    // Debug: Verifica il tipo di post e la tassonomia
-    error_log('Current Post ID: ' . get_the_ID());  // Log del post corrente
-    error_log('Current Term: ' . single_term_title('', false));  // Log del termine corrente
-
-    // Aggiungi il link "Amministrazione Trasparente" se non è già nel breadcrumb
-    if (!in_array("Amministrazione Trasparente", $this->items)) {
-        $this->items[] = "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
-    }
-
-    // Recupera i termini associati al post corrente
-    $terms = get_the_terms(get_the_ID(), 'tipi_cat_amm_trasp');
-    if ($terms) {
-        foreach ($terms as $term) {
-            error_log('Term Name: ' . $term->name);  // Log per vedere se otteniamo i termini
-
-            // Se il termine ha un padre, aggiungiamo il padre prima del termine figlio
-            if ($term->parent) {
-                $parent_term = get_term($term->parent, 'tipi_cat_amm_trasp');
-                if ($parent_term) {
-                    error_log('Parent Term: ' . $parent_term->name);  // Log per vedere il termine padre
-                    // Aggiungi il termine padre
-                    $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($parent_term, 'tipi_cat_amm_trasp')), $parent_term->name);
-                }
-            }
-
-            // Aggiungi il termine figlio
-            $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($term, 'tipi_cat_amm_trasp')), $term->name);
-        }
-    }
-
-    // Recupera il titolo del post
-    $title = get_the_title();
-    if (strlen($title) > 100) {
-        $title = substr($title, 0, 97) . '...';
-    }
-
-    // Verifica se il titolo contiene almeno 5 lettere maiuscole consecutive
-    if (preg_match('/[A-Z]{5,}/', $title)) {
-        $title = ucfirst(strtolower($title));
-    }
-
-    // Aggiungi il titolo del post al breadcrumb
-    $this->items[] = $title;
-
-    return;
-}
+				                   else if (is_tax(array("elemento_trasparenza"))){
+							//$this->items[] =  "<a href='".home_url("amministrazione")."'>".__("Amministrazione", "design_comuni_italia")."</a>";
+				                        $this->items[] =  "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
+				                        $term_name = single_term_title( '', false );
+				                        $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
+						
+				                    }    
 
 
 		    
