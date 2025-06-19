7b3cd40d15e21569aa63462362c7969f413ba54f
Point-Service <tecnico@p-service.it>
2025-06-19 10:39:46 +0200

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -406,48 +406,44 @@ class Breadcrumb_Trail {
 
 		    
 
-					else if (is_tax(array("elemento_trasparenza"))) {
-    $this->items[] =  "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
-    
-    // Recupera i termini associati al post nella tassonomia 'tipi_cat_amm_trasp'
-    $terms = get_the_terms(get_the_ID(), 'tipi_cat_amm_trasp');
-    
-    if ($terms) {
-        foreach ($terms as $term) {
-            // Verifica se il termine ha un termine padre
-            if ($term->parent) {
-                // Ottieni il termine padre
-                $parent_term = get_term($term->parent, 'tipi_cat_amm_trasp');
-                if ($parent_term) {
-                    // Aggiungi il termine padre prima
-                  //  $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($parent_term, 'tipi_cat_amm_trasp')), $parent_term->name);
-                }
-            }
-
-            // Aggiungi il termine figlio dopo il termine padre
-         //   $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($term, 'tipi_cat_amm_trasp')), $term->name);
-        }
-    }
-    
-    // Recupera il titolo della pagina
-    $term_name = single_term_title('', false);
-    
-    // Se il termine supera i 100 caratteri, lo tronca e aggiunge "..."
-    if (strlen($term_name) > 100) {
-        $term_name = substr($term_name, 0, 97) . '...';
-    }
-    
-    // Controlla se il titolo contiene almeno 5 lettere maiuscole consecutive
-    if (preg_match('/[A-Z]{5,}/', $term_name)) {
-        // Se sì, lo trasforma in minuscolo con la prima lettera maiuscola
-        $term_name = ucfirst(strtolower($term_name));
-    }
-
-    // Aggiungi il termine figlio alla lista degli elementi
-    $this->items[] = dci_get_breadcrumb_label($term_name); // Senza __() perché è una variabile dinamica
-    
-    return;
-}
+					if (get_post_type() == 'elemento_trasparenza') {
+					    $this->items[] = "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
+					
+					    // Recupera i termini associati al post nella tassonomia 'tipi_cat_amm_trasp'
+					    $terms = get_the_terms(get_the_ID(), 'tipi_cat_amm_trasp');
+					    if ($terms) {
+					        foreach ($terms as $term) {
+					            // Verifica se il termine ha un termine padre
+					            if ($term->parent) {
+					                // Ottieni il termine padre
+					                $parent_term = get_term($term->parent, 'tipi_cat_amm_trasp');
+					                if ($parent_term) {
+					                    // Aggiungi il nome del termine padre prima
+					                    $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($parent_term, 'tipi_cat_amm_trasp')), $parent_term->name);
+					                }
+					            }
+					
+					            // Aggiungi il termine figlio dopo il termine padre
+					            $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($term, 'tipi_cat_amm_trasp')), $term->name);
+					        }
+					    }
+					
+					    // Recupera il titolo della pagina
+					    $title = get_the_title();
+					    // Se il titolo supera i 100 caratteri, lo tronca e aggiunge "..."
+					    if (strlen($title) > 100) {
+					        $title = substr($title, 0, 97) . '...';
+					    }
+					    // Controlla se il titolo contiene almeno 5 lettere maiuscole consecutive
+					    if (preg_match('/[A-Z]{5,}/', $title)) {
+					        // Se sì, lo trasforma in minuscolo con la prima lettera maiuscola
+					        $title = ucfirst(strtolower($title));
+					    }
+					    // Aggiunge il titolo alla lista degli elementi
+					    $this->items[] = $title;
+					
+					    return;
+					}
 
 
 
@@ -600,45 +596,7 @@ class Breadcrumb_Trail {
 				    
 				    return;
 				}
-		    
-				 if (get_post_type() == 'elemento_trasparenza') {
-					    $this->items[] = "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
-					
-					    // Recupera i termini associati al post nella tassonomia 'tipi_cat_amm_trasp'
-					    $terms = get_the_terms(get_the_ID(), 'tipi_cat_amm_trasp');
-					    if ($terms) {
-					        foreach ($terms as $term) {
-					            // Verifica se il termine ha un termine padre
-					            if ($term->parent) {
-					                // Ottieni il termine padre
-					                $parent_term = get_term($term->parent, 'tipi_cat_amm_trasp');
-					                if ($parent_term) {
-					                    // Aggiungi il nome del termine padre prima
-					                    $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($parent_term, 'tipi_cat_amm_trasp')), $parent_term->name);
-					                }
-					            }
-					
-					            // Aggiungi il termine figlio dopo il termine padre
-					            $this->items[] = sprintf('<a href="%s">%s</a>', esc_url(get_term_link($term, 'tipi_cat_amm_trasp')), $term->name);
-					        }
-					    }
-					
-					    // Recupera il titolo della pagina
-					    $title = get_the_title();
-					    // Se il titolo supera i 100 caratteri, lo tronca e aggiunge "..."
-					    if (strlen($title) > 100) {
-					        $title = substr($title, 0, 97) . '...';
-					    }
-					    // Controlla se il titolo contiene almeno 5 lettere maiuscole consecutive
-					    if (preg_match('/[A-Z]{5,}/', $title)) {
-					        // Se sì, lo trasforma in minuscolo con la prima lettera maiuscola
-					        $title = ucfirst(strtolower($title));
-					    }
-					    // Aggiunge il titolo alla lista degli elementi
-					    $this->items[] = $title;
-					
-					    return;
-					}
+				
 				
 				if (get_post_type() == 'persona_pubblica') {
 				    // Aggiungi il link per l'amministrazione
@@ -883,7 +841,7 @@ class Breadcrumb_Trail {
             $taxonomy_name = $term->taxonomy;
 
             // Stampa il nome della tassonomia
-          // echo "Nome della tassonomia: " . $taxonomy_name;
+          //  echo "Nome della tassonomia: " . $taxonomy_name;
         }
     }
 
@@ -906,21 +864,23 @@ class Breadcrumb_Trail {
                     }
 		
                     else if (is_tax(array("tipi_cat_amm_trasp"))){
-			$this->items[] =  "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
+			//$this->items[] =  "<a href='".home_url("amministrazione")."'>".__("Amministrazione", "design_comuni_italia")."</a>";
+                        $this->items[] =  "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
 		
 			    
                     }           
-   
-                    else if (is_tax(array("elemento_trasparenza"))){		
+
+
+			    
+                    else if (is_tax(array("elemento_trasparenza"))){
+			//$this->items[] =  "<a href='".home_url("amministrazione")."'>".__("Amministrazione", "design_comuni_italia")."</a>";
                         $this->items[] =  "<a href='" . home_url("amministrazione-trasparente") . "'>" . __("Amministrazione Trasparente", "design_comuni_italia") . "</a>";
                         $term_name = single_term_title( '', false );
                         $this->items[] = __(dci_get_breadcrumb_label($term_name), "design_comuni_italia");
 		
-                    } 
-
-			    
+                    }       		    
 		    else if (is_tax(array("tipi_evento"))) {	
      
 			    // Link a "Vivere il Comune"
