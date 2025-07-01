998065789029d5ed55c2e717dbc92bdc39050722
Point-Service <tecnico@p-service.it>
2025-07-01 11:18:23 +0200

    Update categorie.php

--- template-parts/amministrazione-trasparente/categorie.php
+++ template-parts/amministrazione-trasparente/categorie.php
@@ -1,10 +1,8 @@
 <?php
-global $sito_tematico_id, $siti_tematici;
-
-// Recupera le categorie principali (genitori)
+global $sito_tematico_id,$siti_tematici;
 $categorie_genitori = get_terms('tipi_cat_amm_trasp', array(
     'hide_empty' => false,
-    'parent' => 0,  // Le categorie senza genitore
+    'parent' => 0,
     'orderby' => 'ID',
     'order'   => 'ASC'
 ));
@@ -63,39 +61,34 @@ $siti_tematici = !empty(dci_get_option("siti_tematici", "trasparenza")) ? dci_ge
                                 <h2 class="title-custom" onclick="toggleContent('<?= $id_genitore ?>')"><?= $nome_genitore ?></h2>
                                 <div id="<?= $id_genitore ?>" class="content">
                                     <?php
-                                    // Recupera le sottocategorie per questa categoria principale
                                     $sottocategorie = get_terms('tipi_cat_amm_trasp', array(
                                         'hide_empty' => false,
-                                        'parent' => $genitore->term_id  // Ottieni le sottocategorie della categoria principale
+                                        'parent' => $genitore->term_id
                                     ));
-
-                                    // Se ci sono sottocategorie, visualizzale
-                                    if (!empty($sottocategorie)) { ?>
-                                        <ul class="link-list t-primary">
-                                            <?php foreach ($sottocategorie as $sottocategoria) {
-                                                $link = get_term_link($sottocategoria);  // Ottieni il link della sottocategoria
-                                                $nome_sotto = esc_html($sottocategoria->name); ?>
-                                                <li class="mb-3 mt-3">
-                                                    <a class="list-item ps-0 title-medium underline" style="text-decoration:none;" href="<?= $link; ?>">
-                                                        <svg class="icon">
-                                                            <use xlink:href="#it-arrow-right-triangle"></use>
-                                                        </svg>
-                                                        <span><?php echo $sottocategoria->description; ?></span>
-                                                    </a>
-                                                </li>
-                                            <?php } ?>
-                                        </ul>
-                                    <?php } ?>
+                                    ?>
+                                    <ul class="link-list t-primary">
+                                        <?php foreach ($sottocategorie as $sotto) {
+                                            $link = get_term_link($sotto);
+                                            $nome_sotto = esc_html($sotto->name); ?>
+                                            <li class="mb-3 mt-3">
+                                                <a class="list-item ps-0 title-medium underline" style="text-decoration:none;" href="<?= $link; ?>">
+                                                    <svg class="icon">
+                                                        <use xlink:href="#it-arrow-right-triangle"></use>
+                                                    </svg>
+                                                    <span><?= $nome_sotto; ?></span>
+                                                </a>
+                                            </li>
+                                        <?php } ?>
+                                    </ul>
                                 </div>
                             <?php } ?>
                         </div>
                     </div>
                     <!-- Colonna destra: link utili -->
-                    <?php get_template_part("template-parts/amministrazione-trasparente/side-bar"); ?>
+                   <?php get_template_part("template-parts/amministrazione-trasparente/side-bar"); ?>
                 </div>
             </div>
         </form>
     </div>
 </main>
-
 
