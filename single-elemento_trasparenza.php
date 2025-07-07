60672591aa5e74818cdfaabc166f62ad2ef4a984
Point-Service <tecnico@p-service.it>
2025-07-07 12:53:31 +0200

    Update single-elemento_trasparenza.php

--- single-elemento_trasparenza.php
+++ single-elemento_trasparenza.php
@@ -36,31 +36,6 @@ get_header();
         $documenti_collegati = dci_get_meta("post_trasparenza", $prefix, $post->ID);
         
 
-$ck_link = dci_get_meta('open_direct', $prefix, $post->ID) === 'on';
-
-if ($ck_link) {
-    $file_meta = dci_get_meta("file", $prefix, $post->ID);
-    $file_url = '#';
-
-    // Se è un array
-    if (is_array($file_meta) && !empty($file_meta)) {
-        $first_file = $file_meta[0];
-        $file_url = is_array($first_file) && isset($first_file['url'])
-            ? esc_url($first_file['url'])
-            : esc_url($first_file);
-    }
-    // Se è una stringa
-    elseif (is_string($file_meta) && !empty($file_meta)) {
-        $file_url = esc_url($file_meta);
-    }
-
-    // Se abbiamo un URL valido, fai redirect
-    if ($file_url && $file_url !== '#') {
-        echo "<script>window.location.href = '" . esc_js($file_url) . "';</script>";
-        exit; // Ferma l'esecuzione del template
-    }
-}
-
 
 
         
