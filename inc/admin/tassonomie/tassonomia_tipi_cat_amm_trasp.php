77e20bb95a60e8780616cef2842625c5fcf2dffe
Point-Service <tecnico@p-service.it>
2025-07-11 17:31:39 +0200

    Update tassonomia_tipi_cat_amm_trasp.php

--- inc/admin/tassonomie/tassonomia_tipi_cat_amm_trasp.php
+++ inc/admin/tassonomie/tassonomia_tipi_cat_amm_trasp.php
@@ -37,6 +37,46 @@ function dci_register_taxonomy_tipi_cat_amm_trasp() {
 }
 
 
+
+
+// 2. Metabox CMB2: campo Ordinamento nella tassonomia
+add_action('cmb2_admin_init', 'dci_register_taxonomy_metabox');
+function dci_register_taxonomy_metabox() {
+    $prefix = '_dci_';
+
+    $cmb = new_cmb2_box(array(
+        'id'           => $prefix . 'tipi_cat_amm_trasp_metabox',
+        'title'        => __('Impostazioni categoria', 'design_comuni_italia'),
+        'object_types' => array('term'),
+        'taxonomies'   => array('tipi_cat_amm_trasp'),
+    ));
+
+    $cmb->add_field(array(
+        'name'       => __('Ordinamento', 'design_comuni_italia'),
+        'desc'       => __('Numero per definire l’ordine di visualizzazione.', 'design_comuni_italia'),
+        'id'         => $prefix . 'ordinamento',
+        'type'       => 'text',
+        'attributes' => array(
+            'type'  => 'number',
+            'min'   => 0,
+            'step'  => 1,
+        ),
+    ));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 3. Aggiunge colonna "Ordinamento" nella lista dei termini
 add_filter('manage_edit-tipi_cat_amm_trasp_columns', 'dci_add_ordinamento_column');
 function dci_add_ordinamento_column($columns) {
