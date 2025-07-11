94672044f1bfbcf8348829f4655ca0e7e187690f
Point-Service <tecnico@p-service.it>
2025-07-11 17:41:59 +0200

    Update tassonomia_tipi_cat_amm_trasp.php

--- inc/admin/tassonomie/tassonomia_tipi_cat_amm_trasp.php
+++ inc/admin/tassonomie/tassonomia_tipi_cat_amm_trasp.php
@@ -39,31 +39,7 @@ function dci_register_taxonomy_tipi_cat_amm_trasp() {
 
 
 
-// 2. Aggiungi il campo 'ordinamento' ai termini della tassonomia con CMB2
-add_action('cmb2_admin_init', 'dci_register_taxonomy_metabox');
-function dci_register_taxonomy_metabox() {
-    $prefix = '_dci_';
 
-    $cmb = new_cmb2_box(array(
-        'id'            => $prefix . 'tipi_cat_amm_trasp_metabox',
-        'title'         => __('Impostazioni categoria', 'design_comuni_italia'),
-        'object_types'  => array('term'),
-        'taxonomies'    => array('tipi_cat_amm_trasp'),
-        'new_term_section' => true,
-    ));
-
-    $cmb->add_field(array(
-        'name'       => __('Ordinamento', 'design_comuni_italia'),
-        'desc'       => __('Numero per definire l’ordine di visualizzazione della categoria.', 'design_comuni_italia'),
-        'id'         => $prefix . 'ordinamento',
-        'type'       => 'text_small',
-        'attributes' => array(
-            'type'  => 'number',
-            'min'   => 0,
-            'step'  => 1,
-        ),
-    ));
-}
 
 
 
