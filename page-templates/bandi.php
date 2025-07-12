655cb2ae72f28137852770567f39157a07f6b995
Point-Service <tecnico@p-service.it>
2025-07-12 11:58:40 +0200

    Update bandi.php

--- page-templates/bandi.php
+++ page-templates/bandi.php
@@ -16,6 +16,26 @@ $tipi_documento = get_terms( array(
 get_header();
 
 ?>
+
+<?php
+$titolo_bandi = get_field('titolo_bandi');
+$descrizione_bandi = get_field('descrizione_bandi');
+
+if ($titolo_bandi || $descrizione_bandi) : ?>
+  <section class="section section--trasparenza">
+    <div class="container">
+      <?php if ($titolo_bandi): ?>
+        <h2><?php echo esc_html($titolo_bandi); ?></h2>
+      <?php endif; ?>
+      <?php if ($descrizione_bandi): ?>
+        <div class="descrizione-bandi">
+          <?php echo wp_kses_post($descrizione_bandi); ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  </section>
+<?php endif; ?>
+
 	<main>
 		<?php
 		while ( have_posts() ) :
