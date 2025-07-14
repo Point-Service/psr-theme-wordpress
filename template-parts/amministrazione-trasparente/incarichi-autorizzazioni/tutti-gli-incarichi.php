5a547c3a797457d66cc69872b504301d5a9cdbdd
Point-Service <tecnico@p-service.it>
2025-07-14 18:23:29 +0200

    Update tutti-gli-incarichi.php

--- template-parts/amministrazione-trasparente/incarichi-autorizzazioni/tutti-gli-incarichi.php
+++ template-parts/amministrazione-trasparente/incarichi-autorizzazioni/tutti-gli-incarichi.php
@@ -1,8 +1,8 @@
 <?php
 global $post, $load_posts, $load_card_type;
 
-$max_posts = isset($_GET['max_posts']) ? intval($_GET['max_posts']) : 2;
-$load_posts = 2;
+$max_posts = isset($_GET['max_posts']) ? intval($_GET['max_posts']) : 20;
+$load_posts = 20;
 $main_search_query = isset($_GET['search']) ? sanitize_text_field($_GET['search']) : '';
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
