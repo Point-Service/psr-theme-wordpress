7d4d0500aa29aca782d7f4e588d7dc97c9e4e8fd
Point-Service <tecnico@p-service.it>
2025-07-16 09:50:42 +0200

    Update tutti-gli-incarichi.php

--- template-parts/amministrazione-trasparente/incarichi-autorizzazioni/tutti-gli-incarichi.php
+++ template-parts/amministrazione-trasparente/incarichi-autorizzazioni/tutti-gli-incarichi.php
@@ -1,29 +1,35 @@
 <?php
 global $post;
 
-$max_posts = isset($_GET['max_posts']) ? intval($_GET['max_posts']) : 2;
+// Imposta il numero massimo di post per pagina (con fallback)
+$max_posts = isset($_GET['max_posts']) ? intval($_GET['max_posts']) : 5;
+
+// Ricerca
 $main_search_query = isset($_GET['search']) ? sanitize_text_field($_GET['search']) : '';
 
-$paged = max(1, get_query_var('paged') ? get_query_var('paged') : (isset($_GET['paged']) ? intval($_GET['paged']) : 1));
+// Pagina corrente
+$paged = isset($_GET['paged']) ? max(1, intval($_GET['paged'])) : 1;
 
+// Argomenti WP_Query
 $args = array(
     'post_type'      => 'incarichi_dip',
     'posts_per_page' => $max_posts,
+    'paged'          => $paged,
     'orderby'        => 'date',
     'order'          => 'DESC',
-    'paged'          => $paged,
 );
 
+// Aggiungi la ricerca se presente
 if ($main_search_query) {
     $args['s'] = $main_search_query;
 }
 
+// Esegui la query
 $the_query = new WP_Query($args);
-$prefix = "_dci_icad_";
+$prefix = '_dci_icad_';
 ?>
 
 <?php if ($the_query->have_posts()) : ?>
-
     <div class="row">
         <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
             <?php get_template_part('template-parts/amministrazione-trasparente/incarichi-autorizzazioni/card'); ?>
@@ -37,17 +43,23 @@ $prefix = "_dci_icad_";
             <ul class="pagination justify-content-center">
 
                 <?php
-                echo str_replace(
-                    array('<span class="page-numbers current">', '<a class="page-numbers', '</span>', 'prev page-numbers', 'next page-numbers'),
-                    array('<li class="page-item active"><span class="page-link">', '<li class="page-item"><a class="page-link', '</span></li>', 'page-item prev', 'page-item next'),
-                    paginate_links(array(
-                        'total'     => $the_query->max_num_pages,
-                        'current'   => $paged,
-                        'type'      => 'list',
-                        'prev_text' => '&laquo;',
-                        'next_text' => '&raquo;',
-                    ))
-                );
+                $pagination_links = paginate_links(array(
+                    'base'      => add_query_arg('paged', '%#%'),
+                    'format'    => '',
+                    'current'   => $paged,
+                    'total'     => $the_query->max_num_pages,
+                    'prev_text' => '&laquo;',
+                    'next_text' => '&raquo;',
+                    'type'      => 'array',
+                ));
+
+                foreach ((array) $pagination_links as $link) {
+                    if (strpos($link, 'current') !== false) {
+                        echo '<li class="page-item active"><span class="page-link">' . strip_tags($link) . '</span></li>';
+                    } else {
+                        echo '<li class="page-item">' . str_replace('page-numbers', 'page-link', $link) . '</li>';
+                    }
+                }
                 ?>
 
             </ul>
