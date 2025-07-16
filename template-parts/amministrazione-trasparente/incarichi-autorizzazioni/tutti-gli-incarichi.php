f1e9e697e6b0c44a46ca016a1cb6ecd3f6656a5a
Point-Service <tecnico@p-service.it>
2025-07-16 09:36:22 +0200

    Update tutti-gli-incarichi.php

--- template-parts/amministrazione-trasparente/incarichi-autorizzazioni/tutti-gli-incarichi.php
+++ template-parts/amministrazione-trasparente/incarichi-autorizzazioni/tutti-gli-incarichi.php
@@ -1,55 +1,58 @@
 <?php
 global $post;
 
-// Numero di post per pagina, con un valore di fallback di 10
 $max_posts = isset($_GET['max_posts']) ? intval($_GET['max_posts']) : 2;
-// Se è presente un termine di ricerca
 $main_search_query = isset($_GET['search']) ? sanitize_text_field($_GET['search']) : '';
 
-// Determina la pagina corrente
 $paged = max(1, get_query_var('paged') ? get_query_var('paged') : (isset($_GET['paged']) ? intval($_GET['paged']) : 1));
 
-// Impostazioni per la query
 $args = array(
-    'post_type'      => 'incarichi_dip', // Post type "incarichi_dip"
-    'posts_per_page' => $max_posts,      // Numero di post per pagina
-    'orderby'        => 'date',          // Ordina per data
-    'order'          => 'DESC',          // Ordinamento decrescente
-    'paged'          => $paged,         // Pagina corrente
+    'post_type'      => 'incarichi_dip',
+    'posts_per_page' => $max_posts,
+    'orderby'        => 'date',
+    'order'          => 'DESC',
+    'paged'          => $paged,
 );
 
-// Aggiungi il parametro di ricerca, se presente
 if ($main_search_query) {
     $args['s'] = $main_search_query;
 }
 
-// Esegui la query
 $the_query = new WP_Query($args);
 $prefix = "_dci_icad_";
 ?>
+
 <?php if ($the_query->have_posts()) : ?>
 
-    <?php while ($the_query->have_posts()) : $the_query->the_post();
-        // Carica il template per ogni post
-        get_template_part('template-parts/amministrazione-trasparente/incarichi-autorizzazioni/card');
-    endwhile;
-    wp_reset_postdata(); ?>
+    <div class="row">
+        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+            <?php get_template_part('template-parts/amministrazione-trasparente/incarichi-autorizzazioni/card'); ?>
+        <?php endwhile; ?>
+    </div>
 
-<div class="row my-4">
-    <nav class="pagination-wrapper justify-content-center col-12" aria-label="Navigazione pagine">
-        <?php 
-        if ($the_query->max_num_pages > 1) {
-            echo paginate_links(array(
-                'total' => $the_query->max_num_pages,
-                'current' => $paged,
-                'prev_text' => '&laquo; Precedente',
-                'next_text' => 'Successivo &raquo;',
-            ));
-        }
-        ?>
-    </nav>
-</div>
+    <?php wp_reset_postdata(); ?>
 
+    <?php if ($the_query->max_num_pages > 1): ?>
+        <nav class="my-4">
+            <ul class="pagination justify-content-center">
+
+                <?php
+                echo str_replace(
+                    array('<span class="page-numbers current">', '<a class="page-numbers', '</span>', 'prev page-numbers', 'next page-numbers'),
+                    array('<li class="page-item active"><span class="page-link">', '<li class="page-item"><a class="page-link', '</span></li>', 'page-item prev', 'page-item next'),
+                    paginate_links(array(
+                        'total'     => $the_query->max_num_pages,
+                        'current'   => $paged,
+                        'type'      => 'list',
+                        'prev_text' => '&laquo;',
+                        'next_text' => '&raquo;',
+                    ))
+                );
+                ?>
+
+            </ul>
+        </nav>
+    <?php endif; ?>
 
 <?php else : ?>
     <div class="alert alert-info text-center" role="alert">
