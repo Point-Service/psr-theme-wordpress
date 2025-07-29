eb575f4996722f0cc10fc7492035a4cc06267a71
Point-Service <tecnico@p-service.it>
2025-07-29 16:07:11 +0200

    Update notizia_in_evidenza.php

--- template-parts/home/notizia_in_evidenza.php
+++ template-parts/home/notizia_in_evidenza.php
@@ -28,11 +28,11 @@ if (is_array($post_ids) && count($post_ids) > 1):
                     <div class="row align-items-stretch g-0">
 
                                 <!-- Immagine -->
-                            <div class="col-lg-6 px-0 px-lg-2">
-                                <?php if ($img) {
-                                    dci_get_img($img, 'img-wrapper');
-                                } ?>
-                            </div>
+                        <div class="col-lg-6 offset-lg-1 order-1 order-lg-2 px-0 px-lg-2">
+                            <?php if ($img) {
+                                dci_get_img($img, 'img-fluid');
+                            } ?>
+                        </div>
   
                         <!-- Testo -->
                         <div class="col-lg-6 order-2 order-lg-1 d-flex align-items-center">
