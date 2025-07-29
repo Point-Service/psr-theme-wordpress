c921449af0cf5b36027b83f11d501ab138db6e50
Point-Service <tecnico@p-service.it>
2025-07-29 12:43:56 +0200

    Update notizia_in_evidenza.php

--- template-parts/home/notizia_in_evidenza.php
+++ template-parts/home/notizia_in_evidenza.php
@@ -26,14 +26,8 @@ if (is_array($post_ids) && count($post_ids) > 1):
             ?>
                 <div class="carousel-item <?php echo $first ? 'active' : ''; ?>">
                     <div class="row align-items-stretch g-0">
-                    
-                        <!-- Immagine -->
-                    <div class="col-lg-6 offset-lg-1 order-1 order-lg-2 px-0 px-lg-2">
-                        <?php if ($img) {
-                            dci_get_img($img, 'img-fluid');
-                        } ?>
-                    </div>
-                        
+       
+                                
                         <!-- Testo -->
                         <div class="col-lg-6 order-2 order-lg-1 d-flex align-items-center">
                             <div class="card w-100 border-0 rounded-0">
@@ -97,6 +91,13 @@ if (is_array($post_ids) && count($post_ids) > 1):
                             </div>
                         </div>
                     </div>
+                                 
+                                <!-- Immagine -->
+                            <div class="col-lg-6 offset-lg-1 order-1 order-lg-2 px-0 px-lg-2">
+                                <?php if ($img) {
+                                    dci_get_img($img, 'img-fluid');
+                                } ?>
+                            </div>
                 </div>
             <?php
                 $first = false;
