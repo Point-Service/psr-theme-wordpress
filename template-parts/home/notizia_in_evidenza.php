f7ec233ca76e916e21bf5a205174ec76a57bb944
Point-Service <tecnico@p-service.it>
2025-07-29 12:22:38 +0200

    Update notizia_in_evidenza.php

--- template-parts/home/notizia_in_evidenza.php
+++ template-parts/home/notizia_in_evidenza.php
@@ -28,9 +28,11 @@ if (is_array($post_ids) && count($post_ids) > 1):
                     <div class="row align-items-stretch g-0">
                         <!-- Immagine -->
                         <div class="col-lg-6 offset-lg-1 order-1 order-lg-2 px-0 px-lg-2">
-                            <?php if ($img) {
-                                dci_get_img($img, 'img-fluid');
-                            } ?>
+                            <?php if ($img): ?>
+                                <div class="carousel-img-wrapper d-flex justify-content-center align-items-center">
+                                    <?php dci_get_img($img, 'img-responsive-carousel'); ?>
+                                </div>
+                            <?php endif; ?>
                         </div>
                         
                         <!-- Testo -->
@@ -273,15 +275,13 @@ endif;
         height: auto;
     }
 }
-#carosello-notizie .carousel-img-wrapper {
-    width: 100%;
+
+    #carosello-notizie .carousel-img-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     height: 100%;
-    min-height: 300px;
-    max-height: 400px;
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: contain;
-    background-color: #f5f5f5;
+    padding: 1rem;
 }
 </style>
 
