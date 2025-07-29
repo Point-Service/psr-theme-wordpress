a221b0e0689f41cdde24567dba84b1865aad59fd
Point-Service <tecnico@p-service.it>
2025-07-29 16:08:37 +0200

    Update notizia_in_evidenza.php

--- template-parts/home/notizia_in_evidenza.php
+++ template-parts/home/notizia_in_evidenza.php
@@ -27,7 +27,7 @@ if (is_array($post_ids) && count($post_ids) > 1):
                 <div class="carousel-item <?php echo $first ? 'active' : ''; ?>">
                     <div class="row align-items-stretch g-0">
 
-                                <!-- Immagine -->
+                        <!-- Immagine -->
                         <div class="col-lg-6 offset-lg-1 order-1 order-lg-2 px-0 px-lg-2">
                             <?php if ($img) {
                                 dci_get_img($img, 'rounded-top img-fluid');
@@ -210,80 +210,7 @@ endif;
 
 <!-- STILI -->
 <style>
-.carousel-item {
-    min-height: 400px;
-}
-.carousel-inner {
-    border-radius: 0;
-    overflow: hidden;
-}
-#carosello-notizie img.img-fluid,
-#carosello-notizie img.cover-img {
-    width: 100%;
-    height: auto;
-    display: block;
-}
 
-@media (min-width: 992px) {
-    #carosello-notizie .carousel-item .col-lg-6.order-1 {
-        max-height: 100%;
-    }
-
-    #carosello-notizie .carousel-item .d-none.d-lg-block {
-        height: 100%;
-    }
-
-    #carosello-notizie .carousel-item .d-none.d-lg-block img.cover-img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-
-    #carosello-notizie .card-body {
-        padding-left: 3rem;
-        padding-right: 3rem;
-    }
-}
-
-#carosello-notizie .card-body {
-    padding-left: 1rem;
-    padding-right: 1rem;
-}
-
-    #carosello-notizie .img-responsive-carousel {
-    width: 100%;
-    max-height: 100%;
-    object-fit: contain;
-    display: block;
-    margin: 0 auto;
-}
-
-@media (min-width: 992px) {
-    #carosello-notizie .col-lg-6.order-1 {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        max-height: 100%;
-        overflow: hidden;
-        background-color: #f8f9fa; /* opzionale: colore di sfondo */
-    }
-
-    #carosello-notizie .img-responsive-carousel {
-        max-width: 100%;
-        max-height: 400px;
-        height: auto;
-    }
-}
-#carosello-notizie .carousel-img-wrapper {
-    width: 100%;
-    height: 100%;
-    min-height: 300px;
-    max-height: 400px;
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: contain;
-    background-color: #f5f5f5;
-}
 
 /* Sposta a sinistra il testo nel singolo elemento */
 @media (min-width: 992px) {
