7611f6f877823d40543f681a144440367d9fea26
Point-Service <tecnico@p-service.it>
2025-07-30 12:51:26 +0200

    Update notizia_in_evidenza.php

--- template-parts/home/notizia_in_evidenza.php
+++ template-parts/home/notizia_in_evidenza.php
@@ -206,86 +206,113 @@ endif;
 
    <!-- STILI AGGIORNATI -->
 <style>
-#carosello-evidenza {
-    position: relative;
-    overflow: hidden;
-}
+    #carosello-evidenza {
+        position: relative;
+        overflow: hidden;
+    }
 
-#carosello-evidenza .carousel-inner {
-    border-radius: 0;
-    overflow: hidden;
-}
+    #carosello-evidenza .carousel-inner {
+        border-radius: 0;
+        overflow: hidden;
+    }
 
-#carosello-evidenza .carousel-item {
-    min-height: 400px;
-}
+    #carosello-evidenza .carousel-item {
+        min-height: 400px;
+        overflow: visible;
+    }
 
-#carosello-evidenza .carousel-item .row {
-    min-height: 100%;
-}
+    #carosello-evidenza .carousel-item .row {
+        min-height: 400px;
+    }
 
-/* Colonna immagine */
-.col-img {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #f5f5f5;
-    padding: 1rem;
-    min-height: 400px;
-}
+    #carosello-evidenza .carousel-item .row > div {
+        display: flex;
+        flex-direction: column;
+    }
 
-img.img-evidenza {
-    max-width: 90%;
-    max-height: 300px;
-    width: auto;
-    height: auto;
-    object-fit: contain;
-    display: block;
-    margin: 0 auto;
-}
-
-/* Card con testo */
-#carosello-evidenza .card-body {
-    padding: 1rem;
-}
-
-@media (min-width: 992px) {
     .col-img {
-        padding: 2rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #f5f5f5;
+        padding: 1rem;
     }
 
-    #carosello-evidenza .card-body {
-        padding-left: 3rem;
-        padding-right: 3rem;
+    img.img-evidenza {
+        max-width: 90%;
+        max-height: 300px;
+        width: auto;
+        height: auto;
+        object-fit: contain;
+        display: block;
+        margin: 0 auto;
     }
 
-    .row > .col-lg-5.order-2.order-lg-1 {
-        padding-left: 0.5rem;
+    @media (min-width: 992px) {
+        .col-img {
+            padding: 2rem;
+        }
+
+        .row > .col-lg-5.order-2.order-lg-1 {
+            padding-left: 0.5rem;
+        }
+
+        #carosello-evidenza .card-body {
+            padding-left: 3rem;
+            padding-right: 3rem;
+        }
     }
-}
 
-/* Stili aggiuntivi per versione con 1 solo post */
-.row .col-lg-6.offset-lg-1.order-1.order-lg-2 {
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    padding: 1rem;
-    min-height: 400px;
-}
+    /* --------------------------------------------- */
+    /* STILI AGGIUNTIVI PER IL BLOCCO "SINGOLO ELEMENTO" */
 
-.row .col-lg-6.offset-lg-1.order-1.order-lg-2 img.img-fluid {
-    max-width: 90%;
-    max-height: 400px;
-    padding-right: 7px;
-    width: auto;
-    height: auto;
-    object-fit: contain;
-    display: block;
-}
-
-@media (min-width: 992px) {
     .row .col-lg-6.offset-lg-1.order-1.order-lg-2 {
-        padding: 3rem;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding: 1rem;
+        min-height: 400px;
     }
-}
-    </style>
+
+    .row .col-lg-6.offset-lg-1.order-1.order-lg-2 img.img-fluid {
+        max-width: 90%;
+        max-height: 400px;
+        padding-right: 7px;
+        width: auto;
+        height: auto;
+        object-fit: contain;
+        display: block;
+    }
+
+    @media (min-width: 992px) {
+        .row .col-lg-6.offset-lg-1.order-1.order-lg-2 {
+            padding: 3rem;
+        }
+    }
+
+    /* --------------------------------------------- */
+    /* FIX PER PULSANTE NASCOSTO: ESTENSIONE COMPLETA */
+
+    #carosello-evidenza .carousel-item .col-lg-6.order-2.order-lg-1 {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #carosello-evidenza .carousel-item .card {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #carosello-evidenza .carousel-item .card-body {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .read-more {
+        margin-top: auto;
+        padding-top: 1rem;
+    }
+</style>
