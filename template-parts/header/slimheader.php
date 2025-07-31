c46168839236f2c117a12d887a21f4bcf289f8b1
Point-Service <tecnico@p-service.it>
2025-07-31 10:54:25 +0200

    Update slimheader.php

--- template-parts/header/slimheader.php
+++ template-parts/header/slimheader.php
@@ -2,61 +2,65 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <div class="it-header-slim-wrapper-content">
+        <div class="it-header-slim-wrapper-content d-flex flex-column flex-lg-row justify-content-between align-items-start align-items-lg-center gap-2">
 
-          <a class="d-lg-block navbar-brand" target="_blank" href="<?php echo dci_get_option("url_sito_regione"); ?>" target="_blank" aria-label="Vai al portale <?php echo dci_get_option("nome_regione"); ?> - link esterno - apertura nuova scheda" title="Vai al portale <?php echo dci_get_option("nome_regione"); ?>">
-            
-          <?php echo dci_get_option("nome_regione"); ?></a>
-           &nbsp;&nbsp;&nbsp;&nbsp;		
-           <div class="it-header-slim-right-zone" role="navigation">
+          <!-- Nome Regione -->
+          <a class="navbar-brand text-decoration-none" target="_blank"
+             href="<?php echo dci_get_option("url_sito_regione"); ?>"
+             aria-label="Vai al portale <?php echo dci_get_option("nome_regione"); ?> - link esterno - apertura nuova scheda"
+             title="Vai al portale <?php echo dci_get_option("nome_regione"); ?>">
+            <?php echo dci_get_option("nome_regione"); ?>
+          </a>
 
+          <!-- Right Zone -->
+          <div class="it-header-slim-right-zone d-flex flex-column flex-lg-row align-items-start align-items-lg-center gap-2" role="navigation">
 
-
-		   
-            <?php if(dci_get_option("link_ammtrasparente")) { ?>
-            <div class="it-user-wrapper nav-item dropdown">
-               <a aria-expanded="false" class="d-lg-block navbar-brand" data-toggle="dropdown" target="_blank" aria-label="Amministrazione trasparente" href=" <?php echo dci_get_option("link_ammtrasparente"); ?>" data-focus-mouse="false">
+            <!-- Amministrazione Trasparente -->
+            <?php if (dci_get_option("link_ammtrasparente")) { ?>
+              <div class="it-user-wrapper nav-item">
+                <a class="navbar-brand text-decoration-none" target="_blank"
+                   href="<?php echo dci_get_option("link_ammtrasparente"); ?>"
+                   aria-label="Amministrazione trasparente">
                   Amministrazione trasparente
-               </a>
-            </div>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <?php }?>
-            <?php if(dci_get_option("link_albopretorio")) { ?>
-            <div class="it-user-wrapper nav-item dropdown">
-               <a aria-expanded="false" class="d-lg-block navbar-brand" target="_blank" data-toggle="dropdown"  aria-label="Albo pretorio" href="<?php echo dci_get_option("link_albopretorio"); ?>" data-focus-mouse="false">
+                </a>
+              </div>
+            <?php } ?>
+
+            <!-- Albo Pretorio -->
+            <?php if (dci_get_option("link_albopretorio")) { ?>
+              <div class="it-user-wrapper nav-item">
+                <a class="navbar-brand text-decoration-none" target="_blank"
+                   href="<?php echo dci_get_option("link_albopretorio"); ?>"
+                   aria-label="Albo pretorio">
                   Albo pretorio
-               </a>
-            </div>
-            <?php }?>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <?php 
-        	$shortcode_output = do_shortcode('[google-translator]');
-        				
-        	if ($shortcode_output !== '[google-translator]') {
-        	 // echo $shortcode_output;
-        	   }
-	       ?>
+                </a>
+              </div>
+            <?php } ?>
 
+            <!-- Google Translator -->
+            <?php
+              $shortcode_output = do_shortcode('[google-translator]');
+              if ($shortcode_output !== '[google-translator]') {
+                echo '<div class="nav-item">' . $shortcode_output . '</div>';
+              }
+            ?>
 
-
-		   
+            <!-- Login / User -->
+            <div class="nav-item">
               <?php
               if (!is_user_logged_in()) {
-                      get_template_part("template-parts/header/header-anon");  
+                get_template_part("template-parts/header/header-anon");
               } else {
-                  get_template_part("template-parts/header/header-logged");
+                get_template_part("template-parts/header/header-logged");
               }
               ?>
+            </div>
 
-		   
-          </div>
-		 
+          </div><!-- end slim-right-zone -->
 
-
-
-		
-        </div>
+        </div><!-- end wrapper-content -->
       </div>
     </div>
   </div>
 </div>
+
