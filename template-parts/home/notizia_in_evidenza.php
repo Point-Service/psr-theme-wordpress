fa35f6aeea68e309b80c9d1d2e52e6e4f45402f1
Point-Service <tecnico@p-service.it>
2025-07-31 18:20:15 +0200

    Update notizia_in_evidenza.php

--- template-parts/home/notizia_in_evidenza.php
+++ template-parts/home/notizia_in_evidenza.php
@@ -300,7 +300,7 @@ endif;
 
 .single-news.single-news-custom .row .col-lg-5.order-2.order-lg-1 {
   padding-left: 0rem; /* spostato più a sinistra */
-  padding-right: 1.0rem;
+  padding-right: 3rem;
 }
 
 /* Stili originali singolo post senza classe custom (per sicurezza) */
