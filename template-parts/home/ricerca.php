03964a44898286e081ff22d762859f91da0e2dd8
Point-Service <tecnico@p-service.it>
2025-08-06 15:41:51 +0200

    Update ricerca.php

--- template-parts/home/ricerca.php
+++ template-parts/home/ricerca.php
@@ -2,48 +2,49 @@
 $links = dci_get_option('link', 'link_utili');
 ?>
 
-<section id="novita" class="useful-links-section">
-    <div class="section section-muted p-0 py-5">
-        <div class="container">
-            <div class="row d-flex justify-content-center">
-                <div class="col-12 col-lg-6">
-                    <form role="search" id="search-form" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
-                        <div class="cmp-input-search">
-                            <div class="form-group autocomplete-wrapper mb-2 mb-lg-4">
-                                <div class="input-group">
-                                    <label for="autocomplete-three" class="visually-hidden">Cerca una parola chiave</label>
-                                    <input type="search" class="autocomplete form-control" placeholder="Cerca una parola chiave" id="autocomplete-three" name="s" value="<?php echo get_search_query(); ?>" data-bs-autocomplete="[]">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="submit" id="button-3">Invio</button>
-                                    </div>
-                                    <span class="autocomplete-icon" aria-hidden="true">
-                                        <svg class="icon icon-sm icon-primary">
-                                            <use href="#it-search"></use>
-                                        </svg>
-                                    </span>
+<section id="link-utili" class="useful-links-section bg-light py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-8">
+                <h2 class="mb-4 text-primary text-uppercase border-bottom pb-2">Link utili</h2>
+
+                <!-- Ricerca -->
+                <form role="search" method="get" class="search-form mb-4" action="<?php echo esc_url(home_url('/')); ?>">
+                    <div class="input-group shadow-sm">
+                        <input type="search" class="form-control" placeholder="Cerca una parola chiave" name="s" value="<?php echo get_search_query(); ?>" aria-label="Cerca una parola chiave">
+                        <button class="btn btn-primary" type="submit">
+                            <svg class="icon icon-sm me-1" aria-hidden="true">
+                                <use href="#it-search"></use>
+                            </svg>
+                            Cerca
+                        </button>
+                    </div>
+                </form>
+
+                <!-- Lista link -->
+                <?php if ($links) { ?>
+                    <ul class="list-group list-group-flush shadow-sm border rounded">
+                        <?php foreach ($links as $link) { ?>
+                            <li class="list-group-item d-flex align-items-center justify-content-between border-bottom">
+                                <div class="d-flex align-items-center">
+                                    <svg class="icon icon-primary me-2" aria-hidden="true">
+                                        <use href="#it-link"></use>
+                                    </svg>
+                                    <a href="<?php echo esc_url($link['url']); ?>" class="text-decoration-none fw-semibold link-primary">
+                                        <?php echo esc_html($link['testo']); ?>
+                                    </a>
                                 </div>
-                            </div>
-                        </div>
-                    </form>
-                    <?php if ($links) { ?>
-                        <div class="link-list-wrapper">
-                            <div class="link-list-heading text-uppercase mt-2 mt-lg-4 mb-3 ps-0">
-                                Link utili
-                            </div>
-                            <ul class="link-list">
-                                <?php foreach ($links as $link) { ?>
-                                    <li>
-                                        <a class="list-item mb-3 active ps-0" href="<?php echo $link['url']; ?>">
-                                            <span class="text-button-normal"><?php echo $link['testo']; ?>
-                                            </span>
-                                        </a>
-                                    </li>
-                                <?php } ?>
-                            </ul>
-                        </div>
-                    <?php } ?>
-                </div>
+                                <svg class="icon icon-sm text-secondary" aria-hidden="true">
+                                    <use href="#it-external-link"></use>
+                                </svg>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                <?php } else { ?>
+                    <p class="text-muted">Nessun link disponibile al momento.</p>
+                <?php } ?>
             </div>
         </div>
     </div>
 </section>
+
