63af6e20166ed847f88754fd8160fc38cf5df8cc
Point-Service <tecnico@p-service.it>
2025-08-07 10:17:53 +0200

    Update mappa.php

--- template-parts/vivere-comune/mappa.php
+++ template-parts/vivere-comune/mappa.php
@@ -6,8 +6,8 @@
     // Controllo se l'opzione per visualizzare la mappa e se il link non è null
     if ($show_map === 'true' && !empty($link_map) && $link_map != null) {
 ?>
-        <div class="row custom-map-container" style="overflow: hidden; background: transparent; display: flex; justify-content: center; align-items: center; width: 100%; min-height: 100vh;">
-            <div style="position: relative; width: 99%; height: 450px; background: transparent;">
+        <div class="row custom-map-container" style="overflow: hidden; background: transparent; display: flex; justify-content: center; align-items: center; width: 100%; height: 450px;">
+            <div style="position: relative; width: 99%; height: 450px; background: transparent; margin-left: 5%; /* Spostato a destra */">
                 <iframe style="border: 0; width: 100%; height: 100%; max-width: 100%;" src="<?= $link_map ?>" allowfullscreen scrolling="no"></iframe>
             </div>
         </div>
@@ -18,22 +18,6 @@
 /* Sovrascrivere --bs-gutter-y solo per la sezione specifica */
 .custom-map-container .g-4, .custom-map-container .gy-4 {
     --bs-gutter-y: 0 !important;
-}    
-
-/* Assicurarsi che il contenitore occupi tutta l'altezza della pagina */
-.custom-map-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    min-height: 100vh; /* Minima altezza della pagina */
-    background: transparent;
-}
-
-.custom-map-container > div {
-    width: 99%;  /* 99% per lasciare un piccolo margine */
-    max-width: 100%;
-    height: 450px; /* Altezza fissa per l'iframe */
 }
 </style>
 
