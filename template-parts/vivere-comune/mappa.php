67688d1b1d3f25a8ccc65f14189bd00927c5393e
Point-Service <tecnico@p-service.it>
2025-08-07 09:59:24 +0200

    Update mappa.php

--- template-parts/vivere-comune/mappa.php
+++ template-parts/vivere-comune/mappa.php
@@ -6,11 +6,12 @@
     // Controllo se l'opzione per visualizzare la mappa e se il link non è null
     if ($show_map === 'true' && !empty($link_map) && $link_map != null) {
 ?>
-        <div class="row g-4" style="overflow: hidden; max-width: 100%;">
+        <div class="row g-4">
             <div style="position: relative; width: 95%; height: 450px; margin: 0 auto;">
                 <iframe style="border: 0; width: 100%; height: 450px; max-width: 100%;" src="<?= $link_map ?>" allowfullscreen scrolling="no"></iframe>
             </div>
         </div>
 <?php }
 ?>
+
 
