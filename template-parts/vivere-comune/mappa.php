55068236787a25c818b05aa616a87a3892ec4c45
Point-Service <tecnico@p-service.it>
2025-08-07 09:34:45 +0200

    Update mappa.php

--- template-parts/vivere-comune/mappa.php
+++ template-parts/vivere-comune/mappa.php
@@ -7,8 +7,9 @@
 
     if($show_map === 'true' && !empty($link_map) &&$link_map!=null){?>
         <div class="row g-4">
-            <iframe style="border: 0;" src="<?= $link_map ?>" width="98%" height="450px" allowfullscreen></iframe>
+           
         </div>
     <?php }
 ?>
+
 
