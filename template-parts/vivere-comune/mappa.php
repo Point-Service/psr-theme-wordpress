7e4875529e6a30967dd9295d6101942b23aad700
Point-Service <tecnico@p-service.it>
2025-08-07 09:41:39 +0200

    Update mappa.php

--- template-parts/vivere-comune/mappa.php
+++ template-parts/vivere-comune/mappa.php
@@ -7,9 +7,28 @@
     if ($show_map === 'true' && !empty($link_map) && $link_map != null) {
 ?>
         <div class="row g-4">
-            <iframe style="border: 0; width: 80%; height: 450px;" src="<?= $link_map ?>" allowfullscreen scrolling="no"></iframe>
+            <!-- Div per la mappa -->
+            <div id="map" style="height: 450px; width: 100%;"></div>
         </div>
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
+        <script>
+            function initMap() {
+                var mapOptions = {
+                    center: {lat: 40.7128, lng: -74.0060}, // Imposta la posizione iniziale (puoi sostituirla con le coordinate della tua mappa)
+                    zoom: 14, // Zoom della mappa
+                    mapTypeId: 'roadmap', // Tipo di mappa
+                    disableDefaultUI: true, // Disabilita i controlli di default (barra di scorrimento, etc.)
+                };
+
+                var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+                var marker = new google.maps.Marker({
+                    position: {lat: 40.7128, lng: -74.0060}, // Posizione del marker (sostituisci con la posizione corretta)
+                    map: map,
+                    title: 'La tua posizione' // Messaggio del marker
+                });
+            }
+        </script>
 <?php }
 ?>
-
 
