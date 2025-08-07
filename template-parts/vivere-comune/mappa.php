798a293d0438e085484d0936dd352386d7e5357c
Point-Service <tecnico@p-service.it>
2025-08-07 09:44:34 +0200

    Update mappa.php

--- template-parts/vivere-comune/mappa.php
+++ template-parts/vivere-comune/mappa.php
@@ -7,28 +7,10 @@
     if ($show_map === 'true' && !empty($link_map) && $link_map != null) {
 ?>
         <div class="row g-4">
-            <!-- Div per la mappa -->
-            <div id="map" style="height: 450px; width: 100%;"></div>
+            <div style="position: relative; width: 100%; height: 450px; overflow: hidden;">
+                <iframe style="border: 0; width: 100%; height: 100%;" src="<?= $link_map ?>" allowfullscreen scrolling="no"></iframe>
+            </div>
         </div>
-        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
-        <script>
-            function initMap() {
-                var mapOptions = {
-                    center: {lat: 40.7128, lng: -74.0060}, // Imposta la posizione iniziale (puoi sostituirla con le coordinate della tua mappa)
-                    zoom: 14, // Zoom della mappa
-                    mapTypeId: 'roadmap', // Tipo di mappa
-                    disableDefaultUI: true, // Disabilita i controlli di default (barra di scorrimento, etc.)
-                };
-
-                var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-                var marker = new google.maps.Marker({
-                    position: {lat: 40.7128, lng: -74.0060}, // Posizione del marker (sostituisci con la posizione corretta)
-                    map: map,
-                    title: 'La tua posizione' // Messaggio del marker
-                });
-            }
-        </script>
 <?php }
 ?>
 
