7ff8e9f2b0f45572ffe3874525eb63ac194f8b23
Point-Service <tecnico@p-service.it>
2025-08-07 10:06:58 +0200

    Update mappa.php

--- template-parts/vivere-comune/mappa.php
+++ template-parts/vivere-comune/mappa.php
@@ -6,10 +6,16 @@
     // Controllo se l'opzione per visualizzare la mappa e se il link non è null
     if ($show_map === 'true' && !empty($link_map) && $link_map != null) {
 ?>
-        <div class="row g-4" style="overflow: hidden; max-width: 100%; background: transparent;">
+        <div class="row custom-map-container" style="overflow: hidden; max-width: 100%; background: transparent;">
             <div style="position: relative; width: 95%; height: 450px; margin: 0 auto; background: transparent;">
                 <iframe style="border: 0; width: 100%; height: 100%; max-width: 100%;" src="<?= $link_map ?>" allowfullscreen scrolling="no"></iframe>
             </div>
         </div>
 <?php }
 ?>
+<style>
+/* Sovrascrivere --bs-gutter-y solo per la sezione specifica */
+.custom-map-container .g-4, .custom-map-container .gy-4 {
+    --bs-gutter-y: 0 !important;
+}    
+</style>
