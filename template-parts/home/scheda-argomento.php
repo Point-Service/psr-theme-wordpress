2f9cf762d6526e649e2d3d9a91e9b04c4b8040ea
Point-Service <tecnico@p-service.it>
2025-08-19 15:49:23 +0200

    Update scheda-argomento.php

--- template-parts/home/scheda-argomento.php
+++ template-parts/home/scheda-argomento.php
@@ -16,7 +16,7 @@ $links = $argomento_full['argomento_'.$count.'_contenuti'];
     <div class="card-body pb-5">
         <!-- card head -->
         <div class="category-top">
-              <svg class="icon" width="4" height="4">
+              <svg class="icon" style="width:8px; height:8px;">
                 <use xlink:href="#it-bookmark"></use>
             </svg>
         </div>
