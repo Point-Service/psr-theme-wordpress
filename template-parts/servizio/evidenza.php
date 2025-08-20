e99465d3625e40f47ab684284da7568084e20302
Point-Service <tecnico@p-service.it>
2025-08-20 18:12:51 +0200

    Update evidenza.php

--- template-parts/servizio/evidenza.php
+++ template-parts/servizio/evidenza.php
@@ -1,16 +1,15 @@
 <?php
 global $post, $posts;
 
-//Per selezionare i contenuti in evidenza tramite configurazione
+// Per selezionare i contenuti in evidenza tramite configurazione
 $servizi_evidenza = dci_get_option('servizi_evidenziati', 'servizi');
 ?>
 
 <div class="container">
     <div class="row">
-        <?php
-        if (is_array($servizi_evidenza) && count($servizi_evidenza) > 0) { ?>
+        <?php if (is_array($servizi_evidenza) && count($servizi_evidenza) > 0) { ?>
             <div class="col-12">
-                <div class="card shadow-sm px-4 pt-4 pb- rounded border border-light">
+                <div class="card shadow-sm px-4 pt-4 pb-4 rounded border border-light">
                     <div class="link-list-wrap">
                         <h3 class="title-large">
                             <span>Servizi in evidenza</span>
@@ -18,15 +17,44 @@ $servizi_evidenza = dci_get_option('servizi_evidenziati', 'servizi');
                         <ul class="link-list t-primary">
                             <?php foreach ($servizi_evidenza as $servizio_id) {
                                 $post = get_post($servizio_id);
+
+                                // Recupero date dal servizio
+                                $prefix = '_dci_servizio_';
+                                $data_inizio_servizio = dci_get_meta('data_inizio_servizio', $prefix, $post->ID);
+                                $data_fine_servizio = dci_get_meta('data_fine_servizio', $prefix, $post->ID);
+
+                                // Conversione in DateTime
+                                $startDate = DateTime::createFromFormat('d/m/Y', $data_inizio_servizio);
+                                $endDate = $data_fine_servizio ? DateTime::createFromFormat('d/m/Y', $data_fine_servizio) : null;
+                                $oggi = new DateTime();
+
+                                // Valutazione stato
+                                $stato_attivo = false;
+                                if ($startDate && $endDate && $startDate < $endDate) {
+                                    $stato_attivo = ($oggi >= $startDate && $oggi <= $endDate);
+                                }
                                 ?>
                                 <li class="mb-4 mt-4">
-                                    <a class="list-item ps-0 title-medium underline" style="text-decoration:none;"
-                                        href="<?php echo get_permalink($post->ID); ?>">
-                                        <svg class="icon">
-                                            <use xlink:href="#it-arrow-right-triangle"></use>
-                                        </svg>
+                                    <a class="list-item ps-0 title-medium underline" style="text-decoration:none;" href="<?php echo get_permalink($post->ID); ?>">
+                                        <svg class="icon"><use xlink:href="#it-arrow-right-triangle"></use></svg>
                                         <span><?php echo $post->post_title; ?></span>
                                     </a>
+
+                                    <!-- Badge stato -->
+                                    <div class="mt-1">
+                                        <span class="chip chip-simple" data-element="service-status">
+                                            <span class="chip-label">
+                                                <?php echo $stato_attivo ? '<span class="text-success">Servizio attivo</span>' : '<span class="text-danger">Servizio non attivo</span>'; ?>
+                                            </span>
+                                        </span>
+
+                                        <!-- Periodo se disponibile -->
+                                        <?php if ($startDate && $endDate) { ?>
+                                            <div class="service-period">
+                                                <small><strong>Periodo:</strong> <?php echo $startDate->format('d/m/Y'); ?> - <?php echo $endDate->format('d/m/Y'); ?></small>
+                                            </div>
+                                        <?php } ?>
+                                    </div>
                                 </li>
                             <?php } ?>
                         </ul>
