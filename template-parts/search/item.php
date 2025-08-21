4351d660d6813f8de958172c910121a69320829d
Point-Service <tecnico@p-service.it>
2025-08-21 09:41:20 +0200

    Update item.php

--- template-parts/search/item.php
+++ template-parts/search/item.php
@@ -45,19 +45,11 @@ if ($is_servizio) {
             }
             ?>
         </div>
-        <div class="card-body p-0 my-2">
-            <h3 class="green-title-big t-primary mb-8">
-                <a class="text-decoration-none" href="<?= ($post->post_type == 'sito_tematico') ? dci_get_meta('link') : get_permalink() ?>" data-element="service-link"><?php echo the_title(); ?></a>
-            </h3>
-            <p class="text-paragraph">
-                <?php echo $descrizione; ?>
-            </p>
-
-            <?php if ($is_servizio): ?>
+        <?php if ($is_servizio): ?>
                 <!-- Mostra categorie del servizio -->
                 <?php if (is_array($categorie) && count($categorie)): ?>
                     <div class="categories mb-2">
-                        <span class="text-uppercase">Categorie:</span>
+                        <span class="text-uppercase"> / </span>
                         <?php
                         $count = 1;
                         foreach ($categorie as $categoria) {
@@ -69,7 +61,16 @@ if ($is_servizio) {
                         }
                         ?>
                     </div>
-                <?php endif; ?>
+        <?php endif; ?>
+        <div class="card-body p-0 my-2">
+            <h3 class="green-title-big t-primary mb-8">
+                <a class="text-decoration-none" href="<?= ($post->post_type == 'sito_tematico') ? dci_get_meta('link') : get_permalink() ?>" data-element="service-link"><?php echo the_title(); ?></a>
+            </h3>
+            <p class="text-paragraph">
+                <?php echo $descrizione; ?>
+            </p>
+
+
 
                 <!-- Mostra argomenti (tag) con chip -->
                 <?php if (is_array($argomenti) && count($argomenti)): ?>
