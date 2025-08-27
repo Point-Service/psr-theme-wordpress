125aaade65300be2d98b591b5ea2a5bbdda5a3f1
Point-Service <tecnico@p-service.it>
2025-08-27 11:23:10 +0200

    Update amministrazione-trasparente.php

--- page-templates/amministrazione-trasparente.php
+++ page-templates/amministrazione-trasparente.php
@@ -11,20 +11,24 @@ $search_url = esc_url( home_url( '/' ));
 $link_amministrazione=dci_get_option("link_ammtrasparente");
 $url_img="https://saassipa.cultura.gov.it/wp-content/uploads/2020/04/amm_trasp-1024x381.png";
 
-		if (
-		    isset($link_amministrazione) &&
-		    !empty($link_amministrazione) &&
-		    $link_amministrazione != null
-		) {
-		    // Rimuove eventuale slash finale
-		    $normalized_link = rtrim($link_amministrazione, '/');
-		
-		    // Verifica che NON termini con /amministrazione-trasparente
-		    if (substr($normalized_link, -27) !== '/amministrazione-trasparente') {
-		        header("Location: $link_amministrazione");
-		        exit;
-		    }
-		}
+if (
+    isset($link_amministrazione) &&
+    !empty($link_amministrazione) &&
+    $link_amministrazione != null
+) {
+    // Rimuove lo slash finale, se presente
+    $normalized_link = rtrim($link_amministrazione, '/');
+
+    // Costruisce dinamicamente il link da ignorare (senza slash finale)
+    $link_da_ignorare = rtrim(home_url('/amministrazione-trasparente'), '/');
+
+    // Confronto
+    if ($normalized_link !== $link_da_ignorare) {
+      //  header("Location: $link_amministrazione");
+        exit;
+    }
+}
+
 
 
 function info(){?>
@@ -72,5 +76,6 @@ get_header();
 
 <?php
 get_footer();?>
+
 
 
