052f79a83c0cac2ce40ad79dadfef9a2e024626d
Point-Service <tecnico@p-service.it>
2025-08-29 18:31:12 +0200

    Update opzioni_comune.php

--- inc/admin/options/opzioni_comune.php
+++ inc/admin/options/opzioni_comune.php
@@ -205,7 +205,7 @@ function dci_register_comune_options(){
     $header_options->add_field(array(
         'id'      => $prefix . 'ck_collegamenti_contenuti',
         'name'    => __('Collegamenti Contenuti', 'design_comuni_italia'),
-        'desc'    => __('Selezionando "Sì", verranno visualizzati i collegamenti tra i vari contenuti del sito web.', 'design_comuni_italia'),
+        'desc'    => __('Attivando questa opzione, i contenuti selezionati saranno automaticamente collegati tra loro. Selezionando Sì, il sistema gestirà in modo automatico i collegamenti tra le pagine e i contenuto correlati.', 'design_comuni_italia'),
         'type'    => 'radio_inline',
         'default' => 'true',
         'options' => array(
