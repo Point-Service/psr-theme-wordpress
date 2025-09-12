2d6c41b9961961b6b703115b8f3751d1200e88ea
Point-Service <tecnico@p-service.it>
2025-09-12 10:01:03 +0200

    Update evidenza.php

--- template-parts/servizio/evidenza.php
+++ template-parts/servizio/evidenza.php
@@ -41,7 +41,9 @@ $servizi_evidenza = dci_get_option('servizi_evidenziati', 'servizi');
 
                 		        // Recupera lo stato del servizio
                 		        $stato = dci_get_meta("_dci_servizio_stato", $prefix, $post->ID);
-                 
+
+echo $stato;
+    
                                 // Valutazione stato
                                 $stato_attivo = true;
                                 if ($startDate && $endDate && $startDate < $endDate) {
