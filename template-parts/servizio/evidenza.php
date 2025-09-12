be495d3a4b0959c4a9c66498fe7600f22999f346
Point-Service <tecnico@p-service.it>
2025-09-12 09:57:18 +0200

    Update evidenza.php

--- template-parts/servizio/evidenza.php
+++ template-parts/servizio/evidenza.php
@@ -38,27 +38,18 @@ $servizi_evidenza = dci_get_option('servizi_evidenziati', 'servizi');
                                 $endDate = $data_fine_servizio ? DateTime::createFromFormat('d/m/Y', $data_fine_servizio) : null;
                                 $oggi = new DateTime();
 
+
                 		        // Recupera lo stato del servizio
-                		        $stato_attivo = dci_get_meta("_dci_servizio_stato");
+                		        $stato = dci_get_meta("_dci_servizio_stato");
+                
+echo $stato;
 
-                                // Controlla se entrambe le date sono presenti e che la data di inizio sia inferiore alla data di fine
-                                			if ($startDate && $endDate && $startDate < $endDate) {
-                                			    // Verifica se la data di oggi è all'interno del periodo
-                                			    if ($oggi >= $startDate && $oggi <= $endDate) {
-                                			        // Servizio attivo
-                                			        $stato_attivo = "true";
-                                			    } else {
-                                			        // Servizio disattivato automaticamente
-                                			        $stato_attivo = "false";
-                                			    }
-
-                            			    // Aggiorna lo stato del servizio nel database solo se entra in questa condizione
-                            			    update_post_meta($post->ID, "_dci_servizio_stato", $stato_attivo);
-                            			} else {
-                            			    // Se le date non sono valide (entrambe mancanti o data inizio >= data fine), stato è "false"
-                            			    $stato_attivo = "false";
-                            			    // Non aggiorno lo stato nel database se non entra in questa condizione
-                            			}
+    
+                                // Valutazione stato
+                                $stato_attivo = true;
+                                if ($startDate && $endDate && $startDate < $endDate) {
+                                    $stato_attivo = ($oggi >= $startDate && $oggi <= $endDate);
+                                }
 
                                 // Recupero le categorie del servizio
                                 $categorie = get_the_terms($post->ID, 'categorie_servizio');
