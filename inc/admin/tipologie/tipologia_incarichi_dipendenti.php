6307467f15fa901b01a8a120717fcf2ace2011ad
Point-Service <tecnico@p-service.it>
2025-09-19 17:31:06 +0200

    Update tipologia_incarichi_dipendenti.php

--- inc/admin/tipologie/tipologia_incarichi_dipendenti.php
+++ inc/admin/tipologie/tipologia_incarichi_dipendenti.php
@@ -34,7 +34,7 @@ function dci_register_post_type_icad() {
         'supports'        => array( 'title', 'author' ),
         'hierarchical'    => true,
         'public'          => true,
-        'show_in_menu'    => $show_in_menu,  // Mostra il menu solo se la condizione è soddisfatta
+        'show_in_menu'    => NULL,  // Mostra il menu solo se la condizione è soddisfatta
         'menu_icon'       => 'dashicons-media-interactive',
         'has_archive'     => false, 
         'rewrite'         => array(
