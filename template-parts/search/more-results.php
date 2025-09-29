3909f330c9c97256589c088b205c8e55f752eea4
Point-Service <tecnico@p-service.it>
2025-09-29 15:50:04 +0200

    Update more-results.php

--- template-parts/search/more-results.php
+++ template-parts/search/more-results.php
@@ -1,16 +1,6 @@
 <?php 
 global $the_query, $load_posts, $wp_the_query, $load_card_type, $additional_filter, $filter_ids, $label, $label_no_more, $tax_query, $classes;
 
-
-
-
-
-echo 'aaaaaaa';
-
-
-
-
-
 if (!$the_query) $the_query = $wp_query;
 if (!$load_posts) $load_posts = 6;
 
