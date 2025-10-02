948b233de2ab157cdaf00af73295d81bd430b003
Point-Service <tecnico@p-service.it>
2025-10-02 16:11:03 +0200

    Update opzioni_comune.php

--- inc/admin/options/opzioni_comune.php
+++ inc/admin/options/opzioni_comune.php
@@ -217,7 +217,14 @@ function dci_register_comune_options(){
         ),
     ));
 
+  $header_options->add_field(array(
+        'id'    => $prefix . 'portalesoloperusoesterno',
+        'name'  => __('Portale personalizzato per uso Esterno.', 'design_comuni_italia'),
+        'desc'  => __('Opzione per chi usa solo la nostra trasparenza e pnnr https://amministrazionetrasparente.servizipa.cloud.', 'design_comuni_italia'),
+        'type'  => 'title',
+    ));
 
+    
     $header_options->add_field(array(
         'id'      => $prefix . 'ck_portalesoloperusoesterno',
         'name'    => __('Portale Personalizzato per uso esterno.', 'design_comuni_italia'),
