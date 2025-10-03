e3616a1f0e9e6d827ac0d847a2dd5ba7b2672282
Point-Service <tecnico@p-service.it>
2025-10-03 11:05:48 +0200

    Update search-modal.php

--- template-parts/common/search-modal.php
+++ template-parts/common/search-modal.php
@@ -81,7 +81,9 @@
 
                   <div class="row variable-gutters p-4">
                       <div class="col-lg-5">    
-                          <a href="/?s=" class="chip chip-simple chip-lg" data-focus-mouse="false"><span class="chip-label">RICERCA PARAMETRICA</span></a>
+                        <a href="<?php echo esc_url( home_url( '/?s=' ) ); ?>" class="chip chip-simple chip-lg" data-focus-mouse="false">
+                            <span class="chip-label">RICERCA PARAMETRICA</span>
+                        </a>
                         <p></p>
                           <div class="h4 other-link-title">Ricerche frequenti</div>
                           <div class="link-list-wrapper mb-4">
