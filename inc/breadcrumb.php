0c355a28adf556842f53d85a5e556140e43e8830
Point-Service <tecnico@p-service.it>
2025-11-26 12:53:21 +0100

    Update breadcrumb.php

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -520,11 +520,8 @@ class Breadcrumb_Trail {
 				
 
 				if (get_post_type() == 'consiglio') {
-					$this->items[] =  "<a href='".home_url("elenco-consigli-comunali")."'>".__("elenco-consigli-comunali", "design_comuni_italia")."</a>";
+					$this->items[] =  "<a href='".home_url("elenco-consigli-comunali")."'>".__("Elenco Consigli Comunali, "design_comuni_italia")."</a>";
 		            $this->items[] =  "<a href='/consiglio/'>" . __("Consiglio", "design_comuni_italia") . "</a>";		
-
-
-					
 					$this->items[] = get_the_title();
 					return;
 				}
