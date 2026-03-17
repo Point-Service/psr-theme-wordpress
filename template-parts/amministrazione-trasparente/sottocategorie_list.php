4b151ea0614cc4d1f6299c277e38e100a4c00e4c
Point-Service <tecnico@p-service.it>
2026-03-17 17:13:57 +0100

    Update sottocategorie_list.php

--- template-parts/amministrazione-trasparente/sottocategorie_list.php
+++ template-parts/amministrazione-trasparente/sottocategorie_list.php
@@ -7,6 +7,8 @@ if (!isset($term_id)) {
 $sub_sub_categories = get_terms('tipi_cat_amm_trasp', array(
     'hide_empty' => false,
     'parent' => $term_id
+    'orderby' => 'ID',
+    'order' => 'ASC'
 ));
 
 if (!empty($sub_sub_categories) && !is_wp_error($sub_sub_categories)) { ?>
