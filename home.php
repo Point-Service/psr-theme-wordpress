84d2a91332519a8248474ab9f3651106d1139bb2
Point-Service <tecnico@p-service.it>
2026-03-20 09:45:56 +0100

    Update home.php

--- home.php
+++ home.php
@@ -97,6 +97,7 @@ get_header();
 
         ?>
         <?php get_template_part("template-parts/common/valuta-servizio"); ?>
+        <?php get_template_part("template-parts/home/meteo"); ?>
         <?php get_template_part("template-parts/common/assistenza-contatti"); ?>
     </main>
 <?php
