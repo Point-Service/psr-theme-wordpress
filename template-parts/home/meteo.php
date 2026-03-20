3a352119372762fd0fa4492cc6c376734c40cefc
Point-Service <tecnico@p-service.it>
2026-03-20 09:45:03 +0100

    Create meteo.php

---
+++ template-parts/home/meteo.php
@@ -0,0 +1,53 @@
+<section class="weather-section">
+  <div class="container">
+
+    <h2 class="weather-title">Meteo</h2>
+
+    <div class="weather-grid">
+
+      <!-- OGGI -->
+      <div class="weather-card big">
+        <div class="weather-day">OGGI - 20/3/2026</div>
+
+        <div class="weather-content">
+          <div class="weather-info">
+            <div>Temp. media 6.75°C</div>
+            <div>Temp. massima 10.18°C</div>
+            <div>Temp. minima 6.75°C</div>
+            <div>Umidità 59%</div>
+          </div>
+
+          <div class="weather-icon">☀️</div>
+        </div>
+
+        <div class="weather-desc">cielo sereno</div>
+      </div>
+
+      <!-- GIORNI -->
+      <div class="weather-card">
+        <div class="weather-day">SABATO</div>
+        <div class="weather-icon">☁️</div>
+        <div class="weather-desc">cielo coperto</div>
+      </div>
+
+      <div class="weather-card">
+        <div class="weather-day">DOMENICA</div>
+        <div class="weather-icon">🌧️</div>
+        <div class="weather-desc">pioggia moderata</div>
+      </div>
+
+      <div class="weather-card">
+        <div class="weather-day">LUNEDÌ</div>
+        <div class="weather-icon">⛅</div>
+        <div class="weather-desc">nubi sparse</div>
+      </div>
+
+      <div class="weather-card">
+        <div class="weather-day">MARTEDÌ</div>
+        <div class="weather-icon">☁️</div>
+        <div class="weather-desc">cielo coperto</div>
+      </div>
+
+    </div>
+  </div>
+</section>
