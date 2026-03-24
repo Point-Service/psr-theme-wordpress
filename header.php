c5d436529e1edd06a78ec77c169ccf0515c7d161
Point-Service <tecnico@p-service.it>
2026-03-24 16:43:11 +0100

    Head esterno: importa anche blocco header completo del portale

--- header.php
+++ header.php
@@ -14,15 +14,28 @@ $current_group = dci_get_current_group();
 <!doctype html>
 <html lang="it">
 <head>
+    <?php
+    $external_head_html = function_exists('dci_get_external_head_html') ? dci_get_external_head_html() : '';
+    if (!empty($external_head_html)) {
+        echo $external_head_html;
+    } else {
+    ?>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<?php wp_head(); ?>
+		<?php wp_head(); ?>
+    <?php } ?>
 </head>
 <body <?php body_class(); ?>>
 
 <?php get_template_part("template-parts/common/svg"); ?>
 <?php get_template_part("template-parts/common/sprites"); ?>
 <?php get_template_part("template-parts/common/skiplink"); ?>
+
+<?php $external_header_html = function_exists('dci_get_external_header_html') ? dci_get_external_header_html() : ''; ?>
+
+<?php if (!empty($external_header_html)) : ?>
+    <?php echo $external_header_html; ?>
+<?php else : ?>
 
 <header
     class="it-header-wrapper"
@@ -258,6 +271,7 @@ $current_group = dci_get_current_group();
     </div>
   </div>
 </header>
+<?php endif; ?>
 
 <?php get_template_part("template-parts/common/search-modal"); ?>
 <?php
