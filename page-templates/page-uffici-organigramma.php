62b9156ea7bc23b98e5a33d08129171b8739ad63
Point-Service <tecnico@p-service.it>
2026-03-25 12:51:12 +0100

    Update page-uffici-organigramma.php

--- page-templates/page-uffici-organigramma.php
+++ page-templates/page-uffici-organigramma.php
@@ -2,30 +2,27 @@
 global $post, $with_shadow;
 
 get_header();
-
 ?>
-	<main>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			
-			?>
-			<?php
-				$with_shadow = true; 
-				get_template_part("template-parts/hero/hero"); 
-			?>
+
+<main>
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php
+        $with_shadow = true; 
+        get_template_part("template-parts/hero/hero"); 
+        ?>
+
         <br>
-            <?php get_template_part("template-parts/uffici/organigramma-uffici"); ?>	
-		<br>
-			<?php get_template_part("template-parts/argomento/evidenza"); ?>			
-			<?php get_template_part("template-parts/common/valuta-servizio"); ?>	
-			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>			
-		<?php 
-			endwhile; // End of the loop.
-		?>
-	</main>
 
-<?php
-get_footer(); ?>
+        <?php get_template_part("template-parts/uffici/organigramma-uffici"); ?>    
 
+        <br>
 
+        <?php get_template_part("template-parts/argomento/evidenza"); ?>            
+        <?php get_template_part("template-parts/common/valuta-servizio"); ?>    
+        <?php get_template_part("template-parts/common/assistenza-contatti"); ?>            
+
+    <?php endwhile; ?>
+</main>
+
+<?php get_footer(); ?>
