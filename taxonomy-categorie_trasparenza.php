cd9789b585d1cd5749147739bfd17e276caae61b
Point-Service <tecnico@p-service.it>
2026-03-25 10:33:01 +0100

    Update taxonomy-categorie_trasparenza.php

--- taxonomy-categorie_trasparenza.php
+++ taxonomy-categorie_trasparenza.php
@@ -260,7 +260,7 @@ if (!function_exists('dci_render_trasparenza_light_bg_style')) {
 dci_render_trasparenza_light_bg_style();
 
 // Recupera il numero di pagina corrente.
-$paged = max(1, get_query_var('paged'), get_query_var('page'));
+$paged = get_query_var('paged') ? get_query_var('paged') : 1;
 
 $max_posts = isset($_GET['max_posts']) ? intval($_GET['max_posts']) : 10;
 $load_posts = -1;
@@ -300,7 +300,7 @@ if ($order === 'alfabetico_asc' || $order === 'alfabetico_desc') {
 $the_query = new WP_Query($args);
 
 $pagination_links = paginate_links([
-    'total'      => $the_query->max_num_pages-1,
+    'total'      => $the_query->max_num_pages,
     'current'    => $paged,
     'mid_size'   => 2,
     'type'       => 'array',
