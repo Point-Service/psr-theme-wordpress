eb6b7ba6d048a7916cd90795a34d8951f5ed128c
Point-Service <tecnico@p-service.it>
2026-03-25 12:23:39 +0100

    Update uffici_organigramma.php

--- page-templates/uffici_organigramma.php
+++ page-templates/uffici_organigramma.php
@@ -1,1 +1,21 @@
+<?php
+/**
+ * Template Name: Uffici Organigramma
+ * Description: Pagina contenente i contatti dell'ente
+ *
+ * @package Design_Comuni_Italia
+ */
+
+get_header();
+?>
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+<?php get_template_part("template-parts/uffici/organigramma-uffici"); ?>
+
+
+
+
+<?php get_footer(); ?>
 
