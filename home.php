13415ba1cb48c41f0aff3d88eb5cbc59f3708bdd
Point-Service <tecnico@p-service.it>
2026-04-16 11:42:16 +0200

    Update home.php

--- home.php
+++ home.php
@@ -53,7 +53,7 @@ get_header();
         <section id="evidenza" class="evidence-section">
                 <?php  get_template_part("template-parts/home/argomenti"); ?>
                <?php  get_template_part("template-parts/home/strip"); ?>
-                <?php get_template_part("template-parts/home/servizi"); ?>
+                <?php //get_template_part("template-parts/home/servizi"); ?>
             <div class="section py-5 pb-lg-80 px-lg-5 position-relative">
 		        
                 <?php get_template_part("template-parts/home/siti","tematici"); ?>
