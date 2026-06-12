b95d5f37e634173874eba9b71734d0ca0ad33bd8
Point-Service <tecnico@p-service.it>
2026-06-12 17:50:08 +0200

    Update tutti-eventi.php

--- template-parts/vivere-comune/tutti-eventi.php
+++ template-parts/vivere-comune/tutti-eventi.php
@@ -1,128 +1,63 @@
 <?php
 global $the_query, $load_posts, $load_card_type;
 
-$per_page = 9;
-$load_posts = $per_page;
+    $max_posts = isset($_GET['max_posts']) ? $_GET['max_posts'] : -1;
+    $load_posts = 3;
+    $query = isset($_GET['search']) ? dci_removeslashes($_GET['search']) : null;
+    $args = array(
+        's'         => $query,
+        'post_type' => 'evento',
+        'posts_per_page' => $max_posts,
+        'orderby' => 'meta_value',
+        'order' => 'ASC',
+        'meta_key' => '_dci_evento_data_orario_inizio',
+        'post_status'    => 'publish',
+    );
 
-$query = isset($_GET['search'])
-    ? sanitize_text_field(wp_unslash($_GET['search']))
-    : '';
-
-$paged = isset($_GET['paged'])
-    ? max(1, intval($_GET['paged']))
-    : 1;
-
-$args = array(
-    's'                   => $query,
-    'post_type'           => 'evento',
-    'posts_per_page'      => $per_page,
-    'paged'               => $paged,
-    'orderby'             => 'meta_value',
-    'order'               => 'ASC',
-    'meta_key'            => '_dci_evento_data_orario_inizio',
-    'post_status'         => 'publish',
-    'ignore_sticky_posts' => true,
-);
-
-$the_query = new WP_Query($args);
+    $the_query = new WP_Query( $args );
+    $posts = $the_query->posts;
 ?>
 
 <div class="bg-card bg-grey-card py-5">
     <form role="search" id="search-form" method="get" class="search-form">
         <button type="submit" class="d-none"></button>
-
         <div class="container">
-
             <h2 class="title-xxlarge mb-4">
                 Esplora tutti gli eventi
             </h2>
-
-            <div class="cmp-input-search">
-                <div class="form-group autocomplete-wrapper mb-0">
-                    <div class="input-group">
-
-                        <label for="autocomplete-two" class="visually-hidden">
-                            Cerca
-                        </label>
-
-                        <input
-                            type="search"
-                            class="autocomplete form-control"
-                            placeholder="Cerca per parola chiave"
-                            id="autocomplete-two"
-                            name="search"
-                            value="<?php echo esc_attr($query); ?>"
-                            data-bs-autocomplete="[]" />
-
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="submit">
-                                Invio
-                            </button>
+            <div>
+                <div class="cmp-input-search">
+                    <div class="form-group autocomplete-wrapper mb-0">
+                        <div class="input-group">
+                            <label for="autocomplete-two" class="visually-hidden">Cerca</label>
+                            <input type="search" class="autocomplete form-control" placeholder="Cerca per parola chiave"
+                                id="autocomplete-two" name="search" value="<?php echo $query; ?>"
+                                data-bs-autocomplete="[]" />
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit" id="button-3">
+                                    Invio
+                                </button>
+                            </div>
+                            <span class="autocomplete-icon" aria-hidden="true"><svg class="icon icon-sm icon-primary"
+                                    role="img" aria-labelledby="autocomplete-label">
+                                    <use href="#it-search"></use>
+                                </svg>
+                            </span>
                         </div>
-
-                    </div>
-
-                    <p class="u-grey-light text-paragraph-card mt-2 mb-30 mt-lg-3 mb-lg-40">
-                        <?php echo intval($the_query->found_posts); ?> eventi trovati
-                    </p>
-
-                </div>
-            </div>
-
-            <div class="row g-4">
-
-                <?php
-                if ($the_query->have_posts()) :
-
-                    while ($the_query->have_posts()) :
-                        $the_query->the_post();
-
-                        $load_card_type = 'evento';
-
-                        get_template_part(
-                            'template-parts/evento/card-full'
-                        );
-
-                    endwhile;
-
-                else :
-
-                    get_template_part(
-                        'template-parts/content',
-                        'none'
-                    );
-
-                endif;
-                ?>
-
-            </div>
-
-            <?php if ($the_query->max_num_pages > 1) : ?>
-
-                <div class="row my-4">
-                    <div class="col-12 text-center">
-
-                        <?php
-                        echo paginate_links(array(
-                            'base' => add_query_arg(
-                                'paged',
-                                '%#%'
-                            ),
-                            'format' => '',
-                            'current' => $paged,
-                            'total' => $the_query->max_num_pages,
-                            'prev_text' => '«',
-                            'next_text' => '»',
-                        ));
-                        ?>
-
+                        <p id="autocomplete-label" class="u-grey-light text-paragraph-card mt-2 mb-30 mt-lg-3 mb-lg-40">
+                            <?php echo $the_query->found_posts; ?> eventi trovate in ordine alfabetico
+                        </p>
                     </div>
                 </div>
-
-            <?php endif; ?>
-
+            </div>
+            <div class="row g-4" id="load-more">
+                <?php 
+                    foreach ($posts as $post) {
+                    $load_card_type = 'evento';
+                    get_template_part("template-parts/evento/card-full");
+                }?>
+            </div>
+            <?php get_template_part("template-parts/search/more-results"); ?>
         </div>
     </form>
 </div>
-
-<?php wp_reset_postdata(); ?>
